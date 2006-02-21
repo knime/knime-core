6ed578af27a2f10fbb603136da6bd541a20e31a7
Bernd Wiswedel <bernd.wiswedel@knime.com>
2006-02-21 14:45:58 +0000

    linux hack to avoid too large dialogs

--- de.unikn.knime.workbench.ui/src/de/unikn/knime/workbench/ui/wrapper/WrappedNodeDialog.java
+++ de.unikn.knime.workbench.ui/src/de/unikn/knime/workbench/ui/wrapper/WrappedNodeDialog.java
@@ -192,7 +192,7 @@ public class WrappedNodeDialog extends Dialog {
     @Override
     public void create() {
         super.create();
-
+        getShell().setSize(getInitialSize());
     }
 
     /**
