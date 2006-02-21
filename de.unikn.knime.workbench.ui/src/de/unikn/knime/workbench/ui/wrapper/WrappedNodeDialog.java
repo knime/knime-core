5beca18edecf3a9046d95ffb560fe1ebcea82cf0
Bernd Wiswedel <bernd.wiswedel@knime.com>
2006-02-21 15:16:21 +0000

    removed unused imports. Thanks Thomas.

--- de.unikn.knime.workbench.ui/src/de/unikn/knime/workbench/ui/wrapper/WrappedNodeDialog.java
+++ de.unikn.knime.workbench.ui/src/de/unikn/knime/workbench/ui/wrapper/WrappedNodeDialog.java
@@ -26,9 +26,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Map.Entry;
 
 import javax.swing.JPanel;
 
