052134ff33c083c9a189f3faefdde91c2fd8f58f
Bernd Wiswedel <bernd.wiswedel@knime.com>
2006-02-26 15:08:42 +0000

    addes sub progress, removed get methods in execution monitor

--- de.unikn.knime.core/src/de/unikn/knime/core/node/ExecutionMonitor.java
+++ de.unikn.knime.core/src/de/unikn/knime/core/node/ExecutionMonitor.java
@@ -106,21 +106,143 @@ public final class ExecutionMonitor {
     public void setMessage(final String message) {
         m_progress.setMessage(message);
     }
-
-    /**
-     * Returns the current progress value between 0 and 1.
-     * 
-     * @return Progress value.
-     */
-    public double getProgress() {
-        return m_progress.getProgress();
+    
+    /** Creates an execution monitor with a limited progress range.
+     * Classes that use a progress monitor and report in the range of [0,1]
+     * should get such a sub-progress monitor when their job is only partially
+     * contributing to the entire progress. The progress of such sub-jobs is
+     * then automatically scaled to the "right" range. 
+     * @param maxProg The maximum range the sub progress should take 
+     * @return A new exeuction monitor ready to use in sub jobs.
+     * @throws IllegalArgumentException If the argument is not in (0, 1].
+     */ 
+    public ExecutionMonitor createSubProgress(final double maxProg) {
+        if (maxProg > 1.0 || maxProg <= 0.0) {
+            throw new IllegalArgumentException(
+                    "Invalid sub progress size: " + maxProg);
+        }
+        SubNodeProgressMonitor subProgress = 
+            new SubNodeProgressMonitor(m_progress, maxProg);
+        return new ExecutionMonitor(subProgress);
     }
-
-    /**
-     * @return The progress message currently displayed.
+    
+    /** Progress monitor that is used by "sub-progresses", it doesn't have
+     * the range [0, 1] but only [0, b] where b is user-defined. 
      */
-    public String getMessage() {
-        return m_progress.getMessage();
+    private static final class SubNodeProgressMonitor 
+        implements NodeProgressMonitor {
+        
+        private final NodeProgressMonitor m_parent;
+        private final double m_maxProg;
+        private double m_lastProg;
+        
+        /**
+         * Creates new sub progress monitor.
+         * @param parent The parent of this monitor, i.e. where to report
+         * progress to and get the canceled status from.
+         * @param max The maximum progress (w.r.t parent) that this monitor
+         * should report.
+         */
+        SubNodeProgressMonitor(
+                final NodeProgressMonitor parent, final double max) {
+            m_maxProg = max;
+            m_parent = parent;
+        }
+
+        /**
+         * Must not be called. Throws IllegalStateException.
+         * @see NodeProgressMonitor#addProgressListener(NodeProgressListener)
+         */
+        public void addProgressListener(final NodeProgressListener l) {
+            throw new IllegalStateException("This method must not be called.");
+        }
+
+        /**
+         * Delegates to parent.
+         * @see NodeProgressMonitor#checkCanceled()
+         */
+        public void checkCanceled() throws CanceledExecutionException {
+            m_parent.checkCanceled();
+        }
+
+        /**
+         * Must not be called. Throws IllegalStateException.
+         * @see NodeProgressMonitor#getMessage()
+         */
+        public String getMessage() {
+            throw new IllegalStateException("This method must not be called.");
+        }
+
+        /**
+         * Get the subprogress, the value scaled to [0, 1].
+         * @see NodeProgressMonitor#getProgress()
+         */
+        public double getProgress() {
+            return m_lastProg;
+        }
+
+        /**
+         * Must not be called. Throws IllegalStateException.
+         * @see NodeProgressMonitor#removeAllProgressListener()
+         */
+        public void removeAllProgressListener() {
+            throw new IllegalStateException("This method must not be called.");
+        }
+
+        /**
+         * Must not be called. Throws IllegalStateException.
+         * @see NodeProgressMonitor#removeProgressListener(NodeProgressListener)
+         */
+        public void removeProgressListener(final NodeProgressListener l) {
+            throw new IllegalStateException("This method must not be called.");
+        }
+
+        /**
+         * Must not be called. Throws IllegalStateException.
+         * @see NodeProgressMonitor#setExecuteCanceled()
+         */
+        public void setExecuteCanceled() {
+            throw new IllegalStateException("This method must not be called.");
+        }
+
+        /**
+         * Delegates to parent.
+         * @see NodeProgressMonitor#setMessage(String)
+         */
+        public void setMessage(final String message) {
+            m_parent.setMessage(message);
+        }
+
+        /**
+         * @see NodeProgressMonitor#setProgress(double, String)
+         */
+        public void setProgress(
+                final double progress, final String message) {
+            synchronized (m_parent) {
+                double subProgress = calcSubProgress(progress);
+                m_parent.setProgress(subProgress, message);
+            }
+        }
+
+        /**
+         * @see NodeProgressMonitor#setProgress(double)
+         */
+        public void setProgress(final double progress) {
+            synchronized (m_parent) {
+                double subProgress = calcSubProgress(progress);
+                m_parent.setProgress(subProgress);
+            }
+        }
+        
+        
+        private double calcSubProgress(final double progress) {
+            double progressOfParent = m_parent.getProgress();
+            // diff to the last progress update
+            double diff = progress - m_lastProg;
+            m_lastProg = progress;
+            // scaled to our sub range
+            return progressOfParent + diff * m_maxProg;
+        }
     }
 
 } // ExecutionMonitor
