e67ab090c5e6be25c05af7e23c22e5413317b72c
Thorsten Meinl <thorsten.meinl@knime.com>
2006-03-07 12:38:43 +0000

    added this class as an adaption of ColumnSelectionPanel

---
+++ de.unikn.knime.core/src/de/unikn/knime/core/node/util/ColumnSelectionComboxBox.java
@@ -0,0 +1,157 @@
+/*
+ * Created on Mar 6, 2006
+ */
+package de.unikn.knime.core.node.util;
+
+import java.awt.Dimension;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.border.Border;
+
+import de.unikn.knime.core.data.DataCell;
+import de.unikn.knime.core.data.DataColumnSpec;
+import de.unikn.knime.core.data.DataTableSpec;
+import de.unikn.knime.core.data.DataType;
+import de.unikn.knime.core.data.DataValue;
+
+/**
+ * Class extends a JComboxBox to choose a column of a certain type retrieved
+ * from the <code>DataTableSpec</code>.
+ * 
+ * @author Bernd Wiswedel, University of Konstanz
+ * @author Thorsten Meinl, University of Konstanz
+ */
+public class ColumnSelectionComboxBox extends JComboBox {
+
+    private static final long serialVersionUID = 5797563450894378207L;
+
+    /**
+     * Show only columns of types that are compatible to one of theses classes.
+     */
+    private final Class<? extends DataValue>[] m_filterClasses;
+
+    /**
+     * Creates new Panel that will filter columns for particular value classes.
+     * The panel will have a titled border with name "Column Selection".
+     * 
+     * @param filterValueClasses classes derived from DataValue. The combo box
+     *            will allow to select only columns compatible with one of these
+     *            classes. All other columns will be ignored.
+     * 
+     * @see #update(DataTableSpec,DataCell)
+     */
+    public ColumnSelectionComboxBox(
+            final Class<? extends DataValue>... filterValueClasses) {
+        this(" Column Selection ", filterValueClasses);
+    }
+
+    /**
+     * Creates a new column selection panel with the given border title; all
+     * column are included in the combox box.
+     * 
+     * @param borderTitle The border title.
+     */
+    public ColumnSelectionComboxBox(final String borderTitle) {
+        this(borderTitle, DataValue.class);
+    }
+
+    /**
+     * Creates new Panel that will filter columns for particular value classes.
+     * The panel will have a title border with a given title.
+     * 
+     * @param filterValueClasses a class derived from DataValue. The combo box
+     *            will allow to select only columns compatible with one of these
+     *            classes. All other columns will be ignored.
+     * @param borderTitle The title of the border
+     * 
+     * @see #update(DataTableSpec,DataCell)
+     */
+    public ColumnSelectionComboxBox(final String borderTitle,
+            final Class<? extends DataValue>... filterValueClasses) {
+        this(BorderFactory.createTitledBorder(borderTitle), filterValueClasses);
+    }
+
+    /**
+     * Creates new Panel that will filter columns for particular value classes.
+     * The panel will have a border as given. If null, no border is set.
+     * 
+     * @param filterValueClasses classes derived from DataValue. The combo box
+     *            will allow to select only columns compatible with one of
+     *            theses classes. All other columns will be ignored.
+     * @param border Border for the panel or null to have no border.
+     * 
+     * @see #update(DataTableSpec,DataCell)
+     */
+    public ColumnSelectionComboxBox(final Border border,
+            final Class<? extends DataValue>... filterValueClasses) {
+        if (filterValueClasses == null || filterValueClasses.length == 0) {
+            throw new NullPointerException("Classes must not be null");
+        }
+        List<Class<? extends DataValue>> list = Arrays
+                .asList(filterValueClasses);
+        if (list.contains(null)) {
+            throw new NullPointerException("List of value classes must not "
+                    + "contain null elements.");
+        }
+        m_filterClasses = filterValueClasses;
+        if (border != null) {
+            setBorder(border);
+        }
+        setRenderer(new DataColumnSpecListCellRenderer());
+        setMinimumSize(new Dimension(100, 25));
+    }
+
+    /**
+     * Updates this filter panel by removing all current items and adding the
+     * columns according to the content of the argument <code>spec</code>. If
+     * a column name is provided and it is not filtered out the corresponding
+     * item in the combo box will be selected.
+     * 
+     * @param spec To get the column names, types and the current index from.
+     * @param selColName The column name to be set as chosen.
+     */
+    public final void update(final DataTableSpec spec, final DataCell selColName) {
+        removeAllItems();
+        if (spec != null) {
+            DataColumnSpec selectMe = null;
+            for (int c = 0; c < spec.getNumColumns(); c++) {
+                DataColumnSpec current = spec.getColumnSpec(c);
+                DataType type = current.getType();
+                for (Class<? extends DataValue> cl : m_filterClasses) {
+                    if (type.isCompatible(cl)) {
+                        addItem(current);
+                        if (current.getName().equals(selColName)) {
+                            selectMe = current;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (selectMe != null) {
+                setSelectedItem(selectMe);
+            } else {
+                // select last element
+                int size = getItemCount();
+                if (size > 0) {
+                    setSelectedIndex(size - 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Gets the selected column.
+     * 
+     * @return The cell that is currently being selected.
+     */
+    public final DataCell getSelectedColumn() {
+        DataColumnSpec selected = (DataColumnSpec)getSelectedItem();
+        if (selected != null) {
+            return selected.getName();
+        }
+        return null;
+    }
+}
