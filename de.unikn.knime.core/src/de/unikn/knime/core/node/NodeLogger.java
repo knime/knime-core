04d636f8d563183998a74fc0b8a14de7d01e71c5
Thomas Gabriel <thomas.gabriel@knime.com>
2006-06-16 12:15:51 +0000

    bugfix: pattern layout for addWriter() wrong

--- de.unikn.knime.core/src/de/unikn/knime/core/node/NodeLogger.java
+++ de.unikn.knime.core/src/de/unikn/knime/core/node/NodeLogger.java
@@ -450,7 +450,7 @@ public final class NodeLogger {
         }
         // register new appender
         WriterAppender app = new WriterAppender(new PatternLayout(
-                "%-5p\t %c{1}\t m\n"), writer);
+                "%-5p\t %c{1}\t %." + MAX_CHARS + "m\n"), writer);
         app.setImmediateFlush(true);
         LevelRangeFilter filter = new LevelRangeFilter();
         filter.setLevelMin(getLevel(minLevel));
