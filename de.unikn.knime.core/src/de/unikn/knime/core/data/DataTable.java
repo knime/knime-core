f56b5ca83d28a3d3907dda11b4579c6e43b71b89
Thomas Gabriel <thomas.gabriel@knime.com>
2006-07-12 13:06:31 +0000

    getRowCount() removed only provided by BufferedDataTable

--- de.unikn.knime.core/src/de/unikn/knime/core/data/DataTable.java
+++ de.unikn.knime.core/src/de/unikn/knime/core/data/DataTable.java
@@ -56,12 +56,5 @@ public interface DataTable extends Iterable<DataRow> {
      * @see de.unikn.knime.core.data.DataRow
      */
     RowIterator iterator();
-    
-    /**
-     * Returns number of rows in this table.
-     * 
-     * @return number of rows.
-     */
-    int getRowCount();
 
 }
