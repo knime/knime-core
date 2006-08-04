5260a8c9e52a75a6e52ec6a03e0e0a1e1813f13b
Fabian Dill <fabian.dill@knime.com>
2006-08-04 11:24:56 +0000

    removed the unused import

--- org.knime.core/src/org/knime/core/node/NodeFactory.java
+++ org.knime.core/src/org/knime/core/node/NodeFactory.java
@@ -44,7 +44,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.knime.core.eclipseUtil.GlobalClassCreator;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
