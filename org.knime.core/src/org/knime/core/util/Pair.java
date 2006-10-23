6c7f47edae84196a3d310d47307a23fc4696bfa7
Thorsten Meinl <thorsten.meinl@knime.com>
2006-10-23 10:57:59 +0000

    added equals and hashCode to Pair

--- org.knime.core/src/org/knime/core/util/Pair.java
+++ org.knime.core/src/org/knime/core/util/Pair.java
@@ -57,4 +57,25 @@ public final class Pair<T, M> {
      * @return the second object
      */
     public M getSecond() { return m_second; }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object o) {
+        if (!(o instanceof Pair)) { return false; }
+        
+        Pair p = (Pair) o;
+        if (m_first.equals(p.m_first)) { return false; }
+        if (m_second.equals(p.m_second)) { return false; }
+        return true;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return m_first.hashCode() ^ (m_second.hashCode() << 2); 
+    }
 }
