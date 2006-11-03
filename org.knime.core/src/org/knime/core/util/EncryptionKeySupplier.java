f445911f9f4789604511793444b2886d4207a9e4
Christoph Sieb <christoph.sieb@uni-konstanz.de>
2006-11-03 17:30:18 +0000

    encryption centralized in core

---
+++ org.knime.core/src/org/knime/core/util/EncryptionKeySupplier.java
@@ -0,0 +1,42 @@
+/* @(#)$RCSfile$ 
+ * $Revision$ $Date$ $Author$
+ * 
+ * -------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ * 
+ * Copyright, 2003 - 2006
+ * Universitaet Konstanz, Germany.
+ * Lehrstuhl fuer Angewandte Informatik
+ * Prof. Dr. Michael R. Berthold
+ * 
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner.
+ * -------------------------------------------------------------------
+ * 
+ * History
+ *   Nov 3, 2006 (sieb): created
+ */
+package org.knime.core.util;
+
+
+/**
+ * An object that implements this interface can register at
+ * {@link KnimeEncryption}. The object is invoked by the encryptor if no key is
+ * available at the moment of a encrypt or decrypt task.
+ * 
+ * @author Christoph Sieb, University of Konstanz
+ */
+public interface EncryptionKeySupplier {
+
+    /**
+     * This method must return a key that should be use to encrypt/decrypt Knime
+     * used passwords, keys, etc.
+     * 
+     * @return the encryption key as string
+     */
+    public String getEncryptionKey();
+
+}
---
+++ org.knime.core/src/org/knime/core/util/KnimeEncryption.java
@@ -0,0 +1,145 @@
+/* @(#)$RCSfile$ 
+ * $Revision$ $Date$ $Author$
+ * 
+ * -------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ * 
+ * Copyright, 2003 - 2006
+ * Universitaet Konstanz, Germany.
+ * Lehrstuhl fuer Angewandte Informatik
+ * Prof. Dr. Michael R. Berthold
+ * 
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner.
+ * -------------------------------------------------------------------
+ * 
+ * History
+ *   Nov 3, 2006 (sieb): created
+ */
+package org.knime.core.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.knime.core.node.NodeLogger;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ * This class handles the encryption and decryption with the static stored key.
+ * To this class one static key supplyer can be registered that is invoked if no
+ * key is available.
+ * 
+ * @author Christoph Sieb, University of Konstanz
+ */
+public final class KnimeEncryption {
+
+    private static final NodeLogger LOGGER =
+            NodeLogger.getLogger(KnimeEncryption.class);
+
+    private static final String ENCRYPTION_METHOD = "DES/ECB/PKCS5Padding";
+
+    private static Cipher CIPHER;
+
+    private static EncryptionKeySupplier keySupplier;
+
+    private static SecretKey secretKey;
+
+    static {
+
+        try {
+            CIPHER = Cipher.getInstance(ENCRYPTION_METHOD);
+        } catch (Exception e) {
+            LOGGER.error("Cipher <" + ENCRYPTION_METHOD
+                    + "> could not be created.", e);
+            CIPHER = null;
+        }
+    }
+
+    private KnimeEncryption() {
+        // empty private default constructor as this is a static utility class
+    }
+
+    /**
+     * Enrypts password.
+     * 
+     * @param password Char array.
+     * @return The password encrypt.
+     * @throws Exception If something goes wrong.
+     */
+    public static String encrypt(final char[] password) throws Exception {
+        if (secretKey == null && keySupplier != null) {
+            secretKey = createSecretKey(keySupplier.getEncryptionKey());
+        }
+        CIPHER.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] ciphertext = CIPHER.doFinal(new String(password).getBytes());
+        return new BASE64Encoder().encode(ciphertext);
+    }
+
+    /**
+     * Decrypts password.
+     * 
+     * @param password The password to decrypt.
+     * @return The decrypted password.
+     * @throws Exception If something goes wrong.
+     */
+    public static String decrypt(final String password) throws Exception {
+        if (secretKey == null && keySupplier != null) {
+            secretKey = createSecretKey(keySupplier.getEncryptionKey());
+        }
+        CIPHER.init(Cipher.DECRYPT_MODE, secretKey);
+        // perform the decryption
+        byte[] pw = new BASE64Decoder().decodeBuffer(password);
+        byte[] decryptedText = CIPHER.doFinal(pw);
+        return new String(decryptedText);
+    }
+
+    /**
+     * Sets the static encryption key supplier for this global static knime
+     * encryptor.
+     * 
+     * @param supplier the {@link EncryptionKeySupplier} that is asked for a key
+     *            if has not been set so far
+     */
+    public static void setEncryptionKeySupplier(
+            final EncryptionKeySupplier supplier) {
+        keySupplier = supplier;
+    }
+
+    /**
+     * Directly sets an encryption key.
+     * 
+     * @param key the encryption key to set
+     */
+    public static void setEncryptionKey(final SecretKey key) {
+        secretKey = key;
+    }
+
+    private static SecretKey createSecretKey(final String keyAsString) {
+
+        SecretKey secretKey1 = null;
+        try {
+            if (keyAsString.length() < 8) {
+                throw new IllegalArgumentException(
+                        "The encryption key must be at least 8 "
+                                + "characters long.");
+            }
+            byte[] key = keyAsString.getBytes();
+
+            secretKey1 =
+                    SecretKeyFactory.getInstance("DES").generateSecret(
+                            new DESKeySpec(key));
+
+        } catch (Exception e) {
+            secretKey1 = null;
+        }
+
+        return secretKey1;
+    }
+}
