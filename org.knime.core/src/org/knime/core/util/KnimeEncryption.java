0be0b319049de09a122f590eb4aba157d7c2619c
Christoph Sieb <christoph.sieb@uni-konstanz.de>
2006-11-17 16:10:27 +0000

    multiple asking for key if not right

--- org.knime.core/src/org/knime/core/util/KnimeEncryption.java
+++ org.knime.core/src/org/knime/core/util/KnimeEncryption.java
@@ -96,7 +96,16 @@ public final class KnimeEncryption {
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         // perform the decryption
         byte[] pw = new BASE64Decoder().decodeBuffer(password);
-        byte[] decryptedText = cipher.doFinal(pw);
+
+        byte[] decryptedText;
+        try {
+            decryptedText = cipher.doFinal(pw);
+        } catch (Exception e) {
+            secretKey = createSecretKey(keySupplier.getEncryptionKey());
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            decryptedText = cipher.doFinal(pw);
+        }
+        
         return new String(decryptedText);
     }
 
