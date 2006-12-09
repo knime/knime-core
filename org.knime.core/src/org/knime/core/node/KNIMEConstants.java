af125f73c8b0dff381de70c70b2e2b88ba6a59a8
Thorsten Meinl <thorsten.meinl@knime.com>
2006-12-09 19:15:38 +0000

    added system property for settings the maximum number threads used by KNIME: org.knime.core.maxThreads

--- org.knime.core/src/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/org/knime/core/node/KNIMEConstants.java
@@ -32,7 +32,6 @@ import javax.swing.ImageIcon;
 import org.knime.core.internal.KNIMEPath;
 import org.knime.core.util.ThreadPool;
 
-
 /**
  * Class that hold static values about the knime platform. So far only the knime
  * directory, which is located in <code>$HOME$/.knime</code>, the welcome
@@ -41,7 +40,7 @@ import org.knime.core.util.ThreadPool;
  * @author Bernd Wiswedel, University of Konstanz
  */
 public final class KNIMEConstants {
-    
+
     /** Workflow file version. */
     // IMPORTANT: Remember to also update the NodeLogger welcome screen with
     // the current version and the prerequisites
@@ -63,16 +62,16 @@ public final class KNIMEConstants {
     /**
      * <i>Welcome to KNIME Konstanz Information Miner</i>.
      */
-    public static final String WELCOME_MESSAGE = 
-          "****************************************************************\n"
-        + "*** Welcome to KNIME v1.1.0 - the Konstanz Information Miner ***\n"
-        + "*** Copyright, 2003 - 2006, University of Konstanz, Germany. ***\n"
-        + "****************************************************************\n";
+    public static final String WELCOME_MESSAGE =
+            "****************************************************************\n"
+                    + "*** Welcome to KNIME v1.1.0 - the Konstanz Information Miner ***\n"
+                    + "*** Copyright, 2003 - 2006, University of Konstanz, Germany. ***\n"
+                    + "****************************************************************\n";
 
     /** Path to the <i>knime.png</i> icon. */
-    private static final String KNIME_ICON_PATH = KNIMEConstants.class
-            .getPackage().getName().replace('.', '/')
-            + "/knime.png";
+    private static final String KNIME_ICON_PATH =
+            KNIMEConstants.class.getPackage().getName().replace('.', '/')
+                    + "/knime.png";
 
     /** Icon 16 times 16 pixel. */
     public static final ImageIcon KNIME16X16;
@@ -95,23 +94,30 @@ public final class KNIMEConstants {
         } catch (Exception e) {
             // do nothing.
         }
+
+        int maxThreads =
+                Integer.parseInt(System.getProperty(
+                        "org.knime.core.maxThreads",
+                        "" + (Runtime.getRuntime().availableProcessors() + 2)));
+
+        GLOBAL_THREAD_POOL = new ThreadPool(maxThreads);
     }
 
     /** The global thread pool from which all threads should be taken. */
-    public static final ThreadPool GLOBAL_THREAD_POOL =
-        new ThreadPool(2 * Runtime.getRuntime().availableProcessors());
-    
-    /** 
-     * The directory where knime will put log files and configuration files.
-     * If started in eclipse, this is usually ${workspace_path}/.metadata/knime.
-     * Otherwise it's in the current working directory.
-     * This variable does not have a trailing file separator character.
+    public static final ThreadPool GLOBAL_THREAD_POOL;
+
+    /**
+     * The directory where knime will put log files and configuration files. If
+     * started in eclipse, this is usually ${workspace_path}/.metadata/knime.
+     * Otherwise it's in the current working directory. This variable does not
+     * have a trailing file separator character.
+     * 
      * @return The KNIME home dir.
      */
     public static final String getKNIMEHomeDir() {
         return knimeHomeDir.getAbsolutePath();
     }
-    
+
     /**
      * Hides public constructor.
      */
