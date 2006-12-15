2bb885b21bd2c5c1e83cc1a038a5e936459afbea
Michael Berthold <michael.berthold@knime.com>
2006-12-15 15:30:55 +0000

    created.

---
+++ org.knime.core/src/org/knime/core/data/NominalValue.java
@@ -0,0 +1,46 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright 2003 - 
+ * KNIME GmbH, Konstanz, Germany
+ * and
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining
+ * Prof. Dr. Michael R. Berthold
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any quesions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * --------------------------------------------------------------------- *
+ * 
+ * History
+ *   15.12.2006 (M. Berthold): created
+ */
+package org.knime.core.data;
+
+/**
+ * Interface of a {@link org.knime.core.data.def.StringCell}, which does
+ * not enforce additional functionality but indicates that cells imlementing
+ * this value can be used as nominal values. This allows for example to
+ * avoid nominal value enumeration for data cells holding smiles.
+ * 
+ * @author mb, University of Konstanz
+ */
+public interface NominalValue extends DataValue {
+
+    /** Meta information for this value type - reuses underlying base
+     * class because this value does not offer new functionality but
+     * is really only used to differentiate the ability of types.
+     * 
+     * @see DataValue#UTILITY
+     */
+    public static final UtilityFactory UTILITY = DataValue.UTILITY;
+
+}
