efab4c5273a242fcf78410af4e16d0733dcd5a8b
Thomas Gabriel <thomas.gabriel@knime.com>
2006-12-20 11:26:34 +0000

    Copyright changed

--- org.knime.workbench.help/src/org/knime/workbench/help/intro/InvokeInstallSiteAction.java
+++ org.knime.workbench.help/src/org/knime/workbench/help/intro/InvokeInstallSiteAction.java
@@ -1,20 +1,23 @@
-/* @(#)$RCSfile$ 
- * $Revision$ $Date$ $Author$
- * 
- * -------------------------------------------------------------------
+/* 
+ * ------------------------------------------------------------------
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
- * 
- * Copyright, 2003 - 2007
- * Universitaet Konstanz, Germany.
- * Lehrstuhl fuer Angewandte Informatik
+ *
+ * Copyright, 2003 - 2006
+ * University of Konstanz, Germany.
+ * Chair for Bioinformatics and Information Mining
  * Prof. Dr. Michael R. Berthold
- * 
+ *
  * You may not modify, publish, transmit, transfer or sell, reproduce,
  * create derivative works from, distribute, perform, display, or in
  * any way exploit any of the content, in whole or in part, except as
- * otherwise expressly permitted in writing by the copyright owner.
- * -------------------------------------------------------------------
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * --------------------------------------------------------------------- *
  * 
  * History
  *   Dec 18, 2006 (sieb): created
