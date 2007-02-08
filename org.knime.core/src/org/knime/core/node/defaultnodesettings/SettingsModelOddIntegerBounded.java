0c2fcc62a1293498b0d6a876fc62585e72d63bb2
Michael Berthold <michael.berthold@knime.com>
2007-02-08 21:20:27 +0000

    created for time series plugin.

---
+++ org.knime.core/src/org/knime/core/node/defaultnodesettings/SettingsModelOddIntegerBounded.java
@@ -0,0 +1,118 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2007
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * --------------------------------------------------------------------- *
+ * 
+ * History
+ *   22.01.2007 (rs): created
+ */
+package org.knime.core.node.defaultnodesettings;
+
+import org.knime.core.node.InvalidSettingsException;
+
+
+/**
+ * A settingsmodel for integer default components accepting double between a min
+ * and max value.
+ * 
+ * @author ohl, University of Konstanz
+ */
+public class SettingsModelOddIntegerBounded extends SettingsModelIntegerBounded {
+
+    private final int m_minValue;
+
+    private final int m_maxValue;
+
+    /**
+     * Creates a new integer settings model with min and max value for the
+     * accepted int value.
+     * 
+     * @param configName id the value is stored with in config objects.
+     * @param defaultValue the initial value.
+     * @param minValue lower bounds of the acceptable values.
+     * @param maxValue upper bounds of the acceptable values.
+     */
+    public SettingsModelOddIntegerBounded(final String configName,
+            final int defaultValue, final int minValue, final int maxValue) {
+        super(configName, defaultValue, minValue, maxValue);
+
+        if (minValue > maxValue) {
+            throw new IllegalArgumentException("Specified min value must be"
+                    + " smaller than the max value.");
+        }
+
+        m_minValue = minValue;
+        m_maxValue = maxValue;
+
+        // the actual value is the specified default value
+        try {
+            checkBounds(defaultValue);
+        } catch (IllegalArgumentException iae) {
+            throw new IllegalArgumentException("InitialValue: "
+                    + iae.getMessage());
+        }
+    }
+
+    /**
+     * @see org.knime.core.node.defaultnodesettings.SettingsModelInteger
+     *      #createClone()
+     */
+    @Override
+    SettingsModelOddIntegerBounded createClone() {
+        return new SettingsModelOddIntegerBounded(getConfigName(), getIntValue(),
+                m_minValue, m_maxValue);
+    }
+
+    /**
+     * @see org.knime.core.node.defaultnodesettings.SettingsModelInteger
+     *      #validateValue(int)
+     */
+    @Override
+    protected void validateValue(final int value)
+            throws InvalidSettingsException {
+        super.validateValue(value);
+        try {
+            checkBounds(value);
+        } catch (IllegalArgumentException iae) {
+            throw new InvalidSettingsException(iae.getMessage());
+        }
+    }
+
+    /**
+     * @see org.knime.core.node.defaultnodesettings.SettingsModelInteger
+     *      #setIntValue(int)
+     */
+    @Override
+    public void setIntValue(final int newValue) {
+        checkBounds(newValue);
+        super.setIntValue(newValue);
+    }
+
+    private void checkBounds(final double val) {
+        if ((val < m_minValue) || (m_maxValue < val)) {
+            throw new IllegalArgumentException("value (=" + val
+                    + ") must be within the range [" + m_minValue + "..."
+                    + m_maxValue + "].");
+        }
+        if ((val % 2) == 0) {
+            throw new IllegalArgumentException("value (=" + val
+                    + ") must be an odd number.");           
+        }
+    }
+}
