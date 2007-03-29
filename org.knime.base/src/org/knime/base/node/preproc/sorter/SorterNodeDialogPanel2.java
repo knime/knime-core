b6a1184cacfc123a8d6ab3060274989d9505bdd0
Bernd Wiswedel <bernd.wiswedel@knime.com>
2007-03-29 10:46:41 +0000

    unreported bug fix (apparently on linux only): "add column" button added the individual panels, but scrollbar was not shown unless dialog was resized.

--- org.knime.base/src/org/knime/base/node/preproc/sorter/SorterNodeDialogPanel2.java
+++ org.knime.base/src/org/knime/base/node/preproc/sorter/SorterNodeDialogPanel2.java
@@ -199,8 +199,7 @@ public class SorterNodeDialogPanel2 extends JPanel {
             });
             memorybox.add(m_memorycheckb);
             super.add(memorybox);
-            super.validate();
-            repaint();
+            revalidate();
         }
     }
 
