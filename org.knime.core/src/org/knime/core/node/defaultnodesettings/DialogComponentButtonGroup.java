2b7b1cbc2cb3b8dc20aa81f027112533f290fa32
Tobias Kötter <tobias.koetter@knime.com>
2007-05-31 11:09:12 +0000

    New constructor added for simpler creation and bug fixed if no button is default selected the first button gets automatically selected

--- org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentButtonGroup.java
+++ org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentButtonGroup.java
@@ -35,6 +35,7 @@ import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
 import javax.swing.JRadioButton;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -92,6 +93,108 @@ public class DialogComponentButtonGroup extends DialogComponent {
             createButtonGroupBox(m_buttonGroup, label, vertical);
         getComponentPanel().add(buttonBox);
     }
+
+    /**Constructor for class DialogComponentButtonGroup. The 
+     * <code>SettingsModel</code> holds the action command of the selected
+     * radio button.
+     * @param stringModel the model that stores the action command of the 
+     * selected radio button
+     * @param label the optional label of the group. Set to <code>null</code>
+     * for none label. Set an empty <code>String</code> for a border.
+     * @param vertical set to <code>true</code> to have the box in a vertical
+     * orientation
+     * @param buttonLabels the labels of the buttons
+     * @param actionCommands the action command of the buttons in the same order
+     * like the labels
+     * @param defaultAction the default action which should be selected
+     */
+    public DialogComponentButtonGroup(final SettingsModelString stringModel,
+            final String label, final boolean vertical, 
+            final String[] buttonLabels, final String[] actionCommands, 
+            final String defaultAction) {
+        super(stringModel);
+        if (buttonLabels == null || buttonLabels.length < 1) {
+            throw new IllegalArgumentException("Labels must not be empty");
+        }
+        if (actionCommands == null || actionCommands.length < 1) {
+            throw new IllegalArgumentException(
+                    "ActionCommands must not be empty");
+        }
+        if (buttonLabels.length != actionCommands.length) {
+            throw new IllegalArgumentException(
+                    "Labels and actionCommands must be of equal size");
+        }
+        final Set<String> uniqueness = 
+            new HashSet<String>(actionCommands.length);
+        for (String element : actionCommands) {
+            if (!uniqueness.add(element)) {
+                throw new IllegalArgumentException(
+                        "Duplicate action command found");
+            }
+        }
+        m_buttonGroup = 
+            createEnumButtonGroup(buttonLabels, actionCommands, defaultAction, 
+                    new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                updateModel();
+            }
+        });
+        
+        getModel().prependChangeListener(new ChangeListener() {
+            public void stateChanged(final ChangeEvent e) {
+                updateComponent();
+            }
+        });
+        final Box buttonBox = 
+            createButtonGroupBox(m_buttonGroup, label, vertical);
+        getComponentPanel().add(buttonBox);
+    }
+    
+    /**Constructor for class DialogComponentButtonGroup. The given 
+     * <code>SettingsModel</code> holds the selected element.
+     * @param stringModel the model that stores the action command of the 
+     * selected radio button
+     * @param label the optional label of the group. Set to <code>null</code>
+     * for none label. Set an empty <code>String</code> for a border.
+     * @param vertical set to <code>true</code> to have the box in a vertical
+     * orientation
+     * @param defaultElement the default element which should be selected
+     * @param elements the labels/action commands of the buttons
+     */
+    public DialogComponentButtonGroup(final SettingsModelString stringModel,
+            final String label, final boolean vertical, 
+            final String defaultElement,  final String... elements) {
+        super(stringModel);
+        if (elements == null || elements.length < 1) {
+            throw new IllegalArgumentException("Elements must not be empty");
+        }
+        final String[] actionCommands = new String[elements.length];
+        System.arraycopy(elements, 0, actionCommands, 0, elements.length);
+        final Set<String> uniqueness = 
+            new HashSet<String>(actionCommands.length);
+        for (String element : actionCommands) {
+            if (!uniqueness.add(element)) {
+                throw new IllegalArgumentException(
+                        "Duplicate action command found");
+            }
+        }
+        m_buttonGroup = 
+            createEnumButtonGroup(elements, actionCommands, defaultElement, 
+                    new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                updateModel();
+            }
+        });
+        
+        getModel().prependChangeListener(new ChangeListener() {
+            public void stateChanged(final ChangeEvent e) {
+                updateComponent();
+            }
+        });
+        final Box buttonBox = 
+            createButtonGroupBox(m_buttonGroup, label, vertical);
+        getComponentPanel().add(buttonBox);
+    }
     
     /**
      * Creates a <code>ButtonGroup</code> with the given elements as buttons.
@@ -101,10 +204,14 @@ public class DialogComponentButtonGroup extends DialogComponent {
     private static ButtonGroup createEnumButtonGroup(
             final ButtonGroupEnumInterface[] elements, final ActionListener l) {
         final ButtonGroup group = new ButtonGroup();
+        boolean defaultFound = false;
         for (final ButtonGroupEnumInterface element : elements) {
             final JRadioButton button = new JRadioButton(element.getText());
             button.setActionCommand(element.getActionCommand());
-            button.setSelected(element.isDefault());
+            if (element.isDefault()) {
+                button.setSelected(true);
+                defaultFound = true;
+            }
             if (element.getToolTip() != null) {
                 button.setToolTipText(element.getToolTip());
             }
@@ -113,9 +220,48 @@ public class DialogComponentButtonGroup extends DialogComponent {
             }
             group.add(button);
         }
+        if (!defaultFound && group.getButtonCount() > 0) {
+            //select the first button if none is by default selected
+            group.getElements().nextElement().setSelected(true);
+        }
         return group;
     }
     
+    /**
+     * Creates a <code>ButtonGroup</code> with the labels elements as button
+     * labels and the actionCommands as the button action commands.
+     * @param labels the labels of the buttons
+     * @param actionCommands the action commands of the buttons in the same
+     * order like the labels
+     * @param defaultAction the default action which should be selected
+     * @param l the action listener to add to each button
+     * @return the button group
+     */
+    private static ButtonGroup createEnumButtonGroup(final String[] labels, 
+            final String[] actionCommands, final String defaultAction,
+            final ActionListener l) {
+        final ButtonGroup group = new ButtonGroup();
+        boolean defaultFound = false;
+        for (int i = 0, length = actionCommands.length; i < length; i++) {
+            final JRadioButton button = new JRadioButton(labels[i]);
+            button.setActionCommand(actionCommands[i]);
+            if (defaultAction != null 
+                    && actionCommands[i].equals(defaultAction)) {
+                button.setSelected(true);
+                defaultFound = true;
+            }
+            if (l != null) {
+                button.addActionListener(l);
+            }
+            group.add(button);
+        }
+        if (!defaultFound && group.getButtonCount() > 0) {
+            //select the first button if none is by default selected
+            group.getElements().nextElement().setSelected(true);
+        }
+        return group;
+    }
+
 
     /**
      * Creates a <code>Box</code> with the buttons of the given 
@@ -192,8 +338,13 @@ public class DialogComponentButtonGroup extends DialogComponent {
     @Override
     protected void updateComponent() {
         final String val = ((SettingsModelString)getModel()).getStringValue();
-        final String actionCommand = 
-            m_buttonGroup.getSelection().getActionCommand();
+        final ButtonModel selectedButton = m_buttonGroup.getSelection();
+        final String actionCommand;
+        if (selectedButton != null) {
+            actionCommand = selectedButton.getActionCommand();
+        } else {
+            actionCommand = null;
+        }
         boolean update;
         if (val == null) {
             update = (actionCommand != null);
@@ -219,8 +370,14 @@ public class DialogComponentButtonGroup extends DialogComponent {
      */
     private void updateModel() {
         // we transfer the value from the button group into the model
-        ((SettingsModelString)getModel()).setStringValue(
-                m_buttonGroup.getSelection().getActionCommand());
+        final ButtonModel selectedButton = m_buttonGroup.getSelection();
+        final String actionCommand;
+        if (selectedButton != null) {
+            actionCommand = selectedButton.getActionCommand();
+        } else {
+            actionCommand = null;
+        }
+        ((SettingsModelString)getModel()).setStringValue(actionCommand);
 
     }
 
