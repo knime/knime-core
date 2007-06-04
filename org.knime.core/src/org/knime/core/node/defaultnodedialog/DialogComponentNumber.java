55deac6249dc1d27a5eae596981380354e785d11
Peter Ohl <peter.ohl@knime.com>
2007-06-04 11:38:08 +0000

    changed number format of number editor in spinner

--- org.knime.core/src/org/knime/core/node/defaultnodedialog/DialogComponentNumber.java
+++ org.knime.core/src/org/knime/core/node/defaultnodedialog/DialogComponentNumber.java
@@ -102,10 +102,13 @@ public class DialogComponentNumber extends DialogComponent {
         SpinnerNumberModel model = new SpinnerNumberModel(defaultValue,
                 minValue, maxValue, stepSize);
         m_spinner = new JSpinner(model);
+        m_spinner.setEditor(new JSpinner.NumberEditor(m_spinner,
+        "0.0################################################"));
         JSpinner.DefaultEditor editor = (JSpinner.DefaultEditor)m_spinner
                 .getEditor();
         editor.getTextField().setColumns(
                 Double.toString(maxValue).length() + 1);
+
         this.add(m_spinner);
         m_configName = configName;
         m_type = Type.DOUBLE;
--- org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentNumber.java
+++ org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentNumber.java
@@ -123,7 +123,7 @@ public class DialogComponentNumber extends DialogComponent {
         m_spinner = new JSpinner(spinnerModel);
         if (numberModel instanceof SettingsModelDouble) {
             m_spinner.setEditor(new JSpinner.NumberEditor(m_spinner,
-                    "#.0################################################"));
+                    "0.0################################################"));
         }
         JSpinner.DefaultEditor editor =
                 (JSpinner.DefaultEditor)m_spinner.getEditor();
