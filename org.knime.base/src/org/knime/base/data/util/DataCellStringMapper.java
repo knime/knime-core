6b1617f450864a45049b6ea882a65fa40c0dc608
Nicolas Cebron <nicolas.cebron@uni-konstanz.de>
2007-09-19 08:01:38 +0000

    load/save implemented -> private Constructor Public Constructor added

--- org.knime.base/src/org/knime/base/data/util/DataCellStringMapper.java
+++ org.knime.base/src/org/knime/base/data/util/DataCellStringMapper.java
@@ -25,8 +25,12 @@
 package org.knime.base.data.util;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.knime.core.data.DataCell;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.config.ConfigRO;
+import org.knime.core.node.config.ConfigWO;
 
 
 /**
@@ -37,19 +41,49 @@ import org.knime.core.data.DataCell;
  * Strings for unknown DataCells.
  * 
  * @author Michael Berthold, University of Konstanz
+ * @author Nicolas Cebron, University of Konstanz
  */
-public class DataCellStringMapper {
+public final class DataCellStringMapper {
 
-    private HashMap<DataCell, String> m_cellToString = new HashMap<DataCell, String>();
+    private static final String CFG_ORIGSTRINGTOSTRING = "origstringToString";
 
-    private HashMap<String, String> m_origstringToString = new HashMap<String, String>();
+    private static final String CFG_STRINGTOCELL = "stringToCell";
 
-    private HashMap<String, DataCell> m_stringToCell = new HashMap<String, DataCell>();
+    private static final String CFG_UNIQUEINDEX = "unique_index";
+    
+    private final HashMap<DataCell, String> m_cellToString;
 
-    private HashMap<String, String> m_stringToOrigstring = new HashMap<String, String>();
+    private final  HashMap<String, String> m_origstringToString;
+
+    private final HashMap<String, DataCell> m_stringToCell;
+
+    private final HashMap<String, String> m_stringToOrigstring;
 
     private int m_uniqueIndex = 0;
 
+    /**
+     * Public constructor.
+     */
+    public DataCellStringMapper() {
+        m_cellToString = new HashMap<DataCell, String>();
+        m_origstringToString = new HashMap<String, String>();
+        m_stringToCell = new HashMap<String, DataCell>();
+        m_stringToOrigstring = new HashMap<String, String>();
+    }
+    
+    private DataCellStringMapper(final HashMap<DataCell, String> cellToString,
+            final HashMap<String, String> origstringToString,
+            final HashMap<String, DataCell> stringToCell,
+            final HashMap<String, String> stringToOrigstring,
+            final int uniqueIndex) {
+        m_cellToString = cellToString;
+        m_origstringToString = origstringToString;
+        m_stringToCell = stringToCell;
+        m_stringToOrigstring = stringToOrigstring;
+        m_uniqueIndex = uniqueIndex;
+    }
+        
+    
     /**
      * @param cell {@link DataCell} to be replaced
      * @return unique string representation
@@ -139,5 +173,61 @@ public class DataCellStringMapper {
             }
         }
         return newNameBuffer.toString();
+    }
+    
+    /**
+     * Saves the {@link DataCellStringMapper}> to the given {@link ConfigWO}.
+     * 
+     * @param config Save settings to.
+     */
+    public void save(final ConfigWO config) {
+        config.addInt(CFG_UNIQUEINDEX, m_uniqueIndex);
+        ConfigWO origstringToString = config.addConfig(CFG_ORIGSTRINGTOSTRING);
+        for (Map.Entry<String, String> e : m_origstringToString.entrySet()) {
+            String key = e.getKey();
+            String value = e.getValue();
+            origstringToString.addString(key, value);
+        }
+        ConfigWO stringToCell = config.addConfig(CFG_STRINGTOCELL);
+        for (Map.Entry<String, DataCell> e : m_stringToCell.entrySet()) {
+            String key = e.getKey();
+            DataCell v = e.getValue();
+            stringToCell.addDataCell(key, v);
+        }
+    }
+
+    /**
+     * Reads a {@link DataCellStringMapper} from given {@link ConfigRO}.
+     * 
+     * @param config to read the mapper from
+     * @return A new {@link DataCellStringMapper} object.
+     * @throws InvalidSettingsException If the settings could not be read.
+     */
+    public static DataCellStringMapper load(final ConfigRO config)
+            throws InvalidSettingsException {
+        HashMap<String, DataCell> tmpstringToCell =
+                new HashMap<String, DataCell>();
+        HashMap<DataCell, String> tmpcellToString =
+                new HashMap<DataCell, String>();
+        ConfigRO keyConfig = config.getConfig(CFG_STRINGTOCELL);
+        for (String key : keyConfig.keySet()) {
+            DataCell cell = keyConfig.getDataCell(key);
+            tmpstringToCell.put(key, cell);
+            tmpcellToString.put(cell, key);
+        }
+        HashMap<String, String> tmporigstringToString =
+                new HashMap<String, String>();
+        HashMap<String, String> tmpstringToOrigstring =
+                new HashMap<String, String>();
+        keyConfig = config.getConfig(CFG_ORIGSTRINGTOSTRING);
+        for (String key : keyConfig.keySet()) {
+            String value = keyConfig.getString(key);
+            tmporigstringToString.put(key, value);
+            tmpstringToOrigstring.put(value, key);
+        }
+        int tmpuniqueIndex = config.getInt(CFG_UNIQUEINDEX);
+
+        return new DataCellStringMapper(tmpcellToString, tmporigstringToString,
+                tmpstringToCell, tmpstringToOrigstring, tmpuniqueIndex);
     }
 }
