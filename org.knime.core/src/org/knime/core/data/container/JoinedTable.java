cd328884a11b8fae4970a3c6566220f7a18e31e1
Thorsten Meinl <thorsten.meinl@knime.com>
2007-10-01 12:28:24 +0000

    - Changed version number to 1.3.0.ALPHA - fixed inconsistent line endings - fixed copyright years

--- org.knime.core/src/org/knime/core/data/container/JoinedTable.java
+++ org.knime.core/src/org/knime/core/data/container/JoinedTable.java
@@ -3,7 +3,7 @@
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
  * 
- * Copyright, 2003 - 2006
+ * Copyright, 2003 - 2007
  * Universitaet Konstanz, Germany.
  * Lehrstuhl fuer Angewandte Informatik
  * Prof. Dr. Michael R. Berthold
