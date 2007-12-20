90314c8bf95ad3a924c1455f5c70600926d097f4
Bernd Wiswedel <bernd.wiswedel@knime.com>
2007-12-20 13:04:02 +0000

    feature request: added decimal and thousands separator

--- org.knime.base/src/org/knime/base/node/preproc/colconvert/stringtonumber/StringToNumberNodeDialog.java
+++ org.knime.base/src/org/knime/base/node/preproc/colconvert/stringtonumber/StringToNumberNodeDialog.java
@@ -1,4 +1,4 @@
- /* This source code, its documentation and all appendant files
+/* This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
  *
  * Copyright, 2003 - 2007
@@ -21,28 +21,94 @@
  */
 package org.knime.base.node.preproc.colconvert.stringtonumber;
 
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.StringValue;
-import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.NotConfigurableException;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnFilter;
 import org.knime.core.node.defaultnodesettings.SettingsModelFilterString;
 
 /**
- * Dialog for the String to Number Node.
- * Lets the user choose the columns to use.
+ * Dialog for the String to Number Node. Lets the user choose the columns to
+ * use.
  * 
  * @author cebron, University of Konstanz
  */
-public class StringToNumberNodeDialog extends DefaultNodeSettingsPane {
-    
+public class StringToNumberNodeDialog extends NodeDialogPane {
+    @SuppressWarnings("unchecked")
+    private DialogComponentColumnFilter m_filtercomp =
+            new DialogComponentColumnFilter(new SettingsModelFilterString(
+                    StringToNumberNodeModel.CFG_INCLUDED_COLUMNS), 0,
+                    new Class[]{StringValue.class});
+
+    private JTextField m_decimalSeparator = new JTextField(".", 1);
+
+    private JTextField m_thousandsSeparator = new JTextField(",", 1);
+
     /**
      * Constructor.
-     *
+     * 
      */
-    @SuppressWarnings("unchecked")
     public StringToNumberNodeDialog() {
-        addDialogComponent(new DialogComponentColumnFilter(
-                new SettingsModelFilterString(
-                        StringToNumberNodeModel.CFG_INCLUDED_COLUMNS), 0,
-                new Class[]{StringValue.class}));
+        JPanel contentpanel = new JPanel();
+        contentpanel.setLayout(new BoxLayout(contentpanel, BoxLayout.Y_AXIS));
+        JPanel separatorPanel = new JPanel();
+        Border border = BorderFactory.createTitledBorder("Separator settings");
+        separatorPanel.setBorder(border);
+        separatorPanel
+                .setLayout(new BoxLayout(separatorPanel, BoxLayout.X_AXIS));
+        separatorPanel.add(new JLabel("Decimal separator: "));
+        m_decimalSeparator.setMaximumSize(new Dimension(30, 30));
+        separatorPanel.add(m_decimalSeparator);
+        separatorPanel.add(Box.createHorizontalStrut(50));
+        separatorPanel.add(new JLabel("Thousands separator: "));
+        m_thousandsSeparator.setMaximumSize(new Dimension(30, 30));
+        separatorPanel.add(m_thousandsSeparator);
+        contentpanel.add(separatorPanel);
+        contentpanel.add(m_filtercomp.getComponentPanel());
+        super.addTab("Settings", contentpanel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadSettingsFrom(final NodeSettingsRO settings,
+            final DataTableSpec[] specs) throws NotConfigurableException {
+        m_filtercomp.loadSettingsFrom(settings, specs);
+        String decimalsep =
+                settings.getString(StringToNumberNodeModel.CFG_DECIMALSEP,
+                        StringToNumberNodeModel.DEFAULT_DECIMAL_SEPARATOR);
+        m_decimalSeparator.setText(decimalsep);
+        String thousandssep =
+                settings.getString(StringToNumberNodeModel.CFG_THOUSANDSSEP,
+                        StringToNumberNodeModel.DEFAULT_THOUSANDS_SEPARATOR);
+        m_thousandsSeparator.setText(thousandssep);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings)
+            throws InvalidSettingsException {
+        m_filtercomp.saveSettingsTo(settings);
+        settings.addString(StringToNumberNodeModel.CFG_DECIMALSEP,
+                m_decimalSeparator.getText());
+        settings.addString(StringToNumberNodeModel.CFG_THOUSANDSSEP,
+                m_thousandsSeparator.getText());
     }
 }
