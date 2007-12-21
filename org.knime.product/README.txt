961d735ff40c723bf46f3350b31813525f7f940c
Thorsten Meinl <thorsten.meinl@knime.com>
2007-12-21 14:39:43 +0000

    Copied KNIME_Deployment to new branch

---
+++ org.knime.product/README.txt
@@ -0,0 +1,43 @@
+
+KNIME 1.3.2.RC1
+============================================================
+
+Quickstart guide:
+- Extract the archive (if you read this here, you likely already did so).
+- Under Windows launch knime.exe, for Linux use knime.sh (either double
+  click in a file browser or execute it in a shell)
+- KNIME will open and you can start assembling a workflow (KNIME will use 
+  the folder 'workspace' contained in this directory to store the project
+  files in).
+
+
+How to get additional features:
+  KNIME allows to easily integrate new features. These often come under a
+  different license and therefore are not included in this archive. Please
+  see http://www.knime.org for any available extension. For
+  example an R integration feature (http://www.r-project.org) and a 
+  chemistry feature (using the chemistry development kit, CDK, http://cdk.sf.net/) 
+  are available.
+
+  A convenient way to download new KNIME features is via the KNIME 
+  update site. In your current KNIME installation select 
+  "File", "Update KNIME..." (or in the Developer Version via "Help", 
+  "Software Updates",  "Find and Install..."). The Update Wizard opens. 
+  If you expand the "KNIME" site, you see two main categories: 
+  KNIME Extensions and KNIME Development. From the KNIME Extensions category
+  select the features you want to install now. (In the developer version of
+  KNIME you may want to select the new features from the KNIME Development
+  category, as they include the source code. Also, the New Node Wizard
+  extension is only available in the KNIME Development category.)
+  If you select a feature and receive an error about a configuration problem, 
+  click the "Select Required" button. This automatically selects all required 
+  features.
+  You need to restart KNIME after installing new extensions in order to get
+  them activated.
+
+  Another way to install new features is to download the extension in an archive
+  file from our website (www.knime.org). Unpack the file into your KNIME 
+  installation directory (so that all files of the archive's plugin directory 
+  are stored in KNIME's plugin dir and the files of the feature directory are 
+  stored in KNIME's feature dir). Restart KNIME to register the newly added 
+  features.
---
+++ org.knime.product/src/org/knime/product/rcp/KNIMEApplication.java
@@ -0,0 +1,116 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2007
+ * University of Konstanz, Germany.
+ * Chair for Bioinformatics and Information Mining
+ * Prof. Dr. Michael R. Berthold
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ----------------------------------------------------------------------------
+ */
+package org.knime.product.rcp;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IPlatformRunnable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
+import org.knime.core.util.FileLocker;
+
+/**
+ * This class controls all aspects of the application's execution.
+ */
+public class KNIMEApplication implements IPlatformRunnable {
+
+    private static final String PROP_EXIT_CODE = "eclipse.exitcode";
+
+    private static String LOCK_FOLDERNAME = ".metadata";
+
+    private static String LOCK_FILENAME = ".lock";
+
+    /**
+     * @param args The args
+     * @throws Exception on general application error
+     * @return return code, interpreted by OS (e.g. "restart app")
+     * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
+     */
+    public Object run(final Object args) throws Exception {
+        Display display = PlatformUI.createDisplay();
+        // check if the workspace is already in use
+        // at this point its valid, so try to lock it and update the
+        // metadata version information if successful
+        try {
+            Shell shell = new Shell(display, SWT.ON_TOP);
+            if (!lockWorkspace()) {
+                Platform.endSplash();
+                MessageDialog
+                        .openError(
+                                shell,
+                                IDEWorkbenchMessages.IDEApplication_workspaceCannotLockTitle,
+                                IDEWorkbenchMessages.IDEApplication_workspaceCannotLockMessage);
+                return EXIT_OK;
+            }
+        } catch (Exception e) {
+            // do nothing if locking could not be performed
+        }
+
+        try {
+            int returnCode =
+                    PlatformUI.createAndRunWorkbench(display,
+                            new KNIMEApplicationWorkbenchAdvisor());
+
+            // the workbench doesn't support relaunch yet (bug 61809) so
+            // for now restart is used, and exit data properties are checked
+            // here to substitute in the relaunch return code if needed
+            if (returnCode != PlatformUI.RETURN_RESTART) {
+                return EXIT_OK;
+            }
+
+            // if the exit code property has been set to the relaunch code, then
+            // return that code now, otherwise this is a normal restart
+            return EXIT_RELAUNCH.equals(Integer.getInteger(PROP_EXIT_CODE)) ? EXIT_RELAUNCH
+                    : EXIT_RESTART;
+            // if (returnCode == PlatformUI.RETURN_RESTART) {
+            // return IPlatformRunnable.EXIT_RESTART;
+            // }
+            // return IPlatformRunnable.EXIT_OK;
+        } finally {
+            display.dispose();
+        }
+    }
+
+    private boolean lockWorkspace() throws Exception {
+
+        Location instanceLoc = Platform.getInstanceLocation();
+        
+        File lockFileFolder =
+                new File(instanceLoc.getURL().getFile(), LOCK_FOLDERNAME);
+        if (!lockFileFolder.exists()) {
+            lockFileFolder.mkdirs();
+        }
+        File lockFile = new File(lockFileFolder, LOCK_FILENAME);
+        if (!lockFile.exists()) {
+            lockFile.createNewFile();
+        }
+
+        FileLocker locker = new FileLocker(lockFile);
+        return locker.lock();
+    }
+}
