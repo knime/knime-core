d0c4ad9d1e9272ebadd75f748a3cbc851e55fd61
Fabian Dill <fabian.dill@knime.com>
2008-02-14 12:23:55 +0000

    comments, cleanup

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/NewWorkflowEditPartFactory.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/NewWorkflowEditPartFactory.java
@@ -26,15 +26,12 @@ package org.knime.workbench.editor2;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartFactory;
+import org.eclipse.gef.GraphicalViewer;
 import org.knime.core.node.NodeInPort;
-import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeOutPort;
 import org.knime.core.node.NodePort;
 import org.knime.core.node.workflow.ConnectionContainer;
-import org.knime.core.node.workflow.NodeID;
 import org.knime.core.node.workflow.SingleNodeContainer;
-import org.knime.core.node.workflow.WorkflowEvent;
-import org.knime.core.node.workflow.WorkflowListener;
 import org.knime.core.node.workflow.WorkflowManager;
 import org.knime.workbench.editor2.editparts.ConnectionContainerEditPart;
 import org.knime.workbench.editor2.editparts.NodeContainerEditPart;
@@ -49,139 +46,100 @@ import org.knime.workbench.editor2.model.WorkflowOutPortProxy;
 /**
  * This factory creates the GEF <code>EditPart</code>s instances (the
  * controller objects) for given model objects.
- *
+ * 
  * @author Florian Georg, University of Konstanz
+ * @author Fabian Dill, University of Konstanz
  */
-public final class NewWorkflowEditPartFactory implements EditPartFactory,
-    WorkflowListener {
+public final class NewWorkflowEditPartFactory implements EditPartFactory {
 
-    private static final NodeLogger LOGGER = NodeLogger.getLogger(
-            NewWorkflowEditPartFactory.class);
+//    private static final NodeLogger LOGGER =
+//            NodeLogger.getLogger(NewWorkflowEditPartFactory.class);
 
-    private NodeID m_nodeID;
-
-    /**
-     * Private singleton constructor.
-     *
+    /* 
+     * we need this flag to determine between the "root" workflow manager and
+     * all subsequent meta nodes. This means that we implicitely assume that
+     * the "root" workflow manager is the first object to come of type
+     * WorkflowManager. This assumption is correct, since the top model is 
+     * passed to the factory and subsequently is asked for its model children.
+     * 
+     * @see WorkflowGraphicalViewerCreator#createViewer
+     * @see AbstractGraphicalViewer#sertEditPartFactory
+     * @see WorkflowRootEditPart#getModelChildren
      */
-    public  NewWorkflowEditPartFactory() {
-    }
-
-    public void workflowChanged(final WorkflowEvent event) {
-        if (event.getType().equals(WorkflowEvent.Type.NODE_REMOVED)) {
-            m_nodeID = event.getID();
-        }
-    }
-
+    private boolean m_isTop = true;
 
     /**
+     * An instance per {@link GraphicalViewer} is necessary, in order to 
+     * distinguish between the workflow manager of the editor and contained 
+     * subworkflows.  
+     * 
+     */
+    public NewWorkflowEditPartFactory() {
+    }
+
+    /**
+     * Creates the referring edit parts for the following parts of the model.
+     * <ul>
+     *  <li>{@link WorkflowManager}: either {@link WorkflowRootEditPart} or 
+     *  {@link NodeContainerEditPart} (depending on the currently displayed 
+     *  level)</li>
+     *  <li>{@link SingleNodeContainer}: {@link NodeContainerEditPart}</li>
+     *  <li>{@link NodeInPort}: {@link NodeInPortEditPart}</li>
+     *  <li>{@link NodeOutPort}: {@link NodeOutPortEditPart}</li>
+     *  <li>{@link ConnectionContainer}: {@link ConnectionContainerEditPart}
+     *  </li>
+     *  <li>{@link WorkflowInPortProxy}: {@link WorkflowInPortEditPart}</li>
+     *  <li>{@link WorkflowOutPortProxy}: {@link WorkflowOutPortEditPart}</li>
+     * </ul>
+     * 
+     * @see WorkflowRootEditPart#getModelChildren()
+     * 
+     * @throws IllegalArgumentException if any other object is passed
+     * 
      * {@inheritDoc}
      */
     public EditPart createEditPart(final EditPart context, final Object model) {
         EditPart part = null;
-        // TODO: what if workflow manager : meta node?
-        // could the context be used?
-//        LOGGER.debug("creating edit part for: " + model);
         if (model instanceof WorkflowManager) {
-            WorkflowManager wfm = (WorkflowManager)model;
-            if (m_nodeID == null) {
-//                LOGGER.debug("setting node id: " + wfm.getID());
-                m_nodeID = wfm.getID();
-                wfm.addListener(this);
-            }
-//                LOGGER.debug("new : " + wfm.getID() + " old: " + m_nodeID);
-            if (isMetaNode(wfm.getID())) {
-                m_nodeID = wfm.getID();
-//                    LOGGER.debug("detected meta node, "
-//                            + "return node container edit part");
+            if (m_isTop) {
+                m_isTop = false;
+                part = new WorkflowRootEditPart();
+            } else {
                 part = new NodeContainerEditPart();
-                part.setModel(model);
-                return part;
             }
-            //
-            // WorkflowManager -> WorkflowRootEditPart
-            //
-//            LOGGER.debug("workflow root edit part");
-            part = new WorkflowRootEditPart();
         } else if (model instanceof SingleNodeContainer) {
-            //
-            // NodeContainer -> NodeContainerEditPart
-            //
-//            LOGGER.debug("node container edit part");
             part = new NodeContainerEditPart();
         } else if (model instanceof NodeInPort) {
-            //
-            // NodeInPort -> NodeInPortEditPart
-            // (We'll need the ID to be able to associate to the correct port)
-            NodePort port = (NodeInPort) model;
+            NodePort port = (NodeInPort)model;
             part = new NodeInPortEditPart(port.getPortType(), port.getPortID());
         } else if (model instanceof NodeOutPort) {
             //
             // NodeOutPort -> NodeOutPortEditPart
-            // (We'll need the ID to be able to associate to the correct port)
-            NodePort port = (NodeOutPort) model;
-            part = new NodeOutPortEditPart(port.getPortType(),
-                    port.getPortID());
+            NodePort port = (NodeOutPort)model;
+            part =
+                    new NodeOutPortEditPart(port.getPortType(), port
+                            .getPortID());
         } else if (model instanceof ConnectionContainer) {
             //
             // ConnectionContainer -> ConnectionContainerEditPart
             //
-            LOGGER.debug("created connection for : " + model);
-            ConnectionContainer conn = (ConnectionContainer)model;
-            LOGGER.debug("source: " + conn.getSource() + " target: "
-                    + conn.getDest());
             part = new ConnectionContainerEditPart();
         } else if (model instanceof WorkflowInPortProxy) {
             WorkflowInPortProxy inport = (WorkflowInPortProxy)model;
-            if (isMetaNode(inport.getID())) {
-//                LOGGER.debug("must return node in port for node: "
-//                        + inport.getID());
-                part = new NodeInPortEditPart(inport.getPort().getPortType(),
-                        inport.getPort().getPortID());
-                part.setModel(inport.getPort());
-                return part;
-            } else {
-//                LOGGER.debug("must return workflow in port for node: "
-//                        + inport.getID());
-                // TODO replace with workflow out port
-                part = new WorkflowInPortEditPart(
-                        inport.getPort().getPortType(),
-                        inport.getPort().getPortID());
-//                LOGGER.debug("setting model : " + inport.getPort());
-            }
+            part =
+                    new WorkflowInPortEditPart(inport.getPort().getPortType(),
+                            inport.getPort().getPortID());
         } else if (model instanceof WorkflowOutPortProxy) {
             WorkflowOutPortProxy outport = (WorkflowOutPortProxy)model;
-            if (isMetaNode(outport.getID())) {
-//                LOGGER.debug("must return node out port for node: "
-//                        + outport.getID());
-                part = new NodeOutPortEditPart(outport.getPort().getPortType(),
-                        outport.getPort().getPortID());
-                part.setModel(outport.getPort());
-                return part;
-            } else {
-//                LOGGER.debug("must return workflow out port for node: "
-//                        + outport.getID());
-//                LOGGER.debug("setting model for: "  + outport.getID() + " "
-//                        + outport.getPort());
-                part = new WorkflowOutPortEditPart(outport.getPort()
-                        .getPortType(), outport.getPort().getPortID());
-            }
+            part =
+                    new WorkflowOutPortEditPart(
+                            outport.getPort().getPortType(), 
+                            outport.getPort().getPortID());
         } else {
             throw new IllegalArgumentException("unknown model obj: " + model);
         }
-
         // associate the model with the part (= the controller)
-        if (part.getModel() == null) {
-            part.setModel(model);
-        }
-
+        part.setModel(model);
         return part;
     }
-
-    private boolean isMetaNode(final NodeID id) {
-        return id.compareTo(m_nodeID) > 0;
-    }
-
-
-
 }
