a3dc9e553b8d3b9de256a68ce2dea1e62bcb70b1
Michael Berthold <michael.berthold@knime.com>
2008-02-15 13:51:45 +0000

    remove WorkflowManager.shutdown()

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/LoadWorkflowRunnable.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/LoadWorkflowRunnable.java
@@ -105,7 +105,6 @@ class LoadWorkflowRunnable extends PersistWorflowRunnable {
             LOGGER
                     .info("Canceled loading worflow: "
                             + m_workflowFile.getName());
-            m_editor.getWorkflowManager().shutdown();
             m_editor.setWorkflowManager(null);
             m_editor.setLoadingCanceled(true);
             m_editor.setLoadingCanceledMessage(cee.getMessage());
