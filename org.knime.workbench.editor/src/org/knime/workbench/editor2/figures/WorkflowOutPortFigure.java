c53350ac79e6f5fa523a4838b8134b278737511a
Fabian Dill <fabian.dill@knime.com>
2008-02-18 13:32:51 +0000

    get informed about resize events and the new actual parent size for dragged connectinos the size is freezed

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/figures/WorkflowOutPortFigure.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/figures/WorkflowOutPortFigure.java
@@ -29,10 +29,14 @@ import org.knime.core.node.PortType;
  *
  * @author Fabian Dill, University of Konstanz
  */
-public class WorkflowOutPortFigure extends AbstractWorkflowPortFigure  {
+public class WorkflowOutPortFigure extends AbstractWorkflowPortFigure {
 
 //    private static final NodeLogger LOGGER = NodeLogger.getLogger(
 //            WorkflowOutPortFigure.class);
+    
+    private Rectangle m_parentSize;
+    
+    private boolean m_freeze = false;
 
     /**
      *
@@ -49,20 +53,42 @@ public class WorkflowOutPortFigure extends AbstractWorkflowPortFigure  {
 
 
     /**
+     * Sets the parent size when it should be fixed (independent of workbench 
+     * size), for example, when the workbench is resized of if a connection is
+     * dragged.
+     * 
+     * @param rect the workbench's control size (or desired size)
+     */
+    public void setParentSize(final Rectangle rect) {
+        m_freeze = true;
+        m_parentSize = rect;
+        setBounds(getNewBounds(m_parentSize));
+        repaint();
+    }
+    
+    private Rectangle getNewBounds(final Rectangle parent) {
+        int yPos = (parent.height / (getNrPorts() + 1)) 
+            * (getPortIndex() + 1);
+        return new Rectangle(parent.width - SIZE,
+            yPos, SIZE, SIZE);
+    }
+    
+    /**
      * 
      * {@inheritDoc}
      */
     @Override
     protected PointList createShapePoints(final Rectangle rect) {
+        Rectangle parent;
+        if (!m_freeze) {
+            parent = getParent().getBounds().getCopy();
+            m_parentSize = parent;
+        } else {
+            parent = m_parentSize;
+        }
+        setBounds(getNewBounds(parent));
         if (getType().equals(BufferedDataTable.TYPE)) {
             // triangle
-            Rectangle parent = getParent().getBounds().getCopy();
-            int yPos = (parent.height / (getNrPorts() + 1)) 
-                * (getPortIndex() + 1);
-            getBounds().x = parent.width - SIZE;
-            getBounds().y = yPos;
-            getBounds().width = SIZE;
-            getBounds().height = SIZE;
             Rectangle r = getBounds().getCopy();
             PointList list = new PointList(3);
             list.addPoint(r.x, r.y);
@@ -71,14 +97,9 @@ public class WorkflowOutPortFigure extends AbstractWorkflowPortFigure  {
             return list;
         } else {
             // square
-            Rectangle parent = getParent().getBounds().getCopy();
             int yPos = (parent.height / (getNrPorts() + 1)) 
                 * (getPortIndex() + 1);
             int xPos = parent.width - SIZE;
-            getBounds().x = xPos;
-            getBounds().y = yPos;
-            getBounds().width = SIZE;
-            getBounds().height = SIZE;
             PointList list = new PointList(4);
             list.addPoint(new Point(xPos, yPos));
             list.addPoint(new Point(xPos + SIZE, yPos));
@@ -86,6 +107,27 @@ public class WorkflowOutPortFigure extends AbstractWorkflowPortFigure  {
             list.addPoint(new Point(xPos, yPos + SIZE));
             return list;
         }
+    }
+    
+    /**
+     * Tells the figure to use the explicitely size set and not to get the size
+     * from the parent (for example when a connection is dragged outside the 
+     * workbench's size).
+     * 
+     * @see #releaseSizeFreeze()
+     */
+    public void freezeSize() {
+        m_freeze = true;
+    }
+    
+    /**
+     * Tells the figure to retrieve the size from the parent again instead of 
+     * using the explicitely set size.
+     *@see #freezeSize() 
+     */
+    public void releaseSizeFreeze() {
+        m_freeze = false;
+        repaint();
     }
 
     /**
