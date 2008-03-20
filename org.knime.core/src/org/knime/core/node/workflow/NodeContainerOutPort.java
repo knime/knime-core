913ef376038daa9b1ffd78a676a18668e2b1aac8
Michael Berthold <michael.berthold@knime.com>
2008-03-20 20:54:41 +0000

    moved Ports to Workflow from Node

---
+++ org.knime.core/src/org/knime/core/node/workflow/NodeContainerOutPort.java
@@ -0,0 +1,196 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2007
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   Mar 20, 2008 (mb): created
+ */
+package org.knime.core.node.workflow;
+
+import org.knime.core.node.Node;
+import org.knime.core.node.PortObject;
+import org.knime.core.node.PortObjectSpec;
+import org.knime.core.node.property.hilite.HiLiteHandler;
+import org.knime.core.node.workflow.NodeContainer.State;
+
+/** The implementation of an OutPort of a SingleNodeContainer - e.g. a "real"
+ * node.
+ * 
+ * @author M. Berthold, University of Konstanz
+ */
+public class NodeContainerOutPort extends NodePortAdaptor
+implements NodeOutPort {
+
+    /**
+     * The underlying Node.
+     */
+    private final SingleNodeContainer m_snc;
+    
+    /**
+     * The inspector view of this port. Could be null if not opened yet.
+     */
+    private NodeOutPortView m_portView;
+
+    /**
+     * Creates a new output port with a fixed and ID (should unique to all other
+     * output ports of this node) for the given node.
+     *
+     * @param node the underlying node.
+     * @param portID This port ID.
+     */
+    public NodeContainerOutPort(final SingleNodeContainer snc,
+            final int portID) {
+        super(portID, snc.getNode().getOutputType(portID));
+        m_snc = snc;
+        m_portView = null;
+        // TODO register this object as listener to spec/object... changes
+        //   with Node!!
+    }
+
+    /**
+     * Returns the <code>DataTableSpec</code> or null if not available.
+     *
+     * @return The <code>DataTableSpec</code> for this port.
+     */
+    public PortObjectSpec getPortObjectSpec() {
+        return m_snc.getNode().getOutputSpec(getPortID());
+    }
+
+    /**
+     * Returns the DataTable for this port, as set by the node this port is
+     * output for.
+     *
+     * @return PortObject the object for this port. Can be null.
+     */
+    public PortObject getPortObject() {
+        // the following test allows SingleNodeContainers/WFMs to hide
+        // the PortObjects after a Node.execute() until the state of the
+        // SNC/WFM has been adjusted to "EXECUTED"
+        return m_snc.getState().equals(State.EXECUTED)
+                      ? m_snc.getNode().getOutputObject(getPortID()) : null;
+    }
+    
+    /**
+     * Returns the hilite handler for this port as set by the node this port is
+     * output for.
+     *
+     * @return The HiLiteHandler for this port or null.
+     */
+    public HiLiteHandler getHiLiteHandler() {
+        return m_snc.getNode().getHiLiteHandler(getPortID());
+    }
+
+    /**
+     * Sets a new hilite handler in the port. The port will notify all
+     * connected input ports of a really new (i.e. not equal) hightlight handler
+     * available.
+     *
+     * @param hiLiteHdl The new HiLiteHandler for this port.
+     */
+    void setHiLiteHandler(final HiLiteHandler hiLiteHdl) {
+        m_snc.getNode().setHiLiteHandler(getPortID(), hiLiteHdl);
+    }
+
+    /**
+     * Returns the scope object stack of the underlying node.
+     *
+     * @return the scope obj stack container
+     */
+    public ScopeObjectStack getScopeContextStackContainer() {
+        return m_snc.getNode().getScopeObjectStack(getPortID());
+    }
+
+    /**
+     * Sets a new scope object stack.
+     * @param sos scope object stack
+     */
+    void setScopeContextStackContainer(final ScopeObjectStack sos) {
+        m_snc.getNode().setScopeObjectStack(getPortID(), sos);
+    }
+
+
+    /**
+     * Sets a port view for this port. The port view can only be set once.
+     *
+     * @param portView The port view to set.
+     * @throws NullPointerException If the port view is null.
+     * @throws IllegalStateException If the port view was already set.
+     * @see #getPortView()
+     */
+    private final void setPortView(final NodeOutPortView portView) {
+        if (portView == null) {
+            throw new NullPointerException("Can't set port view to null");
+        }
+        if (m_portView != null) {
+            throw new IllegalStateException("Port View can only set once.");
+        }
+        m_portView = portView;
+    }
+
+    /**
+     * Returns the port view for this output port which can be null.
+     *
+     * @return The port view or null.
+     */
+    protected final NodeOutPortView getPortView() {
+        return m_portView;
+    }
+
+    /**
+     * Opens the port view for this port with the given name.
+     *
+     * @param name The name of the port view.
+     */
+    // TODO: return component with convenience method for Frame construction.
+    public void openPortView(final String name) {
+        if (m_portView == null) {
+            setPortView(NodeOutPortView.createOutPortView(getPortType(),
+                    name, getPortName()));
+        }
+        m_portView.update(getPortObject(), getPortObjectSpec());
+        m_portView.openView();
+    }
+
+    /**
+     * Call this when the port is not used anymore, certainly when you've opened
+     * a view before. All port views will be closed and disposed.
+     * 
+     * TODO remove this parameter??
+     * @param Node why?
+     */
+    protected void disposePortView(final Node node) {
+        if (m_portView != null) {
+            m_portView.setVisible(false);
+            m_portView.dispose();
+            m_portView = null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO (tg) This method is not guaranteed to be called!
+        // make sure to blow away the port view
+        disposePortView(null);
+        super.finalize();
+    }
+
+}
