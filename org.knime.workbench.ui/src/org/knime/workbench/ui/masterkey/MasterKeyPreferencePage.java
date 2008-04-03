6e29aa2d922041cef3f6823e85b69e847b64bb26
Fabian Dill <fabian.dill@knime.com>
2008-04-03 12:22:52 +0000

    re-use of the preference page for the dialog

--- org.knime.workbench.ui/src/org/knime/workbench/ui/masterkey/MasterKeyPreferencePage.java
+++ org.knime.workbench.ui/src/org/knime/workbench/ui/masterkey/MasterKeyPreferencePage.java
@@ -24,31 +24,19 @@
  */
 package org.knime.workbench.ui.masterkey;
 
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.knime.core.util.EncryptionKeySupplier;
 import org.knime.workbench.ui.KNIMEUIPlugin;
 import org.knime.workbench.ui.preferences.PreferenceConstants;
-import org.knime.workbench.ui.wrapper.WrappedNodeDialog;
 
 /**
  * Preference page used to enter (or not) a master key for KNIME.
@@ -83,7 +71,7 @@ public class MasterKeyPreferencePage extends FieldEditorPreferencePage
             }
             Display.getDefault().syncExec(new Runnable() {
                 public void run() {
-                    m_lastMasterKey = openDialogAndReaderKey();
+                    m_lastMasterKey = openDialogAndReadKey();
                 }
             });
             m_isDefined = (m_lastMasterKey != null);
@@ -118,7 +106,7 @@ public class MasterKeyPreferencePage extends FieldEditorPreferencePage
                 PreferenceConstants.P_MASTER_KEY, "Confirm: ", 20, parent);
         m_masterKeyConfirm.getTextControl(parent).setEchoChar('*');
         super.addField(m_masterKeyConfirm);
-    }
+    }    
     
     /**
      * {@inheritDoc}
@@ -147,6 +135,7 @@ public class MasterKeyPreferencePage extends FieldEditorPreferencePage
      */
     @Override
     public boolean performOk() {
+        try {
         if (m_isMasterKey.getBooleanValue()) {
             String masterKey = m_masterKey.getStringValue();
             boolean valid = 
@@ -163,11 +152,23 @@ public class MasterKeyPreferencePage extends FieldEditorPreferencePage
         getPreferenceStore().setValue(PreferenceConstants.P_MASTER_KEY_DEFINED, 
                 SUPPLIER.m_isDefined);
         return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    /**
+     * 
+     * @return the entered master key
+     */
+    String getMasterKey() {
+        return m_masterKey.getStringValue();
     }
     
     private static boolean checkMasterKeys(final String masterKey, 
             final String confirmMasterKey) {
-        if (masterKey == null) {
+        if (masterKey == null || masterKey.isEmpty()) {
             MessageBox mb = new MessageBox(Display.getDefault()
                     .getActiveShell(), SWT.ICON_ERROR | SWT.OK);
             mb.setText("Empty master key...");
@@ -207,216 +208,22 @@ public class MasterKeyPreferencePage extends FieldEditorPreferencePage
 
             + "Note: if you have entered a key in a previous session which\n"
             + "has been used to encrypt passwords, those passwords can\n"
-            + "obviously only be decrypted with the same key.\n\n";
+            + "obviously only be decrypted with the same key. \n\n";
 
-    private static String openDialogAndReaderKey() {
-        final String[] masterKey = new String[]{null};
-        Display display;
+    private static String openDialogAndReadKey() {
         try {
-            display = new Display();
-        } catch (Throwable t) {
-            display = Display.getDefault();
+        Shell shell = Display.getDefault().getActiveShell();
+        if (shell == null) {
+            shell = new Shell();
         }
-        // search for suitable parents
-        Shell[] shells = display.getShells();
-        Shell dialogShell = null;
-        if (shells != null) {
-            for (Shell posShell : shells) {
-                if (posShell.getData() != null
-                      && posShell.getData() instanceof WrappedNodeDialog) {
-                    dialogShell = posShell;
-                    break;
-                } else if (posShell.getData() != null
-                   && posShell.getData() instanceof ProgressMonitorDialog) {
-                    dialogShell = posShell;
-                    break;
-                }
-            }
-
-            // if dialogShell is null get another default shell
-            if (dialogShell == null) {
-                if (shells.length > 0) {
-                    dialogShell = shells[0];
-                }
-            }
+        MasterKeyDialog dialog = new MasterKeyDialog(shell); 
+        dialog.open();
+        return dialog.getMasterKey();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        Shell shell = null;
-        if (dialogShell != null) {
-            shell = new Shell(dialogShell);
-        } else {
-            shell = new Shell(display, SWT.ON_TOP | SWT.SHELL_TRIM);
-        }
-
-        try {
-            shell.setImage(AbstractUIPlugin.imageDescriptorFromPlugin(
-                KNIMEUIPlugin.PLUGIN_ID, "/icons/knime.png").createImage());
-        } catch (Throwable e) {
-            // do nothing, is just the icon
-        }
-        shell.setText("KNIME Master Key");
-        shell.setSize(300, 200);
-        shell.forceActive();
-        shell.forceFocus();
-
-        GridLayout gridLayout = new GridLayout();
-
-        gridLayout.numColumns = 1;
-
-        shell.setLayout(gridLayout);
-
-        GridData gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 2;
-
-        final Label label = new Label(shell, SWT.NONE);
-        label.setText(DESCRIPTION);
-        label.setBounds(20, 15, 300, 260);
-        label.setLayoutData(gridData);
-
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 1;
-        final Label keyLable = new Label(shell, SWT.NONE);
-        keyLable.setText("Master key:");
-        keyLable.setLayoutData(gridData);
-        
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 2;
-        final Label spaceLabel2 = new Label(shell, SWT.NONE);
-        spaceLabel2.setText("");
-        spaceLabel2.setLayoutData(gridData);
-
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 1;
-        // gridData.widthHint = 200;
-        final Text text = new Text(shell, SWT.PASSWORD | SWT.BORDER);
-        // text.setBounds(140, 270, 200, 20);
-        text.setSize(200, 20);
-        text.setLayoutData(gridData);
-        
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 1;
-        // gridData.widthHint = 200;
-        final Text confirm = new Text(shell, SWT.PASSWORD | SWT.BORDER);
-        // text.setBounds(140, 270, 200, 20);
-        confirm.setSize(200, 20);
-        confirm.setLayoutData(gridData);
-        
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 2;
-        final Label spaceLabel = new Label(shell, SWT.NONE);
-        spaceLabel.setText("\n");
-        spaceLabel.setLayoutData(gridData);
-        
-        gridLayout.numColumns = 2;
-
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.HORIZONTAL_ALIGN_END;
-        gridData.widthHint = 80;
-        gridData.horizontalSpan = 1;
-        final Button button = new Button(shell, SWT.PUSH);
-        button.setText("OK");
-        // button.setBounds(20, 270, 180, 20);
-        button.setSize(150, 20);
-        button.setLayoutData(gridData);
-
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.HORIZONTAL_ALIGN_END;
-        gridData.widthHint = 80;
-        gridData.horizontalSpan = 1;
-        final Button cancel = new Button(shell, SWT.PUSH);
-        cancel.setText("Cancel");
-        // cancel.setBounds(20, 270, 180, 20);
-        cancel.setSize(150, 20);
-        cancel.setLayoutData(gridData);
-
-        gridData = new GridData();
-        gridData.horizontalAlignment = GridData.FILL;
-        gridData.horizontalSpan = 2;
-
-        final Label info = new Label(shell, SWT.NONE);
-        info.setText("");
-        // label.setBounds(20, 15, 380, 260);
-        info.setLayoutData(gridData);
-
-        Composite parent = shell.getParent();
-        if (parent != null) {
-            shell.setBounds(parent.getBounds().width / 2, parent
-                    .getBounds().height / 2, 300, 200);
-        } else {
-            shell.setBounds(display.getBounds().width / 2, display
-                    .getBounds().height / 2, 300, 200);
-        }
-
-        shell.pack();
-        shell.open();
-
-        text.setFocus();
-        
-        final boolean[] finished = new boolean[1];
-        final Listener buttonListener = new Listener() {
-            public void handleEvent(final Event event) {
-                String key = text.getText();
-                finished[0] = checkMasterKeys(key, confirm.getText());
-                if (finished[0]) {
-                    masterKey[0] = key;
-                }
-            }
-        };
-
-        button.addListener(SWT.Selection, buttonListener);
-
-        KeyListener keyListener = new KeyListener() {
-
-            public void keyPressed(final KeyEvent e) {
-                if (e.character == '\r') {
-                    buttonListener.handleEvent(null);
-                }
-            }
-
-            public void keyReleased(final KeyEvent e) {
-                // do nothing
-            }
-        };
-
-        text.addKeyListener(keyListener);
-        confirm.addKeyListener(keyListener);
-        shell.addKeyListener(keyListener);
-
-        Listener cancelListener = new Listener() {
-            public void handleEvent(final Event event) {
-                MessageBox mb = new MessageBox(Display.getDefault()
-                     .getActiveShell(), SWT.ICON_WARNING | SWT.YES | SWT.NO);
-                mb.setText("Warning...");
-                mb.setMessage(
-                    "All passwords will be saved as plain text. Are you sure?");
-                if (mb.open() == SWT.YES) {
-                    masterKey[0] = null;
-                    finished[0] = true;
-                } else {
-                    finished[0] = false;
-                }
-            }
-
-        };
-
-        cancel.addListener(SWT.Selection, cancelListener);
-
-        while (!shell.isDisposed() && !finished[0]) {
-            if (!display.readAndDispatch()) {
-                display.sleep();
-            }
-        }
-        if (!shell.isDisposed()) {
-            shell.close();
-            shell.dispose();
-        }
-        return masterKey[0];
+        return null;
     }
+    
 
 }
