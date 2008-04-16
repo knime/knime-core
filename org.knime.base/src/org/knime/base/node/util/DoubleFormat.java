3ed896e36f228f2f2bfdeab6a0c793cea62152b1
Bernd Wiswedel <bernd.wiswedel@knime.com>
2008-04-16 11:19:03 +0000

    handle infty and nan cases

--- org.knime.base/src/org/knime/base/node/util/DoubleFormat.java
+++ org.knime.base/src/org/knime/base/node/util/DoubleFormat.java
@@ -83,8 +83,8 @@ public final class DoubleFormat {
      * @return the string representation of <code>d</code>
      */
     public static String formatDouble(final double d) {
-        if (d == 0) {
-            return Double.toString(0.0);
+        if (d == 0.0 || Double.isInfinite(d) || Double.isNaN(d)) {
+            return Double.toString(d);
         }
         double abs = Math.abs(d);
         if (abs < 0.0001) {
