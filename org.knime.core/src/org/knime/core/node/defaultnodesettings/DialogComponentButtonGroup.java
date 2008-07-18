1b09d9ae911a5bd18cc4be9e81c023a574c14ff3
Tobias Kötter <tobias.koetter@knime.com>
2008-07-18 11:46:01 +0000

    Existing constructors replaced with constructors that use the settingsmodel default value.

--- org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentButtonGroup.java
+++ org.knime.core/src/org/knime/core/node/defaultnodesettings/DialogComponentButtonGroup.java
@@ -18,7 +18,7 @@
  * website: www.knime.org
  * email: contact@knime.org
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *    27.04.2007 (Tobias Koetter): created
  */
@@ -46,9 +46,9 @@ import org.knime.core.node.util.ButtonGroupEnumInterface;
 
 /**
  * A standard component to display radio buttons. The given
- * {@link SettingsModelString} holds the value of the 
- * <code>getActionCommand()</code> of the selected 
- * {@link ButtonGroupEnumInterface}. 
+ * {@link SettingsModelString} holds the value of the
+ * <code>getActionCommand()</code> of the selected
+ * {@link ButtonGroupEnumInterface}.
  * @author Tobias Koetter, University of Konstanz
  */
 public class DialogComponentButtonGroup extends DialogComponent {
@@ -56,7 +56,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
     private final ButtonGroup m_buttonGroup;
 
     /**Constructor for class DialogComponentButtonGroup.
-     * @param stringModel the model that stores the action command of the 
+     * @param stringModel the model that stores the action command of the
      * selected radio button
      * @param label the optional label of the group. Set to <code>null</code>
      * for none label. Set an empty <code>String</code> for a border.
@@ -72,7 +72,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
             throw new IllegalArgumentException("Elements must not be null");
         }
         final Set<String> uniqueness = new HashSet<String>(elements.length);
-        for (ButtonGroupEnumInterface element : elements) {
+        for (final ButtonGroupEnumInterface element : elements) {
             if (!uniqueness.add(element.getActionCommand())) {
                 throw new IllegalArgumentException("Duplicate action command "
                         + "found in elements");
@@ -83,21 +83,42 @@ public class DialogComponentButtonGroup extends DialogComponent {
                 updateModel();
             }
         });
-        
+
         getModel().prependChangeListener(new ChangeListener() {
             public void stateChanged(final ChangeEvent e) {
                 updateComponent();
             }
         });
-        final Box buttonBox = 
+        final Box buttonBox =
             createButtonGroupBox(m_buttonGroup, label, vertical);
         getComponentPanel().add(buttonBox);
     }
 
-    /**Constructor for class DialogComponentButtonGroup. The 
+    /**Constructor for class DialogComponentButtonGroup. The
+     * <code>SettingsModel</code> holds the action command of the selected
+     * radio button. The default value of the <code>SettingModel</code> is
+     * selected per default.
+     * @param stringModel the model that stores the action command of the
+     * selected radio button
+     * @param label the optional label of the group. Set to <code>null</code>
+     * for none label. Set an empty <code>String</code> for a border.
+     * @param vertical set to <code>true</code> to have the box in a vertical
+     * orientation
+     * @param buttonLabels the labels of the buttons
+     * @param actionCommands the action command of the buttons in the same order
+     * like the labels
+     */
+    public DialogComponentButtonGroup(final SettingsModelString stringModel,
+            final String label, final boolean vertical,
+            final String[] buttonLabels, final String[] actionCommands) {
+        this(stringModel, label, vertical, buttonLabels, actionCommands,
+                stringModel.getStringValue());
+    }
+
+    /**Constructor for class DialogComponentButtonGroup. The
      * <code>SettingsModel</code> holds the action command of the selected
      * radio button.
-     * @param stringModel the model that stores the action command of the 
+     * @param stringModel the model that stores the action command of the
      * selected radio button
      * @param label the optional label of the group. Set to <code>null</code>
      * for none label. Set an empty <code>String</code> for a border.
@@ -107,10 +128,14 @@ public class DialogComponentButtonGroup extends DialogComponent {
      * @param actionCommands the action command of the buttons in the same order
      * like the labels
      * @param defaultAction the default action which should be selected
+     * @deprecated use
+     * {@link DialogComponentButtonGroup#DialogComponentButtonGroup(SettingsModelString, String, boolean, String[], String[])}
+     * instead
      */
+    @Deprecated
     public DialogComponentButtonGroup(final SettingsModelString stringModel,
-            final String label, final boolean vertical, 
-            final String[] buttonLabels, final String[] actionCommands, 
+            final String label, final boolean vertical,
+            final String[] buttonLabels, final String[] actionCommands,
             final String defaultAction) {
         super(stringModel);
         if (buttonLabels == null || buttonLabels.length < 1) {
@@ -124,35 +149,51 @@ public class DialogComponentButtonGroup extends DialogComponent {
             throw new IllegalArgumentException(
                     "Labels and actionCommands must be of equal size");
         }
-        final Set<String> uniqueness = 
+        final Set<String> uniqueness =
             new HashSet<String>(actionCommands.length);
-        for (String element : actionCommands) {
+        for (final String element : actionCommands) {
             if (!uniqueness.add(element)) {
                 throw new IllegalArgumentException(
                         "Duplicate action command found");
             }
         }
-        m_buttonGroup = 
-            createEnumButtonGroup(buttonLabels, actionCommands, defaultAction, 
+        m_buttonGroup =
+            createEnumButtonGroup(buttonLabels, actionCommands, defaultAction,
                     new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 updateModel();
             }
         });
-        
+
         getModel().prependChangeListener(new ChangeListener() {
             public void stateChanged(final ChangeEvent e) {
                 updateComponent();
             }
         });
-        final Box buttonBox = 
+        final Box buttonBox =
             createButtonGroupBox(m_buttonGroup, label, vertical);
         getComponentPanel().add(buttonBox);
     }
-    
-    /**Constructor for class DialogComponentButtonGroup. The given 
+    /**Constructor for class DialogComponentButtonGroup. The given
+     * <code>SettingsModel</code> holds the selected element. The default
+     * value of the <code>SettingModel</code> is selected per default.
+     * @param stringModel the model that stores the action command of the
+     * selected radio button
+     * @param label the optional label of the group. Set to <code>null</code>
+     * for none label. Set an empty <code>String</code> for a border.
+     * @param vertical set to <code>true</code> to have the box in a vertical
+     * orientation
+     * @param elements the labels/action commands of the buttons
+     */
+    public DialogComponentButtonGroup(final SettingsModelString stringModel,
+            final boolean vertical, final String label,
+            final String... elements) {
+        this(stringModel, label, vertical, stringModel.getStringValue(),
+                elements);
+    }
+    /**Constructor for class DialogComponentButtonGroup. The given
      * <code>SettingsModel</code> holds the selected element.
-     * @param stringModel the model that stores the action command of the 
+     * @param stringModel the model that stores the action command of the
      * selected radio button
      * @param label the optional label of the group. Set to <code>null</code>
      * for none label. Set an empty <code>String</code> for a border.
@@ -160,9 +201,13 @@ public class DialogComponentButtonGroup extends DialogComponent {
      * orientation
      * @param defaultElement the default element which should be selected
      * @param elements the labels/action commands of the buttons
+     * @deprecated use
+     * {@link DialogComponentButtonGroup#DialogComponentButtonGroup(SettingsModelString, String, boolean, String...)}
+     * instead
      */
+    @Deprecated
     public DialogComponentButtonGroup(final SettingsModelString stringModel,
-            final String label, final boolean vertical, 
+            final String label, final boolean vertical,
             final String defaultElement,  final String... elements) {
         super(stringModel);
         if (elements == null || elements.length < 1) {
@@ -170,32 +215,32 @@ public class DialogComponentButtonGroup extends DialogComponent {
         }
         final String[] actionCommands = new String[elements.length];
         System.arraycopy(elements, 0, actionCommands, 0, elements.length);
-        final Set<String> uniqueness = 
+        final Set<String> uniqueness =
             new HashSet<String>(actionCommands.length);
-        for (String element : actionCommands) {
+        for (final String element : actionCommands) {
             if (!uniqueness.add(element)) {
                 throw new IllegalArgumentException(
                         "Duplicate action command found");
             }
         }
-        m_buttonGroup = 
-            createEnumButtonGroup(elements, actionCommands, defaultElement, 
+        m_buttonGroup =
+            createEnumButtonGroup(elements, actionCommands, defaultElement,
                     new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 updateModel();
             }
         });
-        
+
         getModel().prependChangeListener(new ChangeListener() {
             public void stateChanged(final ChangeEvent e) {
                 updateComponent();
             }
         });
-        final Box buttonBox = 
+        final Box buttonBox =
             createButtonGroupBox(m_buttonGroup, label, vertical);
         getComponentPanel().add(buttonBox);
     }
-    
+
     /**
      * Creates a <code>ButtonGroup</code> with the given elements as buttons.
      * @param elements the elements of the button group
@@ -226,7 +271,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
         }
         return group;
     }
-    
+
     /**
      * Creates a <code>ButtonGroup</code> with the labels elements as button
      * labels and the actionCommands as the button action commands.
@@ -237,7 +282,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
      * @param l the action listener to add to each button
      * @return the button group
      */
-    private static ButtonGroup createEnumButtonGroup(final String[] labels, 
+    private static ButtonGroup createEnumButtonGroup(final String[] labels,
             final String[] actionCommands, final String defaultAction,
             final ActionListener l) {
         final ButtonGroup group = new ButtonGroup();
@@ -245,7 +290,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
         for (int i = 0, length = actionCommands.length; i < length; i++) {
             final JRadioButton button = new JRadioButton(labels[i]);
             button.setActionCommand(actionCommands[i]);
-            if (defaultAction != null 
+            if (defaultAction != null
                     && actionCommands[i].equals(defaultAction)) {
                 button.setSelected(true);
                 defaultFound = true;
@@ -264,7 +309,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
 
 
     /**
-     * Creates a <code>Box</code> with the buttons of the given 
+     * Creates a <code>Box</code> with the buttons of the given
      * <code>ButtonGroup</code>. Surrounded by a border if the label is
      * not null.
      * @param group the <code>ButtonGroup</code> to create the box with
@@ -272,7 +317,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
      * for none label
      * @param set to <code>true</code> to have the box in a vertical
      * orientation
-     * @return a <code>Box</code> with all buttons of the given 
+     * @return a <code>Box</code> with all buttons of the given
      * <code>ButtonGroup</code>
      */
     private static Box createButtonGroupBox(final ButtonGroup group,
@@ -289,7 +334,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
               buttonBox.setBorder(BorderFactory.createTitledBorder(
                       BorderFactory.createEtchedBorder(), label));
           }
-          for (final Enumeration<AbstractButton> buttons = group.getElements(); 
+          for (final Enumeration<AbstractButton> buttons = group.getElements();
               buttons.hasMoreElements();) {
               final AbstractButton button = buttons.nextElement();
               buttonBox.add(button);
@@ -352,7 +397,7 @@ public class DialogComponentButtonGroup extends DialogComponent {
             update = !val.equals(actionCommand);
         }
         if (update) {
-            final Enumeration<AbstractButton> buttons = 
+            final Enumeration<AbstractButton> buttons =
                 m_buttonGroup.getElements();
             while (buttons.hasMoreElements()) {
                 final AbstractButton button = buttons.nextElement();
