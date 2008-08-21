94a032788ba5984219cef46c5bb27ab218d16485
Nicolas Cebron <nicolas.cebron@uni-konstanz.de>
2008-08-21 12:31:40 +0000

    bugfix 1358, DataContainers in sortondisk() cache all cells

--- org.knime.base/src/org/knime/base/data/sort/SortedTable.java
+++ org.knime.base/src/org/knime/base/data/sort/SortedTable.java
@@ -59,7 +59,7 @@ public class SortedTable implements DataTable {
     /**
      * Number of cells for each container.
      */
-    private static final int CONTAINERSIZE = 1000000;
+    private static final int CONTAINERSIZE = 100000;
 
     private BufferedDataTable m_sortedTable;
 
@@ -236,7 +236,7 @@ public class SortedTable implements DataTable {
             final ExecutionContext exec) throws CanceledExecutionException {
         final ArrayList<BufferedDataContainer> containerVector =
                 new ArrayList<BufferedDataContainer>();
-        // Initialise RowIterator
+        // Initialize RowIterator
         final RowIterator rowIt = dataTable.iterator();
         final int nrRows = dataTable.getRowCount();
         int currentRowNr = 0;
@@ -246,7 +246,7 @@ public class SortedTable implements DataTable {
         // wrap all DataRows in Containers of size containerSize
         // sort each container before it is'stored'.
         BufferedDataContainer newContainer =
-            exec.createDataContainer(m_spec, true);
+            exec.createDataContainer(m_spec, true, 0);
         int nrRowsinContainer = 0;
         // TODO: can be omitted due to new buffered table with known row size
         final ArrayList<DataRow> containerrowlist = new ArrayList<DataRow>();
@@ -298,7 +298,7 @@ public class SortedTable implements DataTable {
 
         // merge all sorted containers together
         final BufferedDataContainer mergeContainer =
-                exec.createDataContainer(m_spec, true);
+                exec.createDataContainer(m_spec, true, 0);
 
         // an array of RowIterators gives access to all (sorted) containers
         final RowIterator[] currentRowIterators =
