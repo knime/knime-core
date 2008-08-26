8ace9e9644de25252408d07c3067fc1c156b0ffc
Bernd Wiswedel <bernd.wiswedel@knime.com>
2008-08-26 19:33:46 +0000

    workaround to circumvent stack overflow problem on linux/gtk https://bugs.eclipse.org/bugs/show_bug.cgi?id=171432#c15

--- org.knime.workbench.ui/src/org/knime/workbench/ui/wrapper/Panel2CompositeWrapper.java
+++ org.knime.workbench.ui/src/org/knime/workbench/ui/wrapper/Panel2CompositeWrapper.java
@@ -25,10 +25,16 @@
 package org.knime.workbench.ui.wrapper;
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
@@ -45,6 +51,8 @@ public class Panel2CompositeWrapper extends Composite {
     private Frame m_awtFrame;
 
     private JPanel m_awtPanel;
+    /** see {@link #initX11ErrorHandlerFix()} for details. */
+    private static boolean x11ErrorHandlerFixInstalled;
 
     /**
      * Creates a new wrapper.
@@ -64,6 +72,14 @@ public class Panel2CompositeWrapper extends Composite {
         setLayout(gridLayout);
 
         m_awtFrame = SWT_AWT.new_Frame(this);
+        if (!x11ErrorHandlerFixInstalled && "gtk".equals(SWT.getPlatform())) {
+            x11ErrorHandlerFixInstalled = true;
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    initX11ErrorHandlerFix();
+                }
+            });
+        }
         /* Use another panel to enable cursor switching (for instance
          * in an embedded JSplitPane where the mouse cursor changes when you
          * adjust the pane sizes. This is the workaround for bug #594 as
@@ -81,7 +97,7 @@ public class Panel2CompositeWrapper extends Composite {
         m_awtFrame.setVisible(true);
 
     }
-
+    
     /**
      * {@inheritDoc}
      */
@@ -102,5 +118,80 @@ public class Panel2CompositeWrapper extends Composite {
      */
     public JPanel getAwtPanel() {
         return m_awtPanel;
+    }
+    
+    /**
+     * Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=171432
+     * (comment #15)
+     */
+    private static void initX11ErrorHandlerFix() {
+        assert EventQueue.isDispatchThread();
+
+        try {
+            // get XlibWrapper.SetToolkitErrorHandler() and XSetErrorHandler()
+            // methods
+            Class xlibwrapperClass = Class.forName("sun.awt.X11.XlibWrapper");
+            final Method setToolkitErrorHandlerMethod =
+                    xlibwrapperClass.getDeclaredMethod(
+                            "SetToolkitErrorHandler", null);
+            final Method setErrorHandlerMethod =
+                    xlibwrapperClass.getDeclaredMethod("XSetErrorHandler",
+                            new Class[]{Long.TYPE});
+            setToolkitErrorHandlerMethod.setAccessible(true);
+            setErrorHandlerMethod.setAccessible(true);
+
+            // get XToolkit.saved_error_handler field
+            Class xtoolkitClass = Class.forName("sun.awt.X11.XToolkit");
+            final Field savedErrorHandlerField =
+                    xtoolkitClass.getDeclaredField("saved_error_handler");
+            savedErrorHandlerField.setAccessible(true);
+
+            // determine the current error handler and the value of
+            // XLibWrapper.ToolkitErrorHandler
+            // (XlibWrapper.SetToolkitErrorHandler() sets the X11 error handler
+            // to
+            // XLibWrapper.ToolkitErrorHandler and returns the old error
+            // handler)
+            final Object defaultErrorHandler =
+                    setToolkitErrorHandlerMethod.invoke(null, null);
+            final Object toolkitErrorHandler =
+                    setToolkitErrorHandlerMethod.invoke(null, null);
+            setErrorHandlerMethod.invoke(null,
+                    new Object[]{defaultErrorHandler});
+
+            // create timer that watches XToolkit.saved_error_handler whether
+            // its value is equal
+            // to XLibWrapper.ToolkitErrorHandler, which indicates the start of
+            // the trouble
+            Timer timer = new Timer(200, new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        Object savedErrorHandler =
+                                savedErrorHandlerField.get(null);
+                        if (toolkitErrorHandler.equals(savedErrorHandler)) {
+                            // Last saved error handler in
+                            // XToolkit.WITH_XERROR_HANDLER
+                            // is XLibWrapper.ToolkitErrorHandler, which will
+                            // cause
+                            // the StackOverflowError when the next X11 error
+                            // occurs.
+                            // Workaround: restore the default error handler.
+                            // Also update XToolkit.saved_error_handler so that
+                            // this is done only once.
+                            setErrorHandlerMethod.invoke(null,
+                                    new Object[]{defaultErrorHandler});
+                            savedErrorHandlerField.setLong(null,
+                                    ((Long)defaultErrorHandler).longValue());
+                        }
+                    } catch (Exception ex) {
+                        // ignore
+                    }
+
+                }
+            });
+            timer.start();
+        } catch (Exception ex) {
+            // ignore
+        }
     }
 }
