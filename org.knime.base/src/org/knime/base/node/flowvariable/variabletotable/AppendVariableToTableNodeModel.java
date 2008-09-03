a78f0db7e2fe697ca30e64377096dae06b87cc92
Bernd Wiswedel <bernd.wiswedel@knime.com>
2008-09-03 11:02:11 +0000

    append to variable node now "appends"

--- org.knime.base/src/org/knime/base/node/flowvariable/variabletotable/AppendVariableToTableNodeModel.java
+++ org.knime.base/src/org/knime/base/node/flowvariable/variabletotable/AppendVariableToTableNodeModel.java
@@ -26,20 +26,22 @@ package org.knime.base.node.flowvariable.variabletotable;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DataType;
-import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.container.AbstractCellFactory;
+import org.knime.core.data.container.ColumnRearranger;
 import org.knime.core.data.def.DoubleCell;
 import org.knime.core.data.def.IntCell;
 import org.knime.core.data.def.StringCell;
-import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -64,7 +66,7 @@ public class AppendVariableToTableNodeModel extends GenericNodeModel {
     
     /** One input, one output. */
     public AppendVariableToTableNodeModel() {
-        super(new PortType[]{new PortType(PortObject.class)},
+        super(new PortType[]{BufferedDataTable.TYPE},
               new PortType[]{BufferedDataTable.TYPE});
         m_settings = new AppendVariableToTableSettings();
     }
@@ -73,85 +75,94 @@ public class AppendVariableToTableNodeModel extends GenericNodeModel {
     @Override
     protected PortObject[] execute(final PortObject[] inData,
             final ExecutionContext exec) throws Exception {
-        DataTableSpec spec = createOutSpec();
-        BufferedDataContainer cont = exec.createDataContainer(spec);
-        List<Pair<String, ScopeVariable.Type>> vars = 
-            m_settings.getVariablesOfInterest();
-        DataCell[] specs = new DataCell[vars.size()];
-        List<String> lostVariables = new ArrayList<String>();
-        for (int i = 0; i < vars.size(); i++) {
-            Pair<String, ScopeVariable.Type> c = vars.get(i);
-            String name = c.getFirst();
-            DataCell cell = DataType.getMissingCell(); // fallback
-            switch (c.getSecond()) {
-            case DOUBLE:
-                try {
-                    double dValue = peekScopeVariableDouble(c.getFirst());
-                    cell = new DoubleCell(dValue);
-                } catch (NoSuchElementException e) {
-                    lostVariables.add(name + " (Double)");
-                }
-                break;
-            case INTEGER:
-                try {
-                    int iValue = peekScopeVariableInt(c.getFirst());
-                    cell = new IntCell(iValue);
-                } catch (NoSuchElementException e) {
-                    lostVariables.add(name + " (Integer)");
-                }
-                break;
-            case STRING:
-                try {
-                    String sValue = peekScopeVariableString(c.getFirst());
-                    sValue = sValue == null ? "" : sValue;
-                    cell = new StringCell(sValue);
-                } catch (NoSuchElementException e) {
-                    lostVariables.add(name + " (String)");
-                }
-                break;
-            }
-            specs[i] = cell;
-        }
-        cont.addRowToTable(new DefaultRow("values", specs));
-        cont.close();
-        return new BufferedDataTable[]{cont.getTable()};
+        BufferedDataTable t = (BufferedDataTable)inData[0];
+        DataTableSpec ts = t.getSpec();
+        ColumnRearranger ar = createColumnRearranger(ts);
+        BufferedDataTable out = exec.createColumnRearrangeTable(t, ar, exec);
+        return new BufferedDataTable[]{out};
     }
 
     /** {@inheritDoc} */
     @Override
     protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs)
             throws InvalidSettingsException {
-        return new DataTableSpec[]{createOutSpec()};
+        ColumnRearranger ar = createColumnRearranger((DataTableSpec)inSpecs[0]);
+        return new DataTableSpec[]{ar.createSpec()};
     }
     
-    private DataTableSpec createOutSpec() throws InvalidSettingsException {
+    private ColumnRearranger createColumnRearranger(final DataTableSpec spec) 
+        throws InvalidSettingsException {
+        ColumnRearranger arranger = new ColumnRearranger(spec);
+        Set<String> nameHash = new HashSet<String>();
+        for (DataColumnSpec c : spec) {
+            nameHash.add(c.getName());
+        }
         List<Pair<String, ScopeVariable.Type>> vars = 
             m_settings.getVariablesOfInterest();
         if (vars.isEmpty()) {
             throw new InvalidSettingsException("No variables selected");
         }
         DataColumnSpec[] specs = new DataColumnSpec[vars.size()];
+        final DataCell[] values = new DataCell[vars.size()];
         for (int i = 0; i < vars.size(); i++) {
             Pair<String, ScopeVariable.Type> c = vars.get(i);
+            String name = c.getFirst();
             DataType type;
             switch (c.getSecond()) {
             case DOUBLE:
                 type = DoubleCell.TYPE;
+                try {
+                    double dValue = peekScopeVariableDouble(name);
+                    values[i] = new DoubleCell(dValue);
+                } catch (NoSuchElementException e) {
+                    throw new InvalidSettingsException(
+                            "No such flow variable (of type double): " + name);
+                }
                 break;
             case INTEGER:
                 type = IntCell.TYPE;
+                try {
+                    int iValue = peekScopeVariableInt(name);
+                    values[i] = new IntCell(iValue);
+                } catch (NoSuchElementException e) {
+                    throw new InvalidSettingsException(
+                            "No such flow variable (of type int): " + name);
+                }
                 break;
             case STRING:
                 type = StringCell.TYPE;
+                try {
+                    String sValue = peekScopeVariableString(name);
+                    sValue = sValue == null ? "" : sValue;
+                    values[i] = new StringCell(sValue);
+                } catch (NoSuchElementException e) {
+                    throw new InvalidSettingsException(
+                            "No such flow variable (of type String): " + name);
+                }
                 break;
             default:
                 throw new InvalidSettingsException("Unsupported variable type: "
                         + c.getSecond());
             }
-            specs[i] = 
-                new DataColumnSpecCreator(c.getFirst(), type).createSpec();
+            if (nameHash.contains(name) 
+                    && !name.toLowerCase().endsWith("(variable)")) {
+                name = name.concat(" (variable)");
+            }
+            String newName = name;
+            int uniquifier = 1; 
+            while (!nameHash.add(newName)) {
+                newName = name + " (#" + (uniquifier++) + ")"; 
+            }
+            specs[i] = new DataColumnSpecCreator(newName, type).createSpec();
         }
-        return new DataTableSpec(specs);
+        arranger.append(new AbstractCellFactory(specs) {
+            /** {@inheritDoc} */
+            @Override
+            public DataCell[] getCells(final DataRow row) {
+                return values;
+            }
+        });
+        return arranger;
     }
 
     /** {@inheritDoc} */
