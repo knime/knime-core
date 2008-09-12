facc647d50c9821a3c57dfffff755befb1edd991
Thomas Gabriel <thomas.gabriel@knime.com>
2008-09-12 09:26:25 +0000

    removed deprecated constructor with four args for data and model in- and out-ports

--- org.knime.core/src/org/knime/core/node/interrupt/InterruptibleNodeModel.java
+++ org.knime.core/src/org/knime/core/node/interrupt/InterruptibleNodeModel.java
@@ -103,26 +103,6 @@ public abstract class InterruptibleNodeModel extends NodeModel {
     }
 
     /**
-     * Constructs a NodeModel with the desired in- and outports, and model in-
-     * and outports. State is paused, waiting for an intial event to start
-     * executing. Be sure to override load/save ModelContent in your derived
-     * NodeModel.
-     * 
-     * @param nrInPorts - the desired number of inports.
-     * @param nrOutPorts - the desired number of outports.
-     * @param nrPredParamsIns The number of <code>ModelContent</code> elements
-     *            available as inputs.
-     * @param nrPredParamsOuts The number of <code>ModelContent</code> objects
-     *            available at the output.
-     */
-    public InterruptibleNodeModel(final int nrInPorts, final int nrOutPorts,
-            final int nrPredParamsIns, final int nrPredParamsOuts) {
-        super(nrInPorts, nrOutPorts, nrPredParamsIns, nrPredParamsOuts);
-        m_pause = true;
-        m_finished = false;
-    }
-
-    /**
      * Returns the number of processed iterations so far.
      * 
      * @return - the number of processed iterations so far.
