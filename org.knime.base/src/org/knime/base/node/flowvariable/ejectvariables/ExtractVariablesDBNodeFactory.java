5284e9a6545ed7e7d6bac1c918beb3f111e3d716
Michael Berthold <michael.berthold@knime.com>
2008-09-23 12:08:42 +0000

    created.

---
+++ org.knime.base/src/org/knime/base/node/flowvariable/ejectvariables/ExtractVariablesDBNodeFactory.java
@@ -0,0 +1,41 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2008
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   Apr 28, 2008 (wiswedel): created
+ */
+package org.knime.base.node.flowvariable.ejectvariables;
+
+import org.knime.core.node.port.database.DatabasePortObject;
+
+
+/**
+ * 
+ * @author wiswedel, University of Konstanz
+ */
+public class ExtractVariablesDBNodeFactory extends ExtractVariablesNodeFactory {
+    
+    /** Creates factory for {@link DatabasePortObject} type ports. */
+    public ExtractVariablesDBNodeFactory() {
+        super(DatabasePortObject.TYPE);
+    }
+    
+}
---
+++ org.knime.base/src/org/knime/base/node/flowvariable/ejectvariables/ExtractVariablesNodeModel.java
@@ -0,0 +1,112 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2008
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   Apr 28, 2008 (wiswedel): created
+ */
+package org.knime.base.node.flowvariable.ejectvariables;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObject;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObjectSpec;
+
+/**
+ * 
+ * @author Bernd Wiswedel, University of Konstanz
+ */
+public class ExtractVariablesNodeModel extends NodeModel {
+
+    /** One input, one output.
+     * @param inOutType Type of first inport. */
+    protected ExtractVariablesNodeModel(final PortType inType) {
+        super(new PortType[]{inType},
+                new PortType[]{FlowVariablePortObject.TYPE});
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs)
+            throws InvalidSettingsException {
+        // nothing to do: the extraction of the variable stack
+        // is done by the WFM itself
+        // simply output a dummy "Variable"-Port spec.
+        return new PortObjectSpec[]{FlowVariablePortObjectSpec.INSTANCE};
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected PortObject[] execute(final PortObject[] inData,
+            final ExecutionContext exec) throws Exception {
+        // nothing to do: the extraction of the variable stack
+        // is done by the WFM itself
+        // simply output a dummy "Variable"-Port object.
+        return new PortObject[]{new FlowVariablePortObject()};
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void reset() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadInternals(
+            final File nodeInternDir, final ExecutionMonitor exec)
+            throws IOException, CanceledExecutionException {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveInternals(
+            final File nodeInternDir, final ExecutionMonitor exec)
+            throws IOException, CanceledExecutionException {
+    }
+
+}
