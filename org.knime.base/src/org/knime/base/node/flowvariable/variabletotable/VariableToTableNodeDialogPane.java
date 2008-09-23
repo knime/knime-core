6923e0fc1dfe8cef257cf181cd2f845785285230
Bernd Wiswedel <bernd.wiswedel@knime.com>
2008-09-23 20:32:34 +0000

    moved variable to table node to plugin

---
+++ org.knime.base/src/org/knime/base/node/flowvariable/variabletotable/VariableToTableNodeDialogPane.java
@@ -0,0 +1,97 @@
+/*
+ * ------------------------------------------------------------------ *
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2008
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   May 1, 2008 (wiswedel): created
+ */
+package org.knime.base.node.flowvariable.variabletotable;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import org.knime.base.util.scopevariable.ScopeVariableListCellRenderer;
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.workflow.ScopeVariable;
+import org.knime.core.util.Pair;
+
+/**
+ * 
+ * @author wiswedel, University of Konstanz
+ */
+class VariableToTableNodeDialogPane extends NodeDialogPane {
+
+    private final JList m_list;
+
+    /** Inits components. */
+    public VariableToTableNodeDialogPane() {
+        m_list = new JList(new DefaultListModel());
+        m_list.setCellRenderer(new ScopeVariableListCellRenderer());
+        m_list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        addTab("Variable Selection", new JScrollPane(m_list));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadSettingsFrom(final NodeSettingsRO settings,
+            final PortObjectSpec[] specs) throws NotConfigurableException {
+        Map<String, ScopeVariable> scopeVars = getAvailableScopeVariables();
+        VariableToTableSettings sets = new VariableToTableSettings();
+        sets.loadSettingsFrom(settings, scopeVars);
+        DefaultListModel model = (DefaultListModel)m_list.getModel();
+        model.removeAllElements();
+        int[] selIndices = new int[sets.getVariablesOfInterest().size()];
+        int current = 0;
+        int pointer = 0;
+        for (ScopeVariable v : scopeVars.values()) {
+            model.addElement(v);
+            if (sets.getVariablesOfInterest().contains(new Pair<
+                    String, ScopeVariable.Type>(v.getName(), v.getType()))) {
+                selIndices[pointer++] = current;
+            }
+            current += 1;
+        }
+        selIndices = Arrays.copyOf(selIndices, pointer);
+        m_list.setSelectedIndices(selIndices);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings)
+            throws InvalidSettingsException {
+        Object[] sels = m_list.getSelectedValues();
+        ScopeVariable[] svSels = new ScopeVariable[sels.length];
+        System.arraycopy(sels, 0, svSels, 0, sels.length);
+        VariableToTableSettings sets = new VariableToTableSettings();
+        sets.setVariablesOfInterest(svSels);
+        sets.saveSettingsTo(settings);
+    }
+
+}
---
+++ org.knime.base/src/org/knime/base/node/flowvariable/variabletotable/VariableToTableNodeModel.java
@@ -0,0 +1,197 @@
+/*
+ * ------------------------------------------------------------------ *
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2008
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   May 1, 2008 (wiswedel): created
+ */
+package org.knime.base.node.flowvariable.variabletotable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObject;
+import org.knime.core.node.workflow.ScopeVariable;
+import org.knime.core.util.Pair;
+
+/**
+ * 
+ * @author Bernd Wiswedel, University of Konstanz
+ */
+public class VariableToTableNodeModel extends NodeModel {
+
+    private final VariableToTableSettings m_settings;
+    
+    /** One input, one output. */
+    public VariableToTableNodeModel() {
+        super(new PortType[]{FlowVariablePortObject.TYPE},
+              new PortType[]{BufferedDataTable.TYPE});
+        m_settings = new VariableToTableSettings();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected PortObject[] execute(final PortObject[] inData,
+            final ExecutionContext exec) throws Exception {
+        DataTableSpec spec = createOutSpec();
+        BufferedDataContainer cont = exec.createDataContainer(spec);
+        List<Pair<String, ScopeVariable.Type>> vars = 
+            m_settings.getVariablesOfInterest();
+        DataCell[] specs = new DataCell[vars.size()];
+        List<String> lostVariables = new ArrayList<String>();
+        for (int i = 0; i < vars.size(); i++) {
+            Pair<String, ScopeVariable.Type> c = vars.get(i);
+            String name = c.getFirst();
+            DataCell cell = DataType.getMissingCell(); // fallback
+            switch (c.getSecond()) {
+            case DOUBLE:
+                try {
+                    double dValue = peekScopeVariableDouble(c.getFirst());
+                    cell = new DoubleCell(dValue);
+                } catch (NoSuchElementException e) {
+                    lostVariables.add(name + " (Double)");
+                }
+                break;
+            case INTEGER:
+                try {
+                    int iValue = peekScopeVariableInt(c.getFirst());
+                    cell = new IntCell(iValue);
+                } catch (NoSuchElementException e) {
+                    lostVariables.add(name + " (Integer)");
+                }
+                break;
+            case STRING:
+                try {
+                    String sValue = peekScopeVariableString(c.getFirst());
+                    sValue = sValue == null ? "" : sValue;
+                    cell = new StringCell(sValue);
+                } catch (NoSuchElementException e) {
+                    lostVariables.add(name + " (String)");
+                }
+                break;
+            }
+            specs[i] = cell;
+        }
+        cont.addRowToTable(new DefaultRow("values", specs));
+        cont.close();
+        return new BufferedDataTable[]{cont.getTable()};
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs)
+            throws InvalidSettingsException {
+        return new DataTableSpec[]{createOutSpec()};
+    }
+    
+    private DataTableSpec createOutSpec() throws InvalidSettingsException {
+        List<Pair<String, ScopeVariable.Type>> vars = 
+            m_settings.getVariablesOfInterest();
+        if (vars.isEmpty()) {
+            throw new InvalidSettingsException("No variables selected");
+        }
+        DataColumnSpec[] specs = new DataColumnSpec[vars.size()];
+        for (int i = 0; i < vars.size(); i++) {
+            Pair<String, ScopeVariable.Type> c = vars.get(i);
+            DataType type;
+            switch (c.getSecond()) {
+            case DOUBLE:
+                type = DoubleCell.TYPE;
+                break;
+            case INTEGER:
+                type = IntCell.TYPE;
+                break;
+            case STRING:
+                type = StringCell.TYPE;
+                break;
+            default:
+                throw new InvalidSettingsException("Unsupported variable type: "
+                        + c.getSecond());
+            }
+            specs[i] = 
+                new DataColumnSpecCreator(c.getFirst(), type).createSpec();
+        }
+        return new DataTableSpec(specs);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void reset() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_settings.loadSettingsFrom(settings);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_settings.saveSettingsTo(settings);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        new VariableToTableSettings().loadSettingsFrom(settings);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadInternals(final File nodeInternDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveInternals(final File nodeInternDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+    }
+
+}
