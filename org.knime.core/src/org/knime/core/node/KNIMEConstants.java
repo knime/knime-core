28e43cb8632653d92df3e9e57da029c3aca0cc96
Thorsten Meinl <thorsten.meinl@knime.com>
2008-09-26 18:07:16 +0000

    Updated versions and copyright years

--- org.knime.core/src/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/org/knime/core/node/KNIMEConstants.java
@@ -46,7 +46,7 @@ public final class KNIMEConstants {
     /** KNIME's revision number. */
     public static final int REV = 0;
     /** KNIME's build id. */
-    public static final String BUILD = ".0016392";
+    public static final String BUILD = ".0017855";
     
     // IMPORTANT: Remember to also update the NodeLogger welcome screen with
     // the current version and the prerequisites
