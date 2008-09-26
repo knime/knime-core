12b6314f4970256c70184997c830f52007452d89
Fabian Dill <fabian.dill@knime.com>
2008-09-26 13:11:34 +0000

    fixed NullPointerException in configure Now checking for the column name of data spec in rule spec...

--- org.knime.deprecated/src/org/knime/base/node/viz/rulevis2d/Rule2DNodeModel.java
+++ org.knime.deprecated/src/org/knime/base/node/viz/rulevis2d/Rule2DNodeModel.java
@@ -120,11 +120,14 @@ public class Rule2DNodeModel extends NodeModel implements Rule2DDataProvider {
         DataTableSpec ruleSpec = inSpecs[RULES_INPORT];
         List<String>validColummNames = new ArrayList<String>();
         for (int i = 0; i < dataSpec.getNumColumns(); i++) {
-            if (dataSpec.getColumnSpec(i).getName().equals(
-                    ruleSpec.getColumnSpec(i).getName())
-                    && ruleSpec.getColumnSpec(i).getType().isCompatible(
-                            FuzzyIntervalValue.class)) {
-                validColummNames.add(dataSpec.getColumnSpec(i).getName());
+            String colName = dataSpec.getColumnSpec(i).getName();
+            if (ruleSpec.getColumnSpec(colName) != null) {
+                if (dataSpec.getColumnSpec(i).getName().equals(
+                        ruleSpec.getColumnSpec(colName).getName())
+                        && ruleSpec.getColumnSpec(colName).getType()
+                            .isCompatible(FuzzyIntervalValue.class)) {
+                    validColummNames.add(dataSpec.getColumnSpec(i).getName());
+                }
             }
         }
         if (validColummNames.size() == 0) {
