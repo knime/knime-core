0b5b8ce3398f1aa9a5279c474ad96c929062a59e
Bernd Wiswedel <bernd.wiswedel@knime.com>
2008-11-03 13:27:57 +0000

    test based on actual workflow

--- org.knime.testing/unittests/org/knime/core/workflow/ChainOfNodesTest.java
+++ org.knime.testing/unittests/org/knime/core/workflow/ChainOfNodesTest.java
@@ -23,12 +23,14 @@
  */
 package org.knime.core.workflow;
 
-import org.knime.base.node.preproc.filter.column.FilterColumnNodeFactory;
-import org.knime.base.node.util.sampledata.SampleDataNodeFactory;
-import org.knime.base.node.viz.table.TableNodeFactory;
+import java.io.File;
+import java.net.URL;
+
+import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.workflow.NodeID;
 import org.knime.core.node.workflow.WorkflowManager;
 import org.knime.core.node.workflow.NodeContainer.State;
+import org.knime.core.node.workflow.WorkflowPersistor.WorkflowLoadResult;
 
 /**
  * 
@@ -38,41 +40,62 @@ public class ChainOfNodesTest extends WorkflowTestCase {
     
     private NodeID m_dataGen;
     private NodeID m_colFilter;
+    private NodeID m_rowFilter;
     private NodeID m_tblView;
     
     /** {@inheritDoc} */
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        WorkflowManager m = WorkflowManager.ROOT.createAndAddProject();
-        m_dataGen = m.createAndAddNode(new SampleDataNodeFactory());
-        m_colFilter = m.createAndAddNode(new FilterColumnNodeFactory());
-        m_tblView = m.createAndAddNode(new TableNodeFactory());
+        ClassLoader l = getClass().getClassLoader();
+        String workflowDirString = getClass().getPackage().getName();
+        workflowDirString += ".simplechainofnodes";
+        workflowDirString = workflowDirString.replace('.', '/');
+        URL workflowURL = l.getResource(workflowDirString);
+        File workflowDir = new File(workflowURL.getFile());
+        if (!workflowDir.isDirectory()) {
+            throw new Exception("Can't load workflow directory: " 
+                    + workflowDirString);
+        }
+        WorkflowLoadResult loadResult = WorkflowManager.ROOT.load(
+                workflowDir, new ExecutionMonitor());
+        WorkflowManager m = loadResult.getWorkflowManager();
+        NodeID id = m.getID();
+        m_dataGen = new NodeID(id, 1);
+        m_colFilter = new NodeID(id, 2);
+        m_rowFilter = new NodeID(id, 3);
+        m_tblView = new NodeID(id, 4);
         setManager(m);
     }
     
     public void testExecuteOneByOne() throws Exception {
         checkState(m_dataGen, State.CONFIGURED);
-        checkState(m_colFilter, State.IDLE);
-        checkState(m_tblView, State.IDLE);
-        getManager().addConnection(m_dataGen, 0, m_colFilter, 0);
-        getManager().addConnection(m_colFilter, 0, m_tblView, 0);
         checkState(m_colFilter, State.CONFIGURED);
+        checkState(m_rowFilter, State.CONFIGURED);
         checkState(m_tblView, State.CONFIGURED);
         
         executeAndWait(m_dataGen);
         checkState(m_dataGen, State.EXECUTED);
         checkState(m_colFilter, State.CONFIGURED);
+        checkState(m_rowFilter, State.CONFIGURED);
         checkState(m_tblView, State.CONFIGURED);
         
         executeAndWait(m_colFilter);
         checkState(m_dataGen, State.EXECUTED);
         checkState(m_colFilter, State.EXECUTED);
+        checkState(m_rowFilter, State.CONFIGURED);
         checkState(m_tblView, State.CONFIGURED);
 
+        executeAndWait(m_rowFilter);
+        checkState(m_dataGen, State.EXECUTED);
+        checkState(m_colFilter, State.EXECUTED);
+        checkState(m_rowFilter, State.EXECUTED);
+        checkState(m_tblView, State.CONFIGURED);
+        
         executeAndWait(m_tblView);
         checkState(m_dataGen, State.EXECUTED);
         checkState(m_colFilter, State.EXECUTED);
+        checkState(m_rowFilter, State.EXECUTED);
         checkState(m_tblView, State.EXECUTED);
     }
 
