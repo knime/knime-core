583a2bbc892571ababf76ff7d33e0ad6ad1457c2
Peter Ohl <peter.ohl@knime.com>
2009-04-28 11:05:40 +0000

    actually calling saveJobManagerInternals now...

--- org.knime.core/src/org/knime/core/node/workflow/NodeContainerMetaPersistorVersion200.java
+++ org.knime.core/src/org/knime/core/node/workflow/NodeContainerMetaPersistorVersion200.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * ------------------------------------------------------------------
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
@@ -18,7 +18,7 @@
  * website: www.knime.org
  * email: contact@knime.org
  * --------------------------------------------------------------------- *
- * 
+ *
  * History
  *   Sep 25, 2007 (wiswedel): created
  */
@@ -37,24 +37,24 @@ import org.knime.core.node.workflow.NodeMessage.Type;
 import org.knime.core.util.FileUtil;
 
 /**
- * 
+ *
  * @author wiswedel, University of Konstanz
  */
 class NodeContainerMetaPersistorVersion200 extends
         NodeContainerMetaPersistorVersion1xx {
-    
+
     private static final String CFG_STATE = "state";
     private static final String CFG_IS_DELETABLE = "isDeletable";
     private static final String CFG_JOB_MANAGER_CONFIG = "job.manager";
     private static final String CFG_JOB_MANAGER_DIR = "job.manager.dir";
     private static final String CFG_JOB_CONFIG = "execution.job";
-    
+
     /** @param baseDir The node container directory (only important while load)
      */
     NodeContainerMetaPersistorVersion200(final ReferencedFile baseDir) {
         super(baseDir);
     }
-    
+
     /** {@inheritDoc} */
     @Override
     protected NodeExecutionJobManager loadNodeExecutionJobManager(
@@ -75,11 +75,11 @@ class NodeContainerMetaPersistorVersion200 extends
         }
         return settings.getNodeSettings(CFG_JOB_CONFIG);
     }
-    
+
     /** {@inheritDoc} */
     @Override
     protected ReferencedFile loadJobManagerInternalsDirectory(
-            final ReferencedFile parentDir, final NodeSettingsRO settings) 
+            final ReferencedFile parentDir, final NodeSettingsRO settings)
     throws InvalidSettingsException {
         if (!settings.containsKey(CFG_JOB_MANAGER_DIR)) {
             return null;
@@ -91,11 +91,11 @@ class NodeContainerMetaPersistorVersion200 extends
         }
         return new ReferencedFile(parentDir, dir);
     }
-    
+
     /** {@inheritDoc} */
     @Override
-    protected State loadState(final NodeSettingsRO settings, 
-            final NodeSettingsRO parentSettings) 
+    protected State loadState(final NodeSettingsRO settings,
+            final NodeSettingsRO parentSettings)
     throws InvalidSettingsException {
         String stateString = settings.getString(CFG_STATE);
         if (stateString == null) {
@@ -108,7 +108,7 @@ class NodeContainerMetaPersistorVersion200 extends
                     + stateString + "\"");
         }
     }
-    
+
     /** {@inheritDoc} */
     @Override
     protected boolean loadIsDeletable(final NodeSettingsRO settings) {
@@ -118,7 +118,7 @@ class NodeContainerMetaPersistorVersion200 extends
     /** {@inheritDoc} */
     @Override
     protected String loadCustomName(final NodeSettingsRO settings,
-            final NodeSettingsRO parentSettings) 
+            final NodeSettingsRO parentSettings)
     throws InvalidSettingsException {
         if (!settings.containsKey(KEY_CUSTOM_NAME)) {
             return null;
@@ -129,7 +129,7 @@ class NodeContainerMetaPersistorVersion200 extends
     /** {@inheritDoc} */
     @Override
     protected String loadCustomDescription(final NodeSettingsRO settings,
-            final NodeSettingsRO parentSettings) 
+            final NodeSettingsRO parentSettings)
     throws InvalidSettingsException {
         if (!settings.containsKey(KEY_CUSTOM_DESCRIPTION)) {
             return null;
@@ -160,7 +160,7 @@ class NodeContainerMetaPersistorVersion200 extends
         }
         return null;
     }
-    
+
     public void save(final NodeContainer nc, final NodeSettingsWO settings)
         throws IOException {
         saveCustomName(settings, nc);
@@ -170,6 +170,7 @@ class NodeContainerMetaPersistorVersion200 extends
         if (mustAlsoSaveExecutorSettings) {
             saveNodeExecutionJob(settings, nc);
         }
+        saveJobManagerInternalsDirectory(settings, nc);
         saveNodeMessage(settings, nc);
         saveIsDeletable(settings, nc);
     }
@@ -182,7 +183,7 @@ class NodeContainerMetaPersistorVersion200 extends
             NodeExecutionJobManagerPool.saveJobManager(jobManager, s);
         }
     }
-    
+
     protected void saveNodeExecutionJob(
             final NodeSettingsWO settings, final NodeContainer nc) {
         assert nc.getState().equals(State.EXECUTINGREMOTELY)
@@ -195,7 +196,7 @@ class NodeContainerMetaPersistorVersion200 extends
         NodeSettingsWO sub = settings.addNodeSettings(CFG_JOB_CONFIG);
         jobManager.saveReconnectSettings(job, sub);
     }
-    
+
     protected void saveJobManagerInternalsDirectory(
             final NodeSettingsWO settings, final NodeContainer nc) {
         NodeExecutionJobManager jobManager = nc.getJobManager();
@@ -204,7 +205,7 @@ class NodeContainerMetaPersistorVersion200 extends
             ReferencedFile parentRefFile = getNodeContainerDirectory();
             if (parentRefFile == null) {
                 // added this later, make it bullet proof
-                // this if-statement can be deleted if there are no reports 
+                // this if-statement can be deleted if there are no reports
                 // until, let's say, end of 2009
                 getLogger().coding("Node directory must not be null "
                         + "at this time");
@@ -228,10 +229,10 @@ class NodeContainerMetaPersistorVersion200 extends
             } catch (Throwable e) {
                 if (!(e instanceof IOException)) {
                     getLogger().coding("Saving internals of job manager should "
-                            + "only throw IOException, caught " 
+                            + "only throw IOException, caught "
                             + e.getClass().getSimpleName());
                 }
-                String error = "Saving job manager internals failed: " 
+                String error = "Saving job manager internals failed: "
                     + e.getMessage();
                 getLogger().error(error, e);
             }
@@ -263,7 +264,7 @@ class NodeContainerMetaPersistorVersion200 extends
         settings.addString(CFG_STATE, state);
         return mustAlsoSaveExecutorSettings;
     }
-    
+
     protected void saveCustomName(final NodeSettingsWO settings,
             final NodeContainer nc) {
         settings.addString(KEY_CUSTOM_NAME, nc.getCustomName());
@@ -273,8 +274,8 @@ class NodeContainerMetaPersistorVersion200 extends
             final NodeContainer nc) {
         settings.addString(KEY_CUSTOM_DESCRIPTION, nc.getCustomDescription());
     }
-    
-    protected void saveIsDeletable(final NodeSettingsWO settings, 
+
+    protected void saveIsDeletable(final NodeSettingsWO settings,
             final NodeContainer nc) {
         if (!nc.isDeletable()) {
             settings.addBoolean(CFG_IS_DELETABLE, false);
