daaec59f49080cec99f5003b1d8a1f26c461db34
Fabian Dill <fabian.dill@knime.com>
2009-07-28 09:10:22 +0000

    time level names such as week, quarter, month, etc.

---
+++ org.knime.timeseries/src/org/knime/timeseries/util/TimeLevelNames.java
@@ -0,0 +1,49 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2008 - 2009
+ * KNIME.com, Zurich, Switzerland
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   28.07.2009 (Fabian Dill): created
+ */
+package org.knime.timeseries.util;
+
+/**
+ * 
+ * @author Fabian Dill, KNIME.com, Zurich, Switzerland
+ */
+public final class TimeLevelNames {
+    
+    private TimeLevelNames() {
+        // utility class
+    }
+    
+    /** Key for aggregation level year. */ 
+    public static final String YEAR = "Year";
+    /** Key for aggregation level quarter. */
+    public static final String QUARTER = "Quarter";
+    /** Key for aggregation level month. */
+    public static final String MONTH = "Month";
+    /** Key for aggregation level week. */
+    public static final String WEEK = "Week";
+    /** Key for aggregation level day. */
+    public static final String DAY = "Day";
+    /** Key for aggregation level hour. */
+    public static final String HOUR = "Hour";
+    /** Key for aggregation level minute. */
+    public static final String MINUTE = "Minute";
+
+}
