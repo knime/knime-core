b24d32c249b8fee3826d3fc58a55bd249bac8f18
Iris Adä <iris.adae@knime.com>
2009-09-29 14:11:43 +0000

    Extended to use the ScopeVariable.

--- org.knime.core/src/org/knime/core/node/util/FilesHistoryPanel.java
+++ org.knime.core/src/org/knime/core/node/util/FilesHistoryPanel.java
@@ -24,10 +24,13 @@
  */
 package org.knime.core.node.util;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
@@ -39,18 +42,26 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.plaf.basic.BasicComboBoxRenderer;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
 
+import org.knime.core.node.FlowVariableModel;
+import org.knime.core.node.FlowVariableModelButton;
 import org.knime.core.util.SimpleFileFilter;
 
 /**
@@ -71,10 +82,12 @@ public final class FilesHistoryPanel extends JPanel {
 
     private final String[] m_suffixes;
 
-    private final String m_historyID;
+    private final String m_historyID;  
+
+
+    private final JLabel m_warnMsg;
 
     private int m_selectMode = JFileChooser.FILES_ONLY;
-
     /**
      * Creates new instance, sets properties, for instance renderer,
      * accordingly.
@@ -84,6 +97,24 @@ public final class FilesHistoryPanel extends JPanel {
      * @param suffixes the set of suffixes for the file chooser
      */
     public FilesHistoryPanel(final String historyID, final String... suffixes) {
+            this(null, historyID, false, suffixes);
+    }
+
+    /**
+     * Creates new instance, sets properties, for instance renderer,
+     * accordingly.
+     *
+     * @param historyID identifier for the string history, see
+     *            {@link StringHistory}
+     * @param suffixes the set of suffixes for the file chooser
+     * @param fvm model to allow to use a variable instead of the text field.
+     * @param showErrorMessage if true there are error messages if the file 
+     * exists or the path is not available and so on.
+     */
+    public FilesHistoryPanel(final FlowVariableModel fvm,
+            final String historyID,
+            final boolean showErrorMessage,
+            final String... suffixes) {
         if (historyID == null || suffixes == null) {
             throw new NullPointerException("Argument must not be null.");
         }
@@ -95,6 +126,8 @@ public final class FilesHistoryPanel extends JPanel {
         m_textBox = new JComboBox(new DefaultComboBoxModel());
         m_textBox.setEditable(true);
         m_textBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, 25));
+        
+      
         m_textBox.setPreferredSize(new Dimension(300, 25));
         m_textBox.setRenderer(new MyComboBoxRenderer());
         m_textBox.addItemListener(new ItemListener() {
@@ -109,7 +142,39 @@ public final class FilesHistoryPanel extends JPanel {
                 }
             }
         });
+        
+        // install listeners to update warn message whenever file name changes
+        m_textBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(final ItemEvent e) {
+                fileLocationChanged();
+            }
+        });
 
+        /* install action listeners */
+        m_textBox.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(final FocusEvent e) {
+                fileLocationChanged();
+            }
+        });
+        Component editor = m_textBox.getEditor().getEditorComponent();
+        if (editor instanceof JTextComponent) {
+            Document d = ((JTextComponent)editor).getDocument();
+            d.addDocumentListener(new DocumentListener() {
+                public void changedUpdate(final DocumentEvent e) {
+                    fileLocationChanged();
+                }
+
+                public void insertUpdate(final DocumentEvent e) {
+                    fileLocationChanged();
+                }
+
+                public void removeUpdate(final DocumentEvent e) {
+                    fileLocationChanged();
+                }
+            });
+        }
+        
         m_chooseButton = new JButton("Browse...");
         m_chooseButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
@@ -124,10 +189,58 @@ public final class FilesHistoryPanel extends JPanel {
                 }
             }
         });
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        add(m_textBox);
-        add(m_chooseButton);
+        m_warnMsg = new JLabel("");
+        // this ensures correct display of the changing label content...
+        m_warnMsg.setPreferredSize(new Dimension(350, 25));
+        m_warnMsg.setMinimumSize(new Dimension(350, 25));
+        m_warnMsg.setForeground(Color.red);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        Box fileBox = Box.createHorizontalBox();
+        fileBox.add(m_textBox);
+        if (fvm != null) {
+            fileBox.add(Box.createHorizontalStrut(5));
+            fileBox.add(new FlowVariableModelButton(fvm));
+            fvm.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(final ChangeEvent evt) {
+                    FlowVariableModel wvm =
+                            (FlowVariableModel)(evt.getSource());
+                    m_textBox.setEnabled(!wvm.isVariableReplacementEnabled());
+                    m_chooseButton.setEnabled(!wvm
+                            .isVariableReplacementEnabled());
+                    fileLocationChanged();
+                }
+            });
+
+        }
+        fileBox.add(Box.createHorizontalStrut(5));
+        fileBox.add(m_chooseButton);
+        Box warnBox = Box.createHorizontalBox();
+        warnBox.add(m_warnMsg);
+        warnBox.add(Box.createHorizontalGlue());
+        warnBox.add(Box.createVerticalStrut(25));
+
+        add(fileBox);
+        if (showErrorMessage) {
+            add(warnBox);
+        }
+        fileLocationChanged();
         updateHistory();
+    }
+
+    /**
+      * Creates new instance, sets properties, for instance renderer,
+     * accordingly.
+     *
+     * @param historyID identifier for the string history, see
+     *            {@link StringHistory}
+     * @param showErrorMessage if true there are error messages if the file 
+     * exists or the path is not available and so on.
+     */
+    public FilesHistoryPanel(final String historyID, 
+            final boolean showErrorMessage) {
+        this(null, historyID, showErrorMessage);
     }
 
     private String getOutputFileName() {
@@ -277,6 +390,40 @@ public final class FilesHistoryPanel extends JPanel {
             f = new File(new File(System.getProperty("user.home")), fileName);
         }
         return f;
+    }
+    
+    private void fileLocationChanged() {
+        String newMsg = "";
+        String selFile = getSelectedFile();
+        if (selFile != null && selFile.length() > 0) {
+            File newFile = getFile(selFile);
+            if (newFile.exists()) {
+                if (newFile.isDirectory()) {
+                    newMsg = "ERROR: a file can't be a directory";
+                } else {
+                    newMsg = "Warning: selected file exists.";
+                }
+            } else {
+                if (!newFile.getParentFile().exists()) {
+                    newMsg =
+                            "Warning: Directory of specified file doesn't exist"
+                                    + " and will be created";
+                }
+            }
+        }
+
+        if (newMsg.length() == 0) {
+            m_warnMsg.setForeground(m_warnMsg.getBackground());
+            m_warnMsg.setText("A long msg to avoid invisibility of label");
+        } else {
+            m_warnMsg.setForeground(Color.red);
+            m_warnMsg.setText(newMsg);
+        }
+
+        revalidate();
+        repaint();
+        invalidate();
+        repaint();
     }
 
     /** renderer that also supports to show customized tooltip. */
