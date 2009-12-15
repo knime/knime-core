c128de5b0f064f25a8a1065f831b2deeb17cbe10
Dominik Morent <dominik.morent@knime.com>
2009-12-15 09:12:09 +0000

    fixed bug 2119: Scatter matrix displays wrong labels on y-axis http://bimbug.inf.uni-konstanz.de/show_bug.cgi?id=2119

--- org.knime.base/src/org/knime/base/node/viz/plotter/scattermatrix/ScatterMatrixDrawingPane.java
+++ org.knime.base/src/org/knime/base/node/viz/plotter/scattermatrix/ScatterMatrixDrawingPane.java
@@ -44,7 +44,7 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   03.10.2006 (Fabian Dill): created
  */
@@ -62,41 +62,41 @@ import org.knime.base.node.viz.plotter.scatter.ScatterPlotterDrawingPane;
 
 
 /**
- * Holds a matrix of 
+ * Holds a matrix of
  * {@link org.knime.base.node.viz.plotter.scattermatrix.ScatterMatrixElement}s.
- * This class only paints the surrounding rectangles of the matrix elements, 
- * the painting of the actual dots is done by the 
+ * This class only paints the surrounding rectangles of the matrix elements,
+ * the painting of the actual dots is done by the
  * {@link org.knime.base.node.viz.plotter.scatter.ScatterPlotterDrawingPane}.
  * In addition to the scatter matrix elements the coordinates of the scatter
  * matrix elements are plotted at the borders of the drawing pane.
  * The {@link #setBackground(Color)} is overriden, since it sets the color of
  * the scatter matrix elements and not of the whole component.
- * 
- * The 
+ *
+ * The
  * {@link org.knime.base.node.viz.plotter.scattermatrix.ScatterMatrixElement}s
- *  know there associated dots. By setting them with the 
- *  {@link #setScatterMatrixElements(ScatterMatrixElement[][])}, the 
- *  {@link org.knime.base.node.viz.plotter.scatter.DotInfo}s are put into a 
+ *  know there associated dots. By setting them with the
+ *  {@link #setScatterMatrixElements(ScatterMatrixElement[][])}, the
+ *  {@link org.knime.base.node.viz.plotter.scatter.DotInfo}s are put into a
  *  {@link org.knime.base.node.viz.plotter.scatter.DotInfoArray}, which can then
- *  be painted with the inherited functionality of the 
+ *  be painted with the inherited functionality of the
  *  {@link org.knime.base.node.viz.plotter.scatter.ScatterPlotterDrawingPane}.
- * 
+ *
  * @author Fabian Dill, University of Konstanz
  */
 public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
-    
+
 //    private static final NodeLogger LOGGER = NodeLogger.getLogger(
 //            ScatterMatrixDrawingPane.class);
-//    
+//
     private ScatterMatrixElement[][] m_matrixElements;
-    
+
     private int m_hMargin = 50;
-    
+
     private int m_vMargin = 40;
-    
+
     private Color m_backgroundColor = this.getBackground();
-    
-    
+
+
     /**
      * Sets the background color to white.
      *
@@ -104,26 +104,26 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
     public ScatterMatrixDrawingPane() {
         super.setBackground(Color.white);
     }
-    
+
     /**
-     * 
+     *
      * @param hMargin sets the horizontal margin.
      */
     public void setHorizontalMargin(final int hMargin) {
         m_hMargin = hMargin;
     }
-    
+
     /**
-     * 
+     *
      * @param vMargin the vertical margin
      */
     public void setVerticalMargin(final int vMargin) {
         m_vMargin = vMargin;
     }
 
-    
+
     /**
-     * 
+     *
      * @param elements the rectangles with the coordinates.
      */
     public void setScatterMatrixElements(
@@ -147,15 +147,15 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
 //        repaint();
         }
     }
-    
+
     /**
-     * 
+     *
      * @return the scatter matrix elements.
      */
     public ScatterMatrixElement[][] getMatrixElements() {
         return m_matrixElements;
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -164,14 +164,14 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
         m_backgroundColor = bg;
         repaint();
     }
-    
-    
+
+
     /**
      * Paints the rectangles of the scatter matrix elements and the vertical and
-     * horizontal coordinates at the border of the drawing pane. The painting 
-     * of the dots is inherited from the 
+     * horizontal coordinates at the border of the drawing pane. The painting
+     * of the dots is inherited from the
      *{@link org.knime.base.node.viz.plotter.scatter.ScatterPlotterDrawingPane}.
-     * 
+     *
      * {@inheritDoc}
      */
     @Override
@@ -185,15 +185,15 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
         for (int i = 0; i < m_matrixElements.length; i++) {
             for (int j = 0; j < m_matrixElements[i].length; j++) {
                 ScatterMatrixElement element = m_matrixElements[i][j];
-                // matrix element might be null (if no rows available) since 
+                // matrix element might be null (if no rows available) since
                 // the array is initialized with column length
                 if (element == null) {
                     continue;
                 }
-                g.drawRect(element.getCorner().x, element.getCorner().y, 
+                g.drawRect(element.getCorner().x, element.getCorner().y,
                         element.getWidth(), element.getHeight());
                 // paint the background color of the matrix elements
-                // not the background fo the whole panel but only of the matrix 
+                // not the background fo the whole panel but only of the matrix
                 // elements should be changeable
                 Color backupC = g.getColor();
                 g.setColor(m_backgroundColor);
@@ -205,7 +205,7 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
                     // paint at top
                     int y = m_hMargin - 6;
                     paintHorizontalCoordinate(g, element, y, true);
-                } 
+                }
                 if (j % 2 == 1 && i == m_matrixElements.length - 1) {
                     // paint right
                     int x = getWidth() - m_vMargin + 3;
@@ -223,23 +223,23 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
                 }
             }
             super.paintContent(g);
-        } 
+        }
     }
-    
+
     /**
      * Paints the vertical coordinates at the border of the drawing pane.
-     * 
+     *
      * @param g graphics
      * @param element element to provide position and coordinate
      * @param x the x position
      * @param left true if the coordinate is painted on the left side.
      */
-    protected void paintVerticalCoordinate(final Graphics g, 
-            final ScatterMatrixElement element, final int x, 
+    protected void paintVerticalCoordinate(final Graphics g,
+            final ScatterMatrixElement element, final int x,
             final boolean left) {
         Axis paintAxis = new Axis(Axis.VERTICAL, element.getHeight(), !left);
-        paintAxis.setCoordinate(element.getXCoordinate());
-        int axisY = element.getCorner().y; 
+        paintAxis.setCoordinate(element.getYCoordinate());
+        int axisY = element.getCorner().y;
         int transX = x - Axis.SIZE;
         g.translate(transX, axisY);
         paintAxis.paintComponent(g);
@@ -247,24 +247,24 @@ public class ScatterMatrixDrawingPane extends ScatterPlotterDrawingPane {
     }
 
     /**
-     * Paints the horizontal coordinates of the matrix elements at the border 
+     * Paints the horizontal coordinates of the matrix elements at the border
      * of the drawing pane.
-     * 
+     *
      * @param g graphics
      * @param element the element to provide position and coordinate
      * @param y y position
      * @param top true if the coordinate is painted at the top.
      */
-    protected void paintHorizontalCoordinate(final Graphics g, 
-            final ScatterMatrixElement element, final int y, 
+    protected void paintHorizontalCoordinate(final Graphics g,
+            final ScatterMatrixElement element, final int y,
             final boolean top) {
         Axis paintAxis = new Axis(Axis.HORIZONTAL, element.getWidth(), top);
         paintAxis.setCoordinate(element.getXCoordinate());
-        int axisX = element.getCorner().x; 
+        int axisX = element.getCorner().x;
         g.translate(axisX, y);
         paintAxis.paintComponent(g);
         g.translate(-axisX, -y);
     }
-    
-    
+
+
 }
