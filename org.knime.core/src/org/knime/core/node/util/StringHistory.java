d43225e71192abd3555c82235d828d559f364394
Bernd Wiswedel <bernd.wiswedel@knime.com>
2010-06-08 11:09:58 +0000

    bug fix 2291: ConcurrentModificationException in StringHistory http://bimbug.inf.uni-konstanz.de/show_bug.cgi?id=2291

--- org.knime.core/src/org/knime/core/node/util/StringHistory.java
+++ org.knime.core/src/org/knime/core/node/util/StringHistory.java
@@ -44,7 +44,7 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * -------------------------------------------------------------------
- * 
+ *
  */
 package org.knime.core.node.util;
 
@@ -63,37 +63,37 @@ import org.knime.core.node.NodeLogger;
 
 /**
  * Utility class that keeps a list of recently used String, mostly
- * used in File-Reader and Writer that keep track of a list of recently 
+ * used in File-Reader and Writer that keep track of a list of recently
  * accessed file names. This class also makes sure that the list is made
  * persistent and saved to disk when the application closes. The list
  * is read again from file on startup.
- * 
- * <p>Usage in a short way: Determine a (possibly unique) ID for your 
+ *
+ * <p>Usage in a short way: Determine a (possibly unique) ID for your
  * history to use (I assume that there are not that many?) and get a history
- * object by invoking <code>StringHistory.getInstance(yourID)</code>. You 
- * can add recently used String to this object by invoking the 
- * <code>add(String)</code> method and get a list of String from the 
+ * object by invoking <code>StringHistory.getInstance(yourID)</code>. You
+ * can add recently used String to this object by invoking the
+ * <code>add(String)</code> method and get a list of String from the
  * <code>getHistory()</code> method which will return the history in the order
  * the add method on the String objects has been called with the last element
  * added being the first element in the history result.
  * @author Bernd Wiswedel, University of Konstanz
  */
 public final class StringHistory {
-    
-    private static final NodeLogger LOGGER = 
+
+    private static final NodeLogger LOGGER =
         NodeLogger.getLogger(StringHistory.class);
-    
+
     /** List of all StringHistory elements, filled lazy, i.e. on request */
-    private static final Hashtable<String, StringHistory> INSTANCES = 
+    private static final Hashtable<String, StringHistory> INSTANCES =
         new Hashtable<String, StringHistory>();
-    
+
     /** Get a history for an ID. If that ID doesn't yet exist, a new
      * object is created.
      * @param id The ID of interest
-     * @return The History for this id. If this id has been used in a 
+     * @return The History for this id. If this id has been used in a
      *         previous run, the history is loaded from file
      */
-    public static StringHistory getInstance(final String id) {
+    public static synchronized StringHistory getInstance(final String id) {
         StringHistory value = INSTANCES.get(id);
         if (value == null) {
             value = new StringHistory(8, id);
@@ -101,11 +101,11 @@ public final class StringHistory {
         }
         return value;
     }
-    
+
     private final int m_maxLength;
     private final File m_historyFile;
     private final LinkedList<String> m_list;
-    
+
     /**
      * private constructor.
      * @param maxLength The length of the history
@@ -119,14 +119,14 @@ public final class StringHistory {
         m_list = new LinkedList<String>();
         load();
     }
-    
+
     /** Adds a new String to the history. If this string alread exists
      * in the history, it is marked as most recently added and will be returned
      * first on a immediate call of <code>getHistory()</code>
      * @param str The string to add.
      * @throws NullPointerException If argument is null.
      */
-    public void add(final String str) {
+    public synchronized void add(final String str) {
         if (str == null) {
             throw new NullPointerException("String must not be null.");
         }
@@ -141,11 +141,11 @@ public final class StringHistory {
         m_list.addFirst(str);
         save();
     }
-    
+
     /** Get the history in an array with the most recently added element first.
      * @return The history.
      */
-    public String[] getHistory() {
+    public synchronized String[] getHistory() {
         return m_list.toArray(new String[0]);
     }
 
@@ -153,28 +153,28 @@ public final class StringHistory {
     private void load() {
         m_list.clear();
         if (!m_historyFile.exists()) {
-            LOGGER.info("History file '" + m_historyFile.getAbsolutePath() 
+            LOGGER.info("History file '" + m_historyFile.getAbsolutePath()
                     + "' does not exist.");
             return;
         }
         try {
-            BufferedReader reader = 
+            BufferedReader reader =
                 new BufferedReader(new FileReader(m_historyFile));
             String hist = null;
-            while ((hist = reader.readLine()) != null 
+            while ((hist = reader.readLine()) != null
                     && m_list.size() < m_maxLength) {
                 m_list.add(hist);
             }
         } catch (IOException ioe) {
-            LOGGER.warn("Can't read history file '" 
+            LOGGER.warn("Can't read history file '"
                     + m_historyFile.getAbsolutePath() + "'", ioe);
         }
     }
-    
+
     /** Saves to file. */
     private void save() {
         try {
-            BufferedWriter writer = 
+            BufferedWriter writer =
                 new BufferedWriter(new FileWriter(m_historyFile));
             for (String hist : m_list) {
                 writer.write(hist);
@@ -182,7 +182,7 @@ public final class StringHistory {
             }
             writer.close();
         } catch (IOException ioe) {
-            LOGGER.warn("Unable to write to file '" 
+            LOGGER.warn("Unable to write to file '"
                     + m_historyFile.getAbsolutePath() + "'", ioe);
         }
     }
