3b77a5857cbef1f4925f29b91175d4a9620f11a9
Tobias Kötter <tobias.koetter@knime.com>
2010-06-23 12:56:41 +0000

    removed unused LOGGER instance

--- org.knime.base/src/org/knime/base/node/meta/looper/LoopEnd2NodeModel.java
+++ org.knime.base/src/org/knime/base/node/meta/looper/LoopEnd2NodeModel.java
@@ -23,10 +23,6 @@
  */
 package org.knime.base.node.meta.looper;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.knime.base.data.append.column.AppendedColumnRow;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
@@ -39,20 +35,22 @@ import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
-import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.workflow.LoopEndNode;
 import org.knime.core.node.workflow.LoopStartNodeTerminator;
 
+import org.knime.base.data.append.column.AppendedColumnRow;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  *
  * @author Thorsten Meinl, University of Konstanz
  */
 public class LoopEnd2NodeModel extends NodeModel implements LoopEndNode {
-    private static final NodeLogger LOGGER =
-        NodeLogger.getLogger(LoopEndNodeModel.class);
 
     private BufferedDataContainer[] m_resultContainer;
     private int m_count;
@@ -75,10 +73,10 @@ public class LoopEnd2NodeModel extends NodeModel implements LoopEndNode {
     }
 
     private static DataTableSpec createSpec(final DataTableSpec inSpec) {
-        DataColumnSpecCreator crea =
+        final DataColumnSpecCreator crea =
                 new DataColumnSpecCreator(DataTableSpec.getUniqueColumnName(
                         inSpec, "Iteration"), IntCell.TYPE);
-        DataTableSpec newSpec = new DataTableSpec(crea.createSpec());
+        final DataTableSpec newSpec = new DataTableSpec(crea.createSpec());
 
         return new DataTableSpec(inSpec, newSpec);
     }
@@ -105,29 +103,30 @@ public class LoopEnd2NodeModel extends NodeModel implements LoopEndNode {
             };
         }
 
-        IntCell currIterCell = new IntCell(m_count);
-        for (DataRow row : inData[0]) {
-            AppendedColumnRow newRow =
+        final IntCell currIterCell = new IntCell(m_count);
+        for (final DataRow row : inData[0]) {
+            final AppendedColumnRow newRow =
                     new AppendedColumnRow(new DefaultRow(new RowKey(row.getKey()
                     + "#" + m_count), row), currIterCell);
             m_resultContainer[0].addRowToTable(newRow);
         }
 
-        for (DataRow row : inData[1]) {
-            AppendedColumnRow newRow =
+        for (final DataRow row : inData[1]) {
+            final AppendedColumnRow newRow =
                     new AppendedColumnRow(new DefaultRow(new RowKey(row.getKey()
                     + "#" + m_count), row), currIterCell);
             m_resultContainer[1].addRowToTable(newRow);
         }
 
 
-        boolean terminateLoop =
+        final boolean terminateLoop =
             ((LoopStartNodeTerminator)this.getLoopStartNode()).terminateLoop();
         if (terminateLoop) {
             // this was the last iteration - close container and continue
             m_resultContainer[0].close();
             m_resultContainer[1].close();
-            BufferedDataTable[] outTables = {m_resultContainer[0].getTable(),
+            final BufferedDataTable[] outTables =
+                {m_resultContainer[0].getTable(),
                     m_resultContainer[1].getTable()};
             m_resultContainer = null;
             m_count = 0;
