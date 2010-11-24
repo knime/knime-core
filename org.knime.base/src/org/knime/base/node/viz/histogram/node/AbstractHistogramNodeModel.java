8baa46e4f62f585cf0a74348e8ba51e2d650a0e6
Thomas Gabriel <thomas.gabriel@knime.com>
2010-11-24 09:58:46 +0000

    fix 2384: Histogram node doesn't configure correctly with missing aggregation columns

--- org.knime.base/src/org/knime/base/node/viz/histogram/node/AbstractHistogramNodeModel.java
+++ org.knime.base/src/org/knime/base/node/viz/histogram/node/AbstractHistogramNodeModel.java
@@ -45,12 +45,28 @@
  *  when such Node is propagated with or for interoperation with KNIME.
  * -------------------------------------------------------------------
  *
- * History
- *    13.03.2007 (Tobias Koetter): created
  */
 
 package org.knime.base.node.viz.histogram.node;
 
+import java.awt.Color;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.knime.base.node.viz.histogram.datamodel.AbstractHistogramVizModel;
+import org.knime.base.node.viz.histogram.impl.AbstractHistogramPlotter;
+import org.knime.base.node.viz.histogram.util.ColorColumn;
+import org.knime.base.node.viz.histogram.util.NoDomainColumnFilter;
+import org.knime.base.node.viz.histogram.util.SettingsModelColorNameColumns;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTable;
 import org.knime.core.data.DataTableSpec;
@@ -76,25 +92,6 @@ import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.port.PortType;
 import org.knime.core.node.util.ColumnFilter;
-
-import org.knime.base.node.viz.histogram.datamodel.AbstractHistogramVizModel;
-import org.knime.base.node.viz.histogram.impl.AbstractHistogramPlotter;
-import org.knime.base.node.viz.histogram.util.ColorColumn;
-import org.knime.base.node.viz.histogram.util.NoDomainColumnFilter;
-import org.knime.base.node.viz.histogram.util.SettingsModelColorNameColumns;
-
-import java.awt.Color;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 /**
  *
@@ -143,6 +140,7 @@ public abstract class AbstractHistogramNodeModel extends NodeModel {
     public AbstractHistogramNodeModel() {
         super(new PortType[]{BufferedDataTable.TYPE}, new PortType[0]);
         m_allRows.addChangeListener(new ChangeListener() {
+            @Override
             public void stateChanged(final ChangeEvent e) {
                 m_noOfRows.setEnabled(!m_allRows.getBooleanValue());
             }
@@ -408,17 +406,20 @@ public abstract class AbstractHistogramNodeModel extends NodeModel {
             }
         }
         //check if the aggregation columns are available and valid
-        for (final ColorColumn col : m_aggrColName.getColorNameColumns()) {
-            final DataColumnSpec cSpec =
-                tableSpec.getColumnSpec(col.getColumnName());
-            if (cSpec == null) {
-                throw new InvalidSettingsException("Aggregation column '"
+        final ColorColumn[] aggrColNames = m_aggrColName.getColorNameColumns();
+        if (aggrColNames != null) {
+            for (final ColorColumn col : aggrColNames) {
+                final DataColumnSpec cSpec =
+                    tableSpec.getColumnSpec(col.getColumnName());
+                if (cSpec == null) {
+                    throw new InvalidSettingsException("Aggregation column '"
                         + col.getColumnName() + "' not found in input table");
-            } else if (!AbstractHistogramPlotter
-                    .AGGREGATION_COLUMN_FILTER.includeColumn(cSpec)) {
-                throw new InvalidSettingsException("Aggregation column '"
+                } else if (!AbstractHistogramPlotter
+                        .AGGREGATION_COLUMN_FILTER.includeColumn(cSpec)) {
+                    throw new InvalidSettingsException("Aggregation column '"
                         + col.getColumnName() + "' is not numeric "
                         + "(not double compatible) or has an invalid domain");
+                }
             }
         }
         final ColumnFilter filter = NoDomainColumnFilter.getInstance();
