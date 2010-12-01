daf31345d5c4f3e8136b0c42099aa7ebacbf0691
Bernd Wiswedel <bernd.wiswedel@knime.com>
2010-12-01 15:10:46 +0000

    fixed link

--- org.knime.core/src/org/knime/core/node/port/inactive/InactiveBranchConsumer.java
+++ org.knime.core/src/org/knime/core/node/port/inactive/InactiveBranchConsumer.java
@@ -17,23 +17,23 @@
  * website: www.knime.org
  * email: contact@knime.org
  * ---------------------------------------------------------------------
- * 
+ *
  * History
  *   30.09.2010 (berthold_2): created
  */
 package org.knime.core.node.port.inactive;
 
 
-/** Marker interface implemented by nodes 
- * ({@link org.knime.core.node.NodeModelNodeModel}) that are able to consume 
+/** Marker interface implemented by nodes
+ * ({@link org.knime.core.node.NodeModel}) that are able to consume
  * inactive branches. The configure and execute methods of such implementations
- * must accept classes of {@link InactiveBranchPortObject} and 
+ * must accept classes of {@link InactiveBranchPortObject} and
  * {@link InactiveBranchPortObjectSpec} (which kind of violates the assertions
  * made by the method API).
- * 
- * <p>This interface is implemented by nodes such as the End IF and End Case 
- * node. 
- * 
+ *
+ * <p>This interface is implemented by nodes such as the End IF and End Case
+ * node.
+ *
  * @author B. Wiswedel, University of Konstanz
  */
 public interface InactiveBranchConsumer {
