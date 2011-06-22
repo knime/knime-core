2f065abecdf449ffed6ebd9d92f3d222ece69633
Dominik Morent <dominik.morent@knime.com>
2011-06-22 12:36:56 +0000

    when KNIME built a general regression model, for each categorical fields, there is one category used as the default baseline and therefore it didn't show in the ParameterList in PMML. For example, in the attached PMML model: --------------------------------------------  <DataField dataType="string" name="Employment" optype="categorical">       <Value value="Private"/>       <Value value="Consultant"/>       <Value value="SelfEmp"/>       <Value value="PSLocal"/>       <Value value="PSState"/>       <Value value="PSFederal"/>       <Value value="Volunteer"/>     </DataField>
      <ParameterList>
          <Parameter name="p0" label="Intercept"/>
          <Parameter name="p1" label="Age"/>
          <Parameter name="p2" label="[Employment=PSFederal]"/>
          <Parameter name="p3" label="[Employment=PSLocal]"/>
          <Parameter name="p4" label="[Employment=PSState]"/>
          <Parameter name="p5" label="[Employment=Private]"/>
          <Parameter name="p6" label="[Employment=SelfEmp]"/>
          <Parameter name="p7" label="[Employment=Volunteer]"/>
          :
    -----------------------------------------
    Consultant was not included in the ParameterList while it is a DataField. This design for the training is fine, but in the prediction, when the input of Employment is Consultant, each parameter for the Employment should all be 0.

--- org.knime.base/src/org/knime/base/node/mine/regression/logistic/predict/LogRegPredictor.java
+++ org.knime.base/src/org/knime/base/node/mine/regression/logistic/predict/LogRegPredictor.java
@@ -303,8 +303,18 @@ public final class LogRegPredictor extends AbstractCellFactory {
                         row.getCell(m_parameterI.get(parameter));
                     int index = values.indexOf(cell);
                     // these are design variables
-                    x.set(0, i + index - 1, 1);
-                    i += values.size() - 2;
+                    /* When building ageneral regression model, for each 
+                    categorical fields, there is one category used as the 
+                    default baseline and therefore it didn't show in the 
+                    ParameterList in PMML. This design for the training is fine, 
+                    but in the prediction, when the input of Employment is 
+                    the default baseline, the parameters should all be 0. 
+                    See the commit message for an example and more details.
+                    */
+                    if (index > 0) {
+                        x.set(0, i + index - 1, 1);
+                        i += values.size() - 2;
+                    }
                 } else {
                     DataCell cell =
                         row.getCell(m_parameterI.get(parameter));
