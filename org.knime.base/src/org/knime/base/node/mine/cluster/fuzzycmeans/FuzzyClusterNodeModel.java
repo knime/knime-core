b80bd8e5caeb12190a82dbe0b96ca626308ff835
Dominik Morent <dominik.morent@knime.com>
2011-06-27 14:08:22 +0000

    added the optional PMML inport

--- org.knime.base/src/org/knime/base/node/mine/cluster/fuzzycmeans/FuzzyClusterNodeModel.java
+++ org.knime.base/src/org/knime/base/node/mine/cluster/fuzzycmeans/FuzzyClusterNodeModel.java
@@ -271,7 +271,8 @@ public class FuzzyClusterNodeModel extends NodeModel {
      * Constructor, remember parent and initialize status.
      */
     public FuzzyClusterNodeModel() {
-        super(new PortType[]{BufferedDataTable.TYPE}, new PortType[]{
+        super(new PortType[]{BufferedDataTable.TYPE,
+                new PortType(PMMLPortObject.class, true)}, new PortType[]{
                 BufferedDataTable.TYPE, PMMLPortObject.TYPE});
 
         m_nrClusters = INITIAL_NR_CLUSTERS;
