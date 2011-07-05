c3448cfef28e149bc3396e51065a1083d31e29d8
Thorsten Meinl <thorsten.meinl@knime.com>
2011-07-05 08:56:14 +0000

    fixed copyright headers

--- org.knime.workbench.ui.layout/src/org/knime/workbench/ui/layout/align/VerticAlignmentCenter.java
+++ org.knime.workbench.ui.layout/src/org/knime/workbench/ui/layout/align/VerticAlignmentCenter.java
@@ -1,18 +1,48 @@
-/* ------------------------------------------------------------------
- * This source code, its documentation and all appendant files
- * are protected by copyright law. All rights reserved.
+/*
+ * ------------------------------------------------------------------------
  *
- * Copyright (c) KNIME.com, Zurich, Switzerland
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
  *
- * You may not modify, publish, transmit, transfer or sell, reproduce,
- * create derivative works from, distribute, perform, display, or in
- * any way exploit any of the content, in whole or in part, except as
- * otherwise expressly permitted in writing by the copyright owner or
- * as specified in the license file distributed with this product.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
  *
- * If you have any questions please contact the copyright holder:
- * website: www.knime.com
- * email: contact@knime.com
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
  * ---------------------------------------------------------------------
  *
  * Created: Mar 31, 2011
@@ -65,11 +95,11 @@ public class VerticAlignmentCenter {
             public int compare(final NodeContainerEditPart o1,
                     final NodeContainerEditPart o2) {
                 NodeUIInformation ui1 =
-                        (NodeUIInformation)o1.getNodeContainer()
-                                .getUIInformation();
+                        o1.getNodeContainer()
+                        .getUIInformation();
                 NodeUIInformation ui2 =
-                        (NodeUIInformation)o2.getNodeContainer()
-                                .getUIInformation();
+                        o2.getNodeContainer()
+                        .getUIInformation();
                 if (ui1 == null || ui2 == null) {
                     return 0;
                 }
@@ -85,8 +115,8 @@ public class VerticAlignmentCenter {
         HashMap<NodeContainerEditPart, Integer> offsets =
                 new HashMap<NodeContainerEditPart, Integer>();
         NodeUIInformation nui =
-                (NodeUIInformation)nodes[0].getNodeContainer()
-                        .getUIInformation();
+                nodes[0].getNodeContainer()
+                .getUIInformation();
         if (nui == null) {
             LOGGER.warn("Only nodes with location information can be aligned.");
             return Collections.emptyMap();
@@ -95,7 +125,7 @@ public class VerticAlignmentCenter {
 
         for (int i = 1 /* idx 0 is anchor */; i < nodes.length; i++) {
             NodeContainer nc = nodes[i].getNodeContainer();
-            NodeUIInformation ui = (NodeUIInformation)nc.getUIInformation();
+            NodeUIInformation ui = nc.getUIInformation();
             if (ui.getBounds()[0] != refX) {
                 offsets.put(nodes[i], refX - ui.getBounds()[0]);
             }
--- org.knime.workbench.ui/src/org/knime/workbench/ui/navigator/KNIMEResourceNavigatorActionGroup.java
+++ org.knime.workbench.ui/src/org/knime/workbench/ui/navigator/KNIMEResourceNavigatorActionGroup.java
@@ -1,18 +1,48 @@
-/* ------------------------------------------------------------------
- * This source code, its documentation and all appendant files
- * are protected by copyright law. All rights reserved.
+/*
+ * ------------------------------------------------------------------------
  *
- * Copyright (c) KNIME.com, Zurich, Switzerland
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
  *
- * You may not modify, publish, transmit, transfer or sell, reproduce,
- * create derivative works from, distribute, perform, display, or in
- * any way exploit any of the content, in whole or in part, except as
- * otherwise expressly permitted in writing by the copyright owner or
- * as specified in the license file distributed with this product.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
  *
- * If you have any questions please contact the copyright holder:
- * website: www.knime.com
- * email: contact@knime.com
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
  * ---------------------------------------------------------------------
  *
  * Created: Jun 29, 2011
@@ -29,9 +59,9 @@ import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.DeleteResourceAction;
+import org.eclipse.ui.actions.RenameResourceAction;
 import org.eclipse.ui.views.navigator.IResourceNavigator;
 import org.eclipse.ui.views.navigator.MainActionGroup;
-import org.eclipse.ui.views.navigator.ResourceNavigatorRenameAction;
 import org.knime.workbench.ui.navigator.actions.CopyToClipboard;
 import org.knime.workbench.ui.navigator.actions.DeleteAction;
 import org.knime.workbench.ui.navigator.actions.PasteAction;
@@ -115,9 +145,9 @@ public class KNIMEResourceNavigatorActionGroup extends MainActionGroup {
 
         // Rename must be our own action (due to workflow locks). Hence
         // replace the default rename action if it is there. */
-        if (menu.find(ResourceNavigatorRenameAction.ID) != null) {
-            menu.insertBefore(ResourceNavigatorRenameAction.ID, m_renAction);
-            menu.remove(ResourceNavigatorRenameAction.ID);
+        if (menu.find(RenameResourceAction.ID) != null) {
+            menu.insertBefore(RenameResourceAction.ID, m_renAction);
+            menu.remove(RenameResourceAction.ID);
         }
         updateMyActions();
     }
