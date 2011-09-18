7848e2feb980017a31f09403525fa8146df62c5d
Bernd Wiswedel <bernd.wiswedel@knime.com>
2011-09-18 09:21:24 +0000

    missing @Override

--- org.knime.core/src/org/knime/core/data/container/ConcatenateTable.java
+++ org.knime.core/src/org/knime/core/data/container/ConcatenateTable.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * ------------------------------------------------------------------------
  *
  *  Copyright (C) 2003 - 2011
@@ -44,7 +44,7 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * --------------------------------------------------------------------- *
- * 
+ *
  * History
  *   Jan 5, 2007 (wiswedel): created
  */
@@ -60,20 +60,20 @@ import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.RowKey;
 import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.BufferedDataTable.KnowsRowCountTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.Node;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
-import org.knime.core.node.BufferedDataTable.KnowsRowCountTable;
 
 /**
- * 
+ *
  * @author Bernd Wiswedel, University of Konstanz
  */
 public final class ConcatenateTable implements KnowsRowCountTable {
-    
+
     private static final String CFG_INTERNAL_META = "meta_internal";
     private static final String CFG_REFERENCE_IDS = "table_reference_IDS";
     private static final String CFG_ROW_COUNT = "table_rowcount";
@@ -81,8 +81,8 @@ public final class ConcatenateTable implements KnowsRowCountTable {
     private final BufferedDataTable[] m_tables;
     private final DataTableSpec m_spec;
     private final int m_rowCount;
-    
-    private ConcatenateTable(final BufferedDataTable[] tables, 
+
+    private ConcatenateTable(final BufferedDataTable[] tables,
             final DataTableSpec spec, final int rowCount) {
         m_tables = tables;
         m_spec = spec;
@@ -91,12 +91,14 @@ public final class ConcatenateTable implements KnowsRowCountTable {
 
     /** Internal use.
      * {@inheritDoc} */
+    @Override
     public void clear() {
         // left empty, it's up to the node to clear our underlying tables.
     }
-    
+
     /** Internal use.
      * {@inheritDoc} */
+    @Override
     public void ensureOpen() {
         for (BufferedDataTable t : m_tables) {
             // use public method in class Node to do it
@@ -108,6 +110,7 @@ public final class ConcatenateTable implements KnowsRowCountTable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public BufferedDataTable[] getReferenceTables() {
         return m_tables;
     }
@@ -115,6 +118,7 @@ public final class ConcatenateTable implements KnowsRowCountTable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public int getRowCount() {
         return m_rowCount;
     }
@@ -122,26 +126,33 @@ public final class ConcatenateTable implements KnowsRowCountTable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public DataTableSpec getDataTableSpec() {
         return m_spec;
     }
 
     /** {@inheritDoc} */
+    @Override
     public CloseableRowIterator iterator() {
         return new MyIterator();
     }
 
     /** {@inheritDoc} */
+    @Override
     public void putIntoTableRepository(
             final HashMap<Integer, ContainerTable> rep) {
+        // no new tables, ignore
     }
 
     /** {@inheritDoc} */
+    @Override
     public void removeFromTableRepository(
             final HashMap<Integer, ContainerTable> rep) {
+        // no new tables, ignore
     }
 
     /** {@inheritDoc} */
+    @Override
     public void saveToFile(final File f, final NodeSettingsWO s,
             final ExecutionMonitor exec) throws IOException,
             CanceledExecutionException {
@@ -153,7 +164,7 @@ public final class ConcatenateTable implements KnowsRowCountTable {
         subSettings.addIntArray(CFG_REFERENCE_IDS, referenceIDs);
         subSettings.addInt(CFG_ROW_COUNT, m_rowCount);
     }
-    
+
     /** Restore table form node settings object.
      * @param s Containing information.
      * @param spec Associated spec.
@@ -161,9 +172,9 @@ public final class ConcatenateTable implements KnowsRowCountTable {
      * @return The newly instantiated table.
      * @throws InvalidSettingsException If information is invalid.
      */
-    public static ConcatenateTable load(final NodeSettingsRO s, 
-            final DataTableSpec spec, 
-            final Map<Integer, BufferedDataTable> tblRep) 
+    public static ConcatenateTable load(final NodeSettingsRO s,
+            final DataTableSpec spec,
+            final Map<Integer, BufferedDataTable> tblRep)
         throws InvalidSettingsException {
         NodeSettingsRO subSettings = s.getNodeSettings(CFG_INTERNAL_META);
         int[] referenceIDs = subSettings.getIntArray(CFG_REFERENCE_IDS);
@@ -182,7 +193,7 @@ public final class ConcatenateTable implements KnowsRowCountTable {
      * @throws CanceledExecutionException If cancelled.
      */
     public static ConcatenateTable create(
-            final ExecutionMonitor mon, final BufferedDataTable... tables) 
+            final ExecutionMonitor mon, final BufferedDataTable... tables)
             throws CanceledExecutionException {
         DataTableSpec[] specs = new DataTableSpec[tables.length];
         int rowCount = 0;
@@ -197,18 +208,18 @@ public final class ConcatenateTable implements KnowsRowCountTable {
             for (DataRow row : tables[i]) {
                 RowKey key = row.getKey();
                 if (!hash.add(key)) {
-                    throw new IllegalArgumentException("Duplicate row key \"" 
+                    throw new IllegalArgumentException("Duplicate row key \""
                             + key + "\" in table with index " + i);
                 }
                 r++;
-                mon.setProgress(r / (double)rowCount, "Checking tables, row " 
+                mon.setProgress(r / (double)rowCount, "Checking tables, row "
                         + r + "/" + rowCount + " (\"" + row.getKey() + "\")");
             }
             mon.checkCanceled();
         }
         return new ConcatenateTable(tables, finalSpec, rowCount);
     }
-    
+
     /** Creates merged table spec.
      * @param specs the argument tables.
      * @return the new spec
@@ -217,19 +228,19 @@ public final class ConcatenateTable implements KnowsRowCountTable {
     public static DataTableSpec createSpec(final DataTableSpec... specs) {
         return DataTableSpec.mergeDataTableSpecs(specs);
     }
-    
+
     private class MyIterator extends CloseableRowIterator {
         private int m_tableIndex;
         private CloseableRowIterator m_curIterator;
         private DataRow m_next;
-        
+
         /** Creates new iterator. */
         public MyIterator() {
             m_tableIndex = 0;
             m_curIterator = m_tables[m_tableIndex].iterator();
             m_next = internalNext();
         }
-        
+
         /**
          * {@inheritDoc}
          */
@@ -237,7 +248,7 @@ public final class ConcatenateTable implements KnowsRowCountTable {
         public boolean hasNext() {
             return m_next != null;
         }
-        
+
         /**
          * {@inheritDoc}
          */
@@ -247,26 +258,26 @@ public final class ConcatenateTable implements KnowsRowCountTable {
             m_next = internalNext();
             return next;
         }
-        
+
         private DataRow internalNext() {
             if (m_curIterator.hasNext()) {
                 return m_curIterator.next();
-            } 
+            }
             if (m_tableIndex < m_tables.length - 1) {
                 m_tableIndex++;
                 m_curIterator = m_tables[m_tableIndex].iterator();
                 return internalNext();
-            } 
+            }
             return null;
         }
-        
+
         /** {@inheritDoc} */
         @Override
         public void close() {
             m_curIterator.close();
             m_tableIndex = m_tables.length;
         }
-        
+
     }
 
 }
