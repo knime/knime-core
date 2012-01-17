8a2a7b3d7a9446b32e2e3dbf1ee681df972b4235
Thorsten Meinl <thorsten.meinl@knime.com>
2012-01-17 09:37:57 +0000

    added intercept value to coefficients view

--- org.knime.base/src/org/knime/base/node/mine/regression/polynomial/learner/CoefficientTable.java
+++ org.knime.base/src/org/knime/base/node/mine/regression/polynomial/learner/CoefficientTable.java
@@ -43,7 +43,7 @@
  *  propagated with or for interoperation with KNIME.  The owner of a Node
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
- * ------------------------------------------------------------------- * 
+ * ------------------------------------------------------------------- *
  */
 package org.knime.base.node.mine.regression.polynomial.learner;
 
@@ -60,23 +60,25 @@ import javax.swing.table.AbstractTableModel;
 /**
  * This is the view that shows the coefficients in a table and the squared
  * error per row in a line below the table.
- * 
+ *
  * @author Thorsten Meinl, University of Konstanz
  */
-public class CoefficientTable extends JPanel {    
-    private PolyRegLearnerNodeModel m_model;    
+public class CoefficientTable extends JPanel {
+    private PolyRegLearnerNodeModel m_model;
     private static final NumberFormat FORMATTER = new DecimalFormat("0.0000");
     private final AbstractTableModel m_tableModel = new AbstractTableModel() {
         /**
          * {@inheritDoc}
          */
+        @Override
         public int getColumnCount() {
-            return 1 + m_model.getDegree();
+            return 2 + m_model.getDegree();
         }
 
         /**
          * {@inheritDoc}
          */
+        @Override
         public int getRowCount() {
             if (m_model == null) { return 0; }
             if (m_model.getColumnNames() == null) { return 0; }
@@ -86,52 +88,55 @@ public class CoefficientTable extends JPanel {
         /**
          * {@inheritDoc}
          */
+        @Override
         public Object getValueAt(final int rowIndex, final int columnIndex) {
             if (columnIndex == 0) {
                 return m_model.getColumnNames()[rowIndex];
             } else  {
                 return FORMATTER.format(m_model.getBetas()
-                        [rowIndex * m_model.getDegree() + columnIndex]);
+                        [rowIndex * m_model.getDegree() + (columnIndex - 1)]);
             }
         }
 
         @Override
         public String getColumnName(final int column) {
-            if (column == 0) {                
+            if (column == 0) {
                 return "Attribute name";
-            } else {                
-                return "Coefficient (x^" + column + ")";
+            } else if (column == 1) {
+                return "Intercept";
+            } else {
+                return "Coefficient (x^" + (column - 1) + ")";
             }
         }
     };
-    
+
     private final JTable m_table;
     private final JLabel m_squaredError = new JLabel();
-    
+
 
     /**
      * Creates a new coefficient table.
-     * 
+     *
      * @param nodeModel the node model
      */
     public CoefficientTable(final PolyRegLearnerNodeModel nodeModel) {
         super(new BorderLayout());
         m_model = nodeModel;
         m_table = new JTable(m_tableModel);
-        
+
         m_table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        add(new JScrollPane(m_table), BorderLayout.CENTER);        
-        add(m_squaredError, BorderLayout.SOUTH);        
+        add(new JScrollPane(m_table), BorderLayout.CENTER);
+        add(m_squaredError, BorderLayout.SOUTH);
     }
-    
+
     /**
      * Updates the table.
      */
     public void update() {
         m_tableModel.fireTableDataChanged();
-        m_squaredError.setText("  Squared error (per row):  " 
+        m_squaredError.setText("  Squared error (per row):  "
                 + m_model.getSquaredError());
-        
+
         for (int i = 0; i < m_tableModel.getColumnCount(); i++) {
             m_table.getTableHeader().getColumnModel().getColumn(i)
                 .setMinWidth(110);
