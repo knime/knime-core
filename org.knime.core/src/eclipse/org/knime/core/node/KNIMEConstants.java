d9849e0c8c15190a0edf77fcd283605a7757c244
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-01-16 09:54:54 +0000

    updated version numbers and release date for 2.7.1 release

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -278,7 +278,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "January 15, 2013";
+        BUILD_DATE = "January 16, 2013";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle(KNIMEConstants.class);
         if (coreBundle != null) {
