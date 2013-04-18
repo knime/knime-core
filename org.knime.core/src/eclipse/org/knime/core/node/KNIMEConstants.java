f0bf9fdf28c75df9ef49c108f1c6d32d27e1949f
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-04-18 08:37:07 +0000

    setting version number to 2.7.4

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -282,7 +282,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "March 28, 2013";
+        BUILD_DATE = "April 19, 2013";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle(KNIMEConstants.class);
         if (coreBundle != null) {
