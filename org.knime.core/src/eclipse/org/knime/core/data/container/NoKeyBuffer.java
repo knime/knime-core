c1197971a74e88a7c13ad4403c09cf2499649a85
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-05-22 19:25:56 +0000

    also NoKeyBuffer class saves compression information

--- org.knime.core/src/eclipse/org/knime/core/data/container/NoKeyBuffer.java
+++ org.knime.core/src/eclipse/org/knime/core/data/container/NoKeyBuffer.java
@@ -77,10 +77,10 @@ class NoKeyBuffer extends Buffer {
         NodeLogger.getLogger(NoKeyBuffer.class);
 
     /** Current version string. */
-    private static final String VERSION = "noRowKeyContainer_8";
+    private static final String VERSION = "noRowKeyContainer_9";
 
     /** The version number corresponding to VERSION. */
-    private static final int IVERSION = 8;
+    private static final int IVERSION = 9;
 
     private static final HashMap<String, Integer> COMPATIBILITY_MAP;
 
@@ -94,6 +94,7 @@ class NoKeyBuffer extends Buffer {
         COMPATIBILITY_MAP.put("noRowKeyContainer_5", 5);
         COMPATIBILITY_MAP.put("noRowKeyContainer_6", 6);
         COMPATIBILITY_MAP.put("noRowKeyContainer_7", 7);
+        COMPATIBILITY_MAP.put("noRowKeyContainer_8", 8);
         COMPATIBILITY_MAP.put(VERSION, IVERSION);
     }
 
