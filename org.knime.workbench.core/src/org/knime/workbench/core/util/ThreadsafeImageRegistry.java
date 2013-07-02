b43318442c1211a57bdce58cd3cdb38d854dcac2
Thorsten Meinl <thorsten.meinl@knime.com>
2013-07-02 11:23:53 +0000

    fixed deadlock during startup; there is no other way than mostly copying the original class and using a thread-safe map

--- org.knime.workbench.core/src/org/knime/workbench/core/util/ThreadsafeImageRegistry.java
+++ org.knime.workbench.core/src/org/knime/workbench/core/util/ThreadsafeImageRegistry.java
@@ -1,160 +1,369 @@
-/*
- * ------------------------------------------------------------------------
+/*******************************************************************************
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * All rights reserved. This file and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- *  Copyright (C) 2003 - 2013
- *  University of Konstanz, Germany and
- *  KNIME GmbH, Konstanz, Germany
- *  Website: http://www.knime.org; Email: contact@knime.org
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License, Version 3, as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>.
- *
- *  Additional permission under GNU GPL version 3 section 7:
- *
- *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
- *  Hence, KNIME and ECLIPSE are both independent programs and are not
- *  derived from each other. Should, however, the interpretation of the
- *  GNU GPL Version 3 ("License") under any applicable laws result in
- *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
- *  you the additional permission to use and propagate KNIME together with
- *  ECLIPSE with only the license terms in place for ECLIPSE applying to
- *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
- *  license terms of ECLIPSE themselves allow for the respective use and
- *  propagation of ECLIPSE together with KNIME.
- *
- *  Additional permission relating to nodes for KNIME that extend the Node
- *  Extension (and in particular that are based on subclasses of NodeModel,
- *  NodeDialog, and NodeView) and that only interoperate with KNIME through
- *  standard APIs ("Nodes"):
- *  Nodes are deemed to be separate and independent programs and to not be
- *  covered works.  Notwithstanding anything to the contrary in the
- *  License, the License does not apply to Nodes, you are not required to
- *  license Nodes under the License, and you are granted a license to
- *  prepare and propagate Nodes, in each case even if such Nodes are
- *  propagated with or for interoperation with KNIME.  The owner of a Node
- *  may freely choose the license terms applicable to such Node, including
- *  when such Node is propagated with or for interoperation with KNIME.
- * ---------------------------------------------------------------------
- *
- * Created on 19.06.2013 by thor
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Steven Ketcham (sketcham@dsicdi.com) - Bug 42451
+ *     [Dialogs] ImageRegistry throws null pointer exception in
+ *     application with multiple Display's
+ *     Thorsten Meinl - make it thread safe
+ *******************************************************************************/
 package org.knime.workbench.core.util;
 
-import java.util.HashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.DeviceResourceException;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
 
 /**
- * Thread-safe implementation of {@link ImageRegistry}. This is necessary if an {@link ImageRegistry} is access from
- * different threads at the same time because the original ImageRegistry contains an unsynchronized {@link HashMap}.
- *
- * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
- * @since 2.8
+ * An image registry maintains a mapping between symbolic image names
+ * and SWT image objects or special image descriptor objects which
+ * defer the creation of SWT image objects until they are needed.
+ * <p>
+ * An image registry owns all of the image objects registered
+ * with it, and automatically disposes of them when the SWT Display
+ * that creates the images is disposed. Because of this, clients do not
+ * need to (indeed, must not attempt to) dispose of these images themselves.
+ * </p>
+ * <p>
+ * Clients may instantiate this class (it was not designed to be subclassed).
+ * </p>
+ * <p>
+ * Unlike the FontRegistry, it is an error to replace images. As a result
+ * there are no events that fire when values are changed in the registry
+ * </p>
+ * @noextend This class is not intended to be subclassed by clients.
  */
+@SuppressWarnings("hiding")
 public class ThreadsafeImageRegistry extends ImageRegistry {
-    private final ReadWriteLock m_rwLock = new ReentrantReadWriteLock();
+    /**
+     * display used when getting images
+     */
+    private Display display;
 
-    private final Lock m_readLock = m_rwLock.readLock();
+    private ResourceManager manager;
 
-    private final Lock m_writeLock = m_rwLock.writeLock();
+    private Map<String, Entry> table;
+
+    private Runnable disposeRunnable = new Runnable() {
+        @Override
+        public void run() {
+            dispose();
+        }
+    };
+
+    /**
+     * Contains the data for an entry in the registry.
+     */
+    private static class Entry {
+    	/** the image */
+        protected Image image;
+
+        /** the descriptor */
+        protected ImageDescriptor descriptor;
+    }
+
+    private static class OriginalImageDescriptor extends ImageDescriptor {
+        private Image original;
+        private int refCount = 0;
+        private Device originalDisplay;
+
+        /**
+         * @param original the original image
+         * @param originalDisplay the device the image is part of
+         */
+        public OriginalImageDescriptor(final Image original, final Device originalDisplay) {
+            this.original = original;
+            this.originalDisplay = originalDisplay;
+        }
+
+        @Override
+        public Object createResource(final Device device) throws DeviceResourceException {
+            if (device == originalDisplay) {
+                refCount++;
+                return original;
+            }
+            return super.createResource(device);
+        }
+
+        @Override
+        public void destroyResource(final Object toDispose) {
+            if (original == toDispose) {
+                refCount--;
+                if (refCount == 0) {
+                    original.dispose();
+                    original = null;
+                }
+            } else {
+                super.destroyResource(toDispose);
+            }
+        }
+
+        /* (non-Javadoc)
+         * @see org.eclipse.jface.resource.ImageDescriptor#getImageData()
+         */
+        @Override
+        public ImageData getImageData() {
+            return original.getImageData();
+        }
+    }
 
     /**
      * Creates an empty image registry.
      * <p>
-     * There must be an SWT Display created in the current thread before calling this method.
+     * There must be an SWT Display created in the current
+     * thread before calling this method.
      * </p>
      */
     public ThreadsafeImageRegistry() {
-        super();
+        this(Display.getCurrent());
+    }
+
+    /**
+     * Creates an empty image registry using the given resource manager to allocate images.
+     *
+     * @param manager the resource manager used to allocate images
+     *
+     * @since 3.1
+     */
+    public ThreadsafeImageRegistry(final ResourceManager manager) {
+        Assert.isNotNull(manager);
+        Device dev = manager.getDevice();
+        if (dev instanceof Display) {
+            this.display = (Display)dev;
+        }
+        this.manager = manager;
+        manager.disposeExec(disposeRunnable);
     }
 
     /**
      * Creates an empty image registry.
      *
-     * @param display this <code>Display</code> must not be <code>null</code> and must not be disposed in order to use
-     *            this registry
+     * @param display this <code>Display</code> must not be
+     *        <code>null</code> and must not be disposed in order
+     *        to use this registry
      */
     public ThreadsafeImageRegistry(final Display display) {
-        super(display);
+        this(JFaceResources.getResources(display));
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the image associated with the given key in this registry,
+     * or <code>null</code> if none.
+     *
+     * @param key the key
+     * @return the image, or <code>null</code> if none
      */
     @Override
+    @SuppressWarnings("deprecation")
     public Image get(final String key) {
-        m_readLock.lock();
-        try {
-            return super.get(key);
-        } finally {
-            m_readLock.unlock();
+
+        // can be null
+        if (key == null) {
+            return null;
         }
+
+        if (display != null) {
+            /**
+             * NOTE, for backwards compatibility the following images are supported
+             * here, they should never be disposed, hence we explicitly return them
+             * rather then registering images that SWT will dispose.
+             *
+             * Applications should go direclty to SWT for these icons.
+             *
+             * @see Display.getSystemIcon(int ID)
+             */
+            int swtKey = -1;
+            if (key.equals(Dialog.DLG_IMG_INFO)) {
+                swtKey = SWT.ICON_INFORMATION;
+            }
+            if (key.equals(Dialog.DLG_IMG_QUESTION)) {
+                swtKey = SWT.ICON_QUESTION;
+            }
+            if (key.equals(Dialog.DLG_IMG_WARNING)) {
+                swtKey = SWT.ICON_WARNING;
+            }
+            if (key.equals(Dialog.DLG_IMG_ERROR)) {
+                swtKey = SWT.ICON_ERROR;
+            }
+            // if we actually just want to return an SWT image do so without
+            // looking in the registry
+            if (swtKey != -1) {
+                final Image[] image = new Image[1];
+                final int id = swtKey;
+                display.syncExec(new Runnable() {
+                    @Override
+                    public void run() {
+                        image[0] = display.getSystemImage(id);
+                    }
+                });
+                return image[0];
+            }
+        }
+
+        Entry entry = getEntry(key);
+        if (entry == null) {
+            return null;
+        }
+
+        if (entry.image == null) {
+            entry.image = manager.createImageWithDefault(entry.descriptor);
+        }
+
+        return entry.image;
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the descriptor associated with the given key in this registry,
+     * or <code>null</code> if none.
+     *
+     * @param key the key
+     * @return the descriptor, or <code>null</code> if none
+     * @since 2.1
      */
     @Override
     public ImageDescriptor getDescriptor(final String key) {
-        m_readLock.lock();
-        try {
-            return super.getDescriptor(key);
-        } finally {
-            m_readLock.unlock();
+        Entry entry = getEntry(key);
+        if (entry == null) {
+            return null;
         }
+
+        return entry.descriptor;
     }
 
     /**
-     * {@inheritDoc}
+     * Adds (or replaces) an image descriptor to this registry. The first time
+     * this new entry is retrieved, the image descriptor's image will be computed
+     * (via </code>ImageDescriptor.createImage</code>) and remembered.
+     * This method replaces an existing image descriptor associated with the
+     * given key, but fails if there is a real image associated with it.
+     *
+     * @param key the key
+     * @param descriptor the ImageDescriptor
+     * @exception IllegalArgumentException if the key already exists
      */
     @Override
     public void put(final String key, final ImageDescriptor descriptor) {
-        m_writeLock.lock();
-        try {
-            super.put(key, descriptor);
-        } finally {
-            m_writeLock.unlock();
+        Entry entry = getEntry(key);
+        if (entry == null) {
+            entry = new Entry();
+            getTable().put(key, entry);
         }
+
+        if (entry.image != null) {
+            throw new IllegalArgumentException(
+                    "ImageRegistry key already in use: " + key); //$NON-NLS-1$
+        }
+
+        entry.descriptor = descriptor;
     }
 
     /**
-     * {@inheritDoc}
+     * Adds an image to this registry.  This method fails if there
+     * is already an image or descriptor for the given key.
+     * <p>
+     * Note that an image registry owns all of the image objects registered
+     * with it, and automatically disposes of them when the SWT Display is disposed.
+     * Because of this, clients must not register an image object
+     * that is managed by another object.
+     * </p>
+     *
+     * @param key the key
+     * @param image the image, should not be <code>null</code>
+     * @exception IllegalArgumentException if the key already exists
      */
     @Override
     public void put(final String key, final Image image) {
-        m_writeLock.lock();
+        Entry entry = getEntry(key);
+
+        if (entry == null) {
+            entry = new Entry();
+            putEntry(key, entry);
+        }
+
+        if (entry.image != null || entry.descriptor != null) {
+            throw new IllegalArgumentException(
+                    "ImageRegistry key already in use: " + key); //$NON-NLS-1$
+        }
+
+        // Check for a null image here, otherwise the problem won't appear
+        // until dispose.
+        // See https://bugs.eclipse.org/bugs/show_bug.cgi?id=130315
+        Assert.isNotNull(image, "Cannot register a null image."); //$NON-NLS-1$
+        entry.image = image;
+        entry.descriptor = new OriginalImageDescriptor(image, manager.getDevice());
+
         try {
-            super.put(key, image);
-        } finally {
-            m_writeLock.unlock();
+            manager.create(entry.descriptor);
+        } catch (DeviceResourceException e) {
         }
     }
 
     /**
-     * {@inheritDoc}
+     * Removes an image from this registry.
+     * If an SWT image was allocated, it is disposed.
+     * This method has no effect if there is no image or descriptor for the given key.
+     * @param key the key
      */
     @Override
     public void remove(final String key) {
-        m_writeLock.lock();
-        try {
-            super.remove(key);
-        } finally {
-            m_writeLock.unlock();
+        ImageDescriptor descriptor = getDescriptor(key);
+        if (descriptor != null) {
+            manager.destroy(descriptor);
+            getTable().remove(key);
         }
+    }
+
+    private Entry getEntry(final String key) {
+        return getTable().get(key);
+    }
+
+    private void putEntry(final String key, final Entry entry) {
+        getTable().put(key, entry);
+    }
+
+    private Map<String, Entry> getTable() {
+        if (table == null) {
+            table = new ConcurrentHashMap<String, Entry>(16);
+        }
+        return table;
+    }
+
+    /**
+     * Disposes this image registry, disposing any images
+     * that were allocated for it, and clearing its entries.
+     *
+     * @since 3.1
+     */
+    @Override
+    public void dispose() {
+        manager.cancelDisposeExec(disposeRunnable);
+
+        if (table != null) {
+            for (Iterator<Entry> i = table.values().iterator(); i.hasNext();) {
+                Entry entry = i.next();
+                if (entry.image != null) {
+                    manager.destroyImage(entry.descriptor);
+                }
+            }
+            table = null;
+        }
+        display = null;
     }
 }
