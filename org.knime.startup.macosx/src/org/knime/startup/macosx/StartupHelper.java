18691a2a7eac9fce02a2e98508c2249687fd0f1d
Thorsten Meinl <thorsten.meinl@knime.com>
2013-08-06 12:24:59 +0000

    FIXED - bug 4396: KNIME SDK freezes under Mac when opening the update manager http://bimbug.inf.uni-konstanz.de/show_bug.cgi?id=4396

---
+++ org.knime.startup.macosx/src/org/knime/startup/macosx/StartupHelper.java
@@ -0,0 +1,85 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2013
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   06.08.2013 (thor): created
+ */
+package org.knime.startup.macosx;
+
+import javax.swing.SwingUtilities;
+
+import org.eclipse.ui.IStartup;
+
+/**
+ * This class is automatically loaded during Eclipse startup.
+ *
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
+ */
+public class StartupHelper implements IStartup {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void earlyStartup() {
+        if (Boolean.getBoolean("java.awt.headless")) {
+            return;
+        }
+        
+        // This line forces loading AWT libraries. See bug #4396 why this is necessary under MacOS X.
+        // This method is supposed to be called in a thread that is not the main thread, but just to be sure...
+        if ("main".equals(Thread.currentThread().getName())) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    SwingUtilities.isEventDispatchThread();
+                }
+            });
+        } else {
+            SwingUtilities.isEventDispatchThread();
+        }
+    }
+}
