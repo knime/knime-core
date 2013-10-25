adc40bd10c66c5799d1429d0bfd736c797226a90
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-10-25 12:41:06 +0000

    moving domain editor nodes to base (bugs 4440, 4441, 4442)

---
+++ org.knime.base/src/org/knime/base/node/preproc/domain/editnominal/EditNominalDomainNodeModel.java
@@ -0,0 +1,270 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2013
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * Created on Mar 17, 2013 by wiswedel
+ */
+package org.knime.base.node.preproc.domain.editnominal;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnDomain;
+import org.knime.core.data.DataColumnDomainCreator;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataTableSpecCreator;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.streamable.InputPortRole;
+import org.knime.core.node.streamable.OutputPortRole;
+import org.knime.core.node.util.ConvenienceMethods;
+
+/**
+ * Enables the manual managing of possible domain values.
+ *
+ * @author Marcel Hanser
+ */
+final class EditNominalDomainNodeModel extends NodeModel {
+
+    /**
+     *
+     */
+    private static final int MAX_WRITTEN_COLS = 5;
+
+    private static final NodeLogger LOGGER = NodeLogger.getLogger(EditNominalDomainNodeModel.class);
+
+    private EditNominalDomainConfiguration m_configuration;
+
+    /** One in, one out. */
+    EditNominalDomainNodeModel() {
+        super(1, 1);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs) throws InvalidSettingsException {
+        return new DataTableSpec[]{sortPossibleValues(inSpecs[0])};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InputPortRole[] getInputPortRoles() {
+        return new InputPortRole[]{InputPortRole.NONDISTRIBUTED_STREAMABLE};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public OutputPortRole[] getOutputPortRoles() {
+        return new OutputPortRole[]{OutputPortRole.NONDISTRIBUTED};
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec)
+        throws Exception {
+        return new BufferedDataTable[]{exec.createSpecReplacerTable(inData[0],
+            sortPossibleValues(sortPossibleValues(inData[0].getDataTableSpec())))};
+    }
+
+    private DataTableSpec sortPossibleValues(final DataTableSpec orgSpec) throws InvalidSettingsException {
+
+        if (m_configuration == null) {
+            throw new InvalidSettingsException("Missing Configuration.");
+        }
+
+        Set<String> configuredColumns = new HashSet<String>(m_configuration.getConfiguredColumns());
+        String[] columnNames = orgSpec.getColumnNames();
+
+        DataTableSpecCreator creator = new DataTableSpecCreator(orgSpec).dropAllColumns();
+
+        for (int i = 0; i < orgSpec.getNumColumns(); i++) {
+            String name = columnNames[i];
+            if (configuredColumns.remove(name)) {
+                DataColumnSpec orgDataSpec = orgSpec.getColumnSpec(i);
+                if (!StringCell.TYPE.equals(orgDataSpec.getType())) {
+
+                    EditNominalDomainConfiguration.checkSetting(m_configuration.isIgnoreWrongTypes(),
+                        "Column '%s' must be of type '%s' \nbut was of type: '%s'", name, StringCell.TYPE,
+                        orgDataSpec.getType());
+
+                    creator.addColumns(orgDataSpec);
+
+                } else {
+
+                    DataColumnDomain domain = orgDataSpec.getDomain();
+                    DataColumnSpecCreator dataColumnSpecCreator = new DataColumnSpecCreator(orgDataSpec);
+                    DataColumnDomainCreator yetAnotherCreator =
+                        new DataColumnDomainCreator(domain.getLowerBound(), domain.getUpperBound());
+
+                    List<DataCell> sorting = new ArrayList<DataCell>(m_configuration.getSorting(name));
+
+                    Set<DataCell> difference = diff(domain.getValues(), sorting);
+
+                    yetAnotherCreator.setValues(resolveNewValues(sorting, difference));
+
+                    dataColumnSpecCreator.setDomain(yetAnotherCreator.createDomain());
+                    creator.addColumns(dataColumnSpecCreator.createSpec());
+                }
+            } else {
+                creator.addColumns(orgSpec.getColumnSpec(i));
+            }
+        }
+
+        if (!configuredColumns.isEmpty()) {
+            EditNominalDomainConfiguration.checkSetting(m_configuration.isIgnoreNotExistingColumns(),
+                "Following columns do not longer exist in input table: \n%s",
+                ConvenienceMethods.getShortStringFrom(configuredColumns, MAX_WRITTEN_COLS));
+        }
+
+        return creator.createSpec();
+    }
+
+    /**
+     * @param sorting
+     * @param difference
+     * @return
+     */
+    private Set<DataCell> resolveNewValues(final List<DataCell> sorting, final Set<DataCell> difference) {
+        int lastIndexOf = sorting.lastIndexOf(EditNominalDomainConfiguration.UNKNOWN_VALUES_CELL);
+
+        if (lastIndexOf == -1) {
+            LOGGER.assertLog(false, "Unkown cell placeholder is not included!");
+            return new LinkedHashSet<DataCell>(sorting);
+        }
+
+        sorting.remove(lastIndexOf);
+        sorting.addAll(lastIndexOf, difference);
+
+        return new LinkedHashSet<DataCell>(sorting);
+    }
+
+    /**
+     * Returns the relative complement set of the given collections as an immutable set. In other words it computes
+     * <code>a\b</code>.
+     *
+     * @param a the a
+     * @param b the b
+     * @return returns <code>a\b</code>.
+     */
+    static Set<DataCell> diff(final Collection<DataCell> a, final Collection<DataCell> b) {
+        if (a == null || a.isEmpty()) {
+            return Collections.emptySet();
+        }
+        Set<DataCell> toReturn = new LinkedHashSet<DataCell>();
+
+        for (DataCell val : a) {
+            if (!b.contains(val)) {
+                toReturn.add(val);
+            }
+        }
+        return Collections.unmodifiableSet(toReturn);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void reset() {
+        // no internals
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        if (m_configuration != null) {
+            m_configuration.saveSettings(settings);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        EditNominalDomainConfiguration editNumericDomainConfiguration = new EditNominalDomainConfiguration();
+        editNumericDomainConfiguration.loadConfigurationInModel(settings);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+        EditNominalDomainConfiguration editNumericDomainConfiguration = new EditNominalDomainConfiguration();
+        editNumericDomainConfiguration.loadConfigurationInModel(settings);
+        m_configuration = editNumericDomainConfiguration;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void loadInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException,
+        CanceledExecutionException {
+        // no internals
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void saveInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException,
+        CanceledExecutionException {
+        // no internals
+    }
+}
---
+++ org.knime.base/src/org/knime/base/node/preproc/domain/editnominal/dic/EditNominalDomainDicConfiguration.java
@@ -0,0 +1,234 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2013
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * Created on Mar 17, 2013 by wiswedel
+ */
+package org.knime.base.node.preproc.domain.editnominal.dic;
+
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.NominalValue;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.util.filter.InputFilter;
+import org.knime.core.node.util.filter.column.DataColumnSpecFilterConfiguration;
+import org.knime.core.node.util.filter.column.DataTypeColumnFilter;
+
+/**
+ * Config proxy of node.
+ *
+ * @author Marcel Hanser
+ */
+final class EditNominalDomainDicConfiguration extends DataColumnSpecFilterConfiguration {
+
+    /**
+     *
+     */
+    private static final int DEFAULT_MAX_DOMAIN_VALUES = 1000;
+
+    private static final String IGNORE_NOT_PRESENT_COLS = "ignore-not-present-col";
+
+    private static final String IGNORE_NOT_MATHING_TYPES = "ignore-not-matching-types";
+
+    private static final String NEW_DOMAIN_VALUES_FIRST = "new-domain-values-first";
+
+    private static final String MAX_DOMAIN_VALUES = "maximal-domain-values";
+
+    private static final String DATA_COLUMN_FILTER_SPEC_KEY = "datacolfilter";
+
+    private boolean m_ignoreDomainColumns = false;
+
+    private boolean m_ignoreWrongTypes = false;
+
+    private boolean m_addNewValuesFirst = true;
+
+    private int m_maxDomainValues = 1000;
+
+    /**
+     * Constructor.
+     */
+    @SuppressWarnings("unchecked")
+    EditNominalDomainDicConfiguration() {
+        super(DATA_COLUMN_FILTER_SPEC_KEY, new DataTypeColumnFilter(NominalValue.class));
+    }
+
+    /**
+     * Constructor. Creating default values.
+     *
+     * @param orgSpec the table spec of the original table
+     * @param valueSpec the table spec of the additional values
+     */
+    EditNominalDomainDicConfiguration(final DataTableSpec orgSpec, final DataTableSpec valueSpec) {
+        super(DATA_COLUMN_FILTER_SPEC_KEY, createDefaultSpecFilterConfig(orgSpec));
+        loadDefaults(valueSpec, true);
+        setEnforceOption(EnforceOption.EnforceInclusion);
+    }
+
+    /**
+     * Loads the configuration for the dialog with corresponding default values.
+     *
+     * @param settings the settings to load
+     * @param valueSpec the table specification
+     */
+    @Override
+    public void loadConfigurationInDialog(final NodeSettingsRO settings, final DataTableSpec valueSpec) {
+
+        m_ignoreDomainColumns = settings.getBoolean(IGNORE_NOT_PRESENT_COLS, false);
+        m_ignoreWrongTypes = settings.getBoolean(IGNORE_NOT_MATHING_TYPES, false);
+        m_addNewValuesFirst = settings.getBoolean(NEW_DOMAIN_VALUES_FIRST, true);
+        m_maxDomainValues = settings.getInt(MAX_DOMAIN_VALUES, DEFAULT_MAX_DOMAIN_VALUES);
+
+        loadDefaults(valueSpec, true);
+        super.loadConfigurationInDialog(settings, valueSpec);
+
+        setEnforceOption(EnforceOption.EnforceInclusion);
+    }
+
+    /**
+     * Loads the configuration for the model.
+     *
+     * @param settings the settings to load
+     * @throws InvalidSettingsException if the settings are invalid
+     */
+    @Override
+    public void loadConfigurationInModel(final NodeSettingsRO settings) throws InvalidSettingsException {
+
+        m_ignoreDomainColumns = settings.getBoolean(IGNORE_NOT_PRESENT_COLS);
+        m_ignoreWrongTypes = settings.getBoolean(IGNORE_NOT_MATHING_TYPES);
+        m_addNewValuesFirst = settings.getBoolean(NEW_DOMAIN_VALUES_FIRST);
+        m_maxDomainValues = settings.getInt(MAX_DOMAIN_VALUES);
+
+        super.loadConfigurationInModel(settings);
+    }
+
+    /**
+     * Called from dialog's and model's save method.
+     *
+     * @param settings Arg settings.
+     */
+    void saveSettings(final NodeSettingsWO settings) {
+        settings.addBoolean(IGNORE_NOT_PRESENT_COLS, m_ignoreDomainColumns);
+        settings.addBoolean(IGNORE_NOT_MATHING_TYPES, m_ignoreWrongTypes);
+        settings.addBoolean(NEW_DOMAIN_VALUES_FIRST, m_addNewValuesFirst);
+        settings.addInt(MAX_DOMAIN_VALUES, m_maxDomainValues);
+        saveConfiguration(settings);
+
+    }
+
+    /**
+     * @param orgSpec
+     * @return
+     */
+    private static InputFilter<DataColumnSpec> createDefaultSpecFilterConfig(final DataTableSpec orgSpec) {
+        return new InputFilter<DataColumnSpec>() {
+            @Override
+            public boolean include(final DataColumnSpec valueColumnSpec) {
+                DataColumnSpec columnSpec = orgSpec.getColumnSpec(valueColumnSpec.getName());
+                return columnSpec == null ? false : columnSpec.getType().equals(valueColumnSpec.getType())
+                    && valueColumnSpec.getType().isCompatible(NominalValue.class);
+            }
+        };
+    }
+
+    /**
+     * @return the ignoreDomainColumns
+     */
+    boolean isIgnoreDomainColumns() {
+        return m_ignoreDomainColumns;
+    }
+
+    /**
+     * @return the ignoreWrongTypes
+     */
+    boolean isIgnoreWrongTypes() {
+        return m_ignoreWrongTypes;
+    }
+
+    /**
+     * @return the newValuesFirst
+     */
+    boolean isAddNewValuesFirst() {
+        return m_addNewValuesFirst;
+    }
+
+    /**
+     * @return the maxDomainValues
+     */
+    public int getMaxDomainValues() {
+        return m_maxDomainValues;
+    }
+
+    /**
+     * @param newValuesFirst the newValuesFirst to set
+     */
+    void setNewValuesFirst(final boolean newValuesFirst) {
+        m_addNewValuesFirst = newValuesFirst;
+    }
+
+    /**
+     * @param ignoreDomainColumns the ignoreDomainColumns to set
+     */
+    void setIgnoreDomainColumns(final boolean ignoreDomainColumns) {
+        m_ignoreDomainColumns = ignoreDomainColumns;
+    }
+
+    /**
+     * @param ignoreWrongTypes the ignoreWrongTypes to set
+     */
+    void setIgnoreWrongTypes(final boolean ignoreWrongTypes) {
+        m_ignoreWrongTypes = ignoreWrongTypes;
+    }
+
+    /**
+     * @param maxDomainValues the maxDomainValues to set
+     */
+    public void setMaxDomainValues(final int maxDomainValues) {
+        m_maxDomainValues = maxDomainValues;
+    }
+
+}
