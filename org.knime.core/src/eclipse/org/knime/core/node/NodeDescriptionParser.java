205fed0fbd1c27dbeaf00abde66283f0a600a45d
Thorsten Meinl <thorsten.meinl@knime.com>
2014-03-21 13:13:33 +0000

    added missing resolution of v2.10 Node XSD

--- org.knime.core/src/eclipse/org/knime/core/node/NodeDescriptionParser.java
+++ org.knime.core/src/eclipse/org/knime/core/node/NodeDescriptionParser.java
@@ -166,6 +166,9 @@ class NodeDescriptionParser {
                 throw new XmlException("Unsupported document type for node description of " + factoryClass.getName()
                     + ": " + publicId);
             }
+        } else if (namespaceUri.equals(org.knime.node.v210.KnimeNodeDocument.type.getContentModel().getName()
+            .getNamespaceURI())) {
+            return new NodeDescription210Proxy(doc);
         } else if (namespaceUri.equals(org.knime.node.v28.KnimeNodeDocument.type.getContentModel().getName()
             .getNamespaceURI())) {
             return new NodeDescription28Proxy(doc);
