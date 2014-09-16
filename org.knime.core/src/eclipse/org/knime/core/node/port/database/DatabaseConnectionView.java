6cd049e2ce119c2aa2e75591819ab7b4b1c434f0
Tobias Kötter <tobias.koetter@knime.com>
2014-09-16 14:35:19 +0000

    JPanel changed to JTextPane to allow the user to copy content as well as database identifier information and button to copy sql to clip board added

--- org.knime.core/src/eclipse/org/knime/core/node/port/database/DatabaseConnectionView.java
+++ org.knime.core/src/eclipse/org/knime/core/node/port/database/DatabaseConnectionView.java
@@ -47,23 +47,37 @@
  */
 package org.knime.core.node.port.database;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.knime.core.node.ModelContentRO;
 
+/**
+ * The database connection port view.
+ */
 @SuppressWarnings("serial")
 final class DatabaseConnectionView extends JPanel {
+    /**
+     * @param sett the {@link ModelContentRO} with the settings
+     */
     DatabaseConnectionView(final ModelContentRO sett) {
-        super(new BorderLayout());
+        super(new GridBagLayout());
         super.setName("Connection");
         StringBuilder buf = new StringBuilder("<html><body>");
         buf.append("<h2>Database Connection</h2>");
-        buf.append("<br/>");
         buf.append("<strong>Database Driver:</strong><br/>");
         buf.append("<tt>" + sett.getString("driver", "") + "</tt>");
         buf.append("<br/><br/>");
@@ -79,16 +93,59 @@ final class DatabaseConnectionView extends JPanel {
             buf.append("<strong>User Name:</strong><br/>");
             buf.append("<tt>" + sett.getString("user", "") + "</tt>");
         }
-        buf.append("<br/><br/>");
-
-        String sql = sett.getString("statement", null);
+        final String dbIdentifier = sett.getString("databaseIdentifier", null);
+        if (dbIdentifier != null) {
+            buf.append("<br/><br/>");
+            buf.append("<strong>Database Identifier:</strong><br/>");
+            buf.append("<tt>" + dbIdentifier + "</tt>");
+        }
+        final String sql = sett.getString("statement", null);
         if (sql != null) {
+            buf.append("<br/><br/>");
             buf.append("<strong>SQL Statement:</strong><br/>");
             final String query = StringEscapeUtils.escapeHtml4(sql);
             buf.append("<tt>" + query + "</tt>");
         }
         buf.append("</body></html>");
-        final JScrollPane jsp = new JScrollPane(new JLabel(buf.toString()));
-        super.add(jsp, BorderLayout.CENTER);
+        final JTextPane textArea = new JTextPane();
+        textArea.setContentType("text/html");
+        textArea.setEditable(false);
+        textArea.setText(buf.toString());
+        final JScrollPane jsp = new JScrollPane(textArea);
+        jsp.setPreferredSize(new Dimension(300, 300));
+        final GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        super.add(jsp, c);
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        final JButton sqlButton = new JButton("Copy SQL to clipboard");
+        sqlButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    final StringSelection stringSelection = new StringSelection(sql);
+                    final Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+                    clpbrd.setContents(stringSelection, null);
+                } catch (Throwable ex) {
+                    // catch any exception that might occur
+                }
+            }
+        });
+        c.gridy++;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        c.insets = new Insets(5, 0, 5, 5);
+        if (sql != null) {
+            super.add(sqlButton, c);
+        }
     }
 }
