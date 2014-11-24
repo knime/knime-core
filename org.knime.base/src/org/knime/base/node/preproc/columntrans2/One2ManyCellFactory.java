bcfffc9e45294f142e9f353364d0488ac9388add
Tim-Oliver Buchholz <tim-oliver.buchholz@uni-konstanz.de>
2014-11-24 09:23:50 +0000

    removed setElementNames. (default is empty array)

--- org.knime.base/src/org/knime/base/node/preproc/columntrans2/One2ManyCellFactory.java
+++ org.knime.base/src/org/knime/base/node/preproc/columntrans2/One2ManyCellFactory.java
@@ -120,7 +120,6 @@ public class One2ManyCellFactory implements CellFactory {
                     }
                     final DataColumnSpecCreator creator = uniqueNameGenerator.newCreator(newColumnName, IntCell.TYPE);
                     creator.setDomain(domainCreator.createDomain());
-                    creator.setElementNames(new String[]{newColumnName});
                     colSpecs.add(creator.createSpec());
                     linkedCols.add(new Pair<String, String>(newColumnName, value));
 
