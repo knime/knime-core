2b672091034d7cdfa4d221639097132f92038bd5
Thorsten Meinl <thorsten.meinl@knime.com>
2014-12-11 14:42:48 +0000

    FIXED - bug 5858: Updates of KNIME Core disable server and teamspace update sites https://bugs.knime.org/show_bug.cgi?id=5858

--- org.knime.product/src/org/knime/product/p2/RepositoryUpdater.java
+++ org.knime.product/src/org/knime/product/p2/RepositoryUpdater.java
@@ -47,11 +47,20 @@
  */
 package org.knime.product.p2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.EventObject;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.equinox.internal.provisional.p2.core.eventbus.IProvisioningEventBus;
 import org.eclipse.equinox.internal.provisional.p2.core.eventbus.ProvisioningListener;
 import org.eclipse.equinox.internal.provisional.p2.repository.RepositoryEvent;
@@ -59,6 +68,7 @@ import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.repository.IRepository;
 import org.eclipse.equinox.p2.repository.IRepositoryManager;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepositoryManager;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
 import org.knime.core.node.KNIMEConstants;
 import org.knime.core.node.NodeLogger;
 import org.osgi.framework.BundleContext;
@@ -121,6 +131,59 @@ public class RepositoryUpdater implements ProvisioningListener {
     }
 
     /**
+     * Adds default KNIME repositories if they don't already exist.
+     */
+    public void addDefaultRepositories() {
+        BundleContext context = FrameworkUtil.getBundle(getClass()).getBundleContext();
+        ServiceReference<IProvisioningAgent> ref = context.getServiceReference(IProvisioningAgent.class);
+        if (ref != null) {
+            IProvisioningAgent agent = context.getService(ref);
+            try {
+                IMetadataRepositoryManager metadataManager =
+                    (IMetadataRepositoryManager)agent.getService(IMetadataRepositoryManager.SERVICE_NAME);
+                URL usFileUrl = FrameworkUtil.getBundle(getClass()).getEntry("/update-sites.txt");
+
+                if ((metadataManager != null) && (usFileUrl != null)) {
+                    processDefaultRepositories(metadataManager, usFileUrl);
+                }
+            } finally {
+                context.ungetService(ref);
+            }
+        }
+    }
+
+    private void processDefaultRepositories(final IMetadataRepositoryManager repoManager, final URL usFileUrl) {
+        Set<URI> knowRepositories =
+            new HashSet<>(Arrays.asList(repoManager.getKnownRepositories(IRepositoryManager.REPOSITORIES_NON_LOCAL
+                | IRepositoryManager.REPOSITORIES_DISABLED)));
+
+        IEclipsePreferences preferences =
+            InstanceScope.INSTANCE.getNode(FrameworkUtil.getBundle(getClass()).getSymbolicName());
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(usFileUrl.openStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] parts = line.split(",");
+                URI uri = new URI(parts[0]);
+
+                // Only make any modification if this is the first time. We store the information and if the user
+                // has re-added or removed an entry himself, we don't try it a second time
+                if ("remove".equals(parts[1]) && !preferences.getBoolean(uri + "-removed", false)) {
+                    repoManager.removeRepository(uri);
+                    preferences.putBoolean(uri + "-removed", true);
+                } else if ("add".equals(parts[1]) && !knowRepositories.contains(uri)
+                    && !preferences.getBoolean(uri + "-added", false)) {
+                    repoManager.addRepository(uri);
+                    repoManager.setEnabled(uri, (parts.length > 2) && "enable".equals(parts[2]));
+                    preferences.putBoolean(uri + "-added", true);
+                }
+            }
+        } catch (IOException | URISyntaxException ex) {
+            NodeLogger.getLogger(getClass()).error("Error while adding KNIME Update Sites: " + ex.getMessage(), ex);
+        }
+    }
+
+    /**
      * Updates the URI of a single repository. An update is only performed if the URI does not already contains the
      * KNIME ID (see {@link #urlContainsID(URI)}). Only HTTP(S) URIs from knime.org or knime.com hosts are updated.
      *
@@ -176,8 +239,8 @@ public class RepositoryUpdater implements ProvisioningListener {
                 // remove modified artifact repository when the corresponding metadata repository is removed
                 removeOutdatedArtifactRepository(event);
             } else if ((event.getKind() == RepositoryEvent.ENABLEMENT)
-                    && (event.getRepositoryType() == IRepository.TYPE_METADATA)
-                    && isKnimeURI(event.getRepositoryLocation()))  {
+                && (event.getRepositoryType() == IRepository.TYPE_METADATA)
+                && isKnimeURI(event.getRepositoryLocation())) {
                 // en- or disable the corresponding artifact repository
                 updateArtifactRepository(event);
             }
