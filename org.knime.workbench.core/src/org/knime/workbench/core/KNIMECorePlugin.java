7e57f9a4d7f657826009a938b5616ef329d0faca
Thorsten Meinl <thorsten.meinl@knime.com>
2015-07-20 08:03:01 +0000

    follow up fix for bug #6163

--- org.knime.workbench.core/src/org/knime/workbench/core/KNIMECorePlugin.java
+++ org.knime.workbench.core/src/org/knime/workbench/core/KNIMECorePlugin.java
@@ -127,7 +127,7 @@ public class KNIMECorePlugin extends AbstractUIPlugin {
         super.start(context);
 
         if (!Boolean.getBoolean("java.awt.headless")) {
-            createImageRegistry();
+            getImageRegistry();
         }
 
         try {
