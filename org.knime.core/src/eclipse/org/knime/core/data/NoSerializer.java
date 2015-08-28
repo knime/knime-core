8eed2ff72fb14c8652c4caa4601adf8b270ccb11
Thorsten Meinl <thorsten.meinl@knime.com>
2015-08-28 13:52:42 +0000

    IN_PROGRESS - bug 6278: New extension point for datacell implementations https://bugs.knime.org/show_bug.cgi?id=6278
    register old CDKCell

---
+++ org.knime.core/src/eclipse/org/knime/core/data/NoSerializer.java
@@ -0,0 +1,78 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   28.08.2015 (thor): created
+ */
+package org.knime.core.data;
+
+import java.io.IOException;
+
+/**
+ * This is a marker serializer for cell implementations that don't have their own serializer (which is highly
+ * discouraged). Use this class only when registering a serializer at the extension point. The implementation is
+ * non-functional!
+ *
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
+ * @since 3.0
+ * @noreference This class is not intended to be referenced by clients.
+ */
+public final class NoSerializer implements DataCellSerializer<DataCell> {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void serialize(final DataCell cell, final DataCellDataOutput output) throws IOException {
+        throw new UnsupportedOperationException("Don't use this serializer!");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DataCell deserialize(final DataCellDataInput input) throws IOException {
+        throw new UnsupportedOperationException("Don't use this serializer!");
+    }
+}
