967a16bcede8c4696487b27a4c6f765da4f17e07
Thorsten Meinl <thorsten.meinl@knime.com>
2015-09-02 09:11:45 +0000

    fixed NPE sometimes seen during shutdown in Eclipse 4

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/editparts/NodeAnnotationEditPart.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/editparts/NodeAnnotationEditPart.java
@@ -83,6 +83,10 @@ public class NodeAnnotationEditPart extends AnnotationEditPart {
                 annoFig.newContent(anno);
                 // node annotation ignores its x/y ui info and hooks itself to its node
                 NodeContainer node = anno.getNodeContainer();
+                if (node == null) {
+                    return;
+                }
+
                 NodeUIInformation nodeUI = node.getUIInformation();
                 int x = anno.getX();
                 int y = anno.getY();
