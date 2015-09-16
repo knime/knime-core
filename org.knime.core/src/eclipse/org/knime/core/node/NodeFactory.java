a1a17bbb4e1bb594b6d0bf871c797360a35d72f2
Christian Albrecht <christian.albrecht@knime.com>
2015-09-16 12:21:27 +0000

    Resolve file paths correctly on Windows for JS framework.

--- org.knime.core/src/eclipse/org/knime/core/node/NodeFactory.java
+++ org.knime.core/src/eclipse/org/knime/core/node/NodeFactory.java
@@ -304,6 +304,8 @@ public abstract class NodeFactory<T extends NodeModel> {
                 imagePath = imagePath.replaceAll("[^./]+/\\.\\./", "");
                 m = ICON_PATH_PATTERN.matcher(imagePath);
             }
+        } else {
+            imagePath = p.toString();
         }
 
         URL iconURL = getClass().getClassLoader().getResource(imagePath);
