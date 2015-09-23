3919fe5e52519f2db2131bac1f432dd1862e24e3
Bernd Wiswedel <bernd.wiswedel@knime.com>
2015-09-23 15:00:37 +0000

    on bug 2292: Loop restart with dangling branches flaky. https://bugs.knime.org/show_bug.cgi?id=2292

---
+++ org.knime.core.testing/src/org/knime/core/node/workflow/Bug2292_LoopsWithDanglingBranches.java
@@ -0,0 +1,105 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ */
+package org.knime.core.node.workflow;
+
+import static org.knime.core.node.workflow.InternalNodeContainerState.CONFIGURED;
+import static org.knime.core.node.workflow.InternalNodeContainerState.EXECUTED;
+
+/**
+ * bug 2292: Loop restart with dangling branches flaky.
+ * https://bugs.knime.org/show_bug.cgi?id=2292
+ * @author wiswedel, University of Konstanz
+ */
+public class Bug2292_LoopsWithDanglingBranches extends WorkflowTestCase {
+
+    private NodeID m_javaSnippet7;
+    private NodeID m_loopEnd4;
+    private NodeID m_javaSnippet13;
+    private NodeID m_loopEnd12;
+
+    /** {@inheritDoc} */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        NodeID baseID = loadAndSetWorkflow();
+        m_javaSnippet7 = new NodeID(baseID, 7);
+        m_javaSnippet13 = new NodeID(baseID, 13);
+        m_loopEnd4 = new NodeID(baseID, 4);
+        m_loopEnd12 = new NodeID(baseID, 12);
+    }
+
+    /** Exec all - expect everything to be executed. */
+    public void testExecuteAll() throws Exception {
+        checkState(getManager(), CONFIGURED);
+        executeAllAndWait();
+        checkState(getManager(), EXECUTED);
+        checkStateOfMany(EXECUTED, m_loopEnd4, m_javaSnippet7);
+        checkStateOfMany(EXECUTED, m_loopEnd12, m_javaSnippet13);
+    }
+
+    /** Execute top loop (one iteration) - the side branch used to be not executed. */
+    public void testSingleIterationExecuteUpToHere() throws Exception {
+        executeAndWait(m_loopEnd4);
+        checkState(m_loopEnd4, EXECUTED);
+        // TODO Michael if you comment out the sleep it will work - there is some asynchronous state change problem
+        System.out.println(getManager().getNodeContainer(m_javaSnippet7).getNodeContainerState());
+//        Thread.sleep(500);
+        checkState(m_javaSnippet7, EXECUTED); // this used to be configured before 3.0 - fixed as part of the bug fix
+    }
+
+    /** Execute bottom loop (many iterations) - the is executed (and has always been). */
+    public void testManyIterationsExecuteUpToHere() throws Exception {
+        executeAndWait(m_loopEnd12);
+        checkState(m_loopEnd12, EXECUTED);
+        checkState(m_javaSnippet13, EXECUTED); // this was also executed before 2.12 - multiple iterations
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
