8b859974d35cd3ceda2a91725d65abbd4660088d
Thorsten Meinl <thorsten.meinl@knime.com>
2015-11-16 12:35:39 +0000

    Bumped version numbers and set build date to November, 18th

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -314,7 +314,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "September 22, 2015";
+        BUILD_DATE = "November 18, 2015";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle(KNIMEConstants.class);
         if (coreBundle != null) {
