4b16d936930317556d77eb04c2273d53ee125c6f
Christian Albrecht <christian.albrecht@knime.com>
2015-11-24 11:18:51 +0000

    Addendum for Bug 6445 - JSON editor for Subnode layouts, Mac is still having problems with the focus not being propagated to the RSyntaxTextArea, Windows seems to work though, so decided to have a platform-dependent implementation for now, since we reach 80-90% of users with it.

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/subnode/SubnodeLayoutJSONEditorPage.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/subnode/SubnodeLayoutJSONEditorPage.java
@@ -48,20 +48,39 @@
  */
 package org.knime.workbench.editor2.subnode;
 
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Panel;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.layout.PixelConverter;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.knime.core.node.util.ViewUtils;
 import org.knime.core.node.workflow.NodeID;
 import org.knime.core.node.workflow.SubNodeContainer;
 import org.knime.core.node.workflow.WorkflowManager;
@@ -70,6 +89,7 @@ import org.knime.js.core.layout.bs.JSONLayoutPage;
 import org.knime.js.core.layout.bs.JSONLayoutRow;
 import org.knime.js.core.layout.bs.JSONLayoutViewContent;
 
+import com.fasterxml.jackson.core.JsonLocation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
@@ -81,13 +101,20 @@ import com.fasterxml.jackson.databind.ObjectReader;
 public class SubnodeLayoutJSONEditorPage extends WizardPage {
 
     private WorkflowManager m_wfManager;
+
     private SubNodeContainer m_subNodeContainer;
+
     private List<NodeID> m_viewNodes;
 
     private String m_jsonDocument;
+
     private Label m_statusLine;
-    //private RSyntaxTextArea m_textArea;
-    private Text m_textArea;
+
+    private RSyntaxTextArea m_textArea;
+
+    private int m_caretPosition = 5;
+
+    private Text m_text;
 
     /**
      * @param pageName
@@ -103,134 +130,128 @@ public class SubnodeLayoutJSONEditorPage extends WizardPage {
      */
     @Override
     public void createControl(final Composite parent) {
+
         Composite composite = new Composite(parent, SWT.NONE);
         composite.setLayout(new GridLayout(1, true));
         composite.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
 
-        m_textArea = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
-        GridData layoutData = new GridData(GridData.FILL_BOTH);
-        layoutData.widthHint = 600;
-        layoutData.heightHint = 400;
-        m_textArea.setLayoutData(layoutData);
-        m_textArea.addModifyListener(new ModifyListener() {
+        if (isWindows()) {
 
-            @Override
-            public void modifyText(final ModifyEvent e) {
-                m_jsonDocument = m_textArea.getText();
-            }
-        });
-        m_textArea.setText(m_jsonDocument);
+            Composite embedComposite = new Composite(composite, SWT.EMBEDDED);
+            final GridLayout gridLayout = new GridLayout();
+            gridLayout.verticalSpacing = 0;
+            gridLayout.marginWidth = 0;
+            gridLayout.marginHeight = 0;
+            gridLayout.horizontalSpacing = 0;
+            embedComposite.setLayout(gridLayout);
+            embedComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+            Frame frame = SWT_AWT.new_Frame(embedComposite);
+            Panel heavyWeightPanel = new Panel();
+            heavyWeightPanel.setLayout(new BoxLayout(heavyWeightPanel, BoxLayout.Y_AXIS));
+            frame.add(heavyWeightPanel);
+            frame.setFocusTraversalKeysEnabled(false);
 
-        /*Composite embedComposite = new Composite(composite, SWT.EMBEDDED);
-        final GridLayout gridLayout = new GridLayout();
-        gridLayout.verticalSpacing = 0;
-        gridLayout.marginWidth = 0;
-        gridLayout.marginHeight = 0;
-        gridLayout.horizontalSpacing = 0;
-        embedComposite.setLayout(gridLayout);
-        embedComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-        Frame frame = SWT_AWT.new_Frame(embedComposite);
-        Panel heavyWeightPanel = new Panel();
-        heavyWeightPanel.setLayout(new BoxLayout(heavyWeightPanel, BoxLayout.Y_AXIS));
-        frame.add(heavyWeightPanel);
-        frame.setFocusTraversalKeysEnabled(false);
+            m_textArea = new RSyntaxTextArea(10, 60);
+            m_textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON);
+            m_textArea.setCodeFoldingEnabled(true);
+            m_textArea.setAntiAliasingEnabled(true);
+            RTextScrollPane sp = new RTextScrollPane(m_textArea);
+            sp.setDoubleBuffered(true);
+            m_textArea.setText(m_jsonDocument);
+            m_textArea.setEditable(true);
+            m_textArea.setEnabled(true);
+            heavyWeightPanel.add(sp);
 
-        m_textArea = new RSyntaxTextArea(10, 60);
-        m_textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON);
-        m_textArea.setCodeFoldingEnabled(true);
-        m_textArea.setAntiAliasingEnabled(true);
-        RTextScrollPane sp = new RTextScrollPane(m_textArea);
-        m_textArea.setText(m_jsonDocument);
-        m_textArea.setEditable(true);
-        m_textArea.setEnabled(true);
-        heavyWeightPanel.add(sp);
+            Dimension size = sp.getPreferredSize();
+            embedComposite.setSize(size.width, size.height);
 
-        Dimension size = sp.getPreferredSize();
-        embedComposite.setSize(size.width, size.height);
+            // forward focus to RSyntaxTextArea
+            embedComposite.addFocusListener(new FocusAdapter() {
 
-        embedComposite.addFocusListener(new FocusAdapter() {
-
-            @Override
-            public void focusGained(final FocusEvent e) {
-                m_textArea.requestFocus();
-                ViewUtils.runOrInvokeLaterInEDT(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("Focusing");
-                        m_textArea.requestFocus();
-                        m_textArea.setCaretPosition(5);
-                    }
-                });
-            }
-
-            @Override
-            public void focusLost(final FocusEvent e) {
-                ViewUtils.runOrInvokeLaterInEDT(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("Focus out");
-                    }
-                });
-            }
-        });
-
-        m_textArea.requestFocus();
-
-        m_textArea.addFocusListener(new FocusListener() {
-
-            @Override
-            public void focusLost(final java.awt.event.FocusEvent e) {
-                System.out.println("Focus lost");
-
-            }
-
-            @Override
-            public void focusGained(final java.awt.event.FocusEvent e) {
-                System.out.println("Focus gained");
-
-            }
-        });*/
-
-        /*final FocusListener sizeFocusListener = new FocusListener() {
-
-            @Override
-            public void focusLost(final java.awt.event.FocusEvent e) {  do nothing  }
-
-            @Override
-            public void focusGained(final java.awt.event.FocusEvent e) {
-                final Dimension panelSize = textArea.getSize();
-                if (!embedComposite.isDisposed()) {
-                    embedComposite.getDisplay().asyncExec(new Runnable() {
+                @Override
+                public void focusGained(final FocusEvent e) {
+                    ViewUtils.runOrInvokeLaterInEDT(new Runnable() {
                         @Override
                         public void run() {
-                            if (!embedComposite.isDisposed()) {
-                                final Point wrapperSize = embedComposite.getSize();
-                                // Check if size of this component is the same as the panel
-                                System.out.println(panelSize + " " + wrapperSize);
-                                if (panelSize.width != wrapperSize.x || panelSize.height != wrapperSize.y) {
-                                    ViewUtils.invokeLaterInEDT(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            // Set sizes on AWT frame and dialog panel if different from
-                                            // wrapper component
-                                            frame.setSize(wrapperSize.x, wrapperSize.y);
-                                            sp.setSize(wrapperSize.x, wrapperSize.y);
-                                        }
-                                    });
-                                }
-                            }
+                            m_textArea.requestFocus();
+                            m_textArea.setCaretPosition(m_caretPosition);
                         }
                     });
                 }
-            }
-        };
-        sp.addFocusListener(sizeFocusListener);*/
 
-        m_statusLine = new Label(composite, SWT.BORDER);
-        m_statusLine.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, true, false));
-        m_statusLine.setText("OK");
+                @Override
+                public void focusLost(final FocusEvent e) {
+                    // do nothing
+                }
+            });
+
+            // delete content of status line, when something is inserted or deleted
+            m_textArea.getDocument().addDocumentListener(new DocumentListener() {
+
+                @Override
+                public void changedUpdate(final DocumentEvent arg0) {
+                    if (!composite.isDisposed()) {
+                        composite.getDisplay().asyncExec(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (m_statusLine != null && !m_statusLine.isDisposed()) {
+                                    m_statusLine.setText("");
+                                }
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void insertUpdate(final DocumentEvent arg0) { /* do nothing */ }
+
+                @Override
+                public void removeUpdate(final DocumentEvent arg0) { /* do nothing */ }
+
+            });
+
+            // remember caret position
+            m_textArea.addCaretListener(new CaretListener() {
+                @Override
+                public void caretUpdate(final CaretEvent arg0) {
+                    m_caretPosition = arg0.getDot();
+                }
+            });
+
+        } else {
+            m_text = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+            GridData layoutData = new GridData(GridData.FILL_BOTH);
+            layoutData.widthHint = 600;
+            layoutData.heightHint = 400;
+            m_text.setLayoutData(layoutData);
+            m_text.addModifyListener(new ModifyListener() {
+
+                @Override
+                public void modifyText(final ModifyEvent e) {
+                    m_jsonDocument = m_text.getText();
+                    if (m_statusLine != null && !m_statusLine.isDisposed()) {
+                        m_statusLine.setText("");
+                    }
+                }
+            });
+            m_text.setText(m_jsonDocument);
+        }
+
+        // add status line
+        m_statusLine = new Label(composite, SWT.SHADOW_NONE | SWT.WRAP);
+        GridData statusGridData = new GridData(SWT.LEFT | SWT.FILL, SWT.BOTTOM, true, false);
+        int maxHeight = new PixelConverter(m_statusLine).convertHeightInCharsToPixels(3);
+        statusGridData.heightHint = maxHeight;
+        // seems to have no impact on the layout. The height will still be 3 rows (at least on Windows 8)
+        statusGridData.minimumHeight = new PixelConverter(m_statusLine).convertHeightInCharsToPixels(1);
+        m_statusLine.setLayoutData(statusGridData);
+
+        if (!(Thread.currentThread() == composite.getDisplay().getThread())) {
+            System.out.println("Currently not running in the Display Thread");
+        }
 
         setControl(composite);
+
     }
 
     /**
@@ -239,7 +260,7 @@ public class SubnodeLayoutJSONEditorPage extends WizardPage {
      * @param viewNodes
      */
     public void setNodes(final WorkflowManager manager, final SubNodeContainer subnodeContainer,
-            final List<NodeID> viewNodes) {
+        final List<NodeID> viewNodes) {
         m_wfManager = manager;
         m_subNodeContainer = subnodeContainer;
         m_viewNodes = viewNodes;
@@ -291,12 +312,39 @@ public class SubnodeLayoutJSONEditorPage extends WizardPage {
         ObjectMapper mapper = JSONLayoutPage.getConfiguredObjectMapper();
         ObjectReader reader = mapper.readerForUpdating(new JSONLayoutPage());
         try {
-            m_statusLine.setText("");
-            JSONLayoutPage page = reader.readValue(m_jsonDocument);
+            String json = isWindows() ? m_textArea.getText() : m_jsonDocument;
+            JSONLayoutPage page = reader.readValue(json);
             // TODO add more checks in objects (e.g. column widths)
             return true;
         } catch (IOException e) {
-            m_statusLine.setText("Error reading structure: " + e.getMessage());
+            String errorMessage;
+            if (e instanceof JsonProcessingException) {
+                JsonProcessingException jsonException = (JsonProcessingException)e;
+                Throwable cause = null;
+                Throwable newCause = jsonException.getCause();
+                while (newCause instanceof JsonProcessingException) {
+                    if (cause == newCause) {
+                        break;
+                    }
+                    cause = newCause;
+                    newCause = cause.getCause();
+                }
+                if (cause instanceof JsonProcessingException) {
+                    jsonException = (JsonProcessingException)cause;
+                }
+                errorMessage = jsonException.getOriginalMessage().split("\n")[0];
+                JsonLocation location = jsonException.getLocation();
+                if (location != null) {
+                    errorMessage += " at line: " + (location.getLineNr() + 1) + " column: " + location.getColumnNr();
+                }
+            } else {
+                String message = e.getMessage();
+                errorMessage = message;
+            }
+            m_statusLine.setText(errorMessage);
+            int textWidth = isWindows() ? m_textArea.getSize().width : m_text.getSize().x;
+            Point newSize = m_statusLine.computeSize(textWidth, m_statusLine.getSize().y, true);
+            m_statusLine.setSize(newSize);
         }
         return false;
     }
@@ -308,7 +356,8 @@ public class SubnodeLayoutJSONEditorPage extends WizardPage {
         ObjectMapper mapper = JSONLayoutPage.getConfiguredObjectMapper();
         ObjectReader reader = mapper.readerForUpdating(new JSONLayoutPage());
         try {
-            JSONLayoutPage page = reader.readValue(m_jsonDocument);
+            String json = isWindows() ? m_textArea.getText() : m_jsonDocument;
+            JSONLayoutPage page = reader.readValue(json);
             String layoutString = mapper.writeValueAsString(page);
             return layoutString;
         } catch (IOException e) {
@@ -316,6 +365,10 @@ public class SubnodeLayoutJSONEditorPage extends WizardPage {
         }
 
         return "";
+    }
+
+    private boolean isWindows() {
+        return Platform.OS_WIN32.equals(Platform.getOS());
     }
 
 }
