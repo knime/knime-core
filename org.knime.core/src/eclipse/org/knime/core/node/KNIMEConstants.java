d11070fbcf516a68cfe19781830fb98b9fbdc5c6
Thorsten Meinl <thorsten.meinl@knime.com>
2016-01-29 11:59:06 +0000

    Version bump for 3.1.1/4.2.3 release

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -318,7 +318,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "January 28, 2016";
+        BUILD_DATE = "January 29, 2016";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle(KNIMEConstants.class);
         if (coreBundle != null) {
