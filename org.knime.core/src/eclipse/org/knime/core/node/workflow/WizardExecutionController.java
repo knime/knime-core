ff5f4bc7e7390cacbee906278dc165ce22bc90a2
Christian Albrecht <christian.albrecht@knime.com>
2016-07-11 20:52:25 +0000

    RESOLVED AP-4966 - Workflow test cases for (new) wizard execution
    Added test for save and removed unused private methods from WEC

--- org.knime.core/src/eclipse/org/knime/core/node/workflow/WizardExecutionController.java
+++ org.knime.core/src/eclipse/org/knime/core/node/workflow/WizardExecutionController.java
@@ -81,7 +81,6 @@ import org.knime.core.node.web.WebTemplate;
 import org.knime.core.node.web.WebViewContent;
 import org.knime.core.node.wizard.WizardNode;
 import org.knime.core.node.workflow.WorkflowManager.NodeModelFilter;
-import org.knime.core.util.Pair;
 
 /**
  * A utility class received from the workflow manager that allows stepping back and forth in a wizard execution.
@@ -324,15 +323,6 @@ public final class WizardExecutionController extends ExecutionController {
         }
     }
 
-    private static boolean containsDestination(final List<Pair<NodeID, NodeID>> waitingPairs, final NodeID dest) {
-        for (Pair<NodeID, NodeID> p : waitingPairs) {
-            if (p.getSecond().equals(dest)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     /** Created from workflow.
      * @param manager ...
      */
@@ -397,31 +387,6 @@ public final class WizardExecutionController extends ExecutionController {
     @Override
     boolean isHalted(final NodeID source) {
         return m_waitingSubnodes.contains(source);
-    }
-
-    private NodeID findNextWaitingSubnode() {
-        final Workflow flow = m_manager.getWorkflow();
-        Map<NodeID, Set<Integer>> breadthFirstSortedList = flow.createBreadthFirstSortedList(flow.getNodeIDs(), true);
-        for (Map.Entry<NodeID, Set<Integer>> entry : breadthFirstSortedList.entrySet()) {
-            NodeContainer nc = flow.getNode(entry.getKey());
-            if (nc instanceof SubNodeContainer) {
-                SubNodeContainer subnodeNC = (SubNodeContainer)nc;
-                WorkflowManager subnodeMgr = subnodeNC.getWorkflowManager();
-                if (subnodeMgr.getNodeContainerState().isExecuted()) {
-                    continue; // do not prompt executed sub nodes
-                }
-                if (m_promptedSubnodeIDSuffixes.contains(entry.getKey().getIndex())) {
-                    continue;
-                }
-                @SuppressWarnings("rawtypes")
-                final Map<NodeID, WizardNode> wizardNodes =
-                subnodeMgr.findNodes(WizardNode.class, NOT_HIDDEN_FILTER, false);
-                if (!wizardNodes.isEmpty()) {
-                    return subnodeNC.getID();
-                }
-            }
-        }
-        return null;
     }
 
     /** Get the current wizard page. Throws exception if none is available (as per {@link #hasCurrentWizardPage()}.
