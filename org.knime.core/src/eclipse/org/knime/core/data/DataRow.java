1b793f3f19c8a8f6d0ffddd3fbec1680a8b765a8
Bernd Wiswedel <bernd.wiswedel@knime.com>
2016-10-01 10:00:11 +0000

    add stream() method

--- org.knime.core/src/eclipse/org/knime/core/data/DataRow.java
+++ org.knime.core/src/eclipse/org/knime/core/data/DataRow.java
@@ -44,10 +44,13 @@
  */
 package org.knime.core.data;
 
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 /**
  * Container interface for a vector of {@link DataCell}s and a row key for
  * unique identification.
- * 
+ *
  * <p>
  * Each <code>DataRow</code> represents one row of a {@link DataTable} and
  * contains a fixed number of {@link DataCell} elements which are directly
@@ -56,15 +59,15 @@ package org.knime.core.data;
  * <p>
  * A <code>DataRow</code> must not contain a <code>null</code> element or a
  * <code>null</code> key.
- * 
+ *
  * <p>
  * This <code>DataRow</code> interface extends the {@link Iterable} interface
  * but does not allow the removal of {@link DataCell}s. Implementors must
  * therefore throw an {@link UnsupportedOperationException} in the Iterators
  * remove method.
- * 
+ *
  * @author Thomas Gabriel, University of Konstanz
- * 
+ *
  * @see DataTable
  * @see DataCell
  * @see RowIterator
@@ -75,24 +78,31 @@ public interface DataRow extends Iterable<DataCell> {
     /**
      * Returns the length of this row, that is the number of columns of the
      * DataTable (not including the row key).
-     * 
+     *
      * @return length of this row
      */
     int getNumCells();
 
     /**
      * Returns the row key.
-     * 
+     *
      * @return the row key
      */
     RowKey getKey();
 
     /**
      * Returns the {@link DataCell} at the provided index within this row.
-     * 
+     *
      * @param index the index of the cell to retrieve (indices start from 0)
      * @return the {@link DataCell} at the given index
      * @throws IndexOutOfBoundsException if the index is out of range
      */
     DataCell getCell(final int index);
+
+    /** Get a stream of all cells.
+     * @return A non-parallel stream on all cells in the row.
+     * @since 3.3 */
+    default Stream<DataCell> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
 }
