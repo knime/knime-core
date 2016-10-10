e506ce84be479020422b08d15df0229bfb4f24d1
Bernd Wiswedel <bernd.wiswedel@knime.com>
2016-10-10 19:15:06 +0000

    AP-6533: Tooltip in "Memory" tab is not showing actual cell count threshold (problem reported in forum)
    Task-Url: https://knime-com.atlassian.net/browse/AP-6533

--- org.knime.core/src/eclipse/org/knime/core/node/MiscSettingsTab.java
+++ org.knime.core/src/eclipse/org/knime/core/node/MiscSettingsTab.java
@@ -58,6 +58,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+import org.knime.core.data.container.DataContainer;
 import org.knime.core.node.workflow.SingleNodeContainer.MemoryPolicy;
 
 /**
@@ -87,7 +88,7 @@ class MiscSettingsTab extends JPanel {
         cacheSmall.setActionCommand(MemoryPolicy.CacheSmallInMemory.toString());
         m_group.add(cacheSmall);
         cacheSmall.setToolTipText("Tables with less than "
-                + 0 + " cells are kept in "
+                + DataContainer.MAX_CELLS_IN_MEMORY + " cells are kept in "
                 + "main memory, otherwise swapped to disc.");
         JRadioButton cacheOnDisc = new JRadioButton(
                 "Write tables to disc.");
