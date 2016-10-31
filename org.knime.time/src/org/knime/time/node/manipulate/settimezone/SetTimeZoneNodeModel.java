d0c5392f00fa2c46ed40cb221524fefd1bc1c179
Simon Schmid <simon.schmid@uni-konstanz.de>
2016-10-31 10:38:55 +0000

    AP-6596
    WIP

---
+++ org.knime.time/src/org/knime/time/node/manipulate/settimezone/SetTimeZoneNodeModel.java
@@ -0,0 +1,321 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Oct 28, 2016 (simon): created
+ */
+package org.knime.time.node.manipulate.settimezone;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.knime.base.data.replace.ReplacedColumnsDataRow;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.append.AppendedColumnRow;
+import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.container.SingleCellFactory;
+import org.knime.core.data.time.zoneddatetime.ZonedDateTimeCellFactory;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelColumnFilter2;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.streamable.InputPortRole;
+import org.knime.core.node.streamable.OutputPortRole;
+import org.knime.core.node.streamable.PartitionInfo;
+import org.knime.core.node.streamable.PortInput;
+import org.knime.core.node.streamable.PortOutput;
+import org.knime.core.node.streamable.RowInput;
+import org.knime.core.node.streamable.RowOutput;
+import org.knime.core.node.streamable.StreamableOperator;
+import org.knime.core.util.UniqueNameGenerator;
+
+/**
+ * The node model of the node which adds a date to a time cell.
+ *
+ * @author Simon Schmid, KNIME.com, Konstanz, Germany
+ */
+public class SetTimeZoneNodeModel extends org.knime.core.node.NodeModel {
+
+    private final SettingsModelColumnFilter2 m_colSelect = SetTimeZoneNodeDialog.createColSelectModel();
+
+    private final SettingsModelString m_isReplaceOrAppend = SetTimeZoneNodeDialog.createReplaceAppendStringBool();
+
+    private final SettingsModelString m_suffix = SetTimeZoneNodeDialog.createSuffixModel();
+
+    private final SettingsModelString m_timeZone = SetTimeZoneNodeDialog.createTimeZoneSelectModel();
+
+    /**
+     * one in, one out
+     */
+    protected SetTimeZoneNodeModel() {
+        super(1, 1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs) throws InvalidSettingsException {
+        final ColumnRearranger columnRearranger = createColumnRearranger(inSpecs[0]);
+        return new DataTableSpec[]{columnRearranger.createSpec()};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec)
+        throws Exception {
+        final ColumnRearranger columnRearranger = createColumnRearranger(inData[0].getDataTableSpec());
+        final BufferedDataTable out = exec.createColumnRearrangeTable(inData[0], columnRearranger, exec);
+        return new BufferedDataTable[]{out};
+    }
+
+    /**
+     * @param inSpec table input spec
+     * @return the CR describing the output
+     */
+    private ColumnRearranger createColumnRearranger(final DataTableSpec inSpec) {
+        final ColumnRearranger rearranger = new ColumnRearranger(inSpec);
+        final String[] includeList = m_colSelect.applyTo(inSpec).getIncludes();
+        final int[] includeIndeces =
+            Arrays.stream(m_colSelect.applyTo(inSpec).getIncludes()).mapToInt(s -> inSpec.findColumnIndex(s)).toArray();
+        int i = 0;
+        final DataType dataType;
+        //        if (m_addZone.getBooleanValue()) {
+        dataType = ZonedDateTimeCellFactory.TYPE;
+        //        } else {
+        //            dataType = LocalDateTimeCellFactory.TYPE;
+        //        }
+        for (String includedCol : includeList) {
+            if (m_isReplaceOrAppend.getStringValue().equals(SetTimeZoneNodeDialog.OPTION_REPLACE)) {
+                final DataColumnSpecCreator dataColumnSpecCreator = new DataColumnSpecCreator(includedCol, dataType);
+                final AddDateCellFactory cellFac =
+                    new AddDateCellFactory(dataColumnSpecCreator.createSpec(), includeIndeces[i++]);
+                rearranger.replace(cellFac, includedCol);
+            } else {
+                final DataColumnSpec dataColSpec =
+                    new UniqueNameGenerator(inSpec).newColumn(includedCol + m_suffix.getStringValue(), dataType);
+                final AddDateCellFactory cellFac = new AddDateCellFactory(dataColSpec, includeIndeces[i++]);
+                rearranger.append(cellFac);
+            }
+        }
+        return rearranger;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public InputPortRole[] getInputPortRoles() {
+        return new InputPortRole[]{InputPortRole.DISTRIBUTED_STREAMABLE};
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public OutputPortRole[] getOutputPortRoles() {
+        return new OutputPortRole[]{OutputPortRole.DISTRIBUTED};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public StreamableOperator createStreamableOperator(final PartitionInfo partitionInfo,
+        final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        return new StreamableOperator() {
+            @Override
+            public void runFinal(final PortInput[] inputs, final PortOutput[] outputs, final ExecutionContext exec)
+                throws Exception {
+                final RowInput in = (RowInput)inputs[0];
+                final RowOutput out = (RowOutput)outputs[0];
+                final DataTableSpec inSpec = in.getDataTableSpec();
+                final String[] includeList = m_colSelect.applyTo(inSpec).getIncludes();
+                final int[] includeIndeces = Arrays.stream(m_colSelect.applyTo(inSpec).getIncludes())
+                    .mapToInt(s -> inSpec.findColumnIndex(s)).toArray();
+                final boolean isReplace =
+                    m_isReplaceOrAppend.getStringValue().equals(SetTimeZoneNodeDialog.OPTION_REPLACE);
+                final DataType dataType;
+                //                if (m_addZone.getBooleanValue()) {
+                dataType = ZonedDateTimeCellFactory.TYPE;
+                //                } else {
+                //                    dataType = LocalDateTimeCellFactory.TYPE;
+                //                }
+
+                DataRow row;
+                while ((row = in.poll()) != null) {
+                    exec.checkCanceled();
+                    DataCell[] datacells = new DataCell[includeIndeces.length];
+                    for (int i = 0; i < includeIndeces.length; i++) {
+                        if (isReplace) {
+                            final DataColumnSpecCreator dataColumnSpecCreator =
+                                new DataColumnSpecCreator(includeList[i], dataType);
+                            final AddDateCellFactory cellFac =
+                                new AddDateCellFactory(dataColumnSpecCreator.createSpec(), includeIndeces[i]);
+                            datacells[i] = cellFac.getCell(row);
+                        } else {
+                            final DataColumnSpec dataColSpec = new UniqueNameGenerator(inSpec)
+                                .newColumn(includeList[i] + m_suffix.getStringValue(), dataType);
+                            final AddDateCellFactory cellFac = new AddDateCellFactory(dataColSpec, includeIndeces[i]);
+                            datacells[i] = cellFac.getCell(row);
+                        }
+                    }
+                    if (isReplace) {
+                        out.push(new ReplacedColumnsDataRow(row, datacells, includeIndeces));
+                    } else {
+                        out.push(new AppendedColumnRow(row, datacells));
+                    }
+                }
+                in.close();
+                out.close();
+            }
+        };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File nodeInternDir, final ExecutionMonitor exec)
+        throws IOException, CanceledExecutionException {
+        // no internals
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File nodeInternDir, final ExecutionMonitor exec)
+        throws IOException, CanceledExecutionException {
+        // no internals
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_colSelect.saveSettingsTo(settings);
+        m_isReplaceOrAppend.saveSettingsTo(settings);
+        m_suffix.saveSettingsTo(settings);
+        m_timeZone.saveSettingsTo(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_colSelect.validateSettings(settings);
+        m_isReplaceOrAppend.validateSettings(settings);
+        m_suffix.validateSettings(settings);
+        m_timeZone.validateSettings(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_colSelect.loadSettingsFrom(settings);
+        m_isReplaceOrAppend.loadSettingsFrom(settings);
+        m_suffix.loadSettingsFrom(settings);
+        m_timeZone.loadSettingsFrom(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+        // no internals
+    }
+
+    private final class AddDateCellFactory extends SingleCellFactory {
+
+        private final int m_colIndex;
+
+        /**
+         * @param inSpec spec of the column after computation
+         * @param colIndex index of the column to work on
+         */
+        public AddDateCellFactory(final DataColumnSpec inSpec, final int colIndex) {
+            super(inSpec);
+            m_colIndex = colIndex;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public DataCell getCell(final DataRow row) {
+            final DataCell cell = row.getCell(m_colIndex);
+            if (cell.isMissing()) {
+                return cell;
+            }
+            //            final LocalTimeCell localTimeCell = (LocalTimeCell)cell;
+            //            final LocalDate localDate = LocalDate.of(m_year.getIntValue(),
+            //                Month.valueOf(m_month.getStringValue().toUpperCase()), m_day.getIntValue());
+            //            if (m_addZone.getBooleanValue()) {
+            //                return ZonedDateTimeCellFactory.create(ZonedDateTime.of(
+            //                    LocalDateTime.of(localDate, localTimeCell.getLocalTime()), ZoneId.of(m_timeZone.getStringValue())));
+            //            } else {
+            //                return LocalDateTimeCellFactory.create(LocalDateTime.of(localDate, localTimeCell.getLocalTime()));
+            //            }
+            return null;
+        }
+    }
+
+}
