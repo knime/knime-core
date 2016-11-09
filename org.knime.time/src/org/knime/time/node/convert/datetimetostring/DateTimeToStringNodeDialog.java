bfeb0fd113cd845cf913f27213637be39ba68158
Thorsten Meinl <thorsten.meinl@knime.com>
2016-11-09 16:19:05 +0000

    RESOLVED - issue AP-6634: Add preview in Date&Time to String dialog https://knime-com.atlassian.net/browse/AP-6634
    RESOLVED - issue AP-6637: Improved error messages when date&time patterns are incompatible with settings or data
    https://knime-com.atlassian.net/browse/AP-6637
    
    Merged Simon's changed

--- org.knime.time/src/org/knime/time/node/convert/datetimetostring/DateTimeToStringNodeDialog.java
+++ org.knime.time/src/org/knime/time/node/convert/datetimetostring/DateTimeToStringNodeDialog.java
@@ -68,6 +68,7 @@ import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import org.apache.commons.lang3.LocaleUtils;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DataType;
 import org.knime.core.data.time.localdate.LocalDateCellFactory;
@@ -85,6 +86,7 @@ import org.knime.core.node.defaultnodesettings.DialogComponentString;
 import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelColumnFilter2;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.time.node.convert.DateTimeTypes;
 
 /**
  * The node dialog of the node which converts strings to the new date&time types.
@@ -105,12 +107,22 @@ public class DateTimeToStringNodeDialog extends NodeDialogPane {
 
     private final SettingsModelString m_formatModel;
 
+    private SettingsModelString m_localeModel;
+
+    private final JLabel m_previewLabel;
+
+    private final JLabel m_typeFormatWarningLabel;
+
     private DataTableSpec m_spec;
+
+    private final ZonedDateTime m_currentZDT;
 
     /**
      * Setting up all DialogComponents.
      */
     public DateTimeToStringNodeDialog() {
+        m_currentZDT = ZonedDateTime.now();
+
         final SettingsModelColumnFilter2 colSelectModel = DateTimeToStringNodeModel.createColSelectModel();
         m_dialogCompColFilter = new DialogComponentColumnFilter2(colSelectModel, 0);
 
@@ -132,8 +144,8 @@ public class DateTimeToStringNodeDialog extends NodeDialogPane {
         }
         Arrays.sort(availableLocalesString);
 
-        m_dialogCompLocale = new DialogComponentStringSelection(DateTimeToStringNodeModel.createLocaleModel(),
-            "Locale: ", availableLocalesString);
+        m_localeModel = DateTimeToStringNodeModel.createLocaleModel();
+        m_dialogCompLocale = new DialogComponentStringSelection(m_localeModel, "Locale: ", availableLocalesString);
 
         /*
          * create panel with gbc
@@ -193,15 +205,22 @@ public class DateTimeToStringNodeDialog extends NodeDialogPane {
         gbcTypeFormat.weightx = 1;
         gbcTypeFormat.insets = new Insets(0, 20, 0, 0);
         panelTypeFormat.add(m_dialogCompLocale.getComponentPanel(), gbcTypeFormat);
+        // add label for preview of the first cells content
+        m_previewLabel = new JLabel();
+        gbcTypeFormat.insets = new Insets(0, 5, 0, 0);
+        gbcTypeFormat.gridx = 0;
+        gbcTypeFormat.weightx = 0;
+        gbcTypeFormat.gridy++;
+        panelTypeFormat.add(m_previewLabel, gbcTypeFormat);
         // add label for warning
-        final JLabel typeFormatLabel = new JLabel();
+        m_typeFormatWarningLabel = new JLabel();
         gbcTypeFormat.gridx = 0;
         gbcTypeFormat.gridy++;
         gbcTypeFormat.weightx = 0;
         gbcTypeFormat.gridwidth = 3;
-        gbcTypeFormat.insets = new Insets(0, 4, 0, 0);
-        typeFormatLabel.setForeground(Color.RED);
-        panelTypeFormat.add(typeFormatLabel, gbcTypeFormat);
+        gbcTypeFormat.insets = new Insets(6, 4, 0, 0);
+        m_typeFormatWarningLabel.setForeground(Color.RED);
+        panelTypeFormat.add(m_typeFormatWarningLabel, gbcTypeFormat);
         panel.add(panelTypeFormat, gbc);
         /*
          * add tab
@@ -219,47 +238,98 @@ public class DateTimeToStringNodeDialog extends NodeDialogPane {
 
                 final String format = m_formatModel.getStringValue();
                 int i = 0;
-                try {
-                    for (String include : includes) {
-                        final DataType type = m_spec.getColumnSpec(include).getType();
-                        if (type.equals(LocalDateCellFactory.TYPE)) {
+                for (String include : includes) {
+                    final DataType type = m_spec.getColumnSpec(include).getType();
+                    if (type.equals(LocalDateCellFactory.TYPE)) {
+                        try {
                             final LocalDate now1 = LocalDate.now();
                             final DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern(format);
                             now1.format(formatter1);
-                        } else if (type.equals(LocalTimeCellFactory.TYPE)) {
+                            setTypeFormatWarningNull();
+                        } catch (DateTimeException exception) {
+                            setTypeFormatWarningMessage(exception,
+                                "'" + includes[i] + "' is a " + DateTimeTypes.LOCAL_DATE.toString()
+                                    + " and only contains a date, but the format contains time fields!");
+                        } catch (IllegalArgumentException exception) {
+                            setTypeFormatWarningMessage(exception, exception.getMessage());
+                        }
+                    } else if (type.equals(LocalTimeCellFactory.TYPE)) {
+                        try {
                             final LocalTime now2 = LocalTime.now();
                             final DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern(format);
                             now2.format(formatter2);
-                        } else if (type.equals(LocalDateTimeCellFactory.TYPE)) {
+                            setTypeFormatWarningNull();
+                        } catch (DateTimeException exception) {
+                            setTypeFormatWarningMessage(exception,
+                                "'" + includes[i] + "' is a " + DateTimeTypes.LOCAL_TIME.toString()
+                                    + " and only contains a time, but the format contains date fields!");
+                        } catch (IllegalArgumentException exception) {
+                            setTypeFormatWarningMessage(exception, exception.getMessage());
+                        }
+                    } else if (type.equals(LocalDateTimeCellFactory.TYPE)) {
+                        try {
                             final LocalDateTime now3 = LocalDateTime.now();
                             final DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern(format);
                             now3.format(formatter3);
-                        } else if (type.equals(ZonedDateTimeCellFactory.TYPE)) {
+                            setTypeFormatWarningNull();
+                        } catch (DateTimeException exception) {
+                            setTypeFormatWarningMessage(exception, "'" + includes[i] + "' is a "
+                                + DateTimeTypes.LOCAL_DATE_TIME.toString()
+                                + " and only contains a date and a time, but the format contains time zone fields!");
+                        } catch (IllegalArgumentException exception) {
+                            setTypeFormatWarningMessage(exception, exception.getMessage());
+                        }
+                    } else if (type.equals(ZonedDateTimeCellFactory.TYPE)) {
+                        try {
                             final ZonedDateTime now4 = ZonedDateTime.now();
                             final DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern(format);
                             now4.format(formatter4);
-                        } else {
-                            throw new IllegalStateException("Unhandled date&time type: " + type.getName());
+                            setTypeFormatWarningNull();
+                        } catch (DateTimeException exception) {
+                            setTypeFormatWarningMessage(exception,
+                                "'" + includes[i] + "' is a " + DateTimeTypes.ZONED_DATE_TIME.toString()
+                                    + " and only contains a date, time, and time zone or offset!");
+                        } catch (IllegalArgumentException exception) {
+                            setTypeFormatWarningMessage(exception, exception.getMessage());
                         }
-                        i++;
+                    } else {
+                        throw new IllegalStateException("Unhandled date&time type: " + type.getName());
                     }
-                    m_dialogCompFormatSelect.setToolTipText(null);
-                    typeFormatLabel.setToolTipText(null);
-                    ((JComponent)m_dialogCompFormatSelect.getComponentPanel().getComponent(1)).setBorder(null);
-                    typeFormatLabel.setText("");
-                } catch (DateTimeException | IllegalArgumentException ex) {
-                    m_dialogCompFormatSelect.setToolTipText(ex.getMessage());
-                    typeFormatLabel.setToolTipText(ex.getMessage());
-                    typeFormatLabel.setText("Data type of column '" + includes[i]
-                        + "' is not compatible with date format! Hover over for more informations.");
-                    ((JComponent)m_dialogCompFormatSelect.getComponentPanel().getComponent(1))
-                        .setBorder(BorderFactory.createLineBorder(Color.RED));
+                    i++;
                 }
             }
         };
         colSelectModel.addChangeListener(listener);
         m_formatModel.addChangeListener(listener);
+        m_formatModel.addChangeListener(e -> updatePreview());
+        m_localeModel.addChangeListener(e -> updatePreview());
 
+    }
+
+    private void updatePreview() {
+        try {
+            final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(m_formatModel.getStringValue(),
+                LocaleUtils.toLocale(m_localeModel.getStringValue()));
+            final String result = m_currentZDT.format(formatter);
+            m_previewLabel.setText("Preview: " + result);
+        } catch (IllegalArgumentException e) {
+            m_previewLabel.setText("Preview: ");
+        }
+    }
+
+    private void setTypeFormatWarningNull() {
+        m_dialogCompFormatSelect.setToolTipText(null);
+        m_typeFormatWarningLabel.setToolTipText(null);
+        ((JComponent)m_dialogCompFormatSelect.getComponentPanel().getComponent(1)).setBorder(null);
+        m_typeFormatWarningLabel.setText("");
+    }
+
+    private void setTypeFormatWarningMessage(final Exception exception, final String message) {
+        m_dialogCompFormatSelect.setToolTipText(exception.getMessage());
+        m_typeFormatWarningLabel.setToolTipText(exception.getMessage());
+        m_typeFormatWarningLabel.setText(message);
+        ((JComponent)m_dialogCompFormatSelect.getComponentPanel().getComponent(1))
+            .setBorder(BorderFactory.createLineBorder(Color.RED));
     }
 
     /**
@@ -289,6 +359,8 @@ public class DateTimeToStringNodeDialog extends NodeDialogPane {
         // retrieve potential new values from the StringHistory and add them
         // (if not already present) to the combo box...
         m_dialogCompFormatSelect.replaceListItems(DateTimeToStringNodeModel.createPredefinedFormats(), null);
+        // update preview
+        updatePreview();
     }
 
 }
