a611298a91f1c35e26768614fb6f46bf210df1c2
Thorsten Meinl <thorsten.meinl@knime.com>
2016-11-17 10:44:51 +0000

    IN PROGRESS - issue AP-6690: Save-As of workflows does not update mount point root in context https://knime-com.atlassian.net/browse/AP-6690
    split save runnable into three different runnables for save, auto save, and save as

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/AbstractSaveRunnable.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/AbstractSaveRunnable.java
@@ -1,5 +1,6 @@
 /*
  * ------------------------------------------------------------------------
+ *
  *  Copyright by KNIME GmbH, Konstanz, Germany
  *  Website: http://www.knime.org; Email: contact@knime.org
  *
@@ -40,10 +41,10 @@
  *  propagated with or for interoperation with KNIME.  The owner of a Node
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
- * -------------------------------------------------------------------
+ * ---------------------------------------------------------------------
  *
  * History
- *   11.01.2007 (sieb): created
+ *   17.11.2016 (thor): created
  */
 package org.knime.workbench.editor2;
 
@@ -51,62 +52,46 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.swing.UIManager;
-
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.knime.core.internal.ReferencedFile;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.NodeLogger;
 import org.knime.core.node.workflow.WorkflowManager;
-import org.knime.core.node.workflow.WorkflowSaveHelper;
-import org.knime.core.util.FileUtil;
+import org.knime.core.node.workflow.WorkflowPersistor;
+import org.knime.core.util.LockFailedException;
 import org.knime.workbench.ui.navigator.KnimeResourceUtil;
 
 /**
- * A runnable which is used by the {@link WorkflowEditor} to save a workflow with a progress bar. NOTE: As the
- * {@link UIManager} holds a reference to this runnable an own class file is necessary sucht that all references to the
- * created workflow manager can be deleted, otherwise the manager cannot be deleted later and the memeory cannot be
- * freed.
  *
- * @author Christoph Sieb, University of Konstanz
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
  */
-class SaveWorkflowRunnable extends PersistWorkflowRunnable {
-
-    private static final NodeLogger LOGGER = NodeLogger.getLogger(SaveWorkflowRunnable.class);
+abstract class AbstractSaveRunnable extends PersistWorkflowRunnable {
+    private final NodeLogger m_logger = NodeLogger.getLogger(getClass());
 
     private WorkflowEditor m_editor;
-
-    private File m_workflowFile;
 
     private StringBuilder m_exceptionMessage;
 
     private IProgressMonitor m_monitor;
 
-    private final WorkflowSaveHelper m_saveHelper;
-
     /**
      * Creates a runnable that saves the worfklow.
      *
      * @param editor the editor holding the workflow to save
-     * @param workflowFile the file to save the workflow to
      * @param exceptionMessage holding an exception message
-     * @param saveHelper Save options.
      * @param monitor the progress monitor to report the progress to
      */
-    public SaveWorkflowRunnable(final WorkflowEditor editor, final File workflowFile,
-        final StringBuilder exceptionMessage, final WorkflowSaveHelper saveHelper, final IProgressMonitor monitor) {
+    public AbstractSaveRunnable(final WorkflowEditor editor, final StringBuilder exceptionMessage,
+        final IProgressMonitor monitor) {
         m_editor = editor;
-        m_workflowFile = workflowFile;
         m_exceptionMessage = exceptionMessage;
-        m_saveHelper = saveHelper;
         m_monitor = monitor;
     }
 
-    /** {@inheritDoc} */
     @Override
-    public void run(final IProgressMonitor pm) {
+    public final void run(final IProgressMonitor pm) {
+        File workflowDir = getSaveLocation();
         try {
             final WorkflowManager wfm = m_editor.getWorkflowManager();
             ProgressHandler progressHandler =
@@ -114,72 +99,48 @@ class SaveWorkflowRunnable extends PersistWorkflowRunnable {
             final CheckCancelNodeProgressMonitor progressMonitor = new CheckCancelNodeProgressMonitor(pm);
 
             progressMonitor.addProgressListener(progressHandler);
-
-            final File workflowPath = m_workflowFile.getParentFile();
-            final ReferencedFile oldWorkflowPathRef = wfm.getWorkingDir();
-            final File oldWorkflowPath = oldWorkflowPathRef == null ? null : oldWorkflowPathRef.getFile();
             final ExecutionMonitor exec = new ExecutionMonitor(progressMonitor);
-            if (!m_saveHelper.isAutoSave() && oldWorkflowPath != null && !oldWorkflowPath.equals(workflowPath)) {
-                wfm.saveAs(workflowPath, exec);
-            } else {
-                wfm.save(workflowPath, m_saveHelper, exec);
-            }
-            // copy report files, see bug 5717
-            if (m_saveHelper.isAutoSave() && oldWorkflowPath != null) {
-                File reportDesignFile = new File(oldWorkflowPath, "default_report.rptdesign");
-                File reportConfigFile = new File(oldWorkflowPath, "default_report.rptconfig");
-                if (reportDesignFile.isFile()) {
-                    FileUtil.copy(reportDesignFile, new File(workflowPath, reportDesignFile.getName()));
-                }
-                if (reportConfigFile.isFile()) {
-                    FileUtil.copy(reportConfigFile, new File(workflowPath, reportConfigFile.getName()));
-                }
-            }
+
+             save(wfm, exec);
 
             // the refresh used to take place in WorkflowEditor#saveTo but
             // was moved to this runnable as part of bug fix 3028
-            IResource r = KnimeResourceUtil.getResourceForURI(workflowPath.toURI());
+            IResource r = KnimeResourceUtil.getResourceForURI(workflowDir.toURI());
             if (r != null) {
                 String pName = r.getName();
                 pm.setTaskName("Refreshing " + pName + "...");
                 r.refreshLocal(IResource.DEPTH_INFINITE, pm);
             }
         } catch (FileNotFoundException fnfe) {
-            LOGGER.fatal("File not found", fnfe);
+            m_logger.fatal("File not found", fnfe);
             m_exceptionMessage.append("File access problems: " + fnfe.getMessage());
-
             m_monitor.setCanceled(true);
         } catch (IOException ioe) {
-            if (m_workflowFile.length() == 0) {
-                LOGGER.info("New workflow created.");
+            if (new File(workflowDir, WorkflowPersistor.WORKFLOW_FILE).length() == 0) {
+                m_logger.info("New workflow created.");
             } else {
-                LOGGER.error("Could not save workflow: " + m_workflowFile.getName(), ioe);
+                m_logger.error("Could not save workflow: " + workflowDir.getName(), ioe);
                 m_exceptionMessage.append("File access problems: " + ioe.getMessage());
                 m_monitor.setCanceled(true);
             }
         } catch (CanceledExecutionException cee) {
-            LOGGER.info("Canceled saving worflow: " + m_workflowFile.getName());
+            m_logger.info("Canceled saving workflow: " + workflowDir.getName());
             m_exceptionMessage.append("Saving workflow" + " was canceled.");
             m_monitor.setCanceled(true);
-        } catch (Error e) {
-            LOGGER.error("Could not save workflow", e);
-
-            m_exceptionMessage.append("Could not save workflow: " + e.getMessage());
-            m_monitor.setCanceled(true);
-
         } catch (Exception e) {
-            LOGGER.error("Could not save workflow", e);
-
+            m_logger.error("Could not save workflow", e);
             m_exceptionMessage.append("Could not save workflow: " + e.getMessage());
             m_monitor.setCanceled(true);
         } finally {
             pm.subTask("Finished.");
             pm.done();
             m_editor = null;
-            m_workflowFile = null;
             m_exceptionMessage = null;
             m_monitor = null;
         }
-
     }
+
+    protected abstract File getSaveLocation();
+
+    protected abstract void save(WorkflowManager wfm, ExecutionMonitor exec) throws IOException, CanceledExecutionException, LockFailedException;
 }
