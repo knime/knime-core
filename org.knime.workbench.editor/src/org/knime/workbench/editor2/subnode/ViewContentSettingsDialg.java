1d363ef3fdc57a67d4ff6be7e7354c4dcc844bf4
Christian Albrecht <christian.albrecht@knime.com>
2017-01-23 17:43:46 +0100

    AP-6789 Adding components to advanced dialog

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/subnode/ViewContentSettingsDialg.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/subnode/ViewContentSettingsDialg.java
@@ -48,16 +48,19 @@
  */
 package org.knime.workbench.editor2.subnode;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.knime.js.core.layout.bs.JSONLayoutViewContent;
 import org.knime.js.core.layout.bs.JSONLayoutViewContent.ResizeMethod;
 
@@ -72,6 +75,16 @@ public class ViewContentSettingsDialg extends TitleAreaDialog {
     private JSONLayoutViewContent m_viewSettings;
 
     private Combo m_resizeMethodCombo;
+    private Button m_autoResizeButton;
+    private Button m_sizeHeightButton;
+    private Button m_sizeWidthButton;
+    private Button m_scrollingButton;
+    private Text m_resizeIntervalSpinner;
+    private Text m_resizeToleranceSpinner;
+    private Text m_minWidthText;
+    private Text m_maxWidthText;
+    private Text m_minHeightText;
+    private Text m_maxHeightText;
 
 
     /**
@@ -101,10 +114,11 @@ public class ViewContentSettingsDialg extends TitleAreaDialog {
         Composite area = (Composite) super.createDialogArea(parent);
         Composite container = new Composite(area, SWT.NONE);
         container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        GridLayout layout = new GridLayout(2, false);
+        GridLayout layout = new GridLayout(4, false);
         container.setLayout(layout);
 
         createResizeMethodCombo(container);
+        createAdditionalResizeFields(container);
         return area;
     }
 
@@ -113,8 +127,9 @@ public class ViewContentSettingsDialg extends TitleAreaDialog {
         label.setText("Resize Method");
 
         GridData gridData = new GridData();
-        gridData.grabExcessHorizontalSpace = true;
-        gridData.horizontalAlignment = GridData.FILL;
+        gridData.horizontalSpan = 3;
+        //gridData.grabExcessHorizontalSpace = true;
+        gridData.horizontalAlignment = SWT.FILL;
 
         m_resizeMethodCombo = new Combo(container, SWT.DROP_DOWN | SWT.READ_ONLY);
         m_resizeMethodCombo.setLayoutData(gridData);
@@ -122,6 +137,33 @@ public class ViewContentSettingsDialg extends TitleAreaDialog {
             m_resizeMethodCombo.add(resizeMethod.toValue());
         }
         m_resizeMethodCombo.setText(m_viewSettings.getResizeMethod().toValue());
+    }
+
+    private void createAdditionalResizeFields(final Composite container) {
+        Label autoResizeLabel = new Label(container, SWT.NONE);
+        autoResizeLabel.setText("Auto Resize");
+
+        GridData gridData = new GridData();
+        gridData.horizontalSpan = 3;
+        m_autoResizeButton = new Button(container, SWT.CHECK);
+        m_autoResizeButton.setLayoutData(gridData);
+        m_autoResizeButton.setSelection(m_viewSettings.getAutoResize());
+
+        Label sizeHeightLabel = new Label(container, SWT.NONE);
+        sizeHeightLabel.setText("Size Height");
+        m_sizeHeightButton = new Button(container, SWT.CHECK);
+        m_sizeHeightButton.setSelection(m_viewSettings.getSizeHeight());
+
+        Label sizeWidthLabel = new Label(container, SWT.NONE);
+        sizeWidthLabel.setText("Size Width");
+        m_sizeWidthButton = new Button(container, SWT.CHECK);
+        m_sizeWidthButton.setSelection(m_viewSettings.getSizeWidth());
+
+        Label resizeIntervalLabel = new Label(container, SWT.NONE);
+        resizeIntervalLabel.setText("Resize Interval");
+        m_resizeIntervalSpinner = new Text(container, SWT.SINGLE);
+        Integer resizeInterval = m_viewSettings.getResizeInterval();
+        m_resizeIntervalSpinner.setText(resizeInterval == null ? "" : resizeInterval.toString());
     }
 
     /**
@@ -147,7 +189,23 @@ public class ViewContentSettingsDialg extends TitleAreaDialog {
     private void saveViewSettings() {
         try {
             m_viewSettings.setResizeMethod(ResizeMethod.forValue(m_resizeMethodCombo.getText()));
-        } catch (JsonMappingException e) {
+            m_viewSettings.setAutoResize(m_autoResizeButton.getSelection());
+            m_viewSettings.setSizeHeight(m_sizeHeightButton.getSelection());
+            m_viewSettings.setSizeWidth(m_sizeWidthButton.getSelection());
+            m_viewSettings.setScrolling(m_scrollingButton.getSelection());
+            String resizeInterval = m_resizeIntervalSpinner.getText();
+            m_viewSettings.setResizeInterval(StringUtils.isEmpty(resizeInterval) ? null : Integer.parseInt(resizeInterval));
+            String resizeTolerance = m_resizeToleranceSpinner.getText();
+            m_viewSettings.setResizeTolerance(StringUtils.isEmpty(resizeTolerance) ? null : Integer.parseInt(resizeTolerance));
+            String minWidth = m_minWidthText.getText();
+            m_viewSettings.setMinWidth(StringUtils.isEmpty(minWidth) ? null : Integer.parseInt(minWidth));
+            String maxWidth = m_maxWidthText.getText();
+            m_viewSettings.setMaxWidth(StringUtils.isEmpty(maxWidth) ? null : Integer.parseInt(maxWidth));
+            String minHeight = m_minHeightText.getText();
+            m_viewSettings.setMinHeight(StringUtils.isEmpty(minHeight) ? null : Integer.parseInt(minHeight));
+            String maxHeight = m_maxHeightText.getText();
+            m_viewSettings.setMaxHeight(StringUtils.isEmpty(maxHeight) ? null : Integer.parseInt(maxHeight));
+        } catch (JsonMappingException | NumberFormatException e) {
             // TODO Auto-generated catch block
         }
     }
