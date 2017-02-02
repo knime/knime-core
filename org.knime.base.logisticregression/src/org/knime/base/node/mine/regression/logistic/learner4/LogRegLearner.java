198fed95532278f73e724353bf18ae42bbb1efff
Adrian Nembach <adrian.nembach@knime.com>
2017-02-02 09:37:02 +0100

    AP-6952 First implementation using the old approach

---
+++ org.knime.base.logisticregression/src/org/knime/base/node/mine/regression/logistic/learner4/LogRegLearner.java
@@ -0,0 +1,81 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   25.01.2017 (Adrian Nembach): created
+ */
+package org.knime.base.node.mine.regression.logistic.learner4;
+
+import org.knime.base.node.mine.regression.RegressionTrainingData;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+
+/**
+ * A learner object that performs the task of learning a logistic regression model based on
+ * the provided data.
+ *
+ * @author Adrian Nembach, KNIME.com
+ */
+interface LogRegLearner {
+
+    /**
+     * Learns a logistic regression model from the provided data.
+     *
+     * @param data training data from which the model should be learned
+     * @param progressMonitor monitor that allows to report the progress of the training
+     * @return the content of the logistic regression model that is learned
+     * @throws InvalidSettingsException if the settings cause inconsistencies during training
+     * @throws CanceledExecutionException if the training is canceled while in progress
+     */
+    public LogisticRegressionContent learn(final RegressionTrainingData data, ExecutionMonitor progressMonitor) throws CanceledExecutionException, InvalidSettingsException;
+
+    /**
+     * Returns any warnings that occurred during the training process.
+     *
+     * @return Any warnings that occurred during the training process
+     */
+    public String getWarningMessage();
+}
