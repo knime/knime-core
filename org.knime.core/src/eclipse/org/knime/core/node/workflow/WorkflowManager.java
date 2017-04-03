60c5bb44321e6585a50678d434bc4f8539792b13
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2017-04-03 21:07:02 +0200

    AP-7065: added assertion (state check really) for #getWizardExecController
    AP-7065 (Refactor WizardExecutionController for single page and wizard
    execution)

--- org.knime.core/src/eclipse/org/knime/core/node/workflow/WorkflowManager.java
+++ org.knime.core/src/eclipse/org/knime/core/node/workflow/WorkflowManager.java
@@ -2495,6 +2495,8 @@ public final class WorkflowManager extends NodeContainer implements NodeUIInform
      */
     public WizardExecutionController getWizardExecutionController() {
         CheckUtils.checkState(isProject(), "Workflow '%s' is not a project", getNameWithID());
+        CheckUtils.checkState(getContext().getServerAuthToken().isPresent(),
+            "Not running on server -- method not supposed to be called");
         try (WorkflowLock lock = lock()) {
             if (!(m_executionController instanceof WizardExecutionController)) {
                 m_executionController = new WizardExecutionController(this);
