3e157ba406bb326ade1568793fd3b5ca2790b365
Jonathan Hale <jonathan.hale@knime.com>
2017-04-04 11:55:19 +0200

    AP-7087: Fix order of converter factories returned for source/dest queries
    For URIDataCell the DataValue.toString() converter was listed before the
    StringCell.getStringValue() converter which broke backwards
    compatibility.
    
    AP-7087: JavaSnippet handles URI columns different than before

--- org.knime.core/src/eclipse/org/knime/core/data/convert/java/DataCellToJavaConverterRegistry.java
+++ org.knime.core/src/eclipse/org/knime/core/data/convert/java/DataCellToJavaConverterRegistry.java
@@ -58,7 +58,6 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -322,29 +321,26 @@ public final class DataCellToJavaConverterRegistry {
             return Arrays.asList(MissingToNullConverterFactory.getInstance());
         }
 
-        final LinkedBlockingQueue<Class<?>> classes = new LinkedBlockingQueue<>();
-        classes.add(sourceType.getPreferredValueClass());
-        classes.addAll(sourceType.getValueClasses());
-        classes.add(sourceType.getCellClass());
-
         final Collection<DataCellToJavaConverterFactory<? extends DataValue, D>> allFactories = new ArrayList<>();
 
-        while (!classes.isEmpty()) {
-            final Class<?> curClass = classes.poll();
-            // this conversion is fine, since we guarantee the correct type when
-            // inserting into the map.
+        for (final Class<? extends DataValue> curClass : sourceType.getValueClasses()) {
+            if (DataValue.class.equals(curClass)) {
+                // We need to defer DataValue.class to the very end, because these converters are usually very general and low
+                // priority (e.g. DataValue.toString() converter)
+                continue;
+            }
+
             final ArrayList<DataCellToJavaConverterFactory<?, ?>> factories =
                 m_converterFactories.get(new ConversionKey(curClass, destType));
-
             if (factories != null) {
                 allFactories.addAll((Collection<? extends DataCellToJavaConverterFactory<DataCell, D>>)factories);
             }
+        }
 
-            /* check if a supertype has a compatible converter factory */
-            classes.addAll(Arrays.asList(curClass.getInterfaces()));
-            if (curClass.getSuperclass() != null) {
-                classes.add(curClass.getSuperclass());
-            }
+        final ArrayList<DataCellToJavaConverterFactory<?, ?>> factories =
+            m_converterFactories.get(new ConversionKey(DataValue.class, destType));
+        if (factories != null) {
+            allFactories.addAll((Collection<? extends DataCellToJavaConverterFactory<DataCell, D>>)factories);
         }
 
         if (sourceType.isCollectionType() && destType.isArray()) {
