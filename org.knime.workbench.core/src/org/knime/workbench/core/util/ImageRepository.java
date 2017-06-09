af9d4040eea0cea760ea4e04029521e3b87eb871
Simon Schmid <simon.schmid@uni-konstanz.de>
2017-06-09 14:55:58 +0200

    SRV-922: Added icon
    SRV-922 (New context menu: "Open in WebPortal" for KNIME Server workflow)
    https://knime-com.atlassian.net/browse/SRV-922

--- org.knime.workbench.core/src/org/knime/workbench/core/util/ImageRepository.java
+++ org.knime.workbench.core/src/org/knime/workbench/core/util/ImageRepository.java
@@ -267,6 +267,8 @@ public final class ImageRepository {
         ServerShowScheduleInfo("icons/server_space/schedinfo.png"),
         /** ServerSpace Explorer Menu Icon: upload workflow. */
         ServerUploadWorkflow("icons/server_space/upload_wf.png"),
+        /** ServerSpace Explorer Menu Icon: open in web portal. */
+        ServerOpenInWebPortal("icons/server_space/open_in_web.png"),
         /** ServerSpace Explorer Menu Icon: edit meta info. */
         ServerPermissions("icons/server_space/key.png"),
         /** ServerSpace Explorer Menu Icon: create a snapshot. */
