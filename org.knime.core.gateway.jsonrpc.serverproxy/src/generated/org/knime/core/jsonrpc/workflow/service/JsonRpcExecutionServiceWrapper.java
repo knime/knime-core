85bc8783f957a6417b39930876895cd935cee31a
Martin Horn <martin.horn@uni-konstanz.de>
2017-08-04 11:57:11 +0200

    AP-7775 New gateway.jsonrpc, gateway.jsonrpc.serverproxy, gateway.jsonrpc.clientproxy projects added - enables the communication between AP and an executor (via the server) by transmitting json-rpc messages via http (works)
    (AP-7775 Use JSON-RPC 2.0 as message format between AP and Server/Executor)

---
+++ org.knime.core.gateway.jsonrpc.serverproxy/src/generated/org/knime/core/jsonrpc/workflow/service/JsonRpcExecutionServiceWrapper.java
@@ -0,0 +1,90 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.core.jsonrpc.workflow.service;
+
+import org.knime.core.gateway.v0.workflow.entity.WorkflowEnt;
+import org.knime.core.gateway.v0.workflow.service.ExecutionService;
+
+import com.googlecode.jsonrpc4j.JsonRpcMethod;
+import com.googlecode.jsonrpc4j.JsonRpcParam;
+import com.googlecode.jsonrpc4j.JsonRpcService;
+
+/**
+ * Json rpc annotated class that wraps another service and delegates the method calls. 
+ *
+ * @author Martin Horn, University of Konstanz
+ */
+@JsonRpcService(value = "ExecutionService")
+// AUTO-GENERATED CODE; DO NOT MODIFY
+public class JsonRpcExecutionServiceWrapper implements ExecutionService {
+
+	private ExecutionService m_service;
+
+	public JsonRpcExecutionServiceWrapper(ExecutionService service) {
+		m_service = service;
+	}
+
+	/**
+     * {@inheritDoc}
+     */
+    @Override
+    @JsonRpcMethod(value = "getCanExecuteUpToHere")
+    public boolean getCanExecuteUpToHere(@JsonRpcParam(value="workflowID") final String workflowID,@JsonRpcParam(value="nodeID") final String nodeID) {
+		return m_service.getCanExecuteUpToHere(workflowID,nodeID);
+    }
+
+	/**
+     * {@inheritDoc}
+     */
+    @Override
+    @JsonRpcMethod(value = "setExecuteUpToHere")
+    public WorkflowEnt setExecuteUpToHere(@JsonRpcParam(value="workflowID") final String workflowID,@JsonRpcParam(value="nodeID") final String nodeID) {
+		return m_service.setExecuteUpToHere(workflowID,nodeID);
+    }
+
+
+}
