aa3172defb2dc080d200bf99344d9b216c351e72
Thorsten Meinl <thorsten.meinl@knime.com>
2017-09-07 11:50:30 +0200

    Version bump for bugfix release

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -323,7 +323,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "July 12, 2017";
+        BUILD_DATE = "September 11, 2017";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.product");
         if (coreBundle != null) {
