9fa99b333b692aa2b7a8b21409d5e61fecbbd0cf
Jonathan Hale <jonathan.hale@knime.com>
2017-09-13 04:40:26 +0200

    AP-7001: Cleanup JarListPanel.
    AP-7001: Java Snippet: New dialog tab to add installed bundles to code classpath

--- org.knime.jsnippets/src/org/knime/base/node/jsnippet/ui/JarListPanel.java
+++ org.knime.jsnippets/src/org/knime/base/node/jsnippet/ui/JarListPanel.java
@@ -47,8 +47,6 @@ package org.knime.base.node.jsnippet.ui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.net.MalformedURLException;
@@ -67,8 +65,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.event.ListDataListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import org.apache.commons.lang3.StringUtils;
 import org.knime.base.node.jsnippet.util.JavaSnippetUtil;
@@ -81,7 +77,8 @@ import org.knime.core.util.SimpleFileFilter;
 
 /**
  * List of jars required for compilation (separate tab).
- * <p>This class might change and is not meant as public API.
+ * <p>
+ * This class might change and is not meant as public API.
  *
  * @author Bernd Wiswedel, University of Konstanz
  * @since 2.12
@@ -92,85 +89,70 @@ import org.knime.core.util.SimpleFileFilter;
 @SuppressWarnings("serial")
 public class JarListPanel extends JPanel {
 
-    private final JList<String> m_addJarList;
-    private final JButton m_addJarFilesButton;
-    private final JButton m_addJarURLsButton;
-    private final JButton m_removeButton;
+    private final JButton m_addJarFilesButton = new JButton("Add File(s)...");
+
+    private final JButton m_addJarURLsButton = new JButton("Add KNIME URL...");
+
+    private final JButton m_removeButton = new JButton("Remove");
 
     private JFileChooser m_jarFileChooser;
+
     private String[] m_filesCache;
+
+    private DefaultListModel<String> m_listModel = new DefaultListModel<String>();
+
+    private final JList<String> m_addJarList = new JList<String>(m_listModel) {
+        @Override
+        protected void processComponentKeyEvent(final KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_A && e.isControlDown()) {
+                final int end = getModel().getSize() - 1;
+                getSelectionModel().setSelectionInterval(0, end);
+            } else if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+                onJarRemove();
+            }
+        }
+    };
 
     /** Inits GUI. */
     public JarListPanel() {
         super(new BorderLayout());
-        m_addJarList = new JList<String>(new DefaultListModel<String>()) {
-            /** {@inheritDoc} */
-            @Override
-            protected void processComponentKeyEvent(final KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_A && e.isControlDown()) {
-                    int end = getModel().getSize() - 1;
-                    getSelectionModel().setSelectionInterval(0, end);
-                } else if (e.getKeyCode() == KeyEvent.VK_DELETE) {
-                    onJarRemove();
-                }
-            }
-        };
+
         m_addJarList.setCellRenderer(new ConvenientComboBoxRenderer());
+
         add(new JScrollPane(m_addJarList), BorderLayout.CENTER);
-        JPanel southP = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
-        m_addJarFilesButton = new JButton("Add File(s)...");
-        m_addJarFilesButton.addActionListener(new ActionListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                onJarFileAdd();
-            }
-        });
-        m_addJarURLsButton = new JButton("Add KNIME URL...");
+
         m_addJarURLsButton.setToolTipText("Add 'knime' URLs that resolve to local paths");
-        m_addJarURLsButton.addActionListener(new ActionListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                onJarURLAdd();
-            }
-        });
-        m_removeButton = new JButton("Remove");
-        m_removeButton.addActionListener(new ActionListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                onJarRemove();
-            }
-        });
-        m_addJarList.addListSelectionListener(new ListSelectionListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void valueChanged(final ListSelectionEvent e) {
-                m_removeButton.setEnabled(!m_addJarList.isSelectionEmpty());
-            }
-        });
+
+        /* Set action listeners */
+        m_addJarFilesButton.addActionListener(e -> onJarFileAdd());
+        m_addJarURLsButton.addActionListener(e -> onJarURLAdd());
+        m_removeButton.addActionListener(e -> onJarRemove());
+
+        /* Enable remove button only if item is selected */
         m_removeButton.setEnabled(!m_addJarList.isSelectionEmpty());
+        m_addJarList.addListSelectionListener(e -> m_removeButton.setEnabled(!m_addJarList.isSelectionEmpty()));
+
+        final JPanel southP = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
         southP.add(m_addJarFilesButton);
         southP.add(m_addJarURLsButton);
         southP.add(m_removeButton);
         add(southP, BorderLayout.SOUTH);
 
-        JPanel northP = new JPanel(new FlowLayout());
-        JLabel label = new JLabel("<html><body>Specify additional jar files "
-                + "that are necessary for the snippet to run</body></html>");
+        final JPanel northP = new JPanel(new FlowLayout());
+        final JLabel label = new JLabel(
+            "<html><body>Specify additional jar files " + "that are necessary for the snippet to run</body></html>");
         northP.add(label);
         add(northP, BorderLayout.NORTH);
     }
 
     private void onJarFileAdd() {
-        DefaultListModel<String> model = (DefaultListModel<String>)m_addJarList.getModel();
-        Set<String> hash = new HashSet<>(Collections.list(model.elements()));
-        StringHistory history = StringHistory.getInstance("java_snippet_jar_dirs");
+        final DefaultListModel<String> model = (DefaultListModel<String>)m_addJarList.getModel();
+        final Set<String> hash = new HashSet<>(Collections.list(model.elements()));
+        final StringHistory history = StringHistory.getInstance("java_snippet_jar_dirs");
         if (m_jarFileChooser == null) {
             File dir = null;
-            for (String h : history.getHistory()) {
-                File temp = new File(h);
+            for (final String h : history.getHistory()) {
+                final File temp = new File(h);
                 if (temp.isDirectory()) {
                     dir = temp;
                     break;
@@ -180,11 +162,11 @@ public class JarListPanel extends JPanel {
             m_jarFileChooser.setFileFilter(new SimpleFileFilter(".zip", ".jar"));
             m_jarFileChooser.setMultiSelectionEnabled(true);
         }
-        int result = m_jarFileChooser.showDialog(m_addJarList, "Select");
 
+        final int result = m_jarFileChooser.showDialog(m_addJarList, "Select");
         if (result == JFileChooser.APPROVE_OPTION) {
-            for (File f : m_jarFileChooser.getSelectedFiles()) {
-                String s = f.getAbsolutePath();
+            for (final File f : m_jarFileChooser.getSelectedFiles()) {
+                final String s = f.getAbsolutePath();
                 if (hash.add(s)) {
                     model.addElement(s);
                 }
@@ -195,104 +177,91 @@ public class JarListPanel extends JPanel {
 
     @SuppressWarnings("null")
     private void onJarURLAdd() {
-        DefaultListModel<String> model = (DefaultListModel<String>)m_addJarList.getModel();
-        Set<String> hash = new HashSet<>(Collections.list(model.elements()));
+        final Set<String> hash = new HashSet<>(Collections.list(m_listModel.elements()));
         String input = "knime://knime.workflow/example.jar";
-        boolean valid;
-        do {
+
+        boolean valid = false;
+        while (!valid) {
             input = JOptionPane.showInputDialog(this, "Enter a \"knime:\" URL to a JAR file", input);
             if (StringUtils.isEmpty(input)) {
                 valid = true;
             } else {
-                URL url;
                 try {
-                    url = new URL(input);
-                    File file = FileUtil.getFileFromURL(url);
+                    final URL url = new URL(input);
+                    final File file = FileUtil.getFileFromURL(url);
                     CheckUtils.checkArgument(file != null, "Could not resolve to local file");
-                    CheckUtils.checkArgument(file.exists(),
-                        "File does not exists; location resolved to\n%s", file.getAbsolutePath());
+                    CheckUtils.checkArgument(file.exists(), "File does not exists; location resolved to\n%s",
+                        file.getAbsolutePath());
                     valid = true;
-                } catch (MalformedURLException | IllegalArgumentException mfe) {
-                    JOptionPane.showMessageDialog(this, "Invalid URL\n" + mfe.getMessage(),
-                        "Error parsing URL", JOptionPane.ERROR_MESSAGE);
-                    valid = false;
-                    continue;
+                } catch (MalformedURLException | IllegalArgumentException e) {
+                    JOptionPane.showMessageDialog(this, "Invalid URL\n" + e.getMessage(), "Error parsing URL",
+                        JOptionPane.ERROR_MESSAGE);
                 }
             }
-        } while (!valid);
+        }
+
         if (!StringUtils.isEmpty(input) && hash.add(input)) {
-            model.addElement(input);
+            m_listModel.addElement(input);
         }
     }
 
     private void onJarRemove() {
-        DefaultListModel model = (DefaultListModel)m_addJarList.getModel();
-        int[] sels = m_addJarList.getSelectedIndices();
-        int last = Integer.MAX_VALUE;
+        final int[] sels = m_addJarList.getSelectedIndices();
+        final int last = Integer.MAX_VALUE;
         // traverse backwards (editing list in loop body)
         for (int i = sels.length - 1; i >= 0; i--) {
             assert sels[i] < last : "Selection list not ordered";
-            model.remove(sels[i]);
+            m_listModel.remove(sels[i]);
         }
     }
 
     /**
-     * Adds a listener to the list that's notified each time a change
-     * to the list of jar files occurs.
+     * Adds a listener to the list that's notified each time a change to the list of jar files occurs.
+     *
      * @param l the <code>ListDataListener</code> to be added
      */
     public void addListDataListener(final ListDataListener l) {
-        m_addJarList.getModel().addListDataListener(l);
+        m_listModel.addListDataListener(l);
     }
 
     /**
      * Set the files to display.
+     *
      * @param files the files
      */
     public void setJarFiles(final String[] files) {
-        boolean doUpdate = false;
-        if (m_filesCache == null) {
+        if (m_filesCache == null || !Arrays.deepEquals(m_filesCache, files)) {
             m_filesCache = files;
-            doUpdate = true;
-        } else {
-            if (!Arrays.deepEquals(m_filesCache, files)) {
-                m_filesCache = files;
-                doUpdate = true;
-            }
-        }
-        if (doUpdate) {
-            DefaultListModel<String> jarListModel = (DefaultListModel<String>)m_addJarList.getModel();
-            jarListModel.removeAllElements();
-            for (String f : files) {
+
+            m_listModel.removeAllElements();
+            for (final String f : files) {
                 try {
                     JavaSnippetUtil.toFile(f); // validate existence etc.
-                    jarListModel.addElement(f);
+                    m_listModel.addElement(f);
                 } catch (InvalidSettingsException e) {
-                    // ignore
+                    // ignore (skips adding the file to jarListModel)
                 }
             }
         }
     }
 
-
     /**
      * Get the jar files defined in this panel.
+     *
      * @return the jar files
      */
     public String[] getJarFiles() {
-        DefaultListModel<String> jarListModel = (DefaultListModel<String>)m_addJarList.getModel();
-        String[] copy = new String[jarListModel.getSize()];
-        if (jarListModel.getSize() > 0) {
-            jarListModel.copyInto(copy);
+        final String[] copy = new String[m_listModel.getSize()];
+        if (!m_listModel.isEmpty()) {
+            m_listModel.copyInto(copy);
         }
         return copy;
     }
 
     /**
-     * Sets whether or not this component is enabled.
-     * A component that is enabled may respond to user input,
-     * while a component that is not enabled cannot respond to
-     * user input.
+     * Sets whether or not this component is enabled. A component that is enabled may respond to user input, while a
+     * component that is not enabled cannot respond to user input.
+     *
      * @param enabled true if this component should be enabled, false otherwise
      */
     @Override
@@ -305,5 +274,4 @@ public class JarListPanel extends JPanel {
         }
         super.setEnabled(enabled);
     }
-
 }
