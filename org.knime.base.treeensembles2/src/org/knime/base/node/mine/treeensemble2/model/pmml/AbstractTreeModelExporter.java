2cf690f917191d5a13b152d5da08666f31573ef8
Adrian Nembach <adrian.nembach@knime.com>
2017-09-13 11:53:59 +0200

    AP-7916: Move logic to export node conditions to dedicated class
    AP-7916 (Rewrite PMMLTranslator for Regression Trees)

--- org.knime.base.treeensembles2/src/org/knime/base/node/mine/treeensemble2/model/pmml/AbstractTreeModelExporter.java
+++ org.knime.base.treeensembles2/src/org/knime/base/node/mine/treeensemble2/model/pmml/AbstractTreeModelExporter.java
@@ -48,41 +48,18 @@
  */
 package org.knime.base.node.mine.treeensemble2.model.pmml;
 
-import java.math.BigInteger;
-import java.util.List;
-import java.util.Set;
-
 import org.apache.xmlbeans.SchemaType;
-import org.dmg.pmml.ArrayType;
-import org.dmg.pmml.CompoundPredicateDocument.CompoundPredicate;
 import org.dmg.pmml.MININGFUNCTION;
 import org.dmg.pmml.MISSINGVALUESTRATEGY;
 import org.dmg.pmml.NOTRUECHILDSTRATEGY;
 import org.dmg.pmml.NodeDocument;
 import org.dmg.pmml.NodeDocument.Node;
-import org.dmg.pmml.SimplePredicateDocument.SimplePredicate;
-import org.dmg.pmml.SimplePredicateDocument.SimplePredicate.Operator;
-import org.dmg.pmml.SimpleSetPredicateDocument.SimpleSetPredicate;
-import org.dmg.pmml.SimpleSetPredicateDocument.SimpleSetPredicate.BooleanOperator.Enum;
 import org.dmg.pmml.TreeModelDocument;
 import org.dmg.pmml.TreeModelDocument.TreeModel;
 import org.dmg.pmml.TreeModelDocument.TreeModel.SplitCharacteristic;
-import org.knime.base.node.mine.decisiontree2.PMMLArrayType;
-import org.knime.base.node.mine.decisiontree2.PMMLBooleanOperator;
-import org.knime.base.node.mine.decisiontree2.PMMLCompoundPredicate;
-import org.knime.base.node.mine.decisiontree2.PMMLFalsePredicate;
-import org.knime.base.node.mine.decisiontree2.PMMLOperator;
-import org.knime.base.node.mine.decisiontree2.PMMLPredicate;
-import org.knime.base.node.mine.decisiontree2.PMMLSetOperator;
-import org.knime.base.node.mine.decisiontree2.PMMLSimplePredicate;
-import org.knime.base.node.mine.decisiontree2.PMMLSimpleSetPredicate;
-import org.knime.base.node.mine.decisiontree2.PMMLTruePredicate;
 import org.knime.base.node.mine.treeensemble2.model.AbstractTreeModel;
 import org.knime.base.node.mine.treeensemble2.model.AbstractTreeNode;
-import org.knime.base.node.mine.treeensemble2.model.AbstractTreeNodeSurrogateCondition;
-import org.knime.base.node.mine.treeensemble2.model.TreeNodeColumnCondition;
 import org.knime.base.node.mine.treeensemble2.model.TreeNodeCondition;
-import org.knime.base.node.mine.treeensemble2.model.TreeNodeTrueCondition;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.node.port.pmml.PMMLMiningSchemaTranslator;
 import org.knime.core.node.port.pmml.PMMLPortObjectSpec;
@@ -104,6 +81,12 @@ abstract class AbstractTreeModelExporter<T extends AbstractTreeNode> {
         m_treeModel = treeModel;
     }
 
+    /**
+     * Writes the KNIME tree model to a PMML tree model.
+     * @param treeModel the PMML node which should be filled with the KNIME model
+     * @param spec the {@link PMMLPortObjectSpec} of the full PMML document
+     * @return the {@link SchemaType} of the tree model
+     */
     public SchemaType writeModelToPMML(final TreeModel treeModel, final PMMLPortObjectSpec spec) {
         checkColumnTypes(spec);
         PMMLMiningSchemaTranslator.writeMiningSchema(spec, treeModel);
@@ -166,26 +149,17 @@ abstract class AbstractTreeModelExporter<T extends AbstractTreeNode> {
      * @param pmmlNode
      * @param node
      */
+    @SuppressWarnings("unchecked")
     private void addTreeNode(final Node pmmlNode, final T node) {
-        int index = m_nodeIndex++;
+        int index = m_nodeIndex;
+        m_nodeIndex++;
         pmmlNode.setId(Integer.toString(index));
         addNodeContent(index, pmmlNode, node);
 
 
         TreeNodeCondition condition = node.getCondition();
-        if (condition instanceof TreeNodeTrueCondition) {
-            pmmlNode.addNewTrue();
-        } else if (condition instanceof TreeNodeColumnCondition) {
-            final TreeNodeColumnCondition colCondition = (TreeNodeColumnCondition)condition;
-            handleColumnCondition(colCondition, pmmlNode);
-        } else if (condition instanceof AbstractTreeNodeSurrogateCondition) {
-            final AbstractTreeNodeSurrogateCondition surrogateCond = (AbstractTreeNodeSurrogateCondition)condition;
-            setValuesFromPMMLCompoundPredicate(pmmlNode.addNewCompoundPredicate(), surrogateCond.toPMMLPredicate());
-        } else {
-            throw new IllegalStateException(
-                "Unsupported condition (not implemented): " + condition.getClass().getSimpleName());
-        }
 
+        ConditionExporter.exportCondition(condition, pmmlNode);
 
         for (int i = 0; i < node.getNrChildren(); i++) {
             addTreeNode(pmmlNode.addNewNode(), (T)node.getChild(i));
@@ -200,153 +174,6 @@ abstract class AbstractTreeModelExporter<T extends AbstractTreeNode> {
      */
     protected abstract void addNodeContent(final int nodeId, Node pmmlNode, T node);
 
-    private static void handleColumnCondition(final TreeNodeColumnCondition condition, final Node pmmlNode) {
-        final PMMLPredicate knimePredicate = condition.toPMMLPredicate();
-        if (knimePredicate instanceof PMMLCompoundPredicate) {
-            setValuesFromPMMLCompoundPredicate(pmmlNode.addNewCompoundPredicate(),
-                (PMMLCompoundPredicate)knimePredicate);
-        } else if (knimePredicate instanceof PMMLSimplePredicate) {
-            setValuesFromPMMLSimplePredicate(pmmlNode.addNewSimplePredicate(), (PMMLSimplePredicate)knimePredicate);
-        } else if (knimePredicate instanceof PMMLSimpleSetPredicate) {
-            setValuesFromPMMLSimpleSetPredicate(pmmlNode.addNewSimpleSetPredicate(),
-                (PMMLSimpleSetPredicate)knimePredicate);
-        } else {
-            throw new IllegalArgumentException(
-                "A column condition can only contain compound, simple or simple set predicates.");
-        }
-    }
-
-    private static void setValuesFromPMMLSimplePredicate(final SimplePredicate to, final PMMLSimplePredicate from) {
-        to.setField(from.getSplitAttribute());
-        Operator.Enum operator;
-        final PMMLOperator op = from.getOperator();
-        switch (op) {
-            case EQUAL:
-                operator = Operator.EQUAL;
-                to.setValue(from.getThreshold());
-                break;
-            case GREATER_OR_EQUAL:
-                operator = Operator.GREATER_OR_EQUAL;
-                to.setValue(from.getThreshold());
-                break;
-            case GREATER_THAN:
-                operator = Operator.GREATER_THAN;
-                to.setValue(from.getThreshold());
-                break;
-            case IS_MISSING:
-                operator = Operator.IS_MISSING;
-                break;
-            case IS_NOT_MISSING:
-                operator = Operator.IS_NOT_MISSING;
-                break;
-            case LESS_OR_EQUAL:
-                operator = Operator.LESS_OR_EQUAL;
-                to.setValue(from.getThreshold());
-                break;
-            case LESS_THAN:
-                operator = Operator.LESS_THAN;
-                to.setValue(from.getThreshold());
-                break;
-            case NOT_EQUAL:
-                operator = Operator.NOT_EQUAL;
-                to.setValue(from.getThreshold());
-                break;
-            default:
-                throw new IllegalStateException("Unknown pmml operator \"" + op + "\".");
-        }
-        to.setOperator(operator);
-    }
-
-    private static void setValuesFromPMMLSimpleSetPredicate(final SimpleSetPredicate to,
-        final PMMLSimpleSetPredicate from) {
-        to.setField(from.getSplitAttribute());
-        final Enum operator;
-        final PMMLSetOperator setOp = from.getSetOperator();
-        switch (setOp) {
-            case IS_IN:
-                operator = SimpleSetPredicate.BooleanOperator.IS_IN;
-                break;
-            case IS_NOT_IN:
-                operator = SimpleSetPredicate.BooleanOperator.IS_NOT_IN;
-                break;
-            default:
-                throw new IllegalStateException("Unknown set operator \"" + setOp + "\".");
-        }
-        to.setBooleanOperator(operator);
-        final Set<String> values = from.getValues();
-        ArrayType array = to.addNewArray();
-        array.setN(BigInteger.valueOf(values.size()));
-        org.w3c.dom.Node arrayNode = array.getDomNode();
-        arrayNode.appendChild(arrayNode.getOwnerDocument().createTextNode(setToWhitspaceSeparatedString(values)));
-        final org.dmg.pmml.ArrayType.Type.Enum type;
-        final PMMLArrayType arrayType = from.getArrayType();
-        switch (arrayType) {
-            case INT:
-                type = ArrayType.Type.INT;
-                break;
-            case REAL:
-                type = ArrayType.Type.REAL;
-                break;
-            case STRING:
-                type = ArrayType.Type.STRING;
-                break;
-            default:
-                throw new IllegalStateException("Unknown array type \"" + arrayType + "\".");
-        }
-        array.setType(type);
-    }
-
-    private static void setValuesFromPMMLCompoundPredicate(final CompoundPredicate to,
-        final PMMLCompoundPredicate from) {
-        final PMMLBooleanOperator boolOp = from.getBooleanOperator();
-        switch (boolOp) {
-            case AND:
-                to.setBooleanOperator(CompoundPredicate.BooleanOperator.AND);
-                break;
-            case OR:
-                to.setBooleanOperator(CompoundPredicate.BooleanOperator.OR);
-                break;
-            case SURROGATE:
-                to.setBooleanOperator(CompoundPredicate.BooleanOperator.SURROGATE);
-                break;
-            case XOR:
-                to.setBooleanOperator(CompoundPredicate.BooleanOperator.XOR);
-                break;
-            default:
-                throw new IllegalStateException("Unknown boolean predicate \"" + boolOp + "\".");
-        }
-        final List<PMMLPredicate> predicates = from.getPredicates();
-        for (final PMMLPredicate predicate : predicates) {
-            if (predicate instanceof PMMLSimplePredicate) {
-                setValuesFromPMMLSimplePredicate(to.addNewSimplePredicate(), (PMMLSimplePredicate)predicate);
-            } else if (predicate instanceof PMMLSimpleSetPredicate) {
-                setValuesFromPMMLSimpleSetPredicate(to.addNewSimpleSetPredicate(), (PMMLSimpleSetPredicate)predicate);
-            } else if (predicate instanceof PMMLTruePredicate) {
-                to.addNewTrue();
-            } else if (predicate instanceof PMMLFalsePredicate) {
-                to.addNewFalse();
-            } else if (predicate instanceof PMMLCompoundPredicate) {
-                final CompoundPredicate compound = to.addNewCompoundPredicate();
-                final PMMLCompoundPredicate knimeCompound = (PMMLCompoundPredicate)predicate;
-                setValuesFromPMMLCompoundPredicate(compound, knimeCompound);
-            } else {
-                throw new IllegalStateException("Unknown predicate type \"" + predicate + "\".");
-            }
-        }
-    }
-
-    private static String setToWhitspaceSeparatedString(final Set<String> set) {
-        final StringBuilder sb = new StringBuilder();
-        for (final String string : set) {
-            sb.append("\"");
-            sb.append(string);
-            sb.append("\"");
-            sb.append(" ");
-        }
-        return sb.toString();
-    }
-
-
     private static boolean isMultiSplitRecursive(final AbstractTreeNode node) {
         final int nrChildren = node.getNrChildren();
         if (nrChildren > BINARY) {
@@ -360,6 +187,7 @@ abstract class AbstractTreeModelExporter<T extends AbstractTreeNode> {
         }
         return false;
     }
+
 
 
 }
