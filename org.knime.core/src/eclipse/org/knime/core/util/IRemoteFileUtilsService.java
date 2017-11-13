583586347a8d7c813a78701d8f2ec3a14d5ed086
Christian Dietz <christian.dietz@knime.com>
2017-11-13 15:17:20 +0100

    AP-7649: Extend IRemoteFileUtilsService
    now we can do more stuff using knime urls, e.g. asking if an URL exists,
    its size, date modified, create directories etc.

--- org.knime.core/src/eclipse/org/knime/core/util/IRemoteFileUtilsService.java
+++ org.knime.core/src/eclipse/org/knime/core/util/IRemoteFileUtilsService.java
@@ -72,4 +72,68 @@ public interface IRemoteFileUtilsService {
      * @throws IOException if an I/O error occurs while listing the children
      */
     List<URL> listRemoteFiles(URL root, Predicate<URL> filter, boolean recursive) throws IOException;
+
+    /**
+     * Check whether resource at given {@link URL} is a workflow group.
+     *
+     * @param url the {@link URL} of resource to check
+     * @return <source>true</source>, if resource is a workflow group
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    boolean isWorkflowGroup(URL url) throws IOException;
+
+    /**
+     * Deletes resource at given {@link URL}.
+     *
+     * @param url to delete.
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    void delete(URL url) throws IOException;
+
+    /**
+     * Returns size of resource at {@link URL} in bytes.
+     *
+     * @param url to get size from
+     * @return size of resource in bytes
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    long getSize(URL url) throws IOException;
+
+    /**
+     * Creates directory in resource at {@link URL}
+     *
+     * @param url {@link URL} pointing on location where the directory is created
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    void mkDir(URL url) throws IOException;
+
+    /**
+     * Get last modified date of {@link URL}.
+     *
+     * @param url {@link URL} pointing to resource in question
+     * @return Time in UNIX-Time (seconds since 01.01.1970) or 0 if the file does not exist
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    long lastModified(URL url) throws IOException;
+
+    /**
+     * Checks if a resource at a given {@link URL} exists.
+     *
+     * @param url {@link URL} pointing on location to check
+     * @return <source>true</source> if resource exists
+     * @throws IOException if an I/O error occurs during communication
+     *
+     * @since 3.5
+     */
+    boolean exists(URL url) throws IOException;
 }
