e2678b504df53ea4733ff2e2a93da608e32d773e
Adrian Nembach <adrian.nembach@knime.com>
2018-03-19 12:17:42 +0100

    AP-8347: Use Shuffler in ShuffleNodeModel
    AP-8347 (DL Keras: Add option to shuffle input batches / samples after
    each epoch)

--- org.knime.base/src/org/knime/base/node/preproc/shuffle/ShuffleNodeModel.java
+++ org.knime.base/src/org/knime/base/node/preproc/shuffle/ShuffleNodeModel.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * ------------------------------------------------------------------------
  *  Copyright by KNIME AG, Zurich, Switzerland
  *  Website: http://www.knime.com; Email: contact@knime.com
@@ -41,7 +41,7 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   01.08.2005 (cebron): created
  */
@@ -49,19 +49,16 @@ package org.knime.base.node.preproc.shuffle;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import org.knime.base.data.sort.SortedTable;
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
-import org.knime.core.data.container.ColumnRearranger;
 import org.knime.core.data.container.SingleCellFactory;
 import org.knime.core.data.def.IntCell;
+import org.knime.core.data.sort.Shuffler;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -76,21 +73,21 @@ import org.knime.core.node.NodeSettingsWO;
  * possible outcomes are possible and equally likely. The shuffling procedure
  * requires only linear runtime. For further details see "Fisher-Yates shuffle",
  * from Dictionary of Algorithms and Data Structures, Paul E. Black, ed., NIST.
- * 
+ *
  * @author Nicolas Cebron, University of Konstanz
  */
 public class ShuffleNodeModel extends NodeModel {
-    
+
     /** Config identifier for seed field. */
     static final String CFG_SEED = "random_seed";
-        
+
     /**
      * The seed to use or null to use always a different one.
      */
     private Long m_seed;
 
     /**
-     * 
+     *
      */
     public ShuffleNodeModel() {
         super(1, 1);
@@ -109,27 +106,8 @@ public class ShuffleNodeModel extends NodeModel {
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
             final ExecutionContext exec) throws Exception {
-        RandomNumberAppendFactory randomnumfac =
-                RandomNumberAppendFactory.create(m_seed, inData[0]);
-        ColumnRearranger colre =
-                new ColumnRearranger(inData[0].getDataTableSpec());
-        colre.append(randomnumfac);
-        BufferedDataTable intermediate =
-                exec.createColumnRearrangeTable(inData[0], colre, exec
-                        .createSubProgress(.2));
-        List<String> include = new ArrayList<String>();
-        String randomcol = randomnumfac.getColumnSpecs()[0].getName();
-        include.add(randomcol);
-        SortedTable sort =
-                new SortedTable(intermediate, include, new boolean[]{true},
-                        exec.createSubExecutionContext(.75));
-        BufferedDataTable sorted = sort.getBufferedDataTable();
-        colre = new ColumnRearranger(sorted.getDataTableSpec());
-        colre.remove(randomcol);
-        BufferedDataTable result =
-                exec.createColumnRearrangeTable(sorted, colre, exec
-                        .createSubProgress(.05));
-        return new BufferedDataTable[]{result};
+        Shuffler shuffler = new Shuffler(inData[0], m_seed == null ? new Random().nextLong() : m_seed);
+        return new BufferedDataTable[]{shuffler.shuffle(exec)};
     }
 
     /**
@@ -157,7 +135,7 @@ public class ShuffleNodeModel extends NodeModel {
      */
     @Override
     protected void reset() {
-        
+
     }
 
     /**
@@ -173,7 +151,7 @@ public class ShuffleNodeModel extends NodeModel {
      * {@inheritDoc}
      */
     @Override
-    protected void validateSettings(final NodeSettingsRO settings) 
+    protected void validateSettings(final NodeSettingsRO settings)
         throws InvalidSettingsException {
         // seed was not available in knime 1.1.2, backward compatibility
         String seedText = settings.getString(CFG_SEED, null);
@@ -186,7 +164,7 @@ public class ShuffleNodeModel extends NodeModel {
             }
         }
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -206,21 +184,21 @@ public class ShuffleNodeModel extends NodeModel {
             m_seed = null;
         }
     }
-    
+
     /*
      * The CellFactory adds a shuffled number to each input DataRow.
      */
-    private static final class RandomNumberAppendFactory 
+    private static final class RandomNumberAppendFactory
         extends SingleCellFactory {
 
         /** Shuffled row number array. */
         private int[] m_shuffle;
-        
+
         /** Position in array. */
         private int m_pos = 0;
-        
+
         /** Constructor. */
-        private RandomNumberAppendFactory(final Long seed, 
+        private RandomNumberAppendFactory(final Long seed,
                 final int rowCount, final DataColumnSpec appendSpec) {
             super(appendSpec);
             Random random;
@@ -230,13 +208,13 @@ public class ShuffleNodeModel extends NodeModel {
                 random = new Random();
             }
             int nrRows = rowCount;
-            
+
             // initialize
             m_shuffle = new int[nrRows];
             for (int i = 0; i < nrRows; i++) {
                 m_shuffle[i] = i;
             }
-            
+
             // let's shuffle
             for (int i = 0; i < m_shuffle.length; i++) {
                 int r = random.nextInt(i + 1);
@@ -245,7 +223,7 @@ public class ShuffleNodeModel extends NodeModel {
                 m_shuffle[i] = swap;
             }
        }
-        
+
         /** {@inheritDoc} */
         @Override
         public DataCell getCell(final DataRow row) {
@@ -254,9 +232,9 @@ public class ShuffleNodeModel extends NodeModel {
            m_pos++;
            return nextRandomNumberCell;
         }
-        
+
         /** Factory method to create a new random number append factory. */
-        private static RandomNumberAppendFactory create(final Long seed, 
+        private static RandomNumberAppendFactory create(final Long seed,
                 final BufferedDataTable inData) {
             final DataTableSpec spec = inData.getDataTableSpec();
             final int rowCount = inData.getRowCount();
