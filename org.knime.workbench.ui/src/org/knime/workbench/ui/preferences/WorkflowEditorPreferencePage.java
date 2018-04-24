9c116ec9b3ed49275c812773b5b16403a3aa2fee
Martin Horn <martin.horn@knime.com>
2018-04-24 09:16:47 +0200

    AP-8512: Labels in preferences page for remote job view settings changed
    AP-8512 (Status polling of opened jobs)
    https://knime-com.atlassian.net/browse/AP-8512

--- org.knime.workbench.ui/src/org/knime/workbench/ui/preferences/WorkflowEditorPreferencePage.java
+++ org.knime.workbench.ui/src/org/knime/workbench/ui/preferences/WorkflowEditorPreferencePage.java
@@ -130,8 +130,8 @@ public class WorkflowEditorPreferencePage extends FieldEditorPreferencePage impl
 
         addField(new HorizontalLineField(parent));
         addField(new LabelField(parent,
-            "Auto-refreshing of remotely opened jobs (has no effect for local worklows)"));
-        addField(new BooleanFieldEditor(PreferenceConstants.P_AUTO_REFRESH_WORKFLOW, "Auto-refresh remote jobs",
+            "Remote Job View auto-refresh settings"));
+        addField(new BooleanFieldEditor(PreferenceConstants.P_AUTO_REFRESH_WORKFLOW, "Auto-refresh Remote Job View",
             parent));
         addField(new IntegerFieldEditor(PreferenceConstants.P_AUTO_REFRESH_WORKFLOW_INTERVAL_MS,
             "Auto-refresh interval (in ms)", parent));
