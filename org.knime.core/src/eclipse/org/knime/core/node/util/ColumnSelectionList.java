4204f6b0e3fd647ea59fc85b6a74dc1e88de6ce5
Tobias Urhaug <tobias.urhaug@knime.com>
2018-06-08 15:13:13 +0200

    AP-9472: Moved FilterableListModel to ColumnSelectionList
    AP-9472 Call Workflow (Table Based)

--- org.knime.core/src/eclipse/org/knime/core/node/util/ColumnSelectionList.java
+++ org.knime.core/src/eclipse/org/knime/core/node/util/ColumnSelectionList.java
@@ -51,6 +51,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.DefaultListSelectionModel;
 import javax.swing.JList;
 import javax.swing.ListModel;
@@ -345,5 +346,143 @@ public class ColumnSelectionList extends JList {
      */
     public boolean isUserSelectionAllowed() {
         return m_selectionChangeAllowed;
+    }
+
+    /**
+     * A {@link ListModel} which provides a view on a subset of the model. The view can be created by
+     * {@link #filterIndices(int[])} and reset by {@link #showAll()}.
+     *
+     * @author Marcel Hanser, University of Konstanz
+     * @since 2.10
+     */
+    final static class FilterableListModel extends DefaultListModel {
+
+        private static final long serialVersionUID = 1L;
+
+        private final DefaultListModel m_originalObjects;
+
+        private boolean m_filtered = false;
+
+        /**
+         *
+         */
+        public FilterableListModel() {
+            super();
+            this.m_originalObjects = new DefaultListModel();
+        }
+
+        /**
+         * Creates a view containing the elements defined by the given index array of the original added elements.
+         *
+         * @param indices the indices to show
+         */
+        public void filterIndices(final int[] indices) {
+            super.clear();
+            m_filtered = true;
+            for (int i : indices) {
+                super.addElement(m_originalObjects.get(i));
+            }
+        }
+
+        /**
+         * Resets the view.
+         */
+        public void showAll() {
+            super.clear();
+            m_filtered = false;
+            for (int i = 0; i < m_originalObjects.size(); i++) {
+                super.addElement(m_originalObjects.get(i));
+            }
+        }
+
+        /**
+         * @return the original model containing all elements
+         */
+        public ListModel getUnfilteredModel() {
+            return m_originalObjects;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void clear() {
+            super.clear();
+            m_originalObjects.clear();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void removeElementAt(final int index) {
+            checkUnfiltered();
+            super.removeElementAt(index);
+            m_originalObjects.removeElementAt(index);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void addElement(final Object obj) {
+            super.addElement(obj);
+            m_originalObjects.addElement(obj);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean removeElement(final Object obj) {
+            super.removeElement(obj);
+            return m_originalObjects.removeElement(obj);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void removeAllElements() {
+            super.removeAllElements();
+            m_originalObjects.removeAllElements();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void add(final int index, final Object element) {
+            checkUnfiltered();
+            super.add(index, element);
+            m_originalObjects.add(index, element);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Object remove(final int index) {
+            checkUnfiltered();
+            super.remove(index);
+            return m_originalObjects.remove(index);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void removeRange(final int fromIndex, final int toIndex) {
+            checkUnfiltered();
+            super.removeRange(fromIndex, toIndex);
+            m_originalObjects.removeRange(fromIndex, toIndex);
+        }
+
+        private void checkUnfiltered() {
+            if (m_filtered) {
+                throw new IllegalStateException(
+                    "Managing the filtered model with indices while it is filtered leads to unexpected behavior");
+            }
+        }
     }
 }
