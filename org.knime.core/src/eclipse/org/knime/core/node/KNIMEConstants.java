770e852f9c926fcca8b17e889f1136452892c9cd
Thorsten Meinl <thorsten.meinl@knime.com>
2018-07-10 11:37:43 +0200

    Set build date

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -355,7 +355,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "July 11, 2018";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.product");
         if (coreBundle != null) {
