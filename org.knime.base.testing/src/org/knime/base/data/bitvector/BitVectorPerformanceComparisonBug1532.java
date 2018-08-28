3a3a0dfe8b54ca785841f6ae0f7989c4ba093cba
Marc Bux <marc.bux@knime.com>
2018-08-28 12:27:01 +0200

    AP-10258: Reduce amount but increase length of bit vectors in unit test
    AP-10258 (FIx failing unit test BitVectorPerformanceComparisonBug1532)

--- org.knime.base.testing/src/org/knime/base/data/bitvector/BitVectorPerformanceComparisonBug1532.java
+++ org.knime.base.testing/src/org/knime/base/data/bitvector/BitVectorPerformanceComparisonBug1532.java
@@ -88,33 +88,33 @@ public class BitVectorPerformanceComparisonBug1532 {
     @Test
     public void testCompareTanimotoTime() throws Throwable {
         final long seed = System.currentTimeMillis();
-        int size = 5000;
+        int size = 128;
         // NOTE (BW), 22 Oct 2008:
         // If you set the length to 2048 the test case seems to succeed
         // more often than with a random length (as below)
-        int length = 1000 + new Random(seed).nextInt(2000);
+        int length = 16384 + new Random(seed).nextInt(16384);
         NodeLogger.getLogger(BitVectorPerformanceComparisonBug1532.class).info("Using seed " + seed);
         DenseBitVectorCell[] newCells = createNewDenseBitVectorCells(
                 size, length, seed);
         BitVectorCell[] oldCells = createOldBitVectorCells(size, length, seed);
         double[] newValues = new double[size * (size - 1) / 2];
         double[] oldValues = new double[size * (size - 1) / 2];
-        long time = System.currentTimeMillis();
+        long time = System.nanoTime();
         int point = 0;
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 newValues[point++] = getNewTanimoto(newCells[i], newCells[j]);
             }
         }
-        long timeForNew = System.currentTimeMillis() - time;
-        time = System.currentTimeMillis();
+        long timeForNew = System.nanoTime() - time;
+        time = System.nanoTime();
         point = 0;
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 oldValues[point++] = getOldTanimoto(oldCells[i], oldCells[j]);
             }
         }
-        long timeForOld = System.currentTimeMillis() - time;
+        long timeForOld = System.nanoTime() - time;
         NodeLogger.getLogger(BitVectorPerformanceComparisonBug1532.class).info(
             "tanimoto calculation old: " + timeForOld);
         NodeLogger.getLogger(BitVectorPerformanceComparisonBug1532.class).info(
