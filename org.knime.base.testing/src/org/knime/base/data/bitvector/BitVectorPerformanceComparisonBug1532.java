0e46d4f6d0b789d837ab9b919b0a500decd4096f
Marc Bux <marc.bux@knime.com>
2018-08-29 16:17:33 +0200

    AP-10258: Fixed assertion error message from miliseconds to nanoseconds
    AP-10258 (FIx failing unit test BitVectorPerformanceComparisonBug1532)

--- org.knime.base.testing/src/org/knime/base/data/bitvector/BitVectorPerformanceComparisonBug1532.java
+++ org.knime.base.testing/src/org/knime/base/data/bitvector/BitVectorPerformanceComparisonBug1532.java
@@ -121,8 +121,8 @@ public class BitVectorPerformanceComparisonBug1532 {
             "tanimoto calculation new: " + timeForNew);
         /* Note: increased the factor to 2 according to ticket AP-9319. */
         assertTrue("Tanimoto calculation of new bit vector cells takes much " +
-                "longer than calculation on old (java) bit vectors: " + timeForNew + "ms vs. " + timeForOld + "ms",
-                timeForNew < 2 * timeForOld);
+                "longer than calculation on old (java) bit vectors: " + timeForNew + "ns vs. " + timeForOld + "ns",
+                timeForNew < 1.2 * timeForOld);
     }
 
 
