076319fa232cb2f89caedf744e261d9b8553374e
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2018-09-17 13:23:33 +0200

    (No ID) removed obsolete debug code

--- org.knime.core/src/eclipse/org/knime/core/data/container/Buffer.java
+++ org.knime.core/src/eclipse/org/knime/core/data/container/Buffer.java
@@ -76,7 +76,6 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.zip.Deflater;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
@@ -433,11 +432,6 @@ public class Buffer implements KNIMEStreamConstants {
      * null if this buffer is not in write-mode or it does not need to copy blobs.
      */
     private HashMap<BlobAddress, BlobAddress> m_copiedBlobsMap;
-
-    /** To debug AP-8469 -- leaking Buffer objects when running text processing test workflows. */
-    private final String m_fullStackTraceAtConstructionTime = Arrays.stream(Thread.currentThread().getStackTrace())
-            .map(s -> s.toString()).collect(Collectors.joining("\n  "));
-
 
     /**
      * Creates new buffer for <strong>writing</strong>. It has assigned a given spec, and a max row count that may
@@ -1698,8 +1692,6 @@ public class Buffer implements KNIMEStreamConstants {
         try {
             writeAllRowsFromListToFile();
             m_list = null; // don't write to internal cache any more
-        } catch (IllegalStateException ise) {
-            LOGGER.error(ise.getMessage() + "; Construction time call stack:\n" + m_fullStackTraceAtConstructionTime);
         } catch (IOException ioe) {
             LOGGER.error("Failed to swap to disc while freeing memory", ioe);
         }
