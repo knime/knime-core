37a08ed24c04e39b170f18ef5cec175d9274730f
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-10-24 14:45:40 +0200

    AP-10304: Add cell value consumers for Java primitive types
    AP-10304 (Unify Mapping/Conversion Frameworks)

--- org.knime.core/src/eclipse/org/knime/core/data/convert/map/CellValueConsumer.java
+++ org.knime.core/src/eclipse/org/knime/core/data/convert/map/CellValueConsumer.java
@@ -10,20 +10,20 @@ import org.knime.core.data.convert.map.Destination.ConsumerParameters;
  * @param <T> Type of Java value the consumer accepts
  * @param <CP> Subtype of {@link ConsumerParameters} that can be used to configure this consumer
  * @since 3.6
+ * @see CellValueProducer
  */
 @FunctionalInterface
 public interface CellValueConsumer<D extends Destination<?>, T, CP extends Destination.ConsumerParameters<D>> {
 
     /**
-     * Writes the <code>value</code> to <code>destination</code> using given <code>destinationParams</code>.
+     * Writes the given value to the given destination using the given parameters.
      *
      * @param destination The {@link Destination}.
      * @param value The value to write.
-     * @param consumerParams The parameters further specifying how to write to the destination, e.g. to which SQL
-     *            column or table to write. Specific to the type of {@link Destination} and {@link CellValueConsumer}
-     *            that is being used.
+     * @param params The parameters further specifying how to write to the destination, e.g. to which SQL column or
+     *            table to write. Specific to the type of {@link Destination} and {@link CellValueConsumer} that is
+     *            being used.
      * @throws MappingException If an exception occurs while consuming the cell value
      */
-    public void consumeCellValue(final D destination, final T value, final CP consumerParams)
-        throws MappingException;
+    public void consumeCellValue(final D destination, final T value, final CP params) throws MappingException;
 }
---
+++ org.knime.core/src/eclipse/org/knime/core/data/convert/map/PrimitiveCellValueConsumer.java
@@ -0,0 +1,78 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Oct 24, 2018 (marcel): created
+ */
+package org.knime.core.data.convert.map;
+
+import org.knime.core.data.convert.map.Destination.ConsumerParameters;
+
+/**
+ * Base interface for consumers that accept values of a certain Java primitive type and write them to a
+ * {@link Destination} as a certain external type.
+ *
+ * @param <D> Type of {@link Destination} to which this consumer writes.
+ * @param <T> The wrapper type of the Java primitive values that this consumer accepts. E.g., {@link Integer} for the
+ *            Java primitive type {@code int}.
+ * @param <CP> Subtype of {@link ConsumerParameters} that can be used to configure this consumer.
+ * @since 3.7
+ * @author Marcel Wiedenmann, KNIME GmbH, Konstanz, Germany
+ * @see CellValueConsumer
+ */
+public interface PrimitiveCellValueConsumer<D extends Destination<?>, T, CP extends Destination.ConsumerParameters<D>>
+    extends CellValueConsumer<D, T, CP> {
+
+    /**
+     * Writes a missing value to the given destination using the given parameters.
+     *
+     * @param destination The {@link Destination} to which to write the missing value.
+     * @param params The parameters further specifying how to write to the destination, e.g., to which SQL column or
+     *            table to write. Specific to the type of {@link Destination} and {@link CellValueConsumer} that is
+     *            being used.
+     * @throws MappingException If an exception occurs while consuming the missing value.
+     */
+    void consumeMissingCellValue(D destination, CP params) throws MappingException;
+}
