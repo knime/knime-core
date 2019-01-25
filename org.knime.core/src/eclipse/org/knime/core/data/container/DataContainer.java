38d309124763eca3e7c19461ba166d1709979bc6
Marc Bux <marc.bux@knime.com>
2019-01-25 16:28:59 +0100

    AP-11264: Fix new fileStoreHandler not being initialized properly by DC
    AP-11264 (Sorting table in table view causing documents to be shown as
    missing values )

--- org.knime.core/src/eclipse/org/knime/core/data/container/DataContainer.java
+++ org.knime.core/src/eclipse/org/knime/core/data/container/DataContainer.java
@@ -856,6 +856,7 @@ public class DataContainer implements RowAppender {
     protected IWriteFileStoreHandler getFileStoreHandler() {
         if (m_fileStoreHandler == null) {
             m_fileStoreHandler = NotInWorkflowWriteFileStoreHandler.create();
+            m_fileStoreHandler.addToRepository(getDataRepository());
         }
         return m_fileStoreHandler;
     }
