cf19562acace5b6507094535f0c8c361c36a60d1
Tobias Urhaug <tobias.urhaug@knime.com>
2019-07-10 15:31:58 +0200

    AP-12159 Removed class from API
    AP-12159 (AP does not prompt for initial workspace location on first startup)

--- org.knime.core/src/eclipse/org/knime/core/node/workflow/NodeContextDomestique.java
+++ org.knime.core/src/eclipse/org/knime/core/node/workflow/NodeContextDomestique.java
@@ -60,6 +60,7 @@ import org.knime.core.node.workflow.NodeContext.ContextObjectSupplier;
  *
  * @author loki der quaeler
  * @since 4.0
+ * @noreference This class is not intended to be referenced by clients.
  */
 public class NodeContextDomestique {
     //   !!!!!!!!!!!!!!
