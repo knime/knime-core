73ca157910a5bb9242f79bb3b13accf182dd0d94
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2019-09-30 11:12:06 +0200

    build date now to 1nd Oct

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -403,7 +403,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "October 02, 2019";
+        BUILD_DATE = "October 01, 2019";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.product");
         if (coreBundle != null) {
