f01d515f4eadbbd98cc161d46b1d5b0bfcc263a7
loki der quaeler <loki@theori.st>
2019-10-30 10:36:09 -0700

    AP-12959: Marking KAP Shell
    - Providing a method to mark the KAP shell which should be invoked as part of the product's workbench post-startup
    - Providing a method to get the marked KAP shell
    
    AP-12959 (Add visual indicator for nodes whose ports are configurable)
    https://knime-com.atlassian.net/browse/AP-12959

--- org.knime.core.ui/src/eclipse/org/knime/core/ui/util/SWTUtilities.java
+++ org.knime.core.ui/src/eclipse/org/knime/core/ui/util/SWTUtilities.java
@@ -57,6 +57,8 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 
+import org.eclipse.core.runtime.IProduct;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
@@ -80,12 +82,67 @@ import org.knime.core.node.NodeLogger;
  * @since 3.7
  */
 public class SWTUtilities {
+    private static final String KAP_SHELL_DATA_KEY = "knime.rcp.application.shell";
+
+    /**
+     * This method is invoked from {@code KNIMEApplicationWorkbenchAdvisor#postStartup()}.
+     *
+     * There's a fair philosophical argument of "if in some edge cases, the shell at this point is null and then we
+     *      need rely on matching its name to the product, why not always just do that search, as opposed to doing
+     *      the data-marking here and searching on the data-marking later." My response would be that even if we're
+     *      forced to walk the shells at this point (thank you very much, SWT) as we've just launched, the chances
+     *      that some other random shell starting with the product name is also present is much lower (like zero) now
+     *      than at a later time during the application's lifespan.
+     */
+    public static void markKNIMEShell() {
+        Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
+
+        if (shell == null) {
+            // shell would be null at this point if the application was backgrounded while it was launching.. oh SWT.
+            final IProduct product = Platform.getProduct();
+            for (final Shell s : PlatformUI.getWorkbench().getDisplay().getShells()) {
+                if (s.getText().startsWith(product.getName())) {
+                    shell = s;
+                    break;
+                }
+            }
+
+            if (shell == null) {
+                NodeLogger.getLogger(SWTUtilities.class).error("Unable to mark the KAP shell.");
+
+                return;
+            }
+        }
+
+        shell.setData(KAP_SHELL_DATA_KEY, new Object());
+    }
+
+    /**
+     * This method can be called after application startup has completed in order to get the {@link Shell} that
+     *  is the parent of the main KAP workbench.
+     *
+     * @return the {@link Shell} containing the main KAP workbench, or null if we failed to mark one at start-up.
+     */
+    public static Shell getKNIMEWorkbenchShell() {
+        final Display d = PlatformUI.getWorkbench().getDisplay();
+
+        for (final Shell shell : d.getShells()) {
+            if (shell.getData(KAP_SHELL_DATA_KEY) != null) {
+                return shell;
+            }
+        }
+
+        return null;
+    }
+
     /**
      * As discovered in AP-10122, when KNIME receives an action due to mouse event passthrough (a situation possible on
      * macOS where mouse & scroll events can pass to a desktop-visible window even though the owning application is not
      * the application in the foreground), Display.getCurrent().getActiveShell() returns null.
      *
      * This method will attempt to return the applicable Shell instance, by hook or by crook.
+     *
+     * If you're considering calling this, perhaps you really want to call {@link #getKNIMEWorkbenchShell()}.
      *
      * @param display an instance of Display which contains the active shell; if this is null, this method will return
      *            null
@@ -99,10 +156,11 @@ public class SWTUtilities {
         final Shell shell = display.getActiveShell();
 
         if (shell == null) {
+            final IProduct product = Platform.getProduct();
             Shell likelyActiveShell = null;
 
             for (Shell s : display.getShells()) {
-                if (s.getText().contains("KNIME")) {
+                if (s.getText().startsWith(product.getName())) {
                     return s;
                 }
 
