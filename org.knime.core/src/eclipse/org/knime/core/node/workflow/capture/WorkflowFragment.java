8ac098c8241908bd7c2d27e7863813813bfa7ddf
Martin Horn <martin.horn@knime.com>
2020-01-08 16:40:48 +0100

    AP-13296: WorkflowFragment-serialization moved and improved
    * de-/serialization moved to WorkflowFragment itself
    * loading of WorkflowManager only on demand (but not on port object
    copy) because a tmp directory is created each time
    * dispose workflow method added to WorkflowFragment
    * workflow metadata saved in one zip entry (including the workflow's
    name)
    
    AP-13296 (Workflow to build Workflows framework)

--- org.knime.core/src/eclipse/org/knime/core/node/workflow/capture/WorkflowFragment.java
+++ org.knime.core/src/eclipse/org/knime/core/node/workflow/capture/WorkflowFragment.java
@@ -48,13 +48,41 @@
  */
 package org.knime.core.node.workflow.capture;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.knime.core.data.util.NonClosableInputStream;
+import org.knime.core.data.util.NonClosableOutputStream;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.ModelContent;
+import org.knime.core.node.ModelContentRO;
+import org.knime.core.node.ModelContentWO;
+import org.knime.core.node.config.Config;
 import org.knime.core.node.port.PortType;
 import org.knime.core.node.workflow.NodeID;
 import org.knime.core.node.workflow.NodeID.NodeIDSuffix;
+import org.knime.core.node.workflow.UnsupportedWorkflowVersionException;
+import org.knime.core.node.workflow.WorkflowContext;
+import org.knime.core.node.workflow.WorkflowLoadHelper;
 import org.knime.core.node.workflow.WorkflowManager;
+import org.knime.core.node.workflow.WorkflowPersistor.WorkflowLoadResult;
+import org.knime.core.node.workflow.WorkflowSaveHelper;
+import org.knime.core.util.FileUtil;
+import org.knime.core.util.LockFailedException;
 
 /**
  * @author Marc Bux, KNIME GmbH, Berlin, Germany
@@ -62,7 +90,12 @@ import org.knime.core.node.workflow.WorkflowManager;
  */
 public final class WorkflowFragment {
 
-    private final WorkflowManager m_wfm;
+    //cached workflow manager
+    private WorkflowManager m_wfm = null;
+
+    private byte[] m_wfmStream = null;
+
+    private final String m_name;
 
     private final Set<NodeIDSuffix> m_portObjectReferenceReaderNodes;
 
@@ -81,16 +114,73 @@ public final class WorkflowFragment {
     public WorkflowFragment(final WorkflowManager wfm, final List<Port> inputPorts, final List<Port> outputPorts,
         final Set<NodeIDSuffix> portObjectReferenceReaderNodes) {
         m_wfm = wfm;
+        m_name = wfm.getName();
+        m_inputPorts = inputPorts;
+        m_outputPorts = outputPorts;
+        m_portObjectReferenceReaderNodes = portObjectReferenceReaderNodes;
+    }
+
+    private WorkflowFragment(final byte[] wfmStream, final String name, final List<Port> inputPorts,
+        final List<Port> outputPorts, final Set<NodeIDSuffix> portObjectReferenceReaderNodes) {
+        m_wfmStream = wfmStream;
+        m_name = name;
         m_inputPorts = inputPorts;
         m_outputPorts = outputPorts;
         m_portObjectReferenceReaderNodes = portObjectReferenceReaderNodes;
     }
 
     /**
+     * Loads the workflow representing the fragment.
+     *
+     * Already call {@link #disposeWorkflow()} if the returned workflow manager is not needed anymore!
+     *
+     * This method (i.e. lazily loading the workflow) might become unnecessary in the future once the workflow manager
+     * can be de-/serialized directly to/from a stream.
+     *
      * @return the workflow manager representing the fragment
      */
-    public WorkflowManager getWorkflow() {
+    public WorkflowManager loadWorkflow() {
+        if (m_wfm == null) {
+            File tmpDir = null;
+            try {
+                tmpDir = FileUtil.createTempDir("workflow_fragment");
+                ZipInputStream in = new ZipInputStream(new ByteArrayInputStream(m_wfmStream));
+                FileUtil.unzip(in, tmpDir, 1);
+                WorkflowLoadHelper loadHelper =
+                    new WorkflowLoadHelper(new WorkflowContext.Factory(tmpDir).createContext());
+                WorkflowLoadResult loadResult;
+                loadResult =
+                    WorkflowManager.EXTRACTED_WORKFLOW_ROOT.load(tmpDir, new ExecutionMonitor(), loadHelper, false);
+                m_wfm = loadResult.getWorkflowManager();
+            } catch (InvalidSettingsException | CanceledExecutionException | UnsupportedWorkflowVersionException
+                    | LockFailedException | IOException ex) {
+                // should never happen
+                throw new IllegalStateException("Failed loading workflow port object", ex);
+            } finally {
+                if (tmpDir != null) {
+                    FileUtil.deleteRecursively(tmpDir.getAbsoluteFile());
+                }
+            }
+        }
         return m_wfm;
+    }
+
+    /**
+     * Disposes the workflow manager cached by this fragment (either loaded via {@link #loadWorkflow()} or passed to the
+     * constructor). Removes it from the workflow hierarchy and the local reference.
+     */
+    public void disposeWorkflow() {
+        if (m_wfm != null) {
+            WorkflowManager.EXTRACTED_WORKFLOW_ROOT.removeNode(m_wfm.getID());
+            m_wfm = null;
+        }
+    }
+
+    /**
+     * @return the workflow name as stored with the fragment's metadata
+     */
+    public String getName() {
+        return m_name;
     }
 
     /**
@@ -134,6 +224,121 @@ public final class WorkflowFragment {
     public PortType getOutPortType(final Port p) {
         NodeID nodeID = p.getNodeIDSuffix().prependParent(m_wfm.getID());
         return m_wfm.getNodeContainer(nodeID).getOutPort(p.getIndex()).getPortType();
+    }
+
+    /**
+     * Saves the workflow fragment to a zip output stream.
+     *
+     * @param out the stream to write to
+     * @throws IOException if serializing of the workflow fails (output stream will not be closed)
+     */
+    public void save(final ZipOutputStream out) throws IOException {
+        out.putNextEntry(new ZipEntry("metadata.xml"));
+        ModelContent metadata = new ModelContent("metadata.xml");
+        metadata.addString("name", m_name);
+
+        ModelContentWO refNodeIds = metadata.addModelContent("ref_node_ids");
+        refNodeIds.addInt("num_ids", m_portObjectReferenceReaderNodes.size());
+        int i = 0;
+        for (NodeIDSuffix id : m_portObjectReferenceReaderNodes) {
+            refNodeIds.addIntArray("ref_node_id_" + i, id.getSuffixArray());
+            i++;
+        }
+
+        ModelContentWO inputPorts = metadata.addModelContent("input_ports");
+        savePorts(inputPorts, m_inputPorts);
+
+        ModelContentWO outputPorts = metadata.addModelContent("output_ports");
+        savePorts(outputPorts, m_outputPorts);
+
+        metadata.saveToXML(new NonClosableOutputStream.Zip(out));
+
+        if (m_wfmStream == null) {
+            if (m_wfm == null) {
+                //only happens if WorkflowFragment is instantiated with a WorkflowManager
+                //and #disposeWorkflow() is called before #save(...)
+                throw new IllegalStateException("Can't save workflow fragment. Workflow has been disposed already.");
+            }
+            m_wfmStream = wfmToStream(m_wfm);
+        }
+        out.putNextEntry(new ZipEntry("workflow.bin"));
+        out.write(m_wfmStream);
+        out.closeEntry();
+    }
+
+    private static byte[] wfmToStream(final WorkflowManager wfm) throws IOException {
+        File tmpDir = FileUtil.createTempDir("workflow_fragment");
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); ZipOutputStream out = new ZipOutputStream(bos);) {
+            WorkflowSaveHelper saveHelper = new WorkflowSaveHelper(false, false);
+            wfm.save(tmpDir, saveHelper, new ExecutionMonitor());
+            FileUtil.zipDir(out, Collections.singleton(tmpDir), FileUtil.ZIP_INCLUDEALL_FILTER, null);
+            bos.flush();
+            return bos.toByteArray();
+        } catch (LockFailedException | CanceledExecutionException | IOException e) {
+            throw new IOException("Failed saving workflow port object", e);
+        } finally {
+            FileUtil.deleteRecursively(tmpDir.getAbsoluteFile());
+        }
+    }
+
+    /**
+     * Loads a workflow fragment from an zip input stream.
+     *
+     * @param in the stream to read from
+     * @return a new {@link WorkflowFragment} (input stream is not closed)
+     * @throws IOException if a problem occurred while reading (input stream will not be closed)
+     */
+    public static WorkflowFragment load(final ZipInputStream in) throws IOException {
+        ZipEntry entry = in.getNextEntry();
+        if (!entry.getName().equals("metadata.xml")) {
+            throw new IOException("Expected metadata.xml file in stream, got " + entry.getName());
+        }
+        InputStream noneCloseIn = new NonClosableInputStream.Zip(in);
+        ModelContentRO metadata = ModelContent.loadFromXML(noneCloseIn);
+
+        try {
+            ModelContentRO refNodeIds = metadata.getModelContent("ref_node_ids");
+            Set<NodeIDSuffix> ids = new HashSet<NodeID.NodeIDSuffix>();
+            int numIds = refNodeIds.getInt("num_ids");
+            for (int i = 0; i < numIds; i++) {
+                ids.add(new NodeIDSuffix(refNodeIds.getIntArray("ref_node_id_" + i)));
+            }
+            ModelContentRO model = metadata.getModelContent("input_ports");
+            List<Port> inputPorts = loadPorts(model);
+
+            model = metadata.getModelContent("output_ports");
+            List<Port> outputPorts = loadPorts(model);
+
+            entry = in.getNextEntry();
+            if (!entry.getName().equals("workflow.bin")) {
+                throw new IOException("Expected workflow.bin file in stream, got " + entry.getName());
+            }
+
+            byte[] wfmStream = IOUtils.toByteArray(in);
+
+            return new WorkflowFragment(wfmStream, metadata.getString("name"), inputPorts, outputPorts, ids);
+        } catch (InvalidSettingsException e) {
+            throw new IOException("Failed loading workflow port object", e);
+        }
+    }
+
+    private static void savePorts(final ModelContentWO model, final List<Port> ports) {
+        model.addInt("num_ports", ports.size());
+        for (int i = 0; i < ports.size(); i++) {
+            Config portConf = model.addConfig("port_" + i);
+            portConf.addString("node_id", ports.get(i).getNodeIDSuffix().toString());
+            portConf.addInt("index", ports.get(i).getIndex());
+        }
+    }
+
+    private static List<Port> loadPorts(final ModelContentRO model) throws InvalidSettingsException {
+        int size = model.getInt("num_ports");
+        List<Port> ports = new ArrayList<WorkflowFragment.Port>(size);
+        for (int i = 0; i < size; i++) {
+            Config portConf = model.getConfig("port_" + i);
+            ports.add(new Port(NodeIDSuffix.fromString(portConf.getString("node_id")), portConf.getInt("index")));
+        }
+        return ports;
     }
 
     /**
