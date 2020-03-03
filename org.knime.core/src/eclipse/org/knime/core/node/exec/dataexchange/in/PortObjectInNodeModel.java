7705e8bb0fffe0c904eef4bad37d4cade11081b8
Bjoern Lohrmann <bjoern.lohrmann@knime.com>
2020-03-04 00:05:53 +0100

    AP-13735: Remove FSConnection flow variable type
    AP-13735 (Remove FSConnection flow variable type)

--- org.knime.core/src/eclipse/org/knime/core/node/exec/dataexchange/in/PortObjectInNodeModel.java
+++ org.knime.core/src/eclipse/org/knime/core/node/exec/dataexchange/in/PortObjectInNodeModel.java
@@ -58,7 +58,6 @@ import org.knime.core.data.container.DataContainer;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.ExecutionMonitor;
-import org.knime.core.node.FSConnectionFlowVariableProvider;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.Node;
 import org.knime.core.node.NodeModel;
@@ -200,7 +199,6 @@ public final class PortObjectInNodeModel extends NodeModel {
             throws InvalidSettingsException {
         PortObjectIDSettings s = new PortObjectIDSettings();
         s.setCredentialsProvider(getCredentialsProvider());
-        s.setFSConnectionFlowVariableProvider(new FSConnectionFlowVariableProvider(this));
         s.loadSettings(settings);
     }
 
@@ -210,7 +208,6 @@ public final class PortObjectInNodeModel extends NodeModel {
             throws InvalidSettingsException {
         PortObjectIDSettings s = new PortObjectIDSettings();
         s.setCredentialsProvider(getCredentialsProvider());
-        s.setFSConnectionFlowVariableProvider(new FSConnectionFlowVariableProvider(this));
         s.loadSettings(settings);
         m_portObjectIDSettings = s;
     }
