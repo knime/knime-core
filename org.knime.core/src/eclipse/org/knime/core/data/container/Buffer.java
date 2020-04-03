79548928d2e4982a33e56f63391994fe968fda09
Marc Bux <marc.bux@knime.com>
2020-04-03 09:45:23 +0200

    AP-13847: Remove performClear method as it is no longer needed
    AP-13847 (Table does not unregister memory alert listeners on clear)

--- org.knime.core/src/eclipse/org/knime/core/data/container/Buffer.java
+++ org.knime.core/src/eclipse/org/knime/core/data/container/Buffer.java
@@ -1942,10 +1942,6 @@ public class Buffer implements KNIMEStreamConstants {
 
     /** Clears the temp file. Any subsequent iteration will fail! */
     synchronized void clear() {
-        m_lifecycle.onClear();
-    }
-
-    void performClear() {
         /** lock clear flag to prevent concurrent clearing or asynchronous writing to this buffer */
         synchronized (m_isClearedLock) {
             if (!m_isClearedLock.booleanValue()) {
@@ -1985,6 +1981,8 @@ public class Buffer implements KNIMEStreamConstants {
                 m_blobDir = null;
             }
         }
+
+        m_lifecycle.onClear();
     }
 
     private static final int MAX_FILES_TO_CREATE_BEFORE_GC = 10000;
@@ -2543,7 +2541,7 @@ public class Buffer implements KNIMEStreamConstants {
         void onCloseIfCached();
 
         /**
-         * Synchronously called before clearing this buffer.
+         * Synchronously called after clearing this buffer.
          */
         void onClear();
 
@@ -2609,8 +2607,6 @@ public class Buffer implements KNIMEStreamConstants {
                 MemoryAlertSystem.getInstance().removeListener(m_memoryAlertListener);
                 m_memoryAlertListener = null;
             }
-
-            performClear();
         }
 
         @Override
@@ -2705,8 +2701,6 @@ public class Buffer implements KNIMEStreamConstants {
         @Override
         public void onClear() {
             assert Thread.holdsLock(Buffer.this);
-
-            performClear();
 
             if (m_memoryAlertListener != null) {
                 MemoryAlertSystem.getInstanceUncollected().removeListener(m_memoryAlertListener);
