ea9bb54ea32728fa3096d5b460f8ae1c8d18a4f2
Marc Bux <marc.bux@knime.com>
2020-06-04 15:38:56 +0200

    AP-13760: Add test for undeleted files after loop reset
    AP-13760 (Temporary table and file stores in loops not deleted on clear)

---
+++ org.knime.core.tests/src/org/knime/core/node/workflow/BugAP13760_TemporaryFilesInLoopNotDeletedOnClear.java
@@ -0,0 +1,29 @@
+package org.knime.core.node.workflow;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class BugAP13760_TemporaryFilesInLoopNotDeletedOnClear extends WorkflowTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		loadAndSetWorkflow();
+	}
+
+	@Test
+	public void testExecutePlain() throws Exception {
+		executeAllAndWait();
+		final WorkflowManager manager = getManager();
+		manager.getParent().resetAndConfigureNode(getManager().getID());
+		final File tempDir = manager.getContext().getTempLocation();
+		assertEquals(String.format("Files remaining in workflow temp directory after clear: %s.",
+				Arrays.toString(tempDir.list())), 0, countFilesInDirectory(tempDir));
+	}
+
+}
