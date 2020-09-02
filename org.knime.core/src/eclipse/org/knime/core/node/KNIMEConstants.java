9a83bf6c338e47d5bc566050437b3f1595b1258a
Marc Bux <marc.bux@knime.com>
2020-09-02 11:15:25 +0200

    Revert "AP-14947: Re-introduce nightly flag"
    This reverts commit 9caafcdee3f5f387cdb9bb10aecf6e0505e42629.

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -412,7 +412,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "August 26, 2020";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
