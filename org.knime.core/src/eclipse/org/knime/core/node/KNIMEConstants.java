7e552c1010c9cd234b263a233c91b1db9d590fbb
Marc Bux <marc.bux@knime.com>
2020-12-10 18:46:12 +0100

    AP-15833: Version bump for 4.2.4 release

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -412,7 +412,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "November 04, 2020";
+        BUILD_DATE = "December 11, 2020";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
