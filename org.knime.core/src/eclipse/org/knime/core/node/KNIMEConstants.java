b224d0bdbb1df7da5ad80d41b176a651b0b18568
Marc Bux <marc.bux@knime.com>
2021-01-26 17:33:28 +0100

    AP-15985: Version bump for 4.3.1 release

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -420,7 +420,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "January 29, 2021";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
