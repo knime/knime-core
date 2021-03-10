c9daa4d722fefb790c0c42efa613e8841edbbd7a
Marc Bux <marc.bux@knime.com>
2021-03-10 10:37:40 +0100

    AP-16327: Re-introduce nightly flag

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -412,7 +412,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "March 04, 2021";
+        BUILD_DATE = "Nightly build";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
