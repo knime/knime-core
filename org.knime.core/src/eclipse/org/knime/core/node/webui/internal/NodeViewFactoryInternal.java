bf6bcf570e594bcfb1b9c9a2da1806d8b0ae98e1
Martin Horn <martin.horn@knime.com>
2021-08-31 16:33:39 +0200

    NXT-655: Interfaces for web-based node views added
    * pending API!
    * many interfaces are not only for node views but for ui-extensions in
    general (e.g. port views and node dialogs, too)
    
    NXT-655 (API and implementation to register node views)

---
+++ org.knime.core/src/eclipse/org/knime/core/node/webui/internal/NodeViewFactoryInternal.java
@@ -0,0 +1,109 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Aug 25, 2021 (hornm): created
+ */
+package org.knime.core.node.webui.internal;
+
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.webui.NodeView;
+import org.knime.core.node.webui.Page;
+import org.knime.core.node.workflow.NativeNodeContainer;
+import org.knime.core.node.workflow.NodeContainer;
+
+/**
+ * Implemented by {@link NodeFactory}s to register internal(!) node views. Must not be extended by third-party
+ * contributions.
+ *
+ * Pending API!
+ *
+ * @noreference This interface is not intended to be referenced by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ *
+ * @author Martin Horn, KNIME GmbH, Konstanz, Germany
+ * @param <T> the node model this node view will have access to
+ */
+public interface NodeViewFactoryInternal<T extends NodeModel> {
+
+    /**
+     * @param nc the node container to check
+     * @return whether the node container provides a {@link NodeView}
+     */
+    static boolean hasNodeView(final NodeContainer nc) {
+        return nc instanceof NativeNodeContainer
+            && ((NativeNodeContainer)nc).getNode().getFactory() instanceof NodeViewFactoryInternal;
+    }
+
+    /**
+     * Creates a {@link NodeView} from the given node container.
+     *
+     * Make sure to call {@link #hasNodeView(NodeContainer)} first.
+     *
+     * @param nc the node container create the node view from
+     * @return a new node view instance
+     * @throws IllegalArgumentException if the passed node container does not provide a node view
+     */
+    @SuppressWarnings("unchecked")
+    static NodeView<NodeModel> createNodeView(final NodeContainer nc) {
+        if (nc instanceof NativeNodeContainer) {
+            NodeFactory<NodeModel> fac = ((NativeNodeContainer)nc).getNode().getFactory();
+            if (fac instanceof NodeViewFactoryInternal) {
+                return ((NodeViewFactoryInternal<NodeModel>)fac).createNodeView();
+            }
+        }
+        throw new IllegalArgumentException("The node " + nc.getNameWithID() + " doesn't provide a node view");
+    }
+
+    /**
+     * Creates a new node view instance. The {@link Page} provided by internal node views actually represents a
+     * vue-component (i.e. a js-file) provided at 'runtime' (and <b>not</b> a html file).
+     *
+     * @return a new node view instance
+     */
+    NodeView<T> createNodeView();
+
+}
---
+++ org.knime.core/src/eclipse/org/knime/core/node/webui/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Interfaces and classes the are used for the web UI, e.g. for so called ui-extensions such as port views, node views
+ * or node dialogs.
+ *
+ * Pending API!
+ *
+ * @author Martin Horn, KNIME GmbH, Konstanz, Germany
+ * @since 4.5
+ */
+package org.knime.core.node.webui;
