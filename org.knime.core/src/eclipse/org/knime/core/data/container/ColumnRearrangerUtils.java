2f8e614d85f7a75ec27b653e3a5fdc3f27457a31
Adrian Nembach <adrian.nembach@knime.com>
2022-01-10 11:34:56 +0100

    AP-18197: Add ColumnRearrangerUtils#extractRearrangedColumns

--- org.knime.core/src/eclipse/org/knime/core/data/container/ColumnRearrangerUtils.java
+++ org.knime.core/src/eclipse/org/knime/core/data/container/ColumnRearrangerUtils.java
@@ -48,6 +48,11 @@
  */
 package org.knime.core.data.container;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.knime.core.data.DataCellTypeConverter;
+import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.container.ColumnRearranger.SpecAndFactoryObject;
 import org.knime.core.node.util.CheckUtils;
@@ -89,8 +94,126 @@ public final class ColumnRearrangerUtils {
     }
 
     /**
-     * Extracts the original column indices (= index in the original table) for the columns in the rearranger.
-     * NOTE: Only use if the rearranger doesn't add new columns
+     * @param rearranger the rearranger from which to extract the rearranged columns
+     * @return the rearranged columns of the rearranger
+     * @since 5.1
+     */
+    public static List<RearrangedColumn> extractRearrangedColumns(final ColumnRearranger rearranger) {
+        int i = 0;//NOSONAR
+        var rearrangedColumns = new ArrayList<RearrangedColumn>();
+        int newColIdx = 0;
+        for (var specAndFac : rearranger.getIncludes()) {
+            if (specAndFac.isNewColumn() || specAndFac.isConvertedColumn()) {
+                rearrangedColumns.add(new RearrangedColumn(specAndFac, i, newColIdx));
+                newColIdx++;
+            } else {
+                rearrangedColumns.add(new RearrangedColumn(specAndFac, i, -1));
+            }
+            i++;
+        }
+        return rearrangedColumns;
+    }
+
+    /**
+     * Represents a rearranged column in a ColumnRearranger.
+     *
+     * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
+     * @since 5.1
+     */
+    public static final class RearrangedColumn {
+        private final SpecAndFactoryObject m_specAndFactory;
+
+        private final int m_globalIndex;
+
+        private final int m_newColIdx;
+
+        private RearrangedColumn(final SpecAndFactoryObject specAndFactory, final int globalIndex,
+            final int newColIdx) {
+            m_specAndFactory = specAndFactory;
+            m_globalIndex = globalIndex;
+            m_newColIdx = newColIdx;
+        }
+
+        /**
+         * @return the CellFactory if {@link #isNewColumn()} else null
+         */
+        public CellFactory getCellFactory() {
+            return m_specAndFactory.getFactory();
+        }
+
+        /**
+         * @return the converter if {@link #isConvertedColumn()} else null
+         */
+        public DataCellTypeConverter getConverter() {
+            return m_specAndFactory.getConverter();
+        }
+
+        /**
+         * Only call if {@link #isConvertedColumn()}.
+         *
+         * @return the index of the column that is converted
+         */
+        public int getConverterIndex() {
+            return m_specAndFactory.getConverterIndex();
+        }
+
+        /**
+         * Only call if {@link #isNewColumn()}.
+         *
+         * @return the index of the column inside the columns produced by the corresponding CellFactory
+         */
+        public int getIndexInFactory() {
+            return m_specAndFactory.getColumnInFactory();
+        }
+
+        /**
+         * @return indicates whether this is a new column created with a CellFactory
+         */
+        public boolean isNewColumn() {
+            return m_specAndFactory.isNewColumn();
+        }
+
+        /**
+         * @return the index among the appended columns if the column {@link #isNewColumn()} or
+         *         {@link #isConvertedColumn()} otherwise -1
+         */
+        public int getNewColIndex() {
+            return m_newColIdx;
+        }
+
+        /**
+         * @return indicates whether this is a column converted with a DataCellTypeConverter
+         */
+        public boolean isConvertedColumn() {
+            return m_specAndFactory.isConvertedColumn();
+        }
+
+        /**
+         * @return the original index of the column or -1 if {@link #isNewColumn()} or {@link #isConvertedColumn()}
+         */
+        public int getOriginalIndex() {
+            return m_specAndFactory.getOriginalIndex();
+        }
+
+        /**
+         * @return the index of the column in the output
+         */
+        public int getGlobalIndex() {
+            return m_globalIndex;
+        }
+
+        /**
+         * @return the DataColumnSpec of the column
+         */
+        public DataColumnSpec getColumnSpec() {
+            return m_specAndFactory.getColSpec();
+        }
+
+    }
+
+    /**
+     * Extracts the original column indices (= index in the original table) for the columns in the rearranger. NOTE:
+     * Only use if the rearranger doesn't add new columns
      *
      * @param rearranger to extract the column indices from
      * @return the original column indices
