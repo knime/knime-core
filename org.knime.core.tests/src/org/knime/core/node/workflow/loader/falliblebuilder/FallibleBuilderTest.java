67364b3b93b47df2551a8ff0ffc37a724f4a1863
Carl Witt <carl.witt@knime.com>
2022-03-31 22:13:56 +0200

    Test range validation and required fields in fallible builders.

--- org.knime.core.tests/src/org/knime/core/node/workflow/loader/falliblebuilder/FallibleBuilderTest.java
+++ org.knime.core.tests/src/org/knime/core/node/workflow/loader/falliblebuilder/FallibleBuilderTest.java
@@ -1,43 +1,66 @@
 package org.knime.core.node.workflow.loader.falliblebuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
-import org.knime.core.workflow.def.impl.BaseNodeDefBuilder;
+import org.knime.core.workflow.def.ConfigValueByteDef;
 import org.knime.core.workflow.def.impl.ConfigValueByteDefBuilder;
+import org.knime.core.workflow.def.impl.FallibleConfigValueByteDef;
 import org.knime.core.workflow.loader.LoadException;
 
 /**
- * Test basic functionality of builders that collect exceptions in their setters. This includes accessing exceptions of
- * nested objects.
+ * Test basic functionality of builders that collect exceptions in their setters.
+ *
+ * Note that when a required value is not provided, this also does not abort the load process. It will add an exception
+ * to the constructed element. Similarly, a value that is out of an allowed value range will have an exception
+ * associated to it.
  *
  * @author Carl Witt, KNIME AG, Zurich, Switzerland
  */
 public class FallibleBuilderTest {
 
     /**
-     * Test that building fails when passing null to a required field.
+     * Test that constructing a Def attribute with a value that it is out of its range constraint doesn't stop the load
+     * process but is noted as a {@link LoadException}.
      */
     @Test
-    public void testRequiredField() {
-        // explicitly set null
-        final BaseNodeDefBuilder builder = new BaseNodeDefBuilder().setId(null);
-        assertThrows(IllegalArgumentException.class, () -> builder.build());
+    public void testRangeValidationExceptional() {
+        // overflow during build
+        final var byteValueOverflow = new ConfigValueByteDefBuilder().setValue(Integer.MAX_VALUE).build();
+        // causes load exception
+        assertThat(byteValueOverflow.hasExceptions()).isTrue();
+        // stored here
+        assertThat(byteValueOverflow.getValueException()).containsInstanceOf(LoadException.class);
+        // but the originally provided value is kept
+        assertThat(byteValueOverflow.getValue()).isEqualTo(Integer.MAX_VALUE);
 
-        // supplier yield null
-        final BaseNodeDefBuilder builder2 = new BaseNodeDefBuilder().setId(() -> null, 3);
-        assertThrows(IllegalArgumentException.class, () -> builder2.build());
+        // overflow during build
+        final var byteValueOverflow2 = new ConfigValueByteDefBuilder().setValue(() -> Integer.MAX_VALUE, 13).build();
+        // causes load exception
+        assertThat(byteValueOverflow2.hasExceptions()).isTrue();
+        // stored here
+        assertThat(byteValueOverflow2.getValueException()).containsInstanceOf(LoadException.class);
+        // but the originally provided value is kept
+        assertThat(byteValueOverflow2.getValue()).isEqualTo(13);
 
-        // supplier default is null
-        final BaseNodeDefBuilder builder3 = new BaseNodeDefBuilder().setId(() -> {
-            throw new Exception();
-        }, null);
-        assertThrows(IllegalArgumentException.class, () -> builder3.build());
+        // setting null to a numerical value
+        final var byteValueNull = new ConfigValueByteDefBuilder().setConfigType("ConfigValue").setValue(null).build();
+        // assuming a value range only applies when the value is present, no load exception is recorded
+        assertThat(byteValueNull.hasExceptions()).isFalse();
+        assertThat(byteValueNull.getValue()).isNull();
+    }
 
-        // never called
-        final BaseNodeDefBuilder builder4 = new BaseNodeDefBuilder();
-        assertThrows(IllegalArgumentException.class, () -> builder4.build());
+    /**
+     * The non-exceptional case for range validation.
+     *
+     * @see #testRangeValidationExceptional()
+     */
+    @Test
+    public void testRangeValidationDefault() {
+        // overflow during build
+        final var byteValueDef = new ConfigValueByteDefBuilder().setConfigType("ConfigValue").setValue(12).build();
+        assertThat(byteValueDef.hasExceptions()).isFalse();
+        assertThat(byteValueDef.getValue()).isEqualTo(12);
     }
 
     /**
@@ -45,15 +68,48 @@ public class FallibleBuilderTest {
      * process but is noted as a {@link LoadException}.
      */
     @Test
-    public void testRangeValidation() {
-        // overflow during build
-        final var byteValueDef = new ConfigValueByteDefBuilder().setValue(Integer.MAX_VALUE).build();
-        // causes load exception
-        assertThat(byteValueDef.hasExceptions()).isTrue();
-        // stored here
-        assertThat(byteValueDef.getValueException()).containsInstanceOf(LoadException.class);
-        // default value is used instead: minimum range value
-        assertThat(byteValueDef.getValue()).isEqualTo(-128);
+    public void testRequiredFieldExceptional() {
+        // three ways to construct a def with a null value for a required field
+        final var faultyDefs = new FallibleConfigValueByteDef[]{
+            // fall back to null value
+            new ConfigValueByteDefBuilder().setConfigType(() -> {
+                throw new IllegalStateException();
+            }, null).build(),
+            // set null for required value
+            new ConfigValueByteDefBuilder().setConfigType(null).build(),
+            // setter never called
+            new ConfigValueByteDefBuilder().build()//
+        };
+
+        for (int i = 0; i < faultyDefs.length; i++) {
+            // causes load exception
+            assertThat(faultyDefs[i].hasExceptions()).as("Def at index " + i).isTrue();
+            assertThat(faultyDefs[i].getConfigTypeException()).containsInstanceOf(LoadException.class);
+        }
+    }
+
+    /**
+     * The non-exceptional case for required field validation.
+     *
+     * @see #testRequiredFieldExceptional()
+     */
+    @Test
+    public void testRequiredFieldDefault() {
+        final var goodDef = new ConfigValueByteDefBuilder().setConfigType("ConfigValue").build();
+        assertThat(goodDef.hasExceptions()).isFalse();
+
+        final var semiGoodDef = new ConfigValueByteDefBuilder()//
+            // set the required field
+            .setConfigType("ConfigValue")
+            // add an exception to a non-required field
+            .setValue(() -> {
+                throw new Exception();
+            }, null).build();
+        // no problem with the required field
+        assertThat(semiGoodDef.hasExceptions(ConfigValueByteDef.Attribute.CONFIG_TYPE)).isFalse();
+        // also not on the convenience getter
+        assertThat(semiGoodDef.getConfigTypeException()).isEmpty();
+
     }
 
 }
