1339a9b77460e5867d7a0b57c54199f44816254f
swaraj pal singh <swaraj.singh@knime.com>
2022-08-03 12:46:22 +0200

    restored the nightly build flag.

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -465,7 +465,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "July 27, 2022";
+        BUILD_DATE = "Nightly build";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
