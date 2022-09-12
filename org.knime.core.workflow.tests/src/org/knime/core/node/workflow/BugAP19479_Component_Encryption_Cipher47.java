4415d1f876757cbf08f726a05a8f53052c8bdf82
Leonard Wörteler <leonard.woerteler@knime.com>
2022-09-12 13:51:25 +0200

    AP-19479: Test to make sure that 4.7 ciphers are unreadable
    AP-19479 (Copy-pasting a locked Component circumvents password protection)

---
+++ org.knime.core.workflow.tests/src/org/knime/core/node/workflow/BugAP19479_Component_Encryption_Cipher47.java
@@ -0,0 +1,82 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ */
+package org.knime.core.node.workflow;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.workflow.WorkflowPersistor.LoadResultEntry.LoadResultEntryType;
+
+/**
+ * Workflow created in AP 4.7 containing a single locked component.
+ *
+ * @author Leonard Wörteler, KNIME GmbH, Konstanz, Germany
+ */
+public class BugAP19479_Component_Encryption_Cipher47 extends WorkflowTestCase {
+
+    /**
+     * Tries to load a workflow that contains a locked component created in AP 4.7.
+     * This should fail for every version <= 4.6.1.
+     */
+    @Test
+    public void load47LockedComponentIn46Test() throws Exception {
+        final var workflowDir = getDefaultWorkflowDirectory();
+        final var result = loadWorkflow(workflowDir, new ExecutionMonitor());
+        final var wfm = result.getWorkflowManager();
+        setManager(wfm);
+
+        assertTrue("Loading should produce errors.", result.hasErrors());
+        assertTrue("Error message should indicate that cipher could not be initialized.",
+                result.getFilteredError("", LoadResultEntryType.Error).contains("Unable to init cipher"));
+        final NodeID lockedID = wfm.getID().createChild(2);
+        final var locked = wfm.getNodeContainer(lockedID);
+        assertTrue("Locked component should exist.", locked instanceof SubNodeContainer);
+        final var wfm2 = ((SubNodeContainer) locked).getWorkflowManager();
+        assertEquals("Only input and output should be present.", 2, wfm2.getNodeContainers().size());
+    }
+
+}
