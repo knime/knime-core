3ec4de4069c91a7d5cfc17e798f96acd360b6c47
Marc Bux <marc.bux@knime.com>
2022-09-27 12:16:05 +0200

    Version bump for 4.6.3

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -465,7 +465,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "September 30, 2022";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
