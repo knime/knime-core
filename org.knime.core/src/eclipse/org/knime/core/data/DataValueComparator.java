924ed6560936d64d92668f5bbcb5b5afaad31601
Adrian Nembach <adrian.nembach@knime.com>
2022-10-10 17:48:04 +0200

    Expose API to compare DataValues in DataValueComparator
    Only operated on DataCell so far but there is already a protected method
    that does the comparison of DataValues.

--- org.knime.core/src/eclipse/org/knime/core/data/DataValueComparator.java
+++ org.knime.core/src/eclipse/org/knime/core/data/DataValueComparator.java
@@ -41,7 +41,7 @@
  *  may freely choose the license terms applicable to such Node, including
  *  when such Node is propagated with or for interoperation with KNIME.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   07.07.2005 (mb): created
  *   21.06.06 (bw & po): reviewed
@@ -51,29 +51,29 @@ package org.knime.core.data;
 import java.util.Comparator;
 
 /**
- * The comparator used to compare two {@link org.knime.core.data.DataValue} 
+ * The comparator used to compare two {@link org.knime.core.data.DataValue}
  * objects. An instance of this class is returned by each
  * {@link org.knime.core.data.DataValue.UtilityFactory} implementation.
  * Implementations can safely assume that objects passed to the
- * {@link #compareDataValues(DataValue, DataValue)} method support a type cast 
- * to that type of the {@link org.knime.core.data.DataValue} returning this 
+ * {@link #compareDataValues(DataValue, DataValue)} method support a type cast
+ * to that type of the {@link org.knime.core.data.DataValue} returning this
  * comparator.
- * 
+ *
  * <p>
  * Note: this comparator imposes orderings that are inconsistent with equals.
- * That is, the return value zero is not equivalent to equals returning 
- * <code>true</code>, it rather says &quot;don't know&quot;. Meaning, if two 
+ * That is, the return value zero is not equivalent to equals returning
+ * <code>true</code>, it rather says &quot;don't know&quot;. Meaning, if two
  * <code>DataValue</code>s cannot be compared (because they represent completely
- * different value types) the comparator will return zero - and equals will 
+ * different value types) the comparator will return zero - and equals will
  * return <code>false</code>.)
- * 
+ *
  * @see org.knime.core.data.DataValue.UtilityFactory#getComparator()
  * @author Michael Berthold, University of Konstanz
  */
 public abstract class DataValueComparator implements Comparator<DataCell> {
 
     /**
-     * Create a new {@link org.knime.core.data.DataValue} comparator. This 
+     * Create a new {@link org.knime.core.data.DataValue} comparator. This
      * constructor does nothing.
      */
     protected DataValueComparator() {
@@ -81,30 +81,31 @@ public abstract class DataValueComparator implements Comparator<DataCell> {
     }
 
     /**
-     * This final implementation checks and handles 
-     * {@link org.knime.core.data.DataCell}s representing missing values, 
-     * before it delegates the actual comparison 
-     * to the derived class, by calling 
+     * This final implementation checks and handles
+     * {@link org.knime.core.data.DataCell}s representing missing values,
+     * before it delegates the actual comparison
+     * to the derived class, by calling
      * {@link #compareDataValues(DataValue, DataValue)}.
-     * Missing cells are considered to be smaller than any other non-missing 
-     * {@link org.knime.core.data.DataCell}, and two missing cells are not 
-     * comparable (returns 0). Note: return value 0 is not equivalent to 
+     * Missing cells are considered to be smaller than any other non-missing
+     * {@link org.knime.core.data.DataCell}, and two missing cells are not
+     * comparable (returns 0). Note: return value 0 is not equivalent to
      * <code>c1</code> equals <code>c2</code>, rather they are not comparable at
      * all.
-     * 
-     * @param c1 the first {@link org.knime.core.data.DataCell} to compare the 
+     *
+     * @param c1 the first {@link org.knime.core.data.DataCell} to compare the
      *           other with
-     * @param c2 the other {@link org.knime.core.data.DataCell} to compare the 
+     * @param c2 the other {@link org.knime.core.data.DataCell} to compare the
      *           first with
-     * @return a negative number if <code>c1</code> is smaller than 
-     *         <code>c2</code>, a positive number if <code>c1</code> is larger 
+     * @return a negative number if <code>c1</code> is smaller than
+     *         <code>c2</code>, a positive number if <code>c1</code> is larger
      *         than <code>c2</code>, otherwise zero
-     * 
+     *
      * @throws NullPointerException if any of the objects is <code>null</code>
-     * 
+     *
      * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
      * @see #compareDataValues(DataValue, DataValue)
      */
+    @Override
     public final int compare(final DataCell c1, final DataCell c2) {
         if (c1.isMissing()) {
             if (c2.isMissing()) {
@@ -123,21 +124,25 @@ public abstract class DataValueComparator implements Comparator<DataCell> {
     }
 
     /**
-     * Do not call this function, rather call the 
+     * Do not call this function, rather call the
      * {@link #compare(DataCell, DataCell)} method, which handles missing cells.
-     * The derived class should compare the two passed <code>DataValue</code>s. 
-     * It can be safely assumed that both values are castable to the specific 
-     * type of the {@link org.knime.core.data.DataValue} that returned this 
-     * comparator. 
-     * 
-     * @param v1 the first {@link org.knime.core.data.DataValue} to compare the 
+     * The derived class should compare the two passed <code>DataValue</code>s.
+     * It can be safely assumed that both values are castable to the specific
+     * type of the {@link org.knime.core.data.DataValue} that returned this
+     * comparator.
+     *
+     * @param v1 the first {@link org.knime.core.data.DataValue} to compare the
      *           other with
-     * @param v2 the other {@link org.knime.core.data.DataValue} to compare the 
+     * @param v2 the other {@link org.knime.core.data.DataValue} to compare the
      *           first with
-     * @return return -1 if <code>v1</code> is smaller than <code>v2</code>, +1 
+     * @return return -1 if <code>v1</code> is smaller than <code>v2</code>, +1
      *         if <code>v1</code> is larger than <code>v2</code>, 0 otherwise
      */
     protected abstract int compareDataValues(
             final DataValue v1, final DataValue v2);
+
+    public int compareValues(final DataValue v1, final DataValue v2) {
+        return compareDataValues(v1, v2);
+    }
 
 }
