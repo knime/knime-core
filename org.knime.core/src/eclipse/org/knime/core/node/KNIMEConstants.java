9f119d3df200bfcb8c351feeb8d587835c98e588
swaraj pal singh <swaraj.singh@knime.com>
2023-11-16 12:16:21 +0100

    Replaced the nightly flag with the Release date

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -465,7 +465,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "December 06, 2023";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
