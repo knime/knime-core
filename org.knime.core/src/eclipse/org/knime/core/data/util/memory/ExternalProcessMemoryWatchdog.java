ffaf4de5447d02d70199fd590f1aaf0f0054ed8b
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-07-24 11:10:00 +0200

    AP-22948: Add the ExternalProcessMemoryWatchdog
    That periodically tracks the memory usage of external processes and
    makes sure that the total memory allocated by them does not surpass
    a threshold.
    
    AP-22948 (Add watchdog to AP that kills external processes' that use too much memory)

---
+++ org.knime.core/src/eclipse/org/knime/core/data/util/memory/ExternalProcessMemoryWatchdog.java
@@ -0,0 +1,268 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jul 23, 2024 (benjamin): created
+ */
+package org.knime.core.data.util.memory;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.LongConsumer;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.knime.core.node.NodeLogger;
+
+import gnu.trove.map.hash.TObjectIntHashMap;
+
+/**
+ * Watchdog that tracks the memory usage of external processes. If the total memory usage of all external processes
+ * surpasses a threshold, the process with the highest memory usage gets killed forcibly. The watchdog uses the
+ * proportional set size (PSS) of the processes and their subprocesses to determine their memory usage.
+ *
+ * @author Benjamin Wilhelm, KNIME GmbH, Berlin, Germany
+ * @author Carsten Haubold, KNIME GmbH, Konstanz, Germany
+ * @since 5.4
+ */
+public final class ExternalProcessMemoryWatchdog {
+
+    private static final NodeLogger LOGGER = NodeLogger.getLogger(ExternalProcessMemoryWatchdog.class);
+
+    private static final long POLLING_INTERVAL_MS = Long.getLong("knime.externalprocesswatchdog.pollinginterval", 100);
+
+    private static final int MAX_MEMORY_KBYTES =
+        Integer.getInteger("knime.externalprocesswatchdog.maxmemory", 1024 << 10);
+
+    private static final ExternalProcessMemoryWatchdog INSTANCE = new ExternalProcessMemoryWatchdog();
+
+    /**
+     * @return the singleton instance of the watchdog
+     */
+    public static ExternalProcessMemoryWatchdog getInstance() {
+        return INSTANCE;
+    }
+
+    //#region API
+
+    /**
+     * Start tracking the given external process. If the total memory usage of external processes surpasses a threshold,
+     * the process that uses most memory gets killed forcibly ({@link ProcessHandle#destroyForcibly()}). The
+     * <code>killCallback</code> is called for this process.
+     * <P>
+     * Note that the memory usage of the process and all subprocesses is tracked.
+     *
+     * @param process a handle for the process
+     * @param killCallback a callback that gets called before a process is killed by the watchdog. The argument of the
+     *            callback is the current memory usage of the process in kilo-bytes. The callback must not block. The
+     *            killing of the process cannot be prevented by freeing up memory. The callback must only be used to
+     *            record the reason why the process was killed.
+     */
+    public void trackProcess(final ProcessHandle process, final LongConsumer killCallback) {
+        var existingkillCallback = m_processesToKillCallbacks.putIfAbsent(process, killCallback);
+        if (existingkillCallback != null) {
+            throw new IllegalArgumentException("The process " + process + " is already being tracked.");
+        }
+    }
+
+    //#endregion
+
+    //#region INSTANCE
+
+    private ConcurrentMap<ProcessHandle, LongConsumer> m_processesToKillCallbacks = new ConcurrentHashMap<>();
+
+    private ExternalProcessMemoryWatchdog() {
+        // We only track memory usage on Linux systems that support PSS measurements
+        if (PSSUtil.supportsPSS()) {
+            var timer = new Timer("KNIME External Process Watchdog", true); // Daemon thread
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    updateMemoryUsage();
+                }
+            }, 0, POLLING_INTERVAL_MS);
+        } else if (SystemUtils.IS_OS_LINUX) {
+            LOGGER.warn(
+                "PSS measurements are not supported on this system. The external process memory watchdog is disabled.");
+        }
+    }
+
+    private void updateMemoryUsage() {
+        var memoryState = collectMemoryUsageState();
+
+        // Check if the total memory usage surpasses the threshold
+        if (memoryState.getTotalMemoryUsage() > MAX_MEMORY_KBYTES) {
+            killProcessWithHighestMemoryUsage(memoryState);
+        }
+    }
+
+    /** Collect the memory usage of all tracked external processes. Removes dead processes from the tracking. */
+    private ExternalProcessMemoryState collectMemoryUsageState() {
+        var memoryState = new ExternalProcessMemoryState();
+
+        var iterator = m_processesToKillCallbacks.entrySet().iterator();
+        while (iterator.hasNext()) {
+            var entry = iterator.next();
+            var process = entry.getKey();
+
+            // If the process is dead we do not need to track it anymore - remove it from the map
+            if (!process.isAlive()) {
+                iterator.remove();
+                continue;
+            }
+
+            // Collect the current memory usage of the process
+            memoryState.put(process, getMemoryUsage(process));
+        }
+
+        return memoryState;
+    }
+
+    /** Kill the process with the highest memory usage. */
+    private void killProcessWithHighestMemoryUsage(final ExternalProcessMemoryState memoryState) {
+        var processToKill = memoryState.getProcessWithMaxMemoryUsage();
+
+        // Call the kill callback
+        var killCallback = m_processesToKillCallbacks.remove(processToKill);
+        if (killCallback != null) {
+            try {
+                killCallback.accept(memoryState.getMemoryUsage(processToKill));
+            } catch (Throwable ex) { // NOSONAR: We want to make sure the callback cannot crash the watchdog
+                LOGGER.error("Error in kill callback for process " + processToKill + ".", ex);
+            }
+        } else {
+            // NB: This should never happen
+            LOGGER.error("No kill callback found for process " + processToKill + ". Killing it without callback.");
+        }
+
+        // Kill the process
+        processToKill.destroyForcibly();
+    }
+
+    //#endregion
+
+    //#region STATIC UTILS
+
+    /**
+     * Get the memory usage of the given process and all its subprocesses in kb. Uses the PSS of the individual
+     * processes.
+     *
+     * @param process the process handle
+     * @return proportional set size (PSS) of this process and all its subprocesses in kb or 0 if the memory usage could
+     *         not be determined
+     */
+    private static int getMemoryUsage(final ProcessHandle process) {
+        var childrenMem = process.children().mapToInt(ExternalProcessMemoryWatchdog::getMemoryUsage).sum();
+        try {
+            return childrenMem + PSSUtil.getPSS(process.pid());
+        } catch (IOException e) {
+            LOGGER.warn("Failed to get memory usage of external process with pid " + process.pid() + ".", e);
+            return 0;
+        }
+    }
+
+    //#endregion
+
+    //#region ExternalProcessMemoryState
+
+    /** Utility class to store and collect the memory usage of external processes. Not thread-safe! */
+    private static final class ExternalProcessMemoryState {
+
+        private final TObjectIntHashMap<ProcessHandle> m_processToMemoryUsage;
+
+        private int m_totalMemoryUsage;
+
+        private int m_maxMemoryUsage;
+
+        private ProcessHandle m_maxMemoryUsageProcess;
+
+        public ExternalProcessMemoryState() {
+            m_processToMemoryUsage = new TObjectIntHashMap<>();
+            m_totalMemoryUsage = 0;
+            m_maxMemoryUsage = -1;
+        }
+
+        /**
+         * Collect the memory usage of the given process.
+         *
+         * @param process the process
+         * @param memoryUsage the memory usage
+         */
+        public void put(final ProcessHandle process, final int memoryUsage) {
+            m_processToMemoryUsage.put(process, memoryUsage);
+
+            // Update the total memory usage
+            m_totalMemoryUsage += memoryUsage;
+
+            // Update the max memory usage
+            if (memoryUsage > m_maxMemoryUsage) {
+                m_maxMemoryUsage = memoryUsage;
+                m_maxMemoryUsageProcess = process;
+            }
+        }
+
+        /**
+         * @param process the process
+         * @return the memory usage of the given process
+         */
+        public int getMemoryUsage(final ProcessHandle process) {
+            return m_processToMemoryUsage.get(process);
+        }
+
+        /** @return the total memory usage of all processes */
+        public int getTotalMemoryUsage() {
+            return m_totalMemoryUsage;
+        }
+
+        /** @return the process with the highest memory usage */
+        public ProcessHandle getProcessWithMaxMemoryUsage() {
+            return m_maxMemoryUsageProcess;
+        }
+    }
+
+    //#endregion
+}
---
+++ org.knime.core/src/eclipse/org/knime/core/data/util/memory/PSSUtil.java
@@ -0,0 +1,197 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jul 23, 2024 (benjamin): created
+ */
+package org.knime.core.data.util.memory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.knime.core.node.NodeLogger;
+
+/**
+ * Utility class for parsing the PSS (Proportional Set Size) from the /proc filesystem on Linux systems. The PSS is a
+ * accurate measure of the memory usage of a process. It accounts for shared memory by dividing it evenly among the
+ * processes that share it.
+ * <P>
+ * Description from the proc filesystem documentation: <blockquote> The “proportional set size” (PSS) of a process is
+ * the count of pages it has in memory, where each page is divided by the number of processes sharing it. So if a
+ * process has 1000 pages all to itself, and 1000 shared with one other process, its PSS will be 1500. </blockquote>
+ *
+ *
+ * @author Benjamin Wilhelm, KNIME GmbH, Berlin, Germany
+ * @see <a href="https://docs.kernel.org/filesystems/proc.html">proc filesystem documentation</a>
+ * @since 5.4
+ */
+final class PSSUtil {
+
+    private PSSUtil() {
+    }
+
+    private static final NodeLogger LOGGER = NodeLogger.getLogger(PSSUtil.class);
+
+    /** Indicates if the system has smaps_rollup files (the file was added in 2017) */
+    private static final boolean HAS_PROC_SMAPS_ROLLUP = hasProcSmaps(true);
+
+    /**
+     * Indicates if the system has smaps file (does not exist on kernels < 2.6.14 or if CONFIG_MMU kernel configuration
+     * option is not enabled)
+     */
+    private static final boolean HAS_PROC_SMAPS = hasProcSmaps(false);
+
+    /**
+     * The buffer size used for reading files in the /proc filesystem. We use a larger buffer size than the default
+     * because the file will change between each read.
+     * <P>
+     * See https://github.com/giampaolo/psutil/blob/c034e6692cf736b5e87d14418a8153bb03f6cf42/psutil/_common.py#L783-L795
+     */
+    private static final int PROC_FILE_BUFFER_SIZE = 32 * 1024;
+
+    /**
+     * Check if the system supports PSS (Proportional Set Size) measurements. This is only the case on Linux systems
+     * that have the /proc/[pid]/smaps_rollup or /proc/[pid]/smaps file.
+     *
+     * @return <code>true</code> if the system supports PSS measurements
+     */
+    public static boolean supportsPSS() {
+        return SystemUtils.IS_OS_LINUX && (HAS_PROC_SMAPS_ROLLUP || HAS_PROC_SMAPS);
+    }
+
+    /**
+     * Returns the PSS (Proportional Set Size) of the process with the given pid in KB. Uses the /proc filesystem to
+     * determine the PSS. If the kernel supports it, the PSS is read from the /proc/[pid]/smaps_rollup file.
+     *
+     * @param pid the process id
+     * @return the PSS of the process with the given pid in KB
+     * @throws IOException if the /proc/[pid]/smaps file could not be read
+     * @throws UnsupportedOperationException if the PSS is not available on the current platform (call
+     *             {@link #supportsPSS()} to check this beforehand)
+     */
+    public static int getPSS(final long pid) throws IOException {
+        if (!SystemUtils.IS_OS_LINUX) {
+            throw new UnsupportedOperationException("PSS is only available on Linux");
+        }
+
+        if (HAS_PROC_SMAPS_ROLLUP) {
+            try {
+                return readPSSFromSmapsRollup(pid);
+            } catch (final IOException ex) {
+                // NB: Only debug log because we will try the smaps file next
+                LOGGER.debug("Failed to read PSS from smaps_rollup file", ex);
+            }
+        }
+        if (HAS_PROC_SMAPS) {
+            return readPSSFromSmaps(pid);
+        }
+        throw new UnsupportedOperationException(
+            "PSS is not available on this platform. Because the /proc/[pid]/smaps file is not present.");
+    }
+
+    private static int readPSSFromSmapsRollup(final long pid) throws IOException {
+        try (var reader = readFile("/proc/" + pid + "/smaps_rollup")) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Pss:")) {
+                    // NB: Can return immediately because the PSS is only present once in the file
+                    return parsePSSLine(line);
+                }
+            }
+        }
+        throw new IOException("PSS not found in smaps_rollup file");
+    }
+
+    private static int readPSSFromSmaps(final long pid) throws IOException {
+        var totalPss = 0;
+        try (var reader = readFile("/proc/" + pid + "/smaps")) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Pss:")) {
+                    totalPss += parsePSSLine(line);
+                }
+            }
+        }
+        return totalPss;
+    }
+
+    private static BufferedReader readFile(final String path) throws IOException {
+        // NB: The text files in /proc only contain ascii characters
+        return new BufferedReader(new FileReader(path, StandardCharsets.US_ASCII), PROC_FILE_BUFFER_SIZE);
+    }
+
+    /** Parses the PSS memory from a line of the smaps or smaps_rollup file */
+    private static int parsePSSLine(final String line) {
+        // The line looks like "Pss: 1234 kB"
+        var colonIndex = line.indexOf(':');
+        var kbIndex = line.indexOf('k', colonIndex);
+        if (colonIndex != -1 && kbIndex != -1) {
+            var numberStr = line.substring(colonIndex + 1, kbIndex).trim();
+            try {
+                return Integer.parseInt(numberStr);
+            } catch (NumberFormatException e) {
+                // Ignore parse errors
+                // Might happen if the number is just at the border of the buffer and the file was changed between reads
+                // We just ignore the line and continue reading
+            }
+        }
+        return 0;
+    }
+
+    /** Utility to determine if the system generally has smaps_rollup files */
+    private static boolean hasProcSmaps(final boolean rollup) {
+        if (!SystemUtils.IS_OS_LINUX) {
+            return false;
+        }
+        var selfPid = ProcessHandle.current().pid();
+        var smapsPath = Path.of("/proc", "" + selfPid, rollup ? "smaps_rollup" : "smaps");
+        return Files.exists(smapsPath);
+    }
+}
