5fff70ee54f190de871a169c7c402d913a0a7a21
swaraj pal singh <swaraj.singh@knime.com>
2024-09-12 11:54:47 +0200

    DEVOPS-2639 : Reverted the nightly flag

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -465,7 +465,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "September 11, 2024";
+        BUILD_DATE = "Nightly build";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
