b57bf3b0d0e1aa9f7d44e7f0dca795cf6231b51e
Manuel Hotz <manuel.hotz@knime.com>
2024-09-13 11:52:07 +0200

    AP-10222: Close workflow log appender _after_ it is removed from each Log4j logger
    AP-10222 (Attempted to append to closed appender while logging)

--- org.knime.core/src/eclipse/org/knime/core/node/logging/DelegatingLogger.java
+++ org.knime.core/src/eclipse/org/knime/core/node/logging/DelegatingLogger.java
@@ -367,7 +367,6 @@ final class DelegatingLogger {
             synchronized (WF_APPENDER) {
                 final var appender = WF_APPENDER.remove(workflowDirPath);
                 if (appender != null) {
-                    appender.close();
                     //Remove the appender from all open node loggers
                     @SuppressWarnings("unchecked")
                     final Enumeration<Logger> allLoggers =
@@ -375,6 +374,9 @@ final class DelegatingLogger {
                     while (allLoggers.hasMoreElements()) {
                         allLoggers.nextElement().removeAppender(appender);
                     }
+                    // AP-10222: must happen after it is removed from all appenders,
+                    // otherwise it will produce "log4j:ERROR Attempted to append to closed appender"
+                    appender.close();
                 }
             }
         }
