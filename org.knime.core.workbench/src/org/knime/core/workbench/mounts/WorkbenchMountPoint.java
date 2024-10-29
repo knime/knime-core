dbc9dd867df45cc75918185b76a2a82cae0c5196
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2024-10-29 14:29:53 +0100

    AP-23812: wip 1st draft org.knime.core.workbench
    AP-23812 (Move MountTable to new org.knime.core.workbench plug-in (retire workbench code)

---
+++ org.knime.core.workbench/src/org/knime/core/workbench/mounts/WorkbenchMountPoint.java
@@ -0,0 +1,107 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.core.workbench.mounts;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.knime.core.util.auth.Authenticator;
+
+/**
+ * Represents a content tree in the KNIME Explorer.
+ *
+ * @author ohl, University of Konstanz
+ */
+public final class WorkbenchMountPoint {
+
+    private final WorkbenchMountPointDefinition m_definition;
+
+    private final String m_mountID;
+
+    private final String m_storage;
+
+    private Authenticator m_authenticator;
+
+    private final Map<Class<? extends MountPointProvider>, MountPointProvider> m_contentProviders;
+
+    WorkbenchMountPoint(final WorkbenchMountPointDefinition definition, final String mountID, final String storage) {
+        m_definition = definition;
+        m_mountID = mountID;
+        m_storage = storage;
+        m_contentProviders = new LinkedHashMap<>();
+    }
+
+    /**
+     * @return the definition
+     */
+    public WorkbenchMountPointDefinition getDefinition() {
+        return m_definition;
+    }
+
+    public String getMountID() {
+        return m_mountID;
+    }
+
+    public Optional<Authenticator> getAuthenticator() {
+        return Optional.ofNullable(m_authenticator);
+    }
+
+    @SuppressWarnings("unchecked")
+    <T extends MountPointProvider> T getProvider(final Class<T> providerType, final Function<String, T> providerFactory) {
+        return (T)m_contentProviders.computeIfAbsent(providerType, k -> providerFactory.apply(m_storage));
+    }
+
+    public void dispose(final Class<? extends MountPointProvider> cl) {
+        Optional.ofNullable(m_contentProviders.remove(cl)).ifPresent(MountPointProvider::dispose);
+    }
+
+    public void dispose() {
+        m_contentProviders.values().forEach(MountPointProvider::dispose);
+        m_contentProviders.clear();
+    }
+}
