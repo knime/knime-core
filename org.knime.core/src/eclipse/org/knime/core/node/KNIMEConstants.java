c02e2106c49445a245541f7beca01b68d6208f7f
swaraj pal singh <swaraj.singh@knime.com>
2024-11-14 11:28:58 +0100

    DEVOPS-2824: Update the Nightly Flag to Release Date for 5.4.0

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -496,7 +496,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "December 06, 2024";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
