4fabb9f82b70b05fb225042ea8d5c147266c1bac
Franziska Obergfell <franziska.obergfell@knime.com>
2025-02-26 16:49:09 +0100

    NXT-3164: correctly reconnect ports after removing arbitrary native node port
    NXT-3164 (Allow removal of arbitrary port from extensible port groups)

--- org.knime.core/src/eclipse/org/knime/core/node/workflow/action/ReplaceNodeResult.java
+++ org.knime.core/src/eclipse/org/knime/core/node/workflow/action/ReplaceNodeResult.java
@@ -48,7 +48,10 @@
  */
 package org.knime.core.node.workflow.action;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeFactory;
@@ -61,6 +64,7 @@ import org.knime.core.node.workflow.ConnectionUIInformation;
 import org.knime.core.node.workflow.NodeAnnotation;
 import org.knime.core.node.workflow.NodeID;
 import org.knime.core.node.workflow.WorkflowManager;
+import org.knime.core.util.Pair;
 
 /**
  * Result of the replace node operation, e.g.,
@@ -88,6 +92,43 @@ public final class ReplaceNodeResult {
 
     private final NodeAnnotation m_originalNodeAnnotation;
 
+    private final Pair<Map<Integer, Integer>, Map<Integer, Integer>> m_portMappings;
+
+    /**
+     * New instance.
+     *
+     * @param wfm the host workflow manager
+     * @param replacedNodeID the id of the newly created node
+     * @param removedConnections the connections that couldn't be restored after the replacement
+     * @param originalNodeCreationConfig the original creation config of the old node (for the undo)
+     * @param originalNodeFactory factory of the deleted node; or {@code null} if the old node is of the same type as
+     *            the new one (i.e. replacement happened in order to change the port configuration)
+     * @param originalNodeSettings the settings of the deleted node
+     * @param originalNodeAnnotation the original node annotation of the deleted node; won't be restored, if
+     *            {@code null}
+     * @param portMappings mapping old port indices to new ones on port removal, used to reconnect after port removal
+     * @since 5.5
+     */
+    public ReplaceNodeResult(final WorkflowManager wfm, final NodeID replacedNodeID, //NOSONAR
+        final List<ConnectionContainer> removedConnections,
+        final ModifiableNodeCreationConfiguration originalNodeCreationConfig, final NodeFactory<?> originalNodeFactory,
+        final NodeSettings originalNodeSettings, final NodeAnnotation originalNodeAnnotation,
+        final Pair<Map<Integer, Integer>, Map<Integer, Integer>> portMappings) {
+        CheckUtils.checkNotNull(wfm);
+        CheckUtils.checkNotNull(replacedNodeID);
+        CheckUtils.checkNotNull(removedConnections);
+        CheckUtils.checkNotNull(removedConnections);
+        CheckUtils.checkNotNull(originalNodeSettings);
+        m_wfm = wfm;
+        m_replacedNodeID = replacedNodeID;
+        m_removedConnections = removedConnections;
+        m_nodeCreationConfig = originalNodeCreationConfig;
+        m_originalNodeFactory = originalNodeFactory;
+        m_originalNodeSettings = originalNodeSettings;
+        m_originalNodeAnnotation = originalNodeAnnotation;
+        m_portMappings = portMappings;
+    }
+
     /**
      * New instance.
      *
@@ -105,18 +146,8 @@ public final class ReplaceNodeResult {
         final List<ConnectionContainer> removedConnections,
         final ModifiableNodeCreationConfiguration originalNodeCreationConfig, final NodeFactory<?> originalNodeFactory,
         final NodeSettings originalNodeSettings, final NodeAnnotation originalNodeAnnotation) {
-        CheckUtils.checkNotNull(wfm);
-        CheckUtils.checkNotNull(replacedNodeID);
-        CheckUtils.checkNotNull(removedConnections);
-        CheckUtils.checkNotNull(removedConnections);
-        CheckUtils.checkNotNull(originalNodeSettings);
-        m_wfm = wfm;
-        m_replacedNodeID = replacedNodeID;
-        m_removedConnections = removedConnections;
-        m_nodeCreationConfig = originalNodeCreationConfig;
-        m_originalNodeFactory = originalNodeFactory;
-        m_originalNodeSettings = originalNodeSettings;
-        m_originalNodeAnnotation = originalNodeAnnotation;
+        this(wfm, replacedNodeID, removedConnections, originalNodeCreationConfig, originalNodeFactory,
+            originalNodeSettings, originalNodeAnnotation, null);
     }
 
     /**
@@ -130,7 +161,9 @@ public final class ReplaceNodeResult {
      * Performs the undo.
      */
     public void undo() {
-        m_wfm.replaceNode(m_replacedNodeID, m_nodeCreationConfig, m_originalNodeFactory, false);
+        var portChange = Optional.ofNullable(m_portMappings);
+        var portMappings = portChange.isPresent() ? getPortMappingsForPortRemovalUndo() : null;
+        m_wfm.replaceNode(m_replacedNodeID, m_nodeCreationConfig, m_originalNodeFactory, false, portMappings);
         m_removedConnections.stream()
             .filter(c -> m_wfm.canAddConnection(c.getSource(), c.getSourcePort(), c.getDest(), c.getDestPort()))
             .forEach(c -> {
@@ -148,6 +181,77 @@ public final class ReplaceNodeResult {
         } catch (InvalidSettingsException ex) {
             NodeLogger.getLogger(ReplaceNodeResult.class).error("Could not re-apply node settings on undo", ex);
         }
+    }
+
+    /*
+     * map current port indices (after removal) to new indices (after removal undo)
+     */
+    private Pair<Map<Integer, Integer>, Map<Integer, Integer>> getPortMappingsForPortRemovalUndo() {
+        return new Pair<>(getInputPortMapping(), getOutputPortMapping());
+    }
+
+    private Map<Integer, Integer> getOutputPortMapping() {
+        var outgoingPorts = m_portMappings.getSecond();
+        var removedOutgoingPort = false;
+        var outgoingPortIndex = -1;
+        //check if an output port has been removed by finding value -1
+        for (var entry : outgoingPorts.entrySet()) {
+            if (entry.getValue().equals(-1)) {
+                removedOutgoingPort = true;
+                outgoingPortIndex = entry.getKey();
+            }
+        }
+        Map<Integer, Integer> outputPortMapping = new HashMap<>();
+        //no output port removed, indices stay the same for undo
+        if (!removedOutgoingPort) {
+            var portConfig = m_nodeCreationConfig.getPortConfig();
+            var outputLength = portConfig.isPresent() ? (portConfig.get().getInputPorts().length + 1) : 0;
+            for (int i = 0; i < outputLength; i++) {
+                outputPortMapping.put(i, i);
+            }
+        } else { //removed output port at outgoingPortIndex
+            for (int i = 0; i < outgoingPorts.size() - 1; i++) {
+                //indices smaller than outgoingPortIndex are unaffected by removal undo
+                if (i < outgoingPortIndex) {
+                    outputPortMapping.put(i, i);
+                } else { //removed port will be reinserted at outgoingPortIndex, shift all indices >= by one
+                    outputPortMapping.put(i, (i + 1));
+                }
+            }
+        }
+        return outputPortMapping;
+    }
+
+    private Map<Integer, Integer> getInputPortMapping() {
+        var incomingPorts = m_portMappings.getFirst();
+        var removedIncomingPort = false;
+        var incomingPortIndex = -1;
+        //check if an input port has been removed by finding value -1
+        for (var entry : incomingPorts.entrySet()) {
+            if (entry.getValue().equals(-1)) {
+                removedIncomingPort = true;
+                incomingPortIndex = entry.getKey();
+            }
+        }
+        Map<Integer, Integer> inputPortMapping = new HashMap<>();
+        //no input port removed, indices stay the same for undo
+        if (!removedIncomingPort) {
+            var portConfig = m_nodeCreationConfig.getPortConfig();
+            var inputLength = portConfig.isPresent() ? (portConfig.get().getInputPorts().length + 1) : 0;
+            for (int i = 0; i < inputLength; i++) {
+                inputPortMapping.put(i, i);
+            }
+        } else { //removed input port at incomingPortIndex
+            for (int i = 0; i < incomingPorts.size() - 1; i++) {
+                //indices smaller than outgoingPortIndex are unaffected by removal undo
+                if (i < incomingPortIndex) {
+                    inputPortMapping.put(i, i);
+                } else { //removed port will be reinserted at outgoingPortIndex, shift all indices >= by one
+                    inputPortMapping.put(i, (i + 1));
+                }
+            }
+        }
+        return inputPortMapping;
     }
 
 }
