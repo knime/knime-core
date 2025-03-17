f07a05bd367be5363426b5db1660fa94fcb594aa
Manuel Hotz <manuel.hotz@knime.com>
2025-03-17 16:22:48 +0100

    AP-24033: Move HTTP exceptions to shared
    AP-24033 (Remove knime-core dependency from knime-hubclient-sdk)

--- org.knime.core/src/eclipse/org/knime/core/util/exception/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains exception (utils) added as part of AP-19880. Other "util" exceptions live in the parent package due to
- * backward compatibility reasons.
- *
- * @since 5.0
- */
-package org.knime.core.util.exception;
