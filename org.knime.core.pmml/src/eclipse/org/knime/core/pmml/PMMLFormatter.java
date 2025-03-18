8491fc40a0b48188d1df4fc612d93c7750eb3307
Leonard Wörteler <leonard.woerteler@knime.com>
2025-03-18 12:30:16 +0100

    AP-24108: Don't replace UTF-16 surrogates with '?' when serializing PMML
    AP-24108 (Missing Values (Apply) does not work with Emojis like \uD83D\uDCCA)

--- org.knime.core.pmml/src/eclipse/org/knime/core/pmml/PMMLFormatter.java
+++ org.knime.core.pmml/src/eclipse/org/knime/core/pmml/PMMLFormatter.java
@@ -107,18 +107,13 @@ public final class PMMLFormatter {
         }
 
         /**
-         * Test if a character is valid in xml character content. See
-         * http://www.w3.org/TR/REC-xml#NT-Char
+         * Test if a character is valid in xml character content. See http://www.w3.org/TR/REC-xml#NT-Char
          */
-
-        private boolean isBadChar (final char ch)
-        {
-            return ! (
-                (ch >= 0x20 && ch <= 0xD7FF ) ||
-                (ch >= 0xE000 && ch <= 0xFFFD) ||
-                (ch >= 0x10000 && ch <= 0x10FFFF) ||
-                (ch == 0x9) || (ch == 0xA) || (ch == 0xD)
-                );
+        private static boolean isBadChar(final char ch) {
+            // AP-24108: We allow surrogates (0xD800 - 0xDFFF) here and let XMLBeans deal with unmatched ones.
+            //           The ranges in http://www.w3.org/TR/REC-xml#NT-Char talk about Unicode code points, not UTF-16.
+            final var isValidUTF16Char = (ch == 0x9) || (ch == 0xA) || (ch == 0xD) || (ch >= 0x20 && ch <= 0xFFFD);
+            return !isValidUTF16Char;
         }
     }
 
