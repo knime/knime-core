f336a536624694487225bfe3038ee6f8faeeb4c3
Leonard Wörteler <leonard.woerteler@knime.com>
2025-03-19 12:28:07 +0100

    AP-23812: WIP
    AP-23812 (Move MountTable to new org.knime.core.workbench plug-in (retire workbench code))

--- org.knime.core.workbench/src/org/knime/core/workbench/mountpoint/contribution/temp/TempSpaceMountPointState.java
+++ org.knime.core.workbench/src/org/knime/core/workbench/mountpoint/contribution/temp/TempSpaceMountPointState.java
@@ -21,30 +21,52 @@ package org.knime.core.workbench.mountpoint.contribution.temp;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.lang3.function.FailableFunction;
 import org.knime.core.node.util.CheckUtils;
 import org.knime.core.util.User;
 import org.knime.core.workbench.mountpoint.api.WorkbenchMountPointState;
 
 /**
  * Settings for the temp space mount point, moved content out of old (explorer-based) TempSpaceContentProvider.
+ *
  * @author Bernd Wiswedel, KNIME GmbH, Konstanz, Germany
  */
 public final class TempSpaceMountPointState implements WorkbenchMountPointState {
 
-    private File m_tempDir;
+    /**
+     * Supplier for temporary directories, obeying KNIME's customizable TEMP directory location.
+     */
+    public interface TempDirectorySupplier {
 
-    // TODO no commons lang in public API
-    public File initTempDir(final FailableFunction<String, File, IOException> tempFileSupplier) throws Exception {
-        if (m_tempDir == null) {
-            final var prefix = "knime_temp_space_" + User.getUsername() + "_";
-            m_tempDir = tempFileSupplier.apply(prefix);
-        }
-        return m_tempDir;
+        /**
+         * Creates a new temp directory that's deleted when AP shuts down.
+         *
+         * @param prefix directory prefix
+         * @return created temp directory
+         * @throws IOException if something went rwong
+         */
+        File createTempDir(String prefix) throws IOException;
     }
 
+    private File m_tempDir;
+
+    /**
+     * Initializes the temp space's root directory, must be called one before calls to {@link #getTempDir()}.
+     *
+     * @param tempDirSupplier supplier for a new temp directory
+     * @throws Exception if something went wrong
+     */
+    public void initTempDir(final TempDirectorySupplier tempDirSupplier) throws Exception {
+        if (m_tempDir == null) {
+            final var prefix = "knime_temp_space_" + User.getUsername() + "_";
+            m_tempDir = tempDirSupplier.createTempDir(prefix);
+        }
+    }
+
+    /**
+     * @return the root directory of the temp space
+     */
     public File getTempDir() {
-        CheckUtils.checkState(m_tempDir != null, "initTempDir() must be called before getTempDir()");
+        CheckUtils.checkState(m_tempDir != null, "`initTempDir(...)` must be called before `getTempDir()`");
         return m_tempDir;
     }
 
