2aa45f8cbd831694a1b348a8b172bde4eee59f40
Mahesh Kashyap <mahesh.kashyap@knime.com>
2025-06-18 17:17:09 +0200

    "DEVOPS-3251: Enable nightly flag"
    This reverts commit 0f64a7b9f6e7805290d1d24ea915388e42872e60.

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -507,7 +507,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "July 02, 2025";
+        BUILD_DATE = "Nightly build";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
