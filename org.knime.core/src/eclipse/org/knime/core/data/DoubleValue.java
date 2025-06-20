f1da6779e073b9ac9f70a8d94bf295c80cf9e56c
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2025-06-20 22:21:17 +0200

    AP-24537: "Number (Floating Decimal)" -> "Number (Float)"
    AP-24537 (Rename Data Type "Number (Floating Decimal)" -> "Number (Float)" - only affects Nightly)

--- org.knime.core/src/eclipse/org/knime/core/data/DoubleValue.java
+++ org.knime.core/src/eclipse/org/knime/core/data/DoubleValue.java
@@ -102,7 +102,7 @@ public interface DoubleValue extends DataValue {
 
         @Override
         public String getName() {
-            return "Number (Floating Decimal)";
+            return "Number (Float)";
         }
 
         @Override
