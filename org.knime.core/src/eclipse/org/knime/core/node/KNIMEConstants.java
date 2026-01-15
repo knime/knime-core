536e69ec1b654c2c3a7add9768275b35c25574ad
swarajpsingh <swaraj.singh@knime.com>
2026-01-15 12:53:04 +0100

    DEVOPS-3669: Update base branch in Jenkinsfile; version bump for 5.8.2

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -507,7 +507,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "Nightly build";
+        BUILD_DATE = "January 15, 2026";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
