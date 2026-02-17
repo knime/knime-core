a51852dc5c8dbc29f64b3db7e8292f45043e498e
swarajpsingh <swaraj.singh@knime.com>
2026-02-17 18:46:40 +0100

    DEVOPS-3756: Reverted the nightly flag and version to 5.3.5

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -465,7 +465,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "February 09, 2026";
+        BUILD_DATE = "Nightly build";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
