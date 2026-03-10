a0b371452feaf2f9e612ea2b52d5bfbc2d594c46
Sebastian Gerau <Sebastian.gerau@knime.com>
2026-03-10 16:47:30 +0100

    DEVOPS-3800: Update build date.
    DEVOPS-3800 (AP 5.8.3 LTS - 11 March)

--- org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
+++ org.knime.core/src/eclipse/org/knime/core/node/KNIMEConstants.java
@@ -507,7 +507,7 @@ public final class KNIMEConstants {
 
 
     static {
-        BUILD_DATE = "February 24, 2026";
+        BUILD_DATE = "March 10, 2026";
         String versionString;
         Bundle coreBundle = OSGIHelper.getBundle("org.knime.core");
         if (coreBundle != null) {
